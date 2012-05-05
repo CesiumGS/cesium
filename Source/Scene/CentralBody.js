f09e5946b90f7ca5e50f5679b72b887e56db9780
Dan Bagnell <daniel.p.bagnell@gmail.com>
2012-05-04 20:34:15 -0400

    Fix bad merge: master -> imagery.

--- Source/Scene/CentralBody.js
+++ Source/Scene/CentralBody.js
@@ -652,8 +652,8 @@ define([
         var retry = maxTimePassed || (requestFailed && !maxFailed);
 
         // check if tile needs to load image
-        if ((!tile.state || tile.state === TileState.READY) && this._imageQueue.indexOf(tile) === -1) {
-            this._imageQueue.push(tile);
+        if ((!tile.state || tile.state === TileState.READY) && !this._imageQueue.contains(tile)) {
+            this._imageQueue.enqueue(tile);
             tile.state = TileState.IMAGE_LOADING;
         } else if (tile.state === TileState.IMAGE_LOADED && !this._reprojectQueue.contains(tile)) {
             // or re-project the image
