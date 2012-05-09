4a85eb74dc8577d3effa2c7fe2658c7924445788
Ed Mackey <elm19087@gmail.com>
2012-05-09 15:33:42 -0400

    Sandcastle - Hack in Skeleton, work in progress

--- Examples/Sandcastle/CesiumSandcastle.js
+++ Examples/Sandcastle/CesiumSandcastle.js
@@ -1,15 +1,22 @@
 require({
-		baseUrl: '../../ThirdParty/dojo-release-1.7.2-src',
-		packages: [
-			'dojo',
-			'dijit',
-			'dojox']
+        baseUrl: '../../Source',
+        packages: [{
+            name: 'dojo',
+            location: '../ThirdParty/dojo-release-1.7.2-src/dojo'
+        }, {
+            name: 'dijit',
+            location: '../ThirdParty/dojo-release-1.7.2-src/dijit'
+        }, {
+            name: 'dojox',
+            location: '../ThirdParty/dojo-release-1.7.2-src/dojox'
+        }]
 	}, [
         'dojo/parser',
         'dojo/dom-class',
         'dojo/dom-construct',
         'dojo/_base/fx',
         'dojo/_base/window',
+        'dijit/registry',
         'dijit/form/Button',
         'dijit/form/DropDownButton',
         'dijit/form/ToggleButton',
@@ -29,7 +36,14 @@ require({
         'dijit/Toolbar',
         'dijit/ToolbarSeparator',
         'dojo/domReady!'],
-    function (parser, domClass, domConstruct, fx, win) {
+    function (
+            parser,
+            domClass,
+            domConstruct,
+            fx,
+            win,
+            registry
+    ) {
         parser.parse();
         fx.fadeOut({ node: 'loading', onEnd: function () {
             domConstruct.destroy('loading');
@@ -41,4 +55,12 @@ require({
           });
         // TODO: remove debugging
         window.editor = editor;
+
+        //registry.byId('pane3D').on("resize", function () {
+        //    console.log('ed - resize');
+        //});
+
+        //window.pane = dijit.byId('pane3D');
+        //window.oldRe = window.pane.resize;
+        //window.pane.resize = function(changeSize, resultSize) { console.log('re'); window.oldRe(changeSize, resultSize); };
     });
