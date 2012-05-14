486c466a495f7857f0dec3d6dfbd1fe267765c5d
kristiancalhoun <kristian.calhoun@gmail.com>
2012-05-14 11:31:20 -0400

    Added createInertia function to CameraHelpers.

--- Source/Scene/CameraHelpers.js
+++ Source/Scene/CameraHelpers.js
@@ -84,6 +84,67 @@ define([
         }
     }
 
+    /**
+     * This function is similar to maintainInertia except that it does not require a handler.
+     * Instead, the touch start time, touch release time, and last movement are passed as arguments.
+     *
+     * @param {JulianDate} ts The time the mouse touch started.
+     * @param {JulianDate} tr The time the mouse touch released.
+     * @param {Object} lastMovement
+     * @param {Number} decayCoef
+     * @param {Function} action
+     * @param {Object} object
+     * @param {String} lastMovementName
+     */
+    function createInertia(ts, tr, lastMovement, decayCoef, action, object, lastMovementName) {
+        if(ts && tr) {
+            // In VWF, ts and tr will be untyped objects that were originally JulianDates.
+            ts = new JulianDate(ts._julianDayNumber, ts._secondsOfDay, ts._timeStandard);
+            tr = new JulianDate(tr._julianDayNumber, tr._secondsOfDay, tr._timeStandard);
+        }
+        var threshold = ts && tr && ts.getSecondsDifference(tr);
+        if (ts && tr && threshold < inertiaMaxClickTimeThreshold) {
+            var now = new JulianDate();
+            var fromNow = tr.getSecondsDifference(now);
+            if (fromNow > inertiaMaxTimeThreshold) {
+                return;
+            }
+
+            var d = decay(fromNow, decayCoef);
+
+            if (!object[lastMovementName]) {
+                if (!lastMovement) {
+                    return;
+                }
+
+                var motionX = (lastMovement.endPosition.x - lastMovement.startPosition.x) * 0.5;
+                var motionY = (lastMovement.endPosition.y - lastMovement.startPosition.y) * 0.5;
+                object[lastMovementName] = {
+                    startPosition : new Cartesian2(lastMovement.startPosition.x, lastMovement.startPosition.y),
+                    endPosition : new Cartesian2(lastMovement.startPosition.x + motionX * d, lastMovement.startPosition.y + motionY * d),
+                    motion : new Cartesian2(motionX, motionY)
+                };
+            } else {
+                object[lastMovementName] = {
+                    startPosition : object[lastMovementName].endPosition.clone(),
+                    endPosition : new Cartesian2(
+                            object[lastMovementName].endPosition.x + object[lastMovementName].motion.x * d,
+                            object[lastMovementName].endPosition.y + object[lastMovementName].motion.y * d),
+                    motion : new Cartesian2(0.0, 0.0)
+                };
+            }
+
+            // If value from the decreasing exponential function is close to zero,
+            // the end coordinates may be NaN.
+            if (isNaN(object[lastMovementName].endPosition.x) || isNaN(object[lastMovementName].endPosition.y) || sameMousePosition(object[lastMovementName])) {
+                object[lastMovementName] = undefined;
+                return;
+            }
+
+            action.apply(object, [object[lastMovementName]]);
+        }
+    }
+
     function handleZoom(object, movement, distanceMeasure) {
         // distanceMeasure should be the height above the ellipsoid.
         // The zoomRate slows as it approaches the surface and stops 20m above it.
@@ -121,6 +182,7 @@ define([
         move : move,
         handleZoom : handleZoom,
         maintainInertia : maintainInertia,
+        createInertia : createInertia,
         zoom : zoom
     };
 });
