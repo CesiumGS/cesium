5e1fbfad62169421bca92c53f0f6fa555a2acced
Scott Hunter <shunter@agi.com>
2012-05-21 11:18:36 -0400

    Make Texture2DPool work with arbitrary texture sizes.
    Also make it transparent to calling code whether returned textures
    are created or returned from the pool, and use destroy() to release
    textures back into the pool.

--- Source/Scene/Texture2DPool.js
+++ Source/Scene/Texture2DPool.js
@@ -1,284 +1,97 @@
 /*global define*/
 define([
         '../Core/DeveloperError',
-        '../Core/destroyObject'
+        '../Core/destroyObject',
+        '../Renderer/PixelDatatype',
+        '../Renderer/PixelFormat',
+        '../Renderer/Texture'
     ], function(
         DeveloperError,
-        destroyObject) {
+        destroyObject,
+        PixelDatatype,
+        PixelFormat,
+        Texture) {
     "use strict";
 
+    function PooledTexture(texture, textureTypeKey, pool) {
+        this._texture = texture;
+        this._textureTypeKey = textureTypeKey;
+        this._pool = pool;
+    }
+
+    //pass through all methods to the underlying texture
+    Object.keys(Texture.prototype).forEach(function(methodName) {
+        PooledTexture.prototype[methodName] = function() {
+            var texture = this._texture;
+            return texture[methodName].apply(texture, arguments);
+        };
+    });
+
+    //except for destroy, which releases back into the pool
+    PooledTexture.prototype.destroy = function() {
+        var freeList = this._pool._free[this._textureTypeKey];
+        if (typeof freeList === 'undefined') {
+            freeList = this._pool._free[this._textureTypeKey] = [];
+        }
+        freeList.push(this);
+    };
+
     /**
-     * A collection of textures with the same width and height. Textures can be added and removed to
-     * the texture pool, but the textures will not be destroyed until the entire texture pool is
-     * destroyed. The pool can be queried for available textures and be marked as unavailable when
-     * the texture is to be used.
+     * A pool of textures.  Textures created from the pool will be released back into the pool
+     * when destroy() is called, so future calls to create may re-use a released texture.
      * <br/><br/>
-     * Texture pools are useful when textures are being created and released repeatedly.
+     * Texture pools are useful when textures are being created and destroyed repeatedly.
      *
      * @name Texture2DPool
      * @constructor
      *
-     * @exception {DeveloperError} width is required and must be greater than 0.
-     * @exception {DeveloperError} height is required and must be greater than 0.
+     * @param {Context} context The context to use to create textures when needed.
      *
      * @see Texture
-     * @see Context#createTexture2D
      */
-    function Texture2DPool(width, height) {
-        if (typeof width === "undefined" || width <= 0) {
-            throw new DeveloperError("width is required and must be greater than 0.", "width");
+    function Texture2DPool(context) {
+        if (typeof context === 'undefined') {
+            throw new DeveloperError('context is required.', 'context');
         }
 
-        if (typeof height === "undefined" || height <= 0) {
-            throw new DeveloperError("height is required and must be greater than 0.", "height");
-        }
-
-        this._width = width;
-        this._height = height;
-
-        this._used = {
-            head : null,
-            tail : null
-        };
-        this._unused = {
-            head : null,
-            tail : null
-        };
-
-        this._usedCount = 0;
-        this._unusedCount = 0;
+        this._context = context;
+        this._free = {};
     }
 
     /**
-     * Returns the width of every texture in the pool.
+     * Create a texture.  This function takes the same arguments as {@link Context#createTexture2D},
+     * but may return a pooled texture if there are any available.
      *
      * @memberof Texture2DPool
      *
-     * @return {Number} The width of the textures.
+     * @exception {DeveloperError} description is required.
      *
-     * @see Texture2DPool#getHeight
+     * @see Context#createTexture2D
      */
-    Texture2DPool.prototype.getWidth = function() {
-        return this._width;
-    };
-
-    /**
-     * Returns the height of every texture in the pool.
-     *
-     * @memberof Texture2DPool
-     *
-     * @return {Number} The height of the textures.
-     *
-     * @see Texture2DPool#getWidth
-     */
-    Texture2DPool.prototype.getHeight = function() {
-        return this._height;
-    };
-
-    Texture2DPool.prototype._addBack = function(list, texture) {
-        var node = {
-            texture : texture,
-            previous : list.tail,
-            next : null
-        };
-
-        if (list.tail) {
-            list.tail.next = node;
-            list.tail = node;
-        } else {
-            list.head = node;
-            list.tail = node;
-        }
-    };
-
-    /**
-     * Add a texture to the pool.
-     *
-     * @memberof Texture2DPool
-     *
-     * @param {Texture} texture The texture to be added to the pool.
-     * @param {Boolean} unused If left undefined or has a falsy value, the texture is currently being used.
-     * Otherwise, the texture will be marked as available for use.
-     *
-     * @exception {DeveloperError} The texture width and/or height does not match that of every
-     * other texture in the pool.
-     *
-     * @see Texture2DPool#remove
-     * @see Texture2DPool#removeAll
-     * @see Texture2DPool#getTexture
-     */
-    Texture2DPool.prototype.add = function(texture, unused) {
-        if (!texture) {
-            return;
+    Texture2DPool.prototype.createTexture2D = function(description) {
+        if (!description) {
+            throw new DeveloperError('description is required.', 'description');
         }
 
-        if (texture.getWidth() !== this._width || texture.getHeight() !== this._height) {
-            throw new DeveloperError("All textures in this pool must have the same dimensions.", "texture");
-        }
+        var source = description.source;
+        var width = typeof source !== 'undefined' ? source.width : description.width;
+        var height = typeof source !== 'undefined' ? source.height : description.height;
+        var pixelFormat = (description.pixelFormat || PixelFormat.RGBA).value;
+        var pixelDatatype = (description.pixelDatatype || PixelDatatype.UNSIGNED_BYTE).value;
+        var preMultiplyAlpha = description.preMultiplyAlpha || pixelFormat === PixelFormat.RGB || pixelFormat === PixelFormat.LUMINANCE;
 
-        if (unused) {
-            this._addBack(this._unused, texture);
-            ++this._unusedCount;
-        } else {
-            this._addBack(this._used, texture);
-            ++this._usedCount;
-        }
-    };
+        var textureTypeKey = JSON.stringify([width, height, pixelFormat, pixelDatatype, preMultiplyAlpha]);
 
-    /**
-     * If the texture is already being managed by the texture pool, then it will be marked as available
-     * for use. If the texture is not being tracked by the texture pool, then it will be added to the pool
-     * and be marked as available for use.
-     *
-     * @memberof Texture2DPool
-     *
-     * @param {Texture} texture The texture to be marked as available.
-     *
-     * @see Texture2DPool#add
-     * @see Texture2DPool#removeAll
-     * @see Texture2DPool#getTexture
-     */
-    Texture2DPool.prototype.remove = function(texture) {
-        if (!texture) {
-            return;
-        }
-
-        var node = this._used.head;
-        while (node !== null) {
-            if (node.texture === texture) {
-                break;
+        var freeList = this._free[textureTypeKey];
+        if (typeof freeList !== 'undefined' && freeList.length > 0) {
+            var texture = freeList.pop();
+            if (typeof source !== 'undefined') {
+                texture.copyFrom(source);
             }
-            node = node.next;
+            return texture;
         }
 
-        if (node) {
-            if (node.previous) {
-                node.previous.next = node.next;
-                if (node.next) {
-                    node.next.previous = node.previous;
-                } else {
-                    this._used.tail = node.previous;
-                }
-            } else {
-                this._used.head = node.next;
-                if (this._used.head) {
-                    this._used.head.previous = null;
-                } else {
-                    this._used.tail = null;
-                }
-            }
-            --this._usedCount;
-        }
-
-        this.add(texture, true);
-    };
-
-    /**
-     * Marks all textures in the pool as being available for use.
-     *
-     * @memberof Texture2DPool
-     *
-     * @see Texture2DPool#add
-     * @see Texture2DPool#remove
-     * @see Texture2DPool#getTexture
-     */
-    Texture2DPool.prototype.removeAll = function() {
-        if (this._used.head && this._used.tail) {
-            if (this._unused.tail) {
-                this._unused.tail.next = this._used.head;
-                this._unused.tail = this._used.tail;
-            } else {
-                this._unused.head = this._used.head;
-                this._unused.tail = this._used.tail;
-            }
-
-            this._used.head = this._used.tail = null;
-        }
-
-        this._unusedCount += this._usedCount;
-        this._usedCount = 0;
-    };
-
-    /**
-     * Returns a boolean indicating whether any textures in the pool are available for use.
-     *
-     * @memberof Texture2DPool
-     *
-     * @return {Boolean} <code>true</code> if there are textures available for use and false otherwise.
-     *
-     * @see Texture2DPool#getTexture
-     */
-    Texture2DPool.prototype.hasAvailable = function() {
-        return this._unusedCount !== 0;
-    };
-
-    /**
-     * If there is a texture available for use, it will be marked as used and returned.
-     *
-     * @memberof Texture2DPool
-     *
-     * @return {Texture} A texture that can be used or <code>null</code> if none are available.
-     *
-     * @see Texture2DPool#add
-     * @see Texture2DPool#remove
-     * @see Texture2DPool#removeAll
-     * @see Texture2DPool#hasAvailable
-     */
-    Texture2DPool.prototype.getTexture = function() {
-        var node = this._unused.head;
-
-        if (node) {
-            this._unused.head = node.next;
-
-            if (this._unused.head) {
-                this._unused.head.previous = null;
-            } else {
-                this._unused.tail = null;
-            }
-
-            node.next = this._used.head;
-
-            if (this._used.head) {
-                this._used.head.previous = node;
-            } else {
-                this._used.tail = node;
-            }
-
-            this._used.head = node;
-
-            ++this._usedCount;
-            --this._unusedCount;
-
-            return node.texture;
-        }
-
-        return null;
-    };
-
-    /**
-     * Returns the number of textures being managed, includes the number in use and the number not in use.
-     *
-     * @memberof Texture2DPool
-     *
-     * @return {Number} The number of textures being managed.
-     *
-     * @see Texture2DPool#getNumInUse
-     */
-    Texture2DPool.prototype.size = function() {
-        return this._usedCount + this._unusedCount;
-    };
-
-    /**
-     * Returns the number of textures being managed, includes only the number in use.
-     *
-     * @memberof Texture2DPool
-     *
-     * @return {Number} The number of textures in use.
-     *
-     * @see Texture2DPool#size
-     */
-    Texture2DPool.prototype.getNumInUse = function() {
-        return this._usedCount;
+        return new PooledTexture(this._context.createTexture2D(description), textureTypeKey, this);
     };
 
     /**
@@ -317,15 +130,12 @@ define([
      * pool = pool && pool.destroy();
      */
     Texture2DPool.prototype.destroy = function() {
-        var destroyTextures = function(list) {
-            var node = list.head;
-            while (node !== null) {
-                node.texture = node.texture && node.texture.destroy();
-                node = node.next;
-            }
-        };
-        destroyTextures(this._used);
-        destroyTextures(this._unused);
+        var free = this._free;
+        Object.keys(free).forEach(function(textureTypeKey) {
+            free[textureTypeKey].forEach(function(texture) {
+                texture._texture.destroy();
+            });
+        });
         return destroyObject(this);
     };
 
--- Specs/Scene/Texture2DPoolSpec.js
+++ Specs/Scene/Texture2DPoolSpec.js
@@ -6,176 +6,89 @@ defineSuite([
          Texture2DPool,
          destroyObject) {
     "use strict";
-    /*global it,expect,beforeEach,afterEach*/
+    /*global jasmine,it,expect,beforeEach,afterEach*/
 
-    var tWidth = 256;
-    var tHeight = 256;
-
-    var MockTexture = function(width, height) {
-        this.width = width || tWidth;
-        this.height = height || tHeight;
-
-        this.getWidth = function() {
-            return this.width;
-        };
-
-        this.getHeight = function() {
-            return this.height;
-        };
-
-        this.isDestroyed = function() {
-            return false;
-        };
-
-        this.destroy = function() {
-            return destroyObject(this);
-        };
-    };
-
+    var fakeContext;
     var pool;
+
     beforeEach(function() {
-        pool = new Texture2DPool(tWidth, tHeight);
+        fakeContext = jasmine.createSpyObj('Context', ['createTexture2D']);
+        fakeContext.createTexture2D.andCallFake(function(description) {
+            return {
+                getWidth : function() {
+                    return description.width;
+                },
+                getHeight : function() {
+                    return description.height;
+                },
+                isDestroyed : function() {
+                    return false;
+                },
+                destroy : function() {
+                    return destroyObject(this);
+                }
+            };
+        });
+
+        pool = new Texture2DPool(fakeContext);
     });
 
-    it("construction without width throws", function() {
+    it("throws when constructed without context", function() {
         expect(function() {
             return new Texture2DPool();
         }).toThrow();
     });
 
-    it("construction without height throws", function() {
-        expect(function() {
-            return new Texture2DPool(256);
-        }).toThrow();
+    it("creates textures using the context", function() {
+        var description = {
+            width : 1,
+            height : 1
+        };
+
+        var texture = pool.createTexture2D(description);
+        expect(fakeContext.createTexture2D).toHaveBeenCalledWith(description);
+        expect(texture).toBeDefined();
     });
 
-    it("getWidth and getHeight", function() {
-        var width = 512;
-        var height = 1024;
+    it("creates textures of different sizes", function() {
+        var texture1 = pool.createTexture2D({
+            width : 1,
+            height : 1
+        });
+        expect(texture1.getWidth()).toEqual(1);
 
-        var pool = new Texture2DPool(width, height);
-        expect(pool.getWidth()).toEqual(width);
-        expect(pool.getHeight()).toEqual(height);
+        var texture2 = pool.createTexture2D({
+            width : 2,
+            height : 2
+        });
+        expect(texture2.getWidth()).toEqual(2);
     });
 
-    it("add throws when widths aren't equal", function() {
-        expect(function() {
-            pool.add(new MockTexture(tWidth + 1));
-        }).toThrow();
+    it("returns textures to the pool when they are destroyed", function() {
+        var texture1 = pool.createTexture2D({
+            width : 1,
+            height : 1
+        });
+        texture1.destroy();
+
+        var texture2 = pool.createTexture2D({
+            width : 1,
+            height : 1
+        });
+
+        expect(fakeContext.createTexture2D.callCount).toEqual(1);
+        expect(texture2 === texture1).toEqual(true);
     });
 
-    it("add throws when heights aren't equal", function() {
-        expect(function() {
-            pool.add(new MockTexture(tWidth, tHeight + 1));
-        }).toThrow();
-    });
+    it("destroys free textures when the pool is destroyed", function() {
+        var texture1 = pool.createTexture2D({
+            width : 1,
+            height : 1
+        });
+        texture1.destroy();
 
-    it("add", function() {
-        pool.add(new MockTexture());
-        expect(pool.size()).toEqual(1);
-
-        pool.add(new MockTexture(), false);
-        expect(pool.size()).toEqual(2);
-        expect(pool.getNumInUse()).toEqual(2);
-
-        pool.add(new MockTexture(), true);
-        pool.add(new MockTexture(), true);
-        expect(pool.size()).toEqual(4);
-        expect(pool.getNumInUse()).toEqual(2);
-    });
-
-    it("remove", function() {
-        var textures = [];
-        for ( var i = 0; i < 4; ++i) {
-            var texture = new MockTexture();
-            textures.push(texture);
-            pool.add(texture);
-        }
-
-        expect(pool.size()).toEqual(textures.length);
-        expect(pool.getNumInUse()).toEqual(textures.length);
-
-        pool.remove(textures[1]);
-        expect(pool.size()).toEqual(textures.length);
-        expect(pool.getNumInUse()).toEqual(textures.length - 1);
-
-        pool.remove(textures[0]);
-        expect(pool.size()).toEqual(textures.length);
-        expect(pool.getNumInUse()).toEqual(textures.length - 2);
-
-        pool.remove(textures[3]);
-        expect(pool.size()).toEqual(textures.length);
-        expect(pool.getNumInUse()).toEqual(textures.length - 3);
-
-        pool.remove(textures[2]);
-        expect(pool.size()).toEqual(textures.length);
-        expect(pool.getNumInUse()).toEqual(textures.length - 4);
-    });
-
-    it("removeAll", function() {
-        var textures = [];
-        for ( var i = 0; i < 8; ++i) {
-            var texture = new MockTexture();
-            textures.push(texture);
-            pool.add(texture);
-        }
-
-        expect(pool.size()).toEqual(textures.length);
-        expect(pool.getNumInUse()).toEqual(textures.length);
-
-        pool.removeAll();
-        expect(pool.size()).toEqual(textures.length);
-        expect(pool.getNumInUse()).toEqual(0);
-    });
-
-    it("hasAvailable", function() {
-        expect(pool.hasAvailable()).toBeFalsy();
-
-        var mockTexture = new MockTexture();
-        pool.add(mockTexture, true);
-        expect(pool.hasAvailable()).toBeTruthy();
-
-        var texture = pool.getTexture();
-        expect(texture).toEqual(mockTexture);
-        expect(pool.hasAvailable()).toBeFalsy();
-    });
-
-    it("getTexture", function() {
-        var texture = pool.getTexture();
-        expect(texture === null).toBeTruthy();
-
-        var length = 8;
-
-        for ( var i = 0; i < length; ++i) {
-            pool.add(new MockTexture(), true);
-        }
-
-        expect(pool.size()).toEqual(length);
-        expect(pool.getNumInUse()).toEqual(0);
-
-        for ( var j = 0; j < length; ++j) {
-            texture = pool.getTexture();
-
-            expect(texture !== null).toBeTruthy();
-            expect(pool.size()).toEqual(length);
-            expect(pool.getNumInUse()).toEqual(j + 1);
-        }
-    });
-
-    it("destroy", function() {
-        var textures = [];
-        for ( var i = 0; i < 8; ++i) {
-            var texture = new MockTexture();
-            textures.push(texture);
-            pool.add(texture);
-        }
-
-        expect(pool.isDestroyed()).toBeFalsy();
         pool.destroy();
-        expect(pool.isDestroyed()).toBeTruthy();
-
-        for ( var j = 0; j < textures.length; ++j) {
-            expect(textures[j].isDestroyed()).toBeTruthy();
-        }
+        expect(pool.isDestroyed()).toEqual(true);
+        expect(texture1.isDestroyed()).toEqual(true);
     });
 });
