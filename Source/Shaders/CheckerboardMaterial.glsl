b62a7e89c1e1c130eb47888936e759b8036669e4
Ian Lilley <ian.lilley@rocketmail.com>
2012-05-21 18:46:04 -0400

    added aa for checkerboard material

--- Source/Shaders/CheckerboardMaterial.glsl
+++ Source/Shaders/CheckerboardMaterial.glsl
@@ -4,8 +4,21 @@ uniform vec2 u_repeat;
 
 vec4 agi_getMaterialColor(float zDistance, vec2 st, vec3 str)
 {
+    // Fuzz Factor - Controls blurriness between light and dark colors
+    const float fuzz = 0.03;
+    
     // From Stefan Gustavson's Procedural Textures in GLSL in OpenGL Insights
     float b = mod(floor(u_repeat.s * st.s) + floor(u_repeat.t * st.t), 2.0);  // 0.0 or 1.0
-
-    return mix(u_lightColor, u_darkColor, b);
+    
+    // Find the distance from the closest separator (region between two colors)
+    float scaledWidth = fract(u_repeat.s * st.s);
+    scaledWidth = abs(scaledWidth - floor(scaledWidth + .5));
+    float scaledHeight = fract(u_repeat.t * st.t);
+    scaledHeight = abs(scaledHeight - floor(scaledHeight + .5));
+    float scaled = min(scaledWidth,scaledHeight);
+    
+    //anti-aliasing
+    float interpValue = b*agi_getInterpolatedAntialiasedValue(scaled,fuzz);
+    
+    return mix(u_lightColor, u_darkColor, interpValue);
 }
--- Source/Shaders/VerticalStripeMaterial.glsl
+++ Source/Shaders/VerticalStripeMaterial.glsl
@@ -9,17 +9,10 @@ vec4 agi_getMaterialColor(float zDistance, vec2 st, vec3 str)
     
     // Fuzz Factor - Controls blurriness between light and dark colors
     const float fuzz = 0.1;
-    
-    // Controls the width of the light vs. dark stripes
-    const float width = 0.5;
-    
+
     float scaled = fract((st.s - u_offset) * (u_repeat * 0.5));
     
-    float frac1 = clamp(scaled / fuzz, 0.0, 1.0);
-    float frac2 = clamp((scaled - width) / fuzz, 0.0, 1.0);
-    
-    frac1 = frac1 * (1.0 - frac2);
-    frac1 = frac1 * frac1 * (3.0 - (2.0 * frac1));
-    
-    return mix(u_lightColor, u_darkColor, frac1);
+    float interpValue = agi_getInterpolatedAntialiasedValue(scaled, fuzz);
+
+    return mix(u_lightColor, u_darkColor, interpValue);
 }
