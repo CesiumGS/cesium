076301b58a1eb5abbd658559e2312cdbf020b905
Matthew Amato <matt.amato@gmail.com>
2012-06-06 20:50:57 -0400

    Documentation, clean-up and tests for TimeInterval.

--- Source/Core/TimeInterval.js
+++ Source/Core/TimeInterval.js
@@ -7,11 +7,31 @@ define([
         DeveloperError) {
     "use strict";
 
-    var Characteristics_Closed = 0;
-    var Characteristics_StartOpen = 1;
-    var Characteristics_StopOpen = 2;
-    var Characteristics_Empty = 4;
-
+    /**
+     * An interval defined by a start date and a stop date.  The end points are optionally included
+     * in the interval.  With the exception of the <code>data</code> property, the interval should
+     * be treated as immutable.
+     *
+     * @name TimeInterval
+     * @constructor
+     *
+     * @param {JulianDate} start The start date of the interval.
+     * @param {JulianDate} stop The stop date of the interval.
+     * @param {Boolean} [isStartIncluded=true] <code>true</code> if the start date is included in the interval, <code>false</code> otherwise.
+     * @param {Boolean} [isStopIncluded=true] <code>true</code> if the stop date is included in the interval, <code>false</code> otherwise.
+     * @param {Object} [data=undefined] The data associated with this interval.
+     *
+     * @exception {DeveloperError} start must be specified.
+     * @exception {DeveloperError} stop must be specified.
+     *
+     * @see TimeInterval.fromIso8601
+     * @see TimeIntervalCollection
+     * @see JulianDate
+     *
+     * @example
+     * // Construct an Timeinterval closed on one end with a Color pay-load.
+     * var interval = new TimeInterval(JulianDate.fromTotalDays(1000), JulianDate.fromTotalDays(1001), true, false, Color.WHITE);
+     */
     function TimeInterval(start, stop, isStartIncluded, isStopIncluded, data) {
         if (typeof start === 'undefined') {
             throw new DeveloperError("start must be specified.", "start");
@@ -29,87 +49,130 @@ define([
             isStopIncluded = true;
         }
 
+        var stopComparedToStart = JulianDate.compare(stop, start);
+
         this.start = start;
         this.stop = stop;
         this.data = data;
-
-        this._characteristics = Characteristics_Closed;
-        if (!isStartIncluded) {
-            this._characteristics |= Characteristics_StartOpen;
-        }
-        if (!isStopIncluded) {
-            this._characteristics |= Characteristics_StopOpen;
-        }
-
-        var stopComparedToStart = JulianDate.compare(stop, start);
-        // if (stop < start ||
-        //     stop == start && open on either side
-        if (stopComparedToStart < 0 || stopComparedToStart === 0 && this._characteristics !== Characteristics_Closed) {
-            this._characteristics |= Characteristics_Empty;
-        }
-
-        this.isStartIncluded = (this._characteristics & Characteristics_StartOpen) !== Characteristics_StartOpen;
-        this.isStopIncluded = (this._characteristics & Characteristics_StopOpen) !== Characteristics_StopOpen;
-        this.isEmpty = (this._characteristics & Characteristics_Empty) === Characteristics_Empty;
+        this.isStartIncluded = isStartIncluded;
+        this.isStopIncluded = isStopIncluded;
+        this.isEmpty = stopComparedToStart < 0 || (stopComparedToStart === 0 && !isStartIncluded && !isStopIncluded);
     }
 
-    TimeInterval.fromIso8601 = function(iso8601String) {
+    /**
+     * Creates an immutable TimeInterval from an ISO 8601 interval string.
+     *
+     * @memberof TimeInterval
+     *
+     * @param {String} iso8601String A valid ISO8601 interval.
+     * @param {Boolean} [isStartIncluded=true] <code>true</code> if the start date is included in the interval, <code>false</code> otherwise.
+     * @param {Boolean} [isStopIncluded=true] <code>true</code> if the stop date is included in the interval, <code>false</code> otherwise.
+     * @param {Object} [data=undefined] The data associated with this interval.
+     *
+     * @return {TimeInterval} The new {@Link TimeInterval} instance or <code>undefined</code> if an invalid ISO8601 string is provided.
+     *
+     * @see TimeInterval
+     * @see TimeIntervalCollection
+     * @see JulianDate
+     * @see <a href="http://en.wikipedia.org/wiki/ISO_8601">ISO 8601 on Wikipedia</a>.
+     *
+     * @example
+     * // Construct an open Timeinterval with a Cartesian data payload.
+     * var interval = TimeInterval.fromIso8601("2012-03-15T11:02:24.55Z/2012-03-15T12:28:24.03Z", false, false, new Cartesian3(1,2,3));
+     */
+    TimeInterval.fromIso8601 = function(iso8601String, isStartIncluded, isStopIncluded, data) {
         var iso8601Interval = iso8601String.split('/');
         var intervalStart = JulianDate.fromIso8601(iso8601Interval[0]);
         var intervalStop = JulianDate.fromIso8601(iso8601Interval[1]);
-        return new TimeInterval(intervalStart, intervalStop, true, true);
+        return new TimeInterval(intervalStart, intervalStop, isStartIncluded, isStopIncluded, data);
     };
 
+    /**
+     * An empty interval.
+     *
+     * @memberof TimeInterval
+     *
+     */
     TimeInterval.EMPTY = Object.freeze(new TimeInterval(new JulianDate(0, 0), new JulianDate(0, 0), false, false));
 
+    /**
+     * An infinite interval defined by the minimum and maximum JulianDate values.
+     *
+     * @memberof TimeInterval
+     *
+     * @see JulianDate
+     * @see JulianDate.MINIMUM_VALUE
+     * @see JulianDate.MAXIMUM_VALUE
+     */
     TimeInterval.INFINITE = Object.freeze(new TimeInterval(JulianDate.MINIMUM_VALUE, JulianDate.MAXIMUM_VALUE, true, true));
 
-    TimeInterval.prototype.clone = function() {
-        return new TimeInterval(this.start, this.stop, this.isStartIncluded, this.isStopIncluded);
-    };
 
-    TimeInterval.prototype.intersect = function(other) {
-        return this.intersectMergingData(other, undefined);
-    };
-
-    TimeInterval.prototype.intersectMergingData = function(other, mergeCallback) {
+    /**
+     * Computes an interval which is the intersection of this interval with another while
+     * also providing a means to merge the data of the two intervals.
+     *
+     * @param {TimeInterval} other The interval to intersect with this interval.
+     * @param {Function} [mergeCallback=undefined] A callback which takes the data property from
+     * both intervals as input and merges it into a single new value. If the callback is undefined,
+     * this will intersect the two intervals and return the new interval with the data from the calling
+     * interval.
+     *
+     * @return {TimeInterval} The new {@Link TimeInterval} that is the intersection of the two intervals,
+     * with its data representing the merge of the data in the two existing intervals.
+     */
+    TimeInterval.prototype.intersect = function(other, mergeCallback) {
         if (typeof other === 'undefined') {
             return TimeInterval.EMPTY;
         }
 
         var otherStart = other.start, otherStop = other.stop, isStartIncluded, isStopIncluded, outputData;
+        var thisStart = this.start, thisStop = this.stop, thisIsStartIncluded = this.isStartIncluded, thisIsStopIncluded = this.isStopIncluded;
 
-        if (otherStart.greaterThanOrEquals(this.start) && this.stop.greaterThanOrEquals(otherStart)) {
-            isStartIncluded = (!otherStart.equals(this.start) && other.isStartIncluded) || (this.isStartIncluded && other.isStartIncluded);
-            isStopIncluded = this.isStopIncluded && other.isStopIncluded;
+        if (otherStart.greaterThanOrEquals(thisStart) && thisStop.greaterThanOrEquals(otherStart)) {
+
+            isStartIncluded = (!otherStart.equals(thisStart) && other.isStartIncluded) || (thisIsStartIncluded && other.isStartIncluded);
+
+            isStopIncluded = thisIsStopIncluded && other.isStopIncluded;
 
             outputData = typeof mergeCallback !== 'undefined' ? mergeCallback(this.data, other.data) : this.data;
-            if (this.stop.greaterThanOrEquals(otherStop)) {
-                isStopIncluded = isStopIncluded || (!otherStop.equals(this.stop) && other.isStopIncluded);
+
+            if (thisStop.greaterThanOrEquals(otherStop)) {
+                isStopIncluded = isStopIncluded || (!otherStop.equals(thisStop) && other.isStopIncluded);
                 return new TimeInterval(otherStart, otherStop, isStartIncluded, isStopIncluded, outputData);
             }
 
-            isStopIncluded = isStopIncluded || this.isStopIncluded;
-            return new TimeInterval(otherStart, this.stop, isStartIncluded, isStopIncluded, outputData);
+            isStopIncluded = isStopIncluded || thisIsStopIncluded;
+            return new TimeInterval(otherStart, thisStop, isStartIncluded, isStopIncluded, outputData);
         }
 
-        if (otherStart.lessThanOrEquals(this.start) && this.start.lessThanOrEquals(otherStop)) {
-            isStartIncluded = (otherStart.equals(this.start) === false && this.isStartIncluded) || (this.isStartIncluded && other.isStartIncluded);
-            isStopIncluded = this.isStopIncluded && other.isStopIncluded;
+        if (otherStart.lessThanOrEquals(thisStart) && thisStart.lessThanOrEquals(otherStop)) {
+
+            isStartIncluded = (otherStart.equals(thisStart) === false && thisIsStartIncluded) || (thisIsStartIncluded && other.isStartIncluded);
+
+            isStopIncluded = thisIsStopIncluded && other.isStopIncluded;
 
             outputData = typeof mergeCallback !== 'undefined' ? mergeCallback(this.data, other.data) : this.data;
-            if (this.stop.greaterThanOrEquals(otherStop)) {
-                isStopIncluded = isStopIncluded || (otherStop.equals(this.stop) === false && other.isStopIncluded);
-                return new TimeInterval(this.start, otherStop, isStartIncluded, isStopIncluded, outputData);
+            if (thisStop.greaterThanOrEquals(otherStop)) {
+                isStopIncluded = isStopIncluded || (otherStop.equals(thisStop) === false && other.isStopIncluded);
+                return new TimeInterval(thisStart, otherStop, isStartIncluded, isStopIncluded, outputData);
             }
 
-            isStopIncluded = isStopIncluded || this.isStopIncluded;
-            return new TimeInterval(this.start, this.stop, isStartIncluded, isStopIncluded, outputData);
+            isStopIncluded = isStopIncluded || thisIsStopIncluded;
+            return new TimeInterval(thisStart, thisStop, isStartIncluded, isStopIncluded, outputData);
         }
 
         return TimeInterval.EMPTY;
     };
 
+    /**
+     * Returns <code>true</code> if this interval contains the specified date.
+     *
+     * @memberof TimeInterval
+     *
+     * @param {JulianDate} date The date to check for.
+     *
+     * @return {Boolean} <code>true</code> if the TimeInterval contains the specified date, <code>false</code> otherwise.
+     */
     TimeInterval.prototype.contains = function(date) {
         if (this.isEmpty) {
             return false;
@@ -131,24 +194,40 @@ define([
         return startComparedToDate < 0 && dateComparedToStop < 0;
     };
 
+    /**
+     * Returns true if this TimeInterval equals <code>other</code> componentwise.
+     *
+     * @memberof TimeInterval
+     * @param {TimeInterval} other The TimeInterval to compare for equality.
+     * @return {Boolean} <code>true</code> if the TimeIntervals are equal componentwise, <code>false</code> otherwise.
+     */
     TimeInterval.prototype.equals = function(other) {
-        if (typeof other === 'undefined') {
-            return false;
-        }
-        if (this.isEmpty && other.isEmpty) {
-            return true;
-        }
-        return this._characteristics === other._characteristics && this.start.equals(other.start) && this.stop.equals(other.stop);
+        return typeof other !== 'undefined' &&
+               ((this.isEmpty && other.isEmpty) ||
+                (this.isStartIncluded === other.isStartIncluded &&
+                this.isStopIncluded === other.isStopIncluded &&
+                this.start.equals(other.start) &&
+                this.stop.equals(other.stop)));
     };
 
+    /**
+     * Returns <code>true</code> if this TimeInterval equals other componentwise
+     * within the specified epsilon.
+     *
+     * @memberof TimeInterval
+     *
+     * @param {TimeInterval} other The TimeInterval to compare for equality.
+     * @param {Number} [epsilon=0.0] The epsilon to use for equality testing.
+     *
+     * @return {Boolean} <code>true</code> if the TimeIntervals are equal within the specified epsilon, <code>false</code> otherwise.
+     */
     TimeInterval.prototype.equalsEpsilon = function(other, epsilon) {
-        if (typeof other === 'undefined') {
-            return false;
-        }
-        if (this.isEmpty && other.isEmpty) {
-            return true;
-        }
-        return this._characteristics === other._characteristics && this.start.equalsEpsilon(other.start, epsilon) && this.stop.equalsEpsilon(other.stop, epsilon);
+        return typeof other !== 'undefined' &&
+               ((this.isEmpty && other.isEmpty) ||
+                (this.isStartIncluded === other.isStartIncluded &&
+                 this.isStopIncluded === other.isStopIncluded &&
+                 this.start.equalsEpsilon(other.start, epsilon) &&
+                 this.stop.equalsEpsilon(other.stop, epsilon)));
     };
 
     return TimeInterval;
--- Specs/Core/TimeIntervalSpec.js
+++ Specs/Core/TimeIntervalSpec.js
@@ -12,27 +12,27 @@ defineSuite([
         var start = new JulianDate(), stop = start.addDays(1), isStartIncluded = false, isStopIncluded = true, data = {};
 
         var interval = new TimeInterval(start, stop, isStartIncluded, isStopIncluded, data);
-        expect(interval.start === start).toBeTruthy();
-        expect(interval.stop === stop).toBeTruthy();
-        expect(interval.isStartIncluded === isStartIncluded).toBeTruthy();
-        expect(interval.isStopIncluded === isStopIncluded).toBeTruthy();
-        expect(interval.data === data).toBeTruthy();
+        expect(interval.start === start).toEqual(true);
+        expect(interval.stop === stop).toEqual(true);
+        expect(interval.isStartIncluded === isStartIncluded).toEqual(true);
+        expect(interval.isStopIncluded === isStopIncluded).toEqual(true);
+        expect(interval.data === data).toEqual(true);
 
         interval = new TimeInterval(start, stop);
-        expect(interval.start === start).toBeTruthy();
-        expect(interval.stop === stop).toBeTruthy();
-        expect(interval.isStartIncluded).toBeTruthy();
-        expect(interval.isStopIncluded).toBeTruthy();
-        expect(interval.data === undefined).toBeTruthy();
+        expect(interval.start === start).toEqual(true);
+        expect(interval.stop === stop).toEqual(true);
+        expect(interval.isStartIncluded).toEqual(true);
+        expect(interval.isStopIncluded).toEqual(true);
+        expect(interval.data === undefined).toEqual(true);
     });
 
-    it("Fail with undefined start", function() {
+    it("throws with undefined start", function() {
         expect(function() {
             return new TimeInterval(undefined, new JulianDate());
         }).toThrow();
     });
 
-    it("Fail with undefined stop", function() {
+    it("throws with undefined stop", function() {
         expect(function() {
             return new TimeInterval(new JulianDate());
         }).toThrow();
@@ -40,54 +40,55 @@ defineSuite([
 
     it("IsEmpty", function() {
         var interval = new TimeInterval(JulianDate.fromTotalDays(1), JulianDate.fromTotalDays(1));
-        expect(interval.isEmpty).toBeFalsy();
+        expect(interval.isEmpty).toEqual(false);
 
         interval = new TimeInterval(JulianDate.fromTotalDays(1), JulianDate.fromTotalDays(1), false, false);
-        expect(interval.isEmpty).toBeTruthy();
+        expect(interval.isEmpty).toEqual(true);
 
         interval = new TimeInterval(JulianDate.fromTotalDays(5), JulianDate.fromTotalDays(4));
-        expect(interval.isEmpty).toBeTruthy();
+        expect(interval.isEmpty).toEqual(true);
+        expect(interval.contains(JulianDate.fromTotalDays(4.5))).toEqual(false);
     });
 
     it("Contains", function() {
         var interval1 = new TimeInterval(JulianDate.fromTotalDays(2451545), JulianDate.fromTotalDays(2451546));
-        expect(interval1.contains(JulianDate.fromTotalDays(2451545))).toBeTruthy();
-        expect(interval1.contains(JulianDate.fromTotalDays(2451545.5))).toBeTruthy();
-        expect(interval1.contains(JulianDate.fromTotalDays(2451546))).toBeTruthy();
-        expect(interval1.contains(JulianDate.fromTotalDays(2451546.5))).toBeFalsy();
+        expect(interval1.contains(JulianDate.fromTotalDays(2451545))).toEqual(true);
+        expect(interval1.contains(JulianDate.fromTotalDays(2451545.5))).toEqual(true);
+        expect(interval1.contains(JulianDate.fromTotalDays(2451546))).toEqual(true);
+        expect(interval1.contains(JulianDate.fromTotalDays(2451546.5))).toEqual(false);
 
         var interval2 = new TimeInterval(interval1.start, interval1.stop, false, false);
-        expect(interval2.contains(JulianDate.fromTotalDays(2451545))).toBeFalsy();
-        expect(interval2.contains(JulianDate.fromTotalDays(2451545.5))).toBeTruthy();
-        expect(interval2.contains(JulianDate.fromTotalDays(2451546))).toBeFalsy();
-        expect(interval2.contains(JulianDate.fromTotalDays(2451546.5))).toBeFalsy();
+        expect(interval2.contains(JulianDate.fromTotalDays(2451545))).toEqual(false);
+        expect(interval2.contains(JulianDate.fromTotalDays(2451545.5))).toEqual(true);
+        expect(interval2.contains(JulianDate.fromTotalDays(2451546))).toEqual(false);
+        expect(interval2.contains(JulianDate.fromTotalDays(2451546.5))).toEqual(false);
     });
 
     it("Equality", function() {
         var interval1 = new TimeInterval(JulianDate.fromTotalDays(1), JulianDate.fromTotalDays(2));
         var interval2 = new TimeInterval(JulianDate.fromTotalDays(1), JulianDate.fromTotalDays(2));
-        expect(interval1.equals(interval2)).toBeTruthy();
-        expect(interval2.equals(interval1)).toBeTruthy();
-        expect(interval1.equalsEpsilon(interval2, 0)).toBeTruthy();
-        expect(interval2.equalsEpsilon(interval1, 0)).toBeTruthy();
+        expect(interval1.equals(interval2)).toEqual(true);
+        expect(interval2.equals(interval1)).toEqual(true);
+        expect(interval1.equalsEpsilon(interval2, 0)).toEqual(true);
+        expect(interval2.equalsEpsilon(interval1, 0)).toEqual(true);
 
         interval2 = new TimeInterval(JulianDate.fromTotalDays(1), JulianDate.fromTotalDays(3));
-        expect(interval1.equals(interval2)).toBeFalsy();
-        expect(interval2.equals(interval1)).toBeFalsy();
-        expect(interval1.equalsEpsilon(interval2, 0)).toBeFalsy();
-        expect(interval2.equalsEpsilon(interval1, 0)).toBeFalsy();
-        expect(interval1.equalsEpsilon(interval2, 86400)).toBeTruthy();
-        expect(interval2.equalsEpsilon(interval1, 86400)).toBeTruthy();
+        expect(interval1.equals(interval2)).toEqual(false);
+        expect(interval2.equals(interval1)).toEqual(false);
+        expect(interval1.equalsEpsilon(interval2, 0)).toEqual(false);
+        expect(interval2.equalsEpsilon(interval1, 0)).toEqual(false);
+        expect(interval1.equalsEpsilon(interval2, 86400)).toEqual(true);
+        expect(interval2.equalsEpsilon(interval1, 86400)).toEqual(true);
 
         interval1 = new TimeInterval(JulianDate.fromTotalDays(1), JulianDate.fromTotalDays(1), true, true);
         interval2 = new TimeInterval(JulianDate.fromTotalDays(1), new JulianDate(1, 1), true, false);
-        expect(interval1.equals(interval2)).toBeFalsy();
-        expect(interval2.equals(interval1)).toBeFalsy();
-        expect(interval1.equalsEpsilon(interval2, 1.1)).toBeFalsy();
-        expect(interval2.equalsEpsilon(interval1, 1.1)).toBeFalsy();
+        expect(interval1.equals(interval2)).toEqual(false);
+        expect(interval2.equals(interval1)).toEqual(false);
+        expect(interval1.equalsEpsilon(interval2, 1.1)).toEqual(false);
+        expect(interval2.equalsEpsilon(interval1, 1.1)).toEqual(false);
 
-        expect(interval1.equals(undefined)).toBeFalsy();
-        expect(interval1.equalsEpsilon(undefined, 1.0)).toBeFalsy();
+        expect(interval1.equals(undefined)).toEqual(false);
+        expect(interval1.equalsEpsilon(undefined, 1.0)).toEqual(false);
     });
 
     it("Intersect", function() {
@@ -131,12 +132,25 @@ defineSuite([
             var expectedResult = testParameters[i + 2];
             var intersect1 = first.intersect(second);
             var intersect2 = second.intersect(first);
-            expect(intersect1.equals(intersect2)).toBeTruthy();
-            expect(intersect2.equals(intersect1)).toBeTruthy();
-            expect(expectedResult.equals(intersect1)).toBeTruthy();
+            expect(intersect1.equals(intersect2)).toEqual(true);
+            expect(intersect2.equals(intersect1)).toEqual(true);
+            expect(expectedResult.equals(intersect1)).toEqual(true);
         }
 
         var interval = new TimeInterval(JulianDate.fromTotalDays(1), JulianDate.fromTotalDays(2));
         expect(interval.intersect(undefined)).toEqual(TimeInterval.EMPTY);
+    });
+
+    it("Intersect Merging", function() {
+        var oneToThree = new TimeInterval(JulianDate.fromTotalDays(1), JulianDate.fromTotalDays(3), true, true, 2);
+        var twoToFour = new TimeInterval(JulianDate.fromTotalDays(2), JulianDate.fromTotalDays(4), true, true, 3);
+        var twoToThree = oneToThree.intersect(twoToFour, function(lhs, rhs) {
+            return lhs + rhs;
+        });
+        expect(twoToThree.start).toEqual(twoToFour.start);
+        expect(twoToThree.stop).toEqual(oneToThree.stop);
+        expect(twoToThree.isStartIncluded).toEqual(true);
+        expect(twoToThree.isStopIncluded).toEqual(true);
+        expect(twoToThree.data).toEqual(5);
     });
 });
