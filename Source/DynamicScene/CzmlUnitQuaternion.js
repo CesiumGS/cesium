8b55c95c70e2399ea5dac3ab72d688d4f48396fe
Matthew Amato <matt.amato@gmail.com>
2012-06-14 11:33:33 -0400

    Fix issues with quaternion interpolation.

--- Source/DynamicScene/CzmlUnitQuaternion.js
+++ Source/DynamicScene/CzmlUnitQuaternion.js
@@ -9,6 +9,7 @@ define([
 
     var doublesPerCartesian = 3;
     var doublesPerQuaternion = 4;
+    var identity = new Quaternion(0, 0, 0, 1);
 
     var CzmlUnitQuaternion = {
         doublesPerValue : doublesPerQuaternion,
@@ -28,37 +29,28 @@ define([
             for ( var i = 0, len = lastIndex - firstIndex + 1; i < len; i++) {
                 var offset = i * doublesPerCartesian;
                 var value = CzmlUnitQuaternion.createValueFromArray(sourceArray, (firstIndex + i) * doublesPerQuaternion);
+
                 var difference = value.multiply(quaternion0Conjugate).normalize();
 
                 if (difference.w < 0) {
                     difference = difference.negate();
                 }
 
-                if (difference.x === 0 && difference.y === 0 && difference.z === 0) {
-                    destinationArray[offset] = 0;
-                    destinationArray[offset + 1] = 0;
-                    destinationArray[offset + 2] = 0;
-                } else {
-                    var axis = new Cartesian3(difference.x, difference.y, difference.z);
-                    var magnitude = axis.magnitude();
-                    var angle = 2 * Math.atan2(magnitude, difference.w);
-                    var axisX = axis.x / magnitude;
-                    var axisY = axis.y / magnitude;
-                    var axisZ = axis.z / magnitude;
+                var axis = difference.getAxis();
+                var angle = difference.getAngle();
 
-                    destinationArray[offset] = axisX * angle;
-                    destinationArray[offset + 1] = axisY * angle;
-                    destinationArray[offset + 2] = axisZ * angle;
-                }
+                destinationArray[offset] = axis.x * angle;
+                destinationArray[offset + 1] = axis.y * angle;
+                destinationArray[offset + 2] = axis.z * angle;
             }
         },
 
         createValue : function(unwrappedInterval) {
-            return new Quaternion(unwrappedInterval[0], unwrappedInterval[1], unwrappedInterval[2], unwrappedInterval[3], true);
+            return new Quaternion(unwrappedInterval[0], unwrappedInterval[1], unwrappedInterval[2], unwrappedInterval[3], true).conjugate();
         },
 
         createValueFromArray : function(array, startingIndex) {
-            return new Quaternion(array[startingIndex], array[startingIndex + 1], array[startingIndex + 2], array[startingIndex + 3], true);
+            return new Quaternion(array[startingIndex], array[startingIndex + 1], array[startingIndex + 2], array[startingIndex + 3], true).conjugate();
         },
 
         createValueFromInterpolationResult : function(array, sourceArray, firstIndex, lastIndex) {
@@ -69,7 +61,7 @@ define([
 
             var difference;
             if (magnitude === 0) {
-                difference = new Quaternion(0, 0, 0, 1);
+                difference = identity;
             } else {
                 difference = Quaternion.fromAxisAngle(rotationVector, magnitude);
             }
