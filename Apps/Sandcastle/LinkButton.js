6e00a5a0e756317dcfdde24d7fa0b771278907a7
Ed Mackey <elm19087@gmail.com>
2012-06-19 16:24:24 -0400

    Sandcastle - Cleanup of LinkButton.js

--- Apps/Sandcastle/LinkButton.js
+++ Apps/Sandcastle/LinkButton.js
@@ -1,32 +1,33 @@
 define([
-		'dojo/_base/declare',
-		'dojo/dom-class', // domClass.toggle
-		'dijit/_WidgetBase',
-		'dijit/_CssStateMixin',
-		'dijit/_TemplatedMixin',
-		'dojo/text!./templates/LinkButton.html'
-	], function (
-		declare,
-		domClass,
-		_WidgetBase,
-		_CssStateMixin,
-		_TemplatedMixin,
-		template) {
+        'dojo/_base/declare',
+        'dojo/dom-class', // domClass.toggle
+        'dijit/_WidgetBase',
+        'dijit/_CssStateMixin',
+        'dijit/_TemplatedMixin',
+        'dojo/text!./templates/LinkButton.html'
+    ], function (
+        declare,
+        domClass,
+        _WidgetBase,
+        _CssStateMixin,
+        _TemplatedMixin,
+        template) {
 
-		return declare('Sandcastle.LinkButton', [_WidgetBase, _TemplatedMixin, _CssStateMixin], {
-			baseClass: "dijitButton",
-			templateString: template,
-		    showLabel: true,
+        return declare('Sandcastle.LinkButton', [_WidgetBase, _TemplatedMixin, _CssStateMixin], {
+            baseClass: "dijitButton",
+            templateString: template,
+            showLabel: true,
 
-		    _setShowLabelAttr: function(val){
-		        if(this.containerNode){
-		            domClass.toggle(this.containerNode, "dijitDisplayNone", !val);
-		        }
-		        this._set("showLabel", val);
-		    },
+            _setShowLabelAttr: function(val){
+                if(this.containerNode){
+                    domClass.toggle(this.containerNode, "dijitDisplayNone", !val);
+                }
+                this._set("showLabel", val);
+            },
 
-		    _setLabelAttr: function(/*String*/ content){
-		        (this.containerNode||this.focusNode).innerHTML = content;
-		    }
-		});
-	});
+            _setLabelAttr: function(/*String*/ content) {
+                this._set("label", content);
+                (this.containerNode||this.focusNode).innerHTML = content;
+            }
+        });
+    });
