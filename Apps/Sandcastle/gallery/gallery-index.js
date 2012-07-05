aa99b719a165df9b0d0ce7eaf2f2a7e73a23771f
Tom Fili <tfili@agi.com>
2012-07-05 15:19:56 -0400

    Merged in master

---
+++ Apps/Sandcastle/gallery/gallery-index.js
@@ -0,0 +1,7 @@
+// This file is automatically rebuilt by the Cesium build process.
+var gallery_demos = [
+	{'name':'cesiumWidgetDemo', 'img':'cesiumWidgetDemo.jpg'},
+	{'name':'circlesAndEllipses', 'img':'circlesAndEllipses.jpg'},
+	{'name':'polyLineTest', 'img':'polyLineTest.jpg'},
+	{'name':'skeletonDemo', 'img':'skeletonDemo.jpg'}
+];
