827e8b16754f52bd0de26d1f0b3ea20906fe7364
Matthew Amato <matt.amato@gmail.com>
2012-07-09 11:35:58 -0400

    Start of LeapSecond refactoring.
    1. Remove get/setLeapSeconds and expose leapSeconds property directly.
    2. Move default LeapSecond initalization to JulianDate.cs to avoid circular dependancy.
    3. Fix warning in CameraColumbusViewController.

--- Source/Core/JulianDate.js
+++ Source/Core/JulianDate.js
@@ -21,12 +21,8 @@ function(DeveloperError,
     }
 
     function convertTaiToUtc(julianDate, result) {
-        if (!julianDate) {
-            throw new DeveloperError('julianDate is required.');
-        }
-
         var toFind = new LeapSecond(julianDate, 0.0);
-        var leapSeconds = LeapSecond.getLeapSeconds();
+        var leapSeconds = LeapSecond.leapSeconds;
         var index = binarySearch(leapSeconds, toFind, LeapSecond.compareLeapSecondDate);
         if (index < 0) {
             index = ~index;
@@ -740,7 +736,7 @@ function(DeveloperError,
      */
     JulianDate.prototype.getTaiMinusUtc = function() {
         var toFind = new LeapSecond(this, 0.0);
-        var leapSeconds = LeapSecond.getLeapSeconds();
+        var leapSeconds = LeapSecond.leapSeconds;
         var index = binarySearch(leapSeconds, toFind, LeapSecond.compareLeapSecondDate);
         if (index < 0) {
             index = ~index;
@@ -1024,6 +1020,39 @@ function(DeveloperError,
         }
         return Math.abs(this.getSecondsDifference(other)) <= epsilon;
     };
+
+    //To avoid circular dependancies, we load the default list of leap seconds
+    //here, rather than in the LeapSecond class itself.
+    if (typeof LeapSecond.leapSeconds === 'undefined' || LeapSecond.leapSeconds.length === 0) {
+        LeapSecond.leapSeconds = [
+                                  new LeapSecond(new JulianDate(2441317, 43210.0, TimeStandard.TAI), 10), // January 1, 1972 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2441499, 43211.0, TimeStandard.TAI), 11), // July 1, 1972 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2441683, 43212.0, TimeStandard.TAI), 12), // January 1, 1973 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2442048, 43213.0, TimeStandard.TAI), 13), // January 1, 1974 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2442413, 43214.0, TimeStandard.TAI), 14), // January 1, 1975 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2442778, 43215.0, TimeStandard.TAI), 15), // January 1, 1976 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2443144, 43216.0, TimeStandard.TAI), 16), // January 1, 1977 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2443509, 43217.0, TimeStandard.TAI), 17), // January 1, 1978 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2443874, 43218.0, TimeStandard.TAI), 18), // January 1, 1979 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2444239, 43219.0, TimeStandard.TAI), 19), // January 1, 1980 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2444786, 43220.0, TimeStandard.TAI), 20), // July 1, 1981 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2445151, 43221.0, TimeStandard.TAI), 21), // July 1, 1982 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2445516, 43222.0, TimeStandard.TAI), 22), // July 1, 1983 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2446247, 43223.0, TimeStandard.TAI), 23), // July 1, 1985 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2447161, 43224.0, TimeStandard.TAI), 24), // January 1, 1988 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2447892, 43225.0, TimeStandard.TAI), 25), // January 1, 1990 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2448257, 43226.0, TimeStandard.TAI), 26), // January 1, 1991 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2448804, 43227.0, TimeStandard.TAI), 27), // July 1, 1992 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2449169, 43228.0, TimeStandard.TAI), 28), // July 1, 1993 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2449534, 43229.0, TimeStandard.TAI), 29), // July 1, 1994 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2450083, 43230.0, TimeStandard.TAI), 30), // January 1, 1996 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2450630, 43231.0, TimeStandard.TAI), 31), // July 1, 1997 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2451179, 43232.0, TimeStandard.TAI), 32), // January 1, 1999 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2453736, 43233.0, TimeStandard.TAI), 33), // January 1, 2006 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2454832, 43234.0, TimeStandard.TAI), 34), // January 1, 2009 00:00:00 UTC
+                                  new LeapSecond(new JulianDate(2456109, 43235.0, TimeStandard.TAI), 35)  // July 1, 2012 00:00:00 UTC
+                                 ];
+    }
 
     return JulianDate;
 });
--- Source/Core/LeapSecond.js
+++ Source/Core/LeapSecond.js
@@ -1,20 +1,9 @@
 /*global define*/
 define([
-        'require',
-        './DeveloperError',
-        './TimeStandard'
+        './DeveloperError'
     ], function(
-        require,
-        DeveloperError,
-        TimeStandard) {
+        DeveloperError) {
     "use strict";
-
-    var JulianDate = function(a, b, c) {
-        //because of the circular reference between JulianDate and TimeStandard,
-        //we need to require JulianDate later and replace our reference
-        JulianDate = require('./JulianDate');
-        return new JulianDate(a, b, c);
-    };
 
     /**
      * Describes a single leap second, which is constructed from a {@link JulianDate} and a
@@ -72,56 +61,6 @@ define([
     };
 
     /**
-     * Sets the list of currently known leap seconds from user provided data.
-     *
-     * @memberof LeapSecond
-     *
-     * @param {Object} data An array of the form:
-     *                          [
-     *                            {
-     *                              date   : xxx,
-     *                              offset : xxx
-     *                            },
-     *                            ...
-     *                          ]
-     * <code>date</code> should be a String representing the
-     * time of the leap second (in the format used to construct a Javascript Date object), and <code>offset</code>
-     * should be a number representing the difference, in seconds, between the TAI and UTC standards at the
-     * time of this leap second.
-     *
-     * @exception {DeveloperError} <code>data</code> is required.
-     *
-     * @see LeapSecond.getLeapSeconds
-     *
-     * @example
-     * // Set the list of leap seconds using user defined data.
-     * var data = [
-     *     {
-     *          date: 'January 1, 1972 00:00:00 UTC',
-     *          offset: 10.0
-     *     },
-     *     {
-     *          date: 'July 1, 1972 00:00:00 UTC',
-     *          offset: 11.0
-     *     }
-     * ];
-     * LeapSecond.setLeapSeconds(data);
-     * var leapSeconds = LeapSecond.getLeapSeconds();  // An array of two LeapSeconds.
-     */
-    LeapSecond.setLeapSeconds = function(data) {
-        if (!data) {
-            throw new DeveloperError('data is required.');
-        }
-
-        LeapSecond._leapSeconds = [];
-        var numObjects = data.length;
-        for ( var i = 0; i < numObjects; i++) {
-            LeapSecond._leapSeconds.push(new LeapSecond(JulianDate.fromDate(new Date(data[i].date)), data[i].offset));
-        }
-        LeapSecond._leapSeconds.sort(LeapSecond.compareLeapSecondDate);
-    };
-
-    /**
      * Returns a list of leap seconds. If {@link LeapSecond.setLeapSeconds} has not yet been called,
      * then a list of leap seconds that was available when this library was released is returned.
      *
@@ -131,40 +70,7 @@ define([
      *
      * @see LeapSecond.setLeapSeconds
      */
-    LeapSecond.getLeapSeconds = function() {
-        if (!LeapSecond._leapSeconds) {
-            LeapSecond._leapSeconds =
-                [
-                 new LeapSecond(new JulianDate(2441317, 43209.0, TimeStandard.TAI), 10), // January 1, 1972 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2441499, 43210.0, TimeStandard.TAI), 11), // July 1, 1972 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2441683, 43211.0, TimeStandard.TAI), 12), // January 1, 1973 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2442048, 43212.0, TimeStandard.TAI), 13), // January 1, 1974 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2442413, 43213.0, TimeStandard.TAI), 14), // January 1, 1975 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2442778, 43214.0, TimeStandard.TAI), 15), // January 1, 1976 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2443144, 43215.0, TimeStandard.TAI), 16), // January 1, 1977 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2443509, 43216.0, TimeStandard.TAI), 17), // January 1, 1978 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2443874, 43217.0, TimeStandard.TAI), 18), // January 1, 1979 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2444239, 43218.0, TimeStandard.TAI), 19), // January 1, 1980 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2444786, 43219.0, TimeStandard.TAI), 20), // July 1, 1981 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2445151, 43220.0, TimeStandard.TAI), 21), // July 1, 1982 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2445516, 43221.0, TimeStandard.TAI), 22), // July 1, 1983 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2446247, 43222.0, TimeStandard.TAI), 23), // July 1, 1985 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2447161, 43223.0, TimeStandard.TAI), 24), // January 1, 1988 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2447892, 43224.0, TimeStandard.TAI), 25), // January 1, 1990 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2448257, 43225.0, TimeStandard.TAI), 26), // January 1, 1991 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2448804, 43226.0, TimeStandard.TAI), 27), // July 1, 1992 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2449169, 43227.0, TimeStandard.TAI), 28), // July 1, 1993 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2449534, 43228.0, TimeStandard.TAI), 29), // July 1, 1994 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2450083, 43229.0, TimeStandard.TAI), 30), // January 1, 1996 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2450630, 43230.0, TimeStandard.TAI), 31), // July 1, 1997 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2451179, 43231.0, TimeStandard.TAI), 32), // January 1, 1999 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2453736, 43232.0, TimeStandard.TAI), 33), // January 1, 2006 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2454832, 43233.0, TimeStandard.TAI), 34), // January 1, 2009 00:00:00 UTC
-                 new LeapSecond(new JulianDate(2456109, 43234.0, TimeStandard.TAI), 35)  // July 1, 2012 00:00:00 UTC
-                 ];
-        }
-        return LeapSecond._leapSeconds;
-    };
+    LeapSecond.leapSeconds = [];
 
     /**
      * Checks whether two leap seconds are equivalent to each other.
