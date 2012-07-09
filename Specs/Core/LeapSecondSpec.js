7865a3bf8e8ae4f738a72f7fcbc1c74d30025493
Matthew Amato <matt.amato@gmail.com>
2012-07-09 11:35:58 -0400

    Start of LeapSecond refactoring.
    1. Remove get/setLeapSeconds and expose leapSeconds property directly.
    2. Move default LeapSecond initalization to JulianDate.cs to avoid circular dependancy.
    3. Fix warning in CameraColumbusViewController.

--- Specs/Core/LeapSecondSpec.js
+++ Specs/Core/LeapSecondSpec.js
@@ -30,17 +30,17 @@ defineSuite([
     });
 
     it('can get the TAI offset for an index (in range)', function() {
-        var leapSeconds = LeapSecond.getLeapSeconds();
+        var leapSeconds = LeapSecond.leapSeconds;
         var toFind = new LeapSecond(JulianDate.fromDate(new Date('January 1, 1973 00:00:00 UTC')), 12.0);
         var index = binarySearch(leapSeconds, toFind, LeapSecond.compareLeapSecondDate);
-        expect(LeapSecond.getLeapSeconds()[index].offset).toEqual(12.0);
+        expect(LeapSecond.leapSeconds[index].offset).toEqual(12.0);
     });
 
     it('can get the Julian date for an index (in range)', function() {
-        var leapSeconds = LeapSecond.getLeapSeconds();
+        var leapSeconds = LeapSecond.leapSeconds;
         var toFind = new LeapSecond(JulianDate.fromDate(new Date('July 1, 1972 00:00:00 UTC')), 0.0);
         var index = binarySearch(leapSeconds, toFind, LeapSecond.compareLeapSecondDate);
-        expect(LeapSecond.getLeapSeconds()[index].julianDate).toEqual(toFind.julianDate);
+        expect(leapSeconds[index].julianDate).toEqual(toFind.julianDate);
     });
 
     it('can check to see if leap seconds are equal', function() {
@@ -50,95 +50,6 @@ defineSuite([
         var leapSecond3 = new LeapSecond(JulianDate.fromDate(date), 26.0);
         expect(leapSecond1.equals(leapSecond2)).toEqual(true);
         expect(leapSecond1.equals(leapSecond3)).toEqual(false);
-    });
-
-    it('can construct a leapSecond using JSON data', function() {
-        var data = [{
-            date : 'January 1, 1972 00:00:00 UTC',
-            offset : 10
-        }, {
-            date : 'July 1, 1972 00:00:00 UTC',
-            offset : 11
-        }, {
-            date : 'January 1, 1973 00:00:00 UTC',
-            offset : 12
-        }, {
-            date : 'January 1, 1974 00:00:00 UTC',
-            offset : 13
-        }, {
-            date : 'January 1, 1975 00:00:00 UTC',
-            offset : 14
-        }, {
-            date : 'January 1, 1976 00:00:00 UTC',
-            offset : 15
-        }, {
-            date : 'January 1, 1977 00:00:00 UTC',
-            offset : 16
-        }, {
-            date : 'January 1, 1978 00:00:00 UTC',
-            offset : 17
-        }, {
-            date : 'January 1, 1979 00:00:00 UTC',
-            offset : 18
-        }, {
-            date : 'January 1, 1980 00:00:00 UTC',
-            offset : 19
-        }, {
-            date : 'July 1, 1981 00:00:00 UTC',
-            offset : 20
-        }, {
-            date : 'July 1, 1982 00:00:00 UTC',
-            offset : 21
-        }, {
-            date : 'July 1, 1983 00:00:00 UTC',
-            offset : 22
-        }, {
-            date : 'July 1, 1985 00:00:00 UTC',
-            offset : 23
-        }, {
-            date : 'January 1, 1988 00:00:00 UTC',
-            offset : 24
-        }, {
-            date : 'January 1, 1990 00:00:00 UTC',
-            offset : 25
-        }, {
-            date : 'January 1, 1991 00:00:00 UTC',
-            offset : 26
-        }, {
-            date : 'July 1, 1992 00:00:00 UTC',
-            offset : 27
-        }, {
-            date : 'July 1, 1993 00:00:00 UTC',
-            offset : 28
-        }, {
-            date : 'July 1, 1994 00:00:00 UTC',
-            offset : 100
-        }, {
-            date : 'January 1, 1996 00:00:00 UTC',
-            offset : 30
-        }, {
-            date : 'July 1, 1997 00:00:00 UTC',
-            offset : 31
-        }, {
-            date : 'January 1, 1999 00:00:00 UTC',
-            offset : 32
-        }, {
-            date : 'January 1, 2006 00:00:00 UTC',
-            offset : 33
-        }, {
-            date : 'January 1, 2009 00:00:00 UTC',
-            offset : 34
-        }];
-        LeapSecond.setLeapSeconds(data);
-        var jd = JulianDate.fromDate(new Date('July 11, 1994 12:00:00 UTC'));
-        var difference = jd.getTaiMinusUtc();
-        expect(difference).toEqual(100);
-    });
-
-    it('throws an exception if setting leap seconds without data', function() {
-        expect(function() {
-            LeapSecond.setLeapSeconds();
-        }).toThrow();
     });
 
     it('can compare leap second dates (1)', function() {
