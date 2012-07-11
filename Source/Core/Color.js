40046521f921b00dad1c211ff36c729d3d9087fb
Scott Hunter <shunter@agi.com>
2012-07-11 14:13:21 -0400

    Remerge with latest from master.

--- Source/Core/Color.js
+++ Source/Core/Color.js
@@ -1,18 +1,20 @@
 /*global define*/
-define(['./defaultValue'], function(defaultValue) {
+define([
+        './defaultValue'
+    ], function(
+        defaultValue) {
     "use strict";
 
     /**
      * A color, specified using red, green, blue, and alpha values,
      * which range from <code>0</code> (no intensity) to <code>1.0</code> (full intensity).
+     * @param {Number} [red=1.0] The red component.
+     * @param {Number} [green=1.0] The green component.
+     * @param {Number} [blue=1.0] The blue component.
+     * @param {Number} [alpha=1.0] The alpha component.
      *
      * @constructor
      * @alias Color
-     *
-     * @param {Number} [red=0.0] The red value, from 0.0 to 1.0.
-     * @param {Number} [green=0.0] The red value, from 0.0 to 1.0.
-     * @param {Number} [blue=0.0] The red value, from 0.0 to 1.0.
-     * @param {Number} [alpha=1.0] The red value, from 0.0 to 1.0.
      */
     var Color = function(red, green, blue, alpha) {
         /**
@@ -22,7 +24,7 @@ define(['./defaultValue'], function(defaultValue) {
         /**
          * The green component.
          */
-        this.green = defaultValue(green, 1.0);
+        this.green = defaultValue(green);
         /**
          * The blue component.
          */
@@ -31,6 +33,26 @@ define(['./defaultValue'], function(defaultValue) {
          * The alpha component.
          */
         this.alpha = defaultValue(alpha, 1.0);
+    };
+
+    /**
+     * Creates a new Color specified using red, green, blue, and alpha values
+     * that are in the range of 0 to 255, converting them internally to a
+     * range of 0.0 to 1.0.
+     * @memberof Color
+     *
+     * @param {Number} [red=255] The red component.
+     * @param {Number} [green=255] The green component.
+     * @param {Number} [blue=255] The blue component.
+     * @param {Number} [alpha=255] The alpha component.
+     * @returns {Color} A new color instance.
+     */
+    Color.fromBytes = function(red, green, blue, alpha) {
+        red = typeof red === 'undefined' ? 255.0 : red;
+        green = typeof green === 'undefined' ? 255.0 : green;
+        blue = typeof blue === 'undefined' ? 255.0 : blue;
+        alpha = typeof alpha === 'undefined' ? 255.0 : alpha;
+        return new Color(red / 255.0, green / 255.0, blue / 255.0, alpha / 255.0);
     };
 
     /**
@@ -77,7 +99,65 @@ define(['./defaultValue'], function(defaultValue) {
     };
 
     /**
-     * Returns a string containing a CSS color value for this color.
+     * Returns true if the first Color equals the second color.
+     * @memberof Color
+     *
+     * @param {Color} left The first Color to compare for equality.
+     * @param {Color} right The second Color to compare for equality.
+     * @return {Boolean} <code>true</code> if the Colors are equal; otherwise, <code>false</code>.
+     */
+    Color.equals = function(left, right) {
+        return (left === right) ||
+               (typeof left !== 'undefined' &&
+                typeof right !== 'undefined' &&
+                left.red === right.red &&
+                left.green === right.green &&
+                left.blue === right.blue &&
+                left.alpha === right.alpha);
+
+    };
+
+    /**
+     * Returns a duplicate of a Color instance.
+     * @memberof Color
+     *
+     * @param {Color} [result] The object to store the result in, if undefined a new instance will be created.
+     * @return {Color} The modified result parameter or a new instance if result was undefined.
+     */
+    Color.prototype.clone = function(result) {
+        return Color.clone(this, result);
+    };
+
+    /**
+     * Returns true if this Color equals other.
+     * @memberof Color
+     *
+     * @param {Color} other The Color to compare for equality.
+     * @return {Boolean} <code>true</code> if the Colors are equal; otherwise, <code>false</code>.
+     */
+    Color.prototype.equals = function(other) {
+        return Color.equals(this, other);
+    };
+
+    /**
+     * Returns <code>true</code> if this Color equals other componentwise within the specified epsilon.
+     * @memberof Color
+     *
+     * @param {Color} other The Color to compare for equality.
+     * @param {Number} [epsilon=0.0] The epsilon to use for equality testing.
+     * @return {Boolean} <code>true</code> if the Colors are equal within the specified epsilon; otherwise, <code>false</code>.
+     */
+    Color.prototype.equalsEpsilon = function(other, epsilon) {
+        return (this === other) ||
+                ((typeof other !== 'undefined') &&
+                 (Math.abs(this.red - other.red) <= epsilon) &&
+                 (Math.abs(this.green - other.green) <= epsilon) &&
+                 (Math.abs(this.blue - other.blue) <= epsilon) &&
+                 (Math.abs(this.alpha - other.alpha) <= epsilon));
+    };
+
+    /**
+     * Creates a string containing the CSS color value for this color.
      * @memberof Color
      *
      * @return {String} The CSS equivalent of this color.
