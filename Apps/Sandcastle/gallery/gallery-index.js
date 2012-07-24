2a2d0f68a860e2ad8461ac4c589e694100d43fb5
Scott Hunter <shunter@agi.com>
2012-07-24 16:49:53 -0400

    Remove generated file from another branch.

--- Apps/Sandcastle/gallery/gallery-index.js
+++
@@ -1,7 +0,0 @@
-// This file is automatically rebuilt by the Cesium build process.
-var gallery_demos = [
-	{'name':'cesiumWidgetDemo', 'img':'cesiumWidgetDemo.jpg'},
-	{'name':'circlesAndEllipses', 'img':'circlesAndEllipses.jpg'},
-	{'name':'polyLineTest', 'img':'polyLineTest.jpg'},
-	{'name':'skeletonDemo', 'img':'skeletonDemo.jpg'}
-];
