423ef41216711959ec699bc6708384fb1ff857da
Scott Hunter <shunter@agi.com>
2012-07-26 15:35:24 -0400

    Add tests checking that absolute and data URLs work.

--- Specs/DynamicScene/CzmlImageSpec.js
+++ Specs/DynamicScene/CzmlImageSpec.js
@@ -20,7 +20,33 @@ defineSuite([
     it('unwrapInterval resolves link with sourceUri', function() {
         expect(CzmlImage.unwrapInterval(simpleImage, "http://www.agi.com")).toEqual("http://www.agi.com/foo.png");
         expect(CzmlImage.unwrapInterval(simpleImage, "http://www.agi.com/data.czml")).toEqual("http://www.agi.com/foo.png");
+        expect(CzmlImage.unwrapInterval(simpleImage, "http://www.agi.com/subdir/data.czml")).toEqual("http://www.agi.com/subdir/foo.png");
         expect(CzmlImage.unwrapInterval(constantImageInterval, "http://www.agi.com/someQuery?a=1&b=3")).toEqual("http://www.agi.com/foo.png");
+    });
+
+    it('unwrapInterval works with absolute urls', function() {
+        var url = 'http://example.com/someDir/some.png';
+        expect(CzmlImage.unwrapInterval(url, "http://www.agi.com")).toEqual(url);
+        expect(CzmlImage.unwrapInterval(url, "http://www.agi.com/data.czml")).toEqual(url);
+        expect(CzmlImage.unwrapInterval(url, "http://www.agi.com/subdir/data.czml")).toEqual(url);
+
+        var interval = {
+            image : url
+        };
+        expect(CzmlImage.unwrapInterval(interval, "http://www.agi.com/someQuery?a=1&b=3")).toEqual(url);
+    });
+
+    it('unwrapInterval works with data urls', function() {
+        var dataUrl = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIW2Nk+M/wHwAEBgIA5agATwAAAABJRU5ErkJggg==';
+
+        expect(CzmlImage.unwrapInterval(dataUrl, "http://www.agi.com")).toEqual(dataUrl);
+        expect(CzmlImage.unwrapInterval(dataUrl, "http://www.agi.com/data.czml")).toEqual(dataUrl);
+        expect(CzmlImage.unwrapInterval(dataUrl, "http://www.agi.com/data.czml")).toEqual(dataUrl);
+
+        var interval = {
+            image : dataUrl
+        };
+        expect(CzmlImage.unwrapInterval(interval, "http://www.agi.com/someQuery?a=1&b=3")).toEqual(dataUrl);
     });
 
     it('isSampled always returns false', function() {
