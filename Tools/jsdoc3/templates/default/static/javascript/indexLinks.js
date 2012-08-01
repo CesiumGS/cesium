7f00126696f2286acbbb07b888b6429173f886f0
kristiancalhoun <kristian.calhoun@gmail.com>
2012-08-01 14:03:05 -0400

    Dynamically update menu links to maintain current filter.

--- Tools/jsdoc3/templates/default/static/javascript/indexLinks.js
+++ Tools/jsdoc3/templates/default/static/javascript/indexLinks.js
@@ -12,6 +12,16 @@
 		document.filterForm.classFilter.value = '';
 		codeview.filter('');
 	}
+
+	var updateMenuLinks = function() {
+		var links = document.getElementById("ClassList").getElementsByTagName('a');
+		var searchTerm = document.getElementById('ClassFilter').value; 
+		var show = document.getElementById('filterType').value; 
+		for (var i = 0; i < links.length; i++) {
+			var prefix = links[i].href.split('?')[0];
+			links[i].href = prefix + '?classFilter=' + searchTerm + '&show=' + show;
+		}
+	}	
 	
 	function showAll() {
 		document.getElementById("glslItems").style.display = "block";
@@ -34,16 +44,26 @@
 	allLink.onclick = function() {
 		resetFilter();
 		showAll();
+		updateMenuLinks();
 	}
 	
 	jsLink.onclick = function() {
 		resetFilter();
 		showJs();
+		updateMenuLinks();
 	}
 	
 	glslLink.onclick = function() {
 		resetFilter();
 		showGlsl();
+		updateMenuLinks();
+	}
+	
+	var menuLinks = document.getElementById("ClassList").getElementsByTagName('a');
+	for (var i = 0; i < menuLinks.length; i++) {
+		menuLinks[i].onclick = function() {
+			updateMenuLinks();
+		}
 	}
 	
 	if (filterType.value === js) {
