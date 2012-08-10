16a3ed801244908ce6976abb57271c0217b3cb08
Kevin Ring <kevin@kotachrome.com>
2012-08-10 11:35:39 -0400

    Better fix for issue #148
    Reverted my previous change, and then fixed the original bug by using an
    improved implementation of Cartesian3.angleBetween.  The new
    implementation matches the one in STK Components.

--- Source/Scene/CustomSensorVolume.js
+++ Source/Scene/CustomSensorVolume.js
@@ -209,12 +209,20 @@ define([
         var positions = new Float32Array(3 * length);
         var r = isFinite(radius) ? radius : FAR;
 
-        for (var inputIndex = 0, outputIndex = 0; inputIndex < length; ++inputIndex) {
-            var direction = Cartesian3.fromSpherical(directions[inputIndex]);
-            var p = direction.multiplyByScalar(r);
-            positions[outputIndex++] = p.x;
-            positions[outputIndex++] = p.y;
-            positions[outputIndex++] = p.z;
+        for ( var i = length - 2, j = length - 1, k = 0; k < length; i = j++, j = k++) {
+            // PERFORMANCE_IDEA:  We can avoid redundant operations for adjacent edges.
+            var n0 = Cartesian3.fromSpherical(directions[i]);
+            var n1 = Cartesian3.fromSpherical(directions[j]);
+            var n2 = Cartesian3.fromSpherical(directions[k]);
+
+            // Extend position so the volume encompasses the sensor's radius.
+            var theta = Math.max(Cartesian3.angleBetween(n0, n1), Cartesian3.angleBetween(n1, n2));
+            var distance = r / Math.cos(theta * 0.5);
+            var p = n1.multiplyByScalar(distance);
+
+            positions[(j * 3) + 0] = p.x;
+            positions[(j * 3) + 1] = p.y;
+            positions[(j * 3) + 2] = p.z;
         }
 
         return positions;
