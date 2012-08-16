123842462b3583ba3e903d73df5ca3ac30d1a80b
kristiancalhoun <kristian.calhoun@gmail.com>
2012-08-16 13:24:41 -0400

    Added `@see` and `@example`.

--- Source/Core/PolylinePipeline.js
+++ Source/Core/PolylinePipeline.js
@@ -17,7 +17,17 @@ define(['./Cartographic',
          *
          * @param {Ellipsoid} ellipsoid The ellipsoid to wrap around.
          * @param {Array} positions The polyline's Cartesian positions.
+         *
          * @returns An array of polyline segment objects containing the Cartesian and {@link Cartographic} positions and indices.
+         *
+         * @see Polyline
+         * @see PolylineCollection
+         *
+         * @example
+         * var polylines = new PolylineCollection();
+         * polylines.add(...);
+         * var positions = polylines.get(0).getPositions();
+         * var segments = PolylinePipeline.wrapLongitude(ellipsoid, positions);
          */
         wrapLongitude : function(ellipsoid, positions) {
             var segments = [];
