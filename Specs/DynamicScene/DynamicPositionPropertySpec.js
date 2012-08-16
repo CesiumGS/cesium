f95ebfccb162a456dfc3988d230562237fe5051b
Matthew Amato <matt.amato@gmail.com>
2012-08-16 11:16:11 -0400

    Start specs for DynamicPositionProperty.
    1. Fixed several bugs uncovered, stil need to address a failing test.
    2. Also tweak path visualizer to be last by default so that it doesn't screw up caching.

--- Specs/DynamicScene/DynamicPositionPropertySpec.js
+++ Specs/DynamicScene/DynamicPositionPropertySpec.js
@@ -2,11 +2,15 @@
 defineSuite([
          'DynamicScene/DynamicPositionProperty',
          'Core/JulianDate',
-         'Core/Ellipsoid'
+         'Core/Ellipsoid',
+         'Core/Cartesian3',
+         'Core/Cartographic'
      ], function(
           DynamicPositionProperty,
           JulianDate,
-          Ellipsoid) {
+          Ellipsoid,
+          Cartesian3,
+          Cartographic) {
     "use strict";
     /*global jasmine,describe,xdescribe,it,xit,expect,beforeEach,afterEach,beforeAll,afterAll,spyOn,runs,waits,waitsFor*/
 
@@ -106,5 +110,131 @@ defineSuite([
         expect(result.longitude).toEqual(0.1);
         expect(result.latitude).toEqual(0.2);
         expect(result.height).toEqual(1000);
+    });
+
+    var cartesian0 = new Cartesian3(0.0, 0.0, 0.0);
+    var cartesian1 = new Cartesian3(1.0, 1.0, 1.0);
+    var cartesian2 = new Cartesian3(2.0, 2.0, 2.0);
+
+    var cartesianForgetValueRangeCartesian = {
+        interval: '2012-08-01T00:00:00Z/2012-08-01T00:00:02Z',
+        epoch : '2012-08-01T00:00:00Z',
+        cartesian : [0, cartesian0.x, cartesian0.y, cartesian0.z,
+                     1, cartesian1.x, cartesian1.y, cartesian1.z,
+                     2, cartesian2.x, cartesian2.y, cartesian2.z],
+        interpolationAlgorithm : 'LINEAR',
+        interpolationDegree : 1
+    };
+
+    var cartographic3 = new Cartographic(0.3, 0.3, 0.3);
+    var cartographic4 = new Cartographic(0.4, 0.4, 0.4);
+    var cartographic5 = new Cartographic(0.5, 0.5, 0.5);
+
+    var cartographicForgetValueRangeCartesian = {
+            interval: '2012-08-01T00:00:02Z/2012-08-01T00:00:04Z',
+            epoch : '2012-08-01T00:00:02Z',
+            cartographicRadians : [0, cartographic3.longitude, cartographic3.latitude, cartographic3.height,
+                                   1, cartographic4.longitude, cartographic4.latitude, cartographic4.height,
+                                   2, cartographic5.longitude, cartographic5.latitude, cartographic5.height],
+            interpolationAlgorithm : 'LINEAR',
+            interpolationDegree : 1
+        };
+
+    it('getValueRangeCartesian works with single cartesian interval', function() {
+        var property = new DynamicPositionProperty();
+        property.processCzmlIntervals(cartesianForgetValueRangeCartesian);
+        var start = JulianDate.fromIso8601(cartesianForgetValueRangeCartesian.epoch).addSeconds(0);
+        var stop = JulianDate.fromIso8601(cartesianForgetValueRangeCartesian.epoch).addSeconds(2);
+        var result = property.getValueRangeCartesian(start, stop);
+        expect(result.length).toEqual(3);
+        expect(result[0]).toEqual(cartesian0);
+        expect(result[1]).toEqual(cartesian1);
+        expect(result[2]).toEqual(cartesian2);
+    });
+
+    it('getValueRangeCartesian works with a result parameter', function() {
+        var property = new DynamicPositionProperty();
+        property.processCzmlIntervals(cartesianForgetValueRangeCartesian);
+
+        var start = JulianDate.fromIso8601(cartesianForgetValueRangeCartesian.epoch).addSeconds(0);
+        var stop = JulianDate.fromIso8601(cartesianForgetValueRangeCartesian.epoch).addSeconds(2);
+        var existingCartesian0 = new Cartesian3();
+        var existingCartesian1 = new Cartesian3();
+        var existingCartesian2 = new Cartesian3();
+        var result = [existingCartesian0, existingCartesian1, existingCartesian2, new Cartesian3(), new Cartesian3()];
+
+        var returnedResult = property.getValueRangeCartesian(start, stop, undefined, result);
+        expect(result.length).toEqual(3);
+        expect(result[0]).toEqual(cartesian0);
+        expect(result[1]).toEqual(cartesian1);
+        expect(result[2]).toEqual(cartesian2);
+        expect(returnedResult).toBe(result);
+        expect(result[0]).toBe(existingCartesian0);
+        expect(result[1]).toBe(existingCartesian1);
+        expect(result[2]).toBe(existingCartesian2);
+    });
+
+    it('getValueRangeCartesian works with single cartographic interval', function() {
+        var property = new DynamicPositionProperty();
+        property.processCzmlIntervals(cartographicForgetValueRangeCartesian);
+        var start = JulianDate.fromIso8601(cartographicForgetValueRangeCartesian.epoch).addSeconds(0);
+        var stop = JulianDate.fromIso8601(cartographicForgetValueRangeCartesian.epoch).addSeconds(2);
+        var result = property.getValueRangeCartesian(start, stop);
+        expect(result.length).toEqual(3);
+        expect(result[0]).toEqual(Ellipsoid.WGS84.cartographicToCartesian(cartographic3));
+        expect(result[1]).toEqual(Ellipsoid.WGS84.cartographicToCartesian(cartographic4));
+        expect(result[2]).toEqual(Ellipsoid.WGS84.cartographicToCartesian(cartographic5));
+    });
+
+    it('getValueRangeCartesian works across intervals', function() {
+        var property = new DynamicPositionProperty();
+        property.processCzmlIntervals(cartesianForgetValueRangeCartesian);
+        property.processCzmlIntervals(cartographicForgetValueRangeCartesian);
+
+        var start = JulianDate.fromIso8601(cartesianForgetValueRangeCartesian.epoch).addSeconds(1);
+        var stop = JulianDate.fromIso8601(cartesianForgetValueRangeCartesian.epoch).addSeconds(3);
+        var result = property.getValueRangeCartesian(start, stop);
+        expect(result.length).toEqual(3);
+        expect(result[0]).toEqual(cartesian1);
+        expect(result[1]).toEqual(Ellipsoid.WGS84.cartographicToCartesian(cartographic3));
+        expect(result[2]).toEqual(Ellipsoid.WGS84.cartographicToCartesian(cartographic4));
+    });
+
+    it('getValueRangeCartesian works with no data', function() {
+        var property = new DynamicPositionProperty();
+        var result = property.getValueRangeCartesian(new JulianDate(), new JulianDate());
+        expect(result.length).toEqual(0);
+    });
+
+    it('getValueRangeCartesian works if requested interval is before any data.', function() {
+        var property = new DynamicPositionProperty();
+        property.processCzmlIntervals(cartesianForgetValueRangeCartesian);
+        var start = JulianDate.fromIso8601(cartesianForgetValueRangeCartesian.epoch).addSeconds(-10);
+        var stop = JulianDate.fromIso8601(cartesianForgetValueRangeCartesian.epoch).addSeconds(-5);
+        var result = property.getValueRangeCartesian(start, stop);
+        expect(result.length).toEqual(0);
+    });
+
+    it('getValueRangeCartesian works if requested interval is after all data.', function() {
+        var property = new DynamicPositionProperty();
+        property.processCzmlIntervals(cartesianForgetValueRangeCartesian);
+        var start = JulianDate.fromIso8601(cartesianForgetValueRangeCartesian.epoch).addSeconds(10);
+        var stop = JulianDate.fromIso8601(cartesianForgetValueRangeCartesian.epoch).addSeconds(5);
+        var result = property.getValueRangeCartesian(start, stop);
+        expect(result.length).toEqual(0);
+    });
+
+    it('getValueRangeCartesian throws with no start time', function() {
+        var property = new DynamicPositionProperty();
+        expect(function() {
+            property.getValueRangeCartesian(undefined, new JulianDate());
+        }).toThrow();
+    });
+
+    it('getValueRangeCartesian throws with no stop time', function() {
+        var property = new DynamicPositionProperty();
+        expect(function() {
+            property.getValueRangeCartesian(new JulianDate(), undefined);
+        }).toThrow();
     });
 });
