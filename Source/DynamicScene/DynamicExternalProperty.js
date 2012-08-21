33cf4b715cc991142bcde88d2806abdb02a24104
Matthew Ford <mford2776@hotmail.com>
2012-08-21 16:23:29 -0400

    use setTimeout so that if the server takes longer than the requested refreshInterval, it does not queue up requests. Requests then could return in random order.

--- Source/DynamicScene/DynamicExternalProperty.js
+++ Source/DynamicScene/DynamicExternalProperty.js
@@ -1,57 +1,58 @@
 /*global define*/
 define([
-        '../DynamicScene/processCzml',
-        '../Core/TimeIntervalCollection'
+        '../DynamicScene/processCzml'
     ], function(
-        processCzml,
-        TimeIntervalCollection
+        processCzml
         ) {
     "use strict";
 
 
     var DynamicExternalDocumentProperty = function() {
-        this._dynamicProperties = [];
-        this._propertyIntervals = new TimeIntervalCollection();
-        this._cachedTime = undefined;
-        this._cachedInterval = undefined;
-        this._fit = {};
+        this._source = undefined;
+        this._updaterFunctions = undefined;
     };
 
 
     DynamicExternalDocumentProperty.prototype.processCzmlIntervals = function(object, packet, dynamicObjectCollection, sourceUri, updaterFunctions) {
         if(object.polling){
+            var doc = dynamicObjectCollection;
+            if(doc.compositeCollection){
+                doc = doc.compositeCollection;
+            }
+            var collections = doc.getCollections();
+            var newDoc = doc.createDynamicObjectCollection();
+            collections.splice(collections.length, 0, newDoc);
+            doc.setCollections(collections);
+
             var that = this;
-            var source = object.polling;
+            this._source = object.polling;
+            this._updaterFunctions = updaterFunctions;
             var refreshInterval = object.refreshInterval;
             if(typeof refreshInterval !== 'undefined'){
-                setInterval(function(){
-                    that.refreshInterval(source, dynamicObjectCollection, updaterFunctions);
-                }, refreshInterval * 1000);
+                (function poll(){
+                    setTimeout(function(){
+                        DynamicExternalDocumentProperty.refreshInterval(that._source, newDoc, that._updaterFunctions, poll);
+                    }, refreshInterval * 1000);
+                })();
             }
             else{
-                this.refreshInterval(source, dynamicObjectCollection, updaterFunctions);
+                DynamicExternalDocumentProperty.refreshInterval(this._source, newDoc, this._updaterFunctions);
             }
         }
-
     };
 
-    DynamicExternalDocumentProperty.prototype.refreshInterval = function(source, dynamicObjectCollection, updaterFunctions){
-        var doc = dynamicObjectCollection;
+    DynamicExternalDocumentProperty.refreshInterval = function(source, dynamicObjectCollection, updaterFunctions, poll){
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.open("GET", source, true);
         xmlHttp.setRequestHeader('Accept', 'application/json');
+        xmlHttp.setRequestHeader("Cache-Control", "no-cache");
         xmlHttp.responseType='text';
         xmlHttp.onload = function(){
             if(xmlHttp.status === 200){
                 var text = JSON.parse(xmlHttp.responseText);
-                if(doc.compositeCollection){
-                    processCzml(text, doc, source, updaterFunctions);
-                }else{
-                    var collections = doc.getCollections();
-                    var newLayer = doc.createDynamicObjectCollection();
-                    collections.splice(1, 0, newLayer);
-                    doc.setCollections(collections);
-                    processCzml(text, newLayer, source, updaterFunctions);
+                processCzml(text, dynamicObjectCollection, source, updaterFunctions);
+                if(typeof poll !== 'undefined'){
+                    poll();
                 }
             }
         };
