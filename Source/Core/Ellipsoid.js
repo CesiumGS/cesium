603267476e92a7d3be3de77ab62b3129f8806d6d
Patrick Cozzi <pjcozzi@gmail.com>
2012-08-27 10:05:36 -0400

    Fixed cartesianToCartographic when passed an object literal

--- Source/Core/Ellipsoid.js
+++ Source/Core/Ellipsoid.js
@@ -244,7 +244,7 @@ define([
         var pos = Cartesian3.clone(position);
         var n = this.geodeticSurfaceNormal(pos);
         var p = this.scaleToGeodeticSurface(pos);
-        var h = position.subtract(p);
+        var h = pos.subtract(p);
         var height = CesiumMath.sign(h.dot(pos)) * h.magnitude();
         return new Cartographic(Math.atan2(n.y, n.x), Math.asin(n.z / n.magnitude()), height);
     };
