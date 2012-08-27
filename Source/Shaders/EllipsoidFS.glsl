ac2f0cf5ed2cb6742f54c206a1246ab04195b104
Patrick Cozzi <pjcozzi@gmail.com>
2012-08-27 11:30:23 -0400

    Added material support to ellipsoid

--- Source/Shaders/EllipsoidFS.glsl
+++ Source/Shaders/EllipsoidFS.glsl
@@ -2,6 +2,9 @@ uniform vec3 u_radii;
 
 varying vec3 v_positionEC;
 
+// TODO: uniform
+vec3 oneOverEllipsoidRadiiSquared = 1.0 / (u_radii * u_radii);
+
 void main()
 {
  
@@ -15,12 +18,40 @@ void main()
     {
         // TODO: start is zero if inside the ellipsoid
             
-        // TODO: Use material system.        
-        gl_FragColor = vec4(0.0, 1.0, 0.0, 0.75);
+
+            
+        vec3 positionEC = czm_pointAlongRay(ray, intersection.start);
+        vec3 positionMC = (czm_inverseModelView * vec4(positionEC, 1.0)).xyz;
+        
+        vec3 normalMC = normalize(czm_geodeticSurfaceNormal(positionMC, vec3(0.0), oneOverEllipsoidRadiiSquared));  // normalized surface normal in model coordinates
+        vec3 normalEC = normalize(czm_normal * normalMC);                                                           // normalized surface normal in eye coordiantes
+        
+        vec2 st = czm_ellipsoidWgs84TextureCoordinates(normalMC);
+        vec3 positionToEyeEC = normalize(-positionEC); 
+                    
+        czm_materialInput materialInput;
+        materialInput.s = st.s;
+        materialInput.st = st;
+        materialInput.str = (positionMC + u_radii) / u_radii;
+        materialInput.normalEC = normalEC;
+        materialInput.tangentToEyeMatrix = czm_eastNorthUpToEyeCoordinates(positionMC, normalEC);
+        materialInput.positionToEyeWC = positionToEyeEC;
+        materialInput.positionMC = positionMC;
+        
+        czm_material material = czm_getMaterial(materialInput);
+
+		vec4 color; 
+//#ifdef AFFECTED_BY_LIGHTING
+		color = czm_lightValuePhong(czm_sunDirectionEC, positionToEyeEC, material);
+//#else
+//		color = vec4(material.diffuse, material.alpha);
+//#endif
+		
+		gl_FragColor = color;
     }
     else
     {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 0.75);
-//        discard;
+//        gl_FragColor = vec4(1.0, 0.0, 0.0, 0.75);
+        discard;
     }
 }
