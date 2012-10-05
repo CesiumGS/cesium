802855a622d608b3b03b618d0e2b46ed2fe3ea26
Kevin Ring <kevin@kotachrome.com>
2012-10-05 14:35:26 -0400

    Adjust TerrainProviders for BoundingSphere method rename.

--- Source/Scene/WebMapServiceTerrainProvider.js
+++ Source/Scene/WebMapServiceTerrainProvider.js
@@ -253,7 +253,7 @@ define([
 
         TerrainProvider.createTileEllipsoidGeometryFromBuffers(context, tile, buffers);
         tile.maxHeight = buffers.statistics.maxHeight;
-        tile.boundingSphere3D = BoundingSphere.fromFlatArray(buffers.vertices, tile.center, 5);
+        tile.boundingSphere3D = BoundingSphere.fromPointsAsFlatArray(buffers.vertices, tile.center, 5);
 
         var ellipsoid = this.tilingScheme.ellipsoid;
         var extent = tile.extent;
