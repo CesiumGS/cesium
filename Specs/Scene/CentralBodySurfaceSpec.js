86971d4a6e9dae7d7e83b1ed5f22c3cc1c311b7d
Kevin Ring <kevin@kotachrome.com>
2012-10-05 07:35:02 -0400

    WIP surface rendering test.

--- Specs/Scene/CentralBodySurfaceSpec.js
+++ Specs/Scene/CentralBodySurfaceSpec.js
@@ -1,19 +1,33 @@
 /*global defineSuite*/
 defineSuite([
          'Scene/CentralBodySurface',
+         'Specs/createContext',
+         'Specs/destroyContext',
+         'Specs/frameState',
+         'Specs/render',
+         'Scene/CentralBody',
          'Scene/EllipsoidTerrainProvider',
          'Scene/ImageryLayerCollection'
      ], function(
          CentralBodySurface,
+         createContext,
+         destroyContext,
+         frameState,
+         render,
+         CentralBody,
          EllipsoidTerrainProvider,
          ImageryLayerCollection) {
     "use strict";
     /*global jasmine,describe,xdescribe,it,xit,expect,beforeEach,afterEach,beforeAll,afterAll,spyOn,runs,waits,waitsFor*/
 
+    var context;
+
     beforeAll(function() {
+        context = createContext();
     });
 
     afterAll(function() {
+        destroyContext(context);
     });
 
     beforeEach(function() {
@@ -43,6 +57,27 @@ defineSuite([
             }
             expect(constructWithoutImageryLayerCollection).toThrow();
             expect(surface).toBeUndefined();
+        });
+    });
+
+    describe('layer updating', function() {
+        it('removing a layer removes it from all tiles', function() {
+            var cb = new CentralBody();
+            var surface = cb._surface;
+            expect(surface).not.toBeUndefined();
+
+            var commandLists;
+
+            // update until the load queue is empty.
+            waitsFor(function() {
+                commandLists = [];
+                cb.update(context, frameState, commandLists);
+                return typeof surface._tileLoadQueue.head === 'undefined';
+            });
+
+            runs(function() {
+                cb.destroy();
+            });
         });
     });
 });
