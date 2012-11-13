72edb25bae75de5c2397c709485422d3d35a60c0
Kevin Ring <kevin@kotachrome.com>
2012-11-13 16:56:06 -0500

    Discard fragments with an alpha of 0.

--- Source/Shaders/BillboardCollectionFS.glsl
+++ Source/Shaders/BillboardCollectionFS.glsl
@@ -11,8 +11,20 @@ varying vec4 v_color;
 void main()
 {
 #ifdef RENDER_FOR_PICK
+    vec4 vertexColor = vec4(1.0, 1.0, 1.0, 1.0);
+#else
+    vec4 vertexColor = v_color;
+#endif
+    
+    vec4 color = texture2D(u_atlas, v_textureCoordinates) * vertexColor;
+    if (color.a == 0.0)
+    {
+        discard;
+    }
+    
+#ifdef RENDER_FOR_PICK
     gl_FragColor = v_pickColor;
 #else
-    gl_FragColor = texture2D(u_atlas, v_textureCoordinates) * v_color;
+    gl_FragColor = color;
 #endif
 }
