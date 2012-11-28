0d742b0ed9de93b83e95c3f131c46b3d373a3cbb
Patrick Cozzi <pjcozzi@gmail.com>
2012-11-28 11:45:50 -0500

    Changes based on reivew

--- Specs/Scene/SkyBoxSpec.js
+++ Specs/Scene/SkyBoxSpec.js
@@ -5,7 +5,6 @@ defineSuite([
          'Specs/destroyContext',
          'Specs/createCamera',
          'Specs/createFrameState',
-         'Core/loadImage',
          'Core/Cartesian3',
          'Scene/SceneMode',
          'ThirdParty/when'
@@ -15,7 +14,6 @@ defineSuite([
          destroyContext,
          createCamera,
          createFrameState,
-         loadImage,
          Cartesian3,
          SceneMode,
          when) {
@@ -23,6 +21,7 @@ defineSuite([
     /*global jasmine,describe,xdescribe,it,xit,expect,beforeEach,afterEach,beforeAll,afterAll,spyOn,runs,waits,waitsFor*/
 
     var context;
+    var loadedImage;
 
     beforeAll(function() {
         context = createContext();
@@ -32,40 +31,38 @@ defineSuite([
         destroyContext(context);
     });
 
-    it('draws a sky box from Images', function() {
-        var loadedImage;
-        when(loadImage('./Data/Images/Blue.png'), function(image) {
-            loadedImage = image;
-        });
+    it('create images', function() {
+        loadedImage = new Image();
+        loadedImage.src = './Data/Images/Blue.png';
 
         waitsFor(function() {
-            return typeof loadedImage !== 'undefined';
+            return loadedImage.complete;
         }, 'The image should load.', 5000);
+    });
 
-        runs(function() {
-            var s = new SkyBox({
-                positiveX : loadedImage,
-                negativeX : loadedImage,
-                positiveY : loadedImage,
-                negativeY : loadedImage,
-                positiveZ : loadedImage,
-                negativeZ : loadedImage
-            });
-
-            context.clear();
-            expect(context.readPixels()).toEqual([0, 0, 0, 0]);
-
-            var us = context.getUniformState();
-            var frameState = createFrameState(createCamera(
-                context, new Cartesian3(7000000.0, 0.0, 0.0), Cartesian3.ZERO, Cartesian3.UNIT_Z, 1.0, 20000000.0));
-            us.update(frameState);
-
-            var command = s.update(context, frameState);
-            command.execute(context);
-            expect(context.readPixels()).toEqual([0, 0, 255, 255]);
-
-            s.destroy();
+    it('draws a sky box from Images', function() {
+        var s = new SkyBox({
+            positiveX : loadedImage,
+            negativeX : loadedImage,
+            positiveY : loadedImage,
+            negativeY : loadedImage,
+            positiveZ : loadedImage,
+            negativeZ : loadedImage
         });
+
+        context.clear();
+        expect(context.readPixels()).toEqual([0, 0, 0, 0]);
+
+        var us = context.getUniformState();
+        var frameState = createFrameState(createCamera(
+            context, new Cartesian3(7000000.0, 0.0, 0.0), Cartesian3.ZERO, Cartesian3.UNIT_Z, 1.0, 20000000.0));
+        us.update(frameState);
+
+        var command = s.update(context, frameState);
+        command.execute(context);
+        expect(context.readPixels()).toEqual([0, 0, 255, 255]);
+
+        s.destroy();
     });
 
     it('does not render when show is false', function() {
@@ -165,36 +162,153 @@ defineSuite([
         }).toThrow();
     });
 
-    it('throws when constructed without all source urls', function() {
+    it('throws when constructed without positiveX', function() {
         expect(function() {
             return new SkyBox({
-                positiveX : './Data/Images/Blue.png',
-                negativeX : './Data/Images/Blue.png'
+                negativeX : './Data/Images/Blue.png',
+                positiveY : './Data/Images/Blue.png',
+                negativeY : './Data/Images/Blue.png',
+                positiveZ : './Data/Images/Blue.png',
+                negativeZ : './Data/Images/Blue.png'
             });
         }).toThrow();
     });
 
-    it('throws when constructed with sources of different types', function() {
-        var loadedImage;
-        when(loadImage('./Data/Images/Blue.png'), function(image) {
-            loadedImage = image;
-        });
+    it('throws when constructed without negativeX', function() {
+        expect(function() {
+            return new SkyBox({
+                positiveX : './Data/Images/Blue.png',
+                positiveY : './Data/Images/Blue.png',
+                negativeY : './Data/Images/Blue.png',
+                positiveZ : './Data/Images/Blue.png',
+                negativeZ : './Data/Images/Blue.png'
+            });
+        }).toThrow();
+    });
 
-        waitsFor(function() {
-            return typeof loadedImage !== 'undefined';
-        }, 'The image should load.', 5000);
+    it('throws when constructed without positiveY', function() {
+        expect(function() {
+            return new SkyBox({
+                positiveX : './Data/Images/Blue.png',
+                negativeX : './Data/Images/Blue.png',
+                negativeY : './Data/Images/Blue.png',
+                positiveZ : './Data/Images/Blue.png',
+                negativeZ : './Data/Images/Blue.png'
+            });
+        }).toThrow();
+    });
 
-        runs(function() {
-            expect(function() {
-                return new SkyBox({
-                    positiveX : './Data/Images/Blue.png',
-                    negativeX : './Data/Images/Blue.png',
-                    positiveY : './Data/Images/Blue.png',
-                    negativeY : './Data/Images/Blue.png',
-                    positiveZ : './Data/Images/Blue.png',
-                    negativeZ : loadedImage
-                });
-            }).toThrow();
-        });
+    it('throws when constructed without negativeY', function() {
+        expect(function() {
+            return new SkyBox({
+                positiveX : './Data/Images/Blue.png',
+                negativeX : './Data/Images/Blue.png',
+                positiveY : './Data/Images/Blue.png',
+                positiveZ : './Data/Images/Blue.png',
+                negativeZ : './Data/Images/Blue.png'
+            });
+        }).toThrow();
+    });
+
+    it('throws when constructed without positiveZ', function() {
+        expect(function() {
+            return new SkyBox({
+                positiveX : './Data/Images/Blue.png',
+                negativeX : './Data/Images/Blue.png',
+                positiveY : './Data/Images/Blue.png',
+                negativeY : './Data/Images/Blue.png',
+                negativeZ : './Data/Images/Blue.png'
+            });
+        }).toThrow();
+    });
+
+    it('throws when constructed without negativeZ', function() {
+        expect(function() {
+            return new SkyBox({
+                positiveX : './Data/Images/Blue.png',
+                negativeX : './Data/Images/Blue.png',
+                positiveY : './Data/Images/Blue.png',
+                negativeY : './Data/Images/Blue.png',
+                positiveZ : './Data/Images/Blue.png'
+            });
+        }).toThrow();
+    });
+
+    it('throws when constructed when positiveX is a different type', function() {
+        expect(function() {
+            return new SkyBox({
+                positiveX : loadedImage,
+                negativeX : './Data/Images/Blue.png',
+                positiveY : './Data/Images/Blue.png',
+                negativeY : './Data/Images/Blue.png',
+                positiveZ : './Data/Images/Blue.png',
+                negativeZ : './Data/Images/Blue.png'
+            });
+        }).toThrow();
+    });
+
+    it('throws when constructed when negativeX is a different type', function() {
+        expect(function() {
+            return new SkyBox({
+                positiveX : './Data/Images/Blue.png',
+                negativeX : loadedImage,
+                positiveY : './Data/Images/Blue.png',
+                negativeY : './Data/Images/Blue.png',
+                positiveZ : './Data/Images/Blue.png',
+                negativeZ : './Data/Images/Blue.png'
+            });
+        }).toThrow();
+    });
+
+    it('throws when constructed when positiveY is a different type', function() {
+        expect(function() {
+            return new SkyBox({
+                positiveX : './Data/Images/Blue.png',
+                negativeX : './Data/Images/Blue.png',
+                positiveY : loadedImage,
+                negativeY : './Data/Images/Blue.png',
+                positiveZ : './Data/Images/Blue.png',
+                negativeZ : './Data/Images/Blue.png'
+            });
+        }).toThrow();
+    });
+
+    it('throws when constructed when negativeY is a different type', function() {
+        expect(function() {
+            return new SkyBox({
+                positiveX : './Data/Images/Blue.png',
+                negativeX : './Data/Images/Blue.png',
+                positiveY : './Data/Images/Blue.png',
+                negativeY : loadedImage,
+                positiveZ : './Data/Images/Blue.png',
+                negativeZ : './Data/Images/Blue.png'
+            });
+        }).toThrow();
+    });
+
+    it('throws when constructed when positiveZ is a different type', function() {
+        expect(function() {
+            return new SkyBox({
+                positiveX : './Data/Images/Blue.png',
+                negativeX : './Data/Images/Blue.png',
+                positiveY : './Data/Images/Blue.png',
+                negativeY : './Data/Images/Blue.png',
+                positiveZ : loadedImage,
+                negativeZ : './Data/Images/Blue.png'
+            });
+        }).toThrow();
+    });
+
+    it('throws when constructed when negativeZ is a different type', function() {
+        expect(function() {
+            return new SkyBox({
+                positiveX : './Data/Images/Blue.png',
+                negativeX : './Data/Images/Blue.png',
+                positiveY : './Data/Images/Blue.png',
+                negativeY : './Data/Images/Blue.png',
+                positiveZ : './Data/Images/Blue.png',
+                negativeZ : loadedImage
+            });
+        }).toThrow();
     });
 });
