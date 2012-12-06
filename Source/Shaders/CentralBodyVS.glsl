162ea3174782f40b1c4026eb1af075c6e82f3bc0
Kevin Ring <kevin@kotachrome.com>
2012-12-06 09:16:14 -0500

    Remove old varying from CB vertex shader.

--- Source/Shaders/CentralBodyVS.glsl
+++ Source/Shaders/CentralBodyVS.glsl
@@ -1,6 +1,5 @@
 attribute vec3 position3D;
 attribute vec2 textureCoordinates;
-attribute float waterMask;
 
 uniform float u_morphTime;
 
@@ -16,7 +15,6 @@ varying vec3 v_positionMC;
 varying vec3 v_positionEC;
 
 varying vec2 v_textureCoordinates;
-varying float v_waterMask;
 
 // These functions are generated at runtime.
 vec4 getPosition(vec3 position3DWC);
@@ -87,5 +85,4 @@ void main()
     v_positionEC = (czm_modelView * vec4(position3DWC, 1.0)).xyz;
     v_positionMC = position3DWC;                                 // position in model coordinates
     v_textureCoordinates = textureCoordinates;
-    v_waterMask = waterMask;
 }
