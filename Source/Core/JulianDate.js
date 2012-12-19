5167aadf6cfd8b55b78aded2550adadc0c75e012
Matthew Amato <matt.amato@gmail.com>
2012-12-19 17:18:45 -0500

    Add doc to new JulianDate methods.

--- Source/Core/JulianDate.js
+++ Source/Core/JulianDate.js
@@ -17,14 +17,53 @@ define([
         sprintf) {
     "use strict";
 
+    /**
+     * The object returned by {@link JulianDate#toGregorianDate}.
+     *
+     * @alias GregorianDate
+     * @see JulianDate#toGregorianDate
+     * @constructor
+     */
     var GregorianDate = function(year, month, day, hour, minute, second, millisecond, isLeapSecond) {
+        /**
+         * The year, a whole number.
+         * @type Number
+         */
         this.year = year;
+        /**
+         * The month, a whole number with range [1, 12].
+         * @type Number
+         */
         this.month = month;
+        /**
+         * The day, a whole number with range 1.
+         * @type Number
+         */
         this.day = day;
+        /**
+         * The hour, a whole number with range [0, 23].
+         * @type Number
+         */
         this.hour = hour;
+        /**
+         * The minute, a whole number with range [0, 59].
+         * @type Number
+         */
         this.minute = minute;
+        /**
+         * The second, a whole number with range [0, 60], with 60 representing a leap second.
+         * @type Number
+         */
         this.second = second;
+        /**
+         * The millisecond, a floating point number with range [0.0, 1000.0).
+         * @type Number
+         */
         this.millisecond = millisecond;
+        /**
+         * True if this date is during a leap second.
+         * @type Boolean
+         */
         this.isLeapSecond = isLeapSecond;
     };
 
@@ -678,6 +717,12 @@ define([
 
     var toGregorianDateScratch = new JulianDate(0, 0, TimeStandard.TAI);
 
+    /**
+     * Creates a GregorianDate representation of this date in UTC.
+     * @memberof JulianDate
+     *
+     * @return {GregorianDate} A gregorian date.
+     */
     JulianDate.prototype.toGregorianDate = function() {
         var isLeapSecond = false;
         var thisUtc = convertTaiToUtc(this, toGregorianDateScratch);
@@ -732,9 +777,8 @@ define([
     };
 
     /**
-     * Creates a new JavaScript Date object equivalent to the Julian date
-     * (accurate to the nearest millisecond in the UTC time standard).
-     *
+     * Creates a JavaScript Date representation of this date in UTC.
+     * Javascript dates are only accurate to the nearest millisecond.
      * @memberof JulianDate
      *
      * @return {Date} A new JavaScript Date equivalent to this Julian date.
@@ -749,10 +793,10 @@ define([
     };
 
     /**
-     * Creates an ISO8601 string represenation of this Julian date.
+     * Creates an ISO8601 string represenation of this Julian date in UTC.
      * @memberof JulianDate
      *
-     * @param {Number} [precision=0] The other Julian date, which is the end of the interval.
+     * @param {Number} [precision] The number of fractional digits used to represent the seconds component.  By default, the most precise representation is used.
      * @return {String} An ISO8601 string represenation of this Julian date.
      */
     JulianDate.prototype.toIso8601 = function(precision) {
