9ee9b29747a8cccadca1e6127dc6c3aa68c194cd
Matthew Amato <matt.amato@gmail.com>
2013-01-09 13:29:59 -0500

    Updates after review.

--- Source/Core/Color.js
+++ Source/Core/Color.js
@@ -9,156 +9,6 @@ define([
         DeveloperError) {
     "use strict";
 
-    var cssColorNames = {
-        'aliceblue' : '#F0F8FF',
-        'antiquewhite' : '#FAEBD7',
-        'aqua' : '#00FFFF',
-        'aquamarine' : '#7FFFD4',
-        'azure' : '#F0FFFF',
-        'beige' : '#F5F5DC',
-        'bisque' : '#FFE4C4',
-        'black' : '#000000',
-        'blanchedalmond' : '#FFEBCD',
-        'blue' : '#0000FF',
-        'blueviolet' : '#8A2BE2',
-        'brown' : '#A52A2A',
-        'burlywood' : '#DEB887',
-        'cadetblue' : '#5F9EA0',
-        'chartreuse' : '#7FFF00',
-        'chocolate' : '#D2691E',
-        'coral' : '#FF7F50',
-        'cornflowerblue' : '#6495ED',
-        'cornsilk' : '#FFF8DC',
-        'crimson' : '#DC143C',
-        'cyan' : '#00FFFF',
-        'darkblue' : '#00008B',
-        'darkcyan' : '#008B8B',
-        'darkgoldenrod' : '#B8860B',
-        'darkgray' : '#A9A9A9',
-        'darkgreen' : '#006400',
-        'darkgrey' : '#A9A9A9',
-        'darkkhaki' : '#BDB76B',
-        'darkmagenta' : '#8B008B',
-        'darkolivegreen' : '#556B2F',
-        'darkorange' : '#FF8C00',
-        'darkorchid' : '#9932CC',
-        'darkred' : '#8B0000',
-        'darksalmon' : '#E9967A',
-        'darkseagreen' : '#8FBC8F',
-        'darkslateblue' : '#483D8B',
-        'darkslategray' : '#2F4F4F',
-        'darkslategrey' : '#2F4F4F',
-        'darkturquoise' : '#00CED1',
-        'darkviolet' : '#9400D3',
-        'deeppink' : '#FF1493',
-        'deepskyblue' : '#00BFFF',
-        'dimgray' : '#696969',
-        'dimgrey' : '#696969',
-        'dodgerblue' : '#1E90FF',
-        'firebrick' : '#B22222',
-        'floralwhite' : '#FFFAF0',
-        'forestgreen' : '#228B22',
-        'fuchsia' : '#FF00FF',
-        'gainsboro' : '#DCDCDC',
-        'ghostwhite' : '#F8F8FF',
-        'gold' : '#FFD700',
-        'goldenrod' : '#DAA520',
-        'gray' : '#808080',
-        'green' : '#008000',
-        'greenyellow' : '#ADFF2F',
-        'grey' : '#808080',
-        'honeydew' : '#F0FFF0',
-        'hotpink' : '#FF69B4',
-        'indianred' : '#CD5C5C',
-        'indigo' : '#4B0082',
-        'ivory' : '#FFFFF0',
-        'khaki' : '#F0E68C',
-        'lavender' : '#E6E6FA',
-        'lavenderblush' : '#FFF0F5',
-        'lawngreen' : '#7CFC00',
-        'lemonchiffon' : '#FFFACD',
-        'lightblue' : '#ADD8E6',
-        'lightcoral' : '#F08080',
-        'lightcyan' : '#E0FFFF',
-        'lightgoldenrodyellow' : '#FAFAD2',
-        'lightgray' : '#D3D3D3',
-        'lightgreen' : '#90EE90',
-        'lightgrey' : '#D3D3D3',
-        'lightpink' : '#FFB6C1',
-        'lightsalmon' : '#FFA07A',
-        'lightseagreen' : '#20B2AA',
-        'lightskyblue' : '#87CEFA',
-        'lightslategray' : '#778899',
-        'lightslategrey' : '#778899',
-        'lightsteelblue' : '#B0C4DE',
-        'lightyellow' : '#FFFFE0',
-        'lime' : '#00FF00',
-        'limegreen' : '#32CD32',
-        'linen' : '#FAF0E6',
-        'magenta' : '#FF00FF',
-        'maroon' : '#800000',
-        'mediumaquamarine' : '#66CDAA',
-        'mediumblue' : '#0000CD',
-        'mediumorchid' : '#BA55D3',
-        'mediumpurple' : '#9370DB',
-        'mediumseagreen' : '#3CB371',
-        'mediumslateblue' : '#7B68EE',
-        'mediumspringgreen' : '#00FA9A',
-        'mediumturquoise' : '#48D1CC',
-        'mediumvioletred' : '#C71585',
-        'midnightblue' : '#191970',
-        'mintcream' : '#F5FFFA',
-        'mistyrose' : '#FFE4E1',
-        'moccasin' : '#FFE4B5',
-        'navajowhite' : '#FFDEAD',
-        'navy' : '#000080',
-        'oldlace' : '#FDF5E6',
-        'olive' : '#808000',
-        'olivedrab' : '#6B8E23',
-        'orange' : '#FFA500',
-        'orangered' : '#FF4500',
-        'orchid' : '#DA70D6',
-        'palegoldenrod' : '#EEE8AA',
-        'palegreen' : '#98FB98',
-        'paleturquoise' : '#AFEEEE',
-        'palevioletred' : '#DB7093',
-        'papayawhip' : '#FFEFD5',
-        'peachpuff' : '#FFDAB9',
-        'peru' : '#CD853F',
-        'pink' : '#FFC0CB',
-        'plum' : '#DDA0DD',
-        'powderblue' : '#B0E0E6',
-        'purple' : '#800080',
-        'red' : '#FF0000',
-        'rosybrown' : '#BC8F8F',
-        'royalblue' : '#4169E1',
-        'saddlebrown' : '#8B4513',
-        'salmon' : '#FA8072',
-        'sandybrown' : '#F4A460',
-        'seagreen' : '#2E8B57',
-        'seashell' : '#FFF5EE',
-        'sienna' : '#A0522D',
-        'silver' : '#C0C0C0',
-        'skyblue' : '#87CEEB',
-        'slateblue' : '#6A5ACD',
-        'slategray' : '#708090',
-        'slategrey' : '#708090',
-        'snow' : '#FFFAFA',
-        'springgreen' : '#00FF7F',
-        'steelblue' : '#4682B4',
-        'tan' : '#D2B48C',
-        'teal' : '#008080',
-        'thistle' : '#D8BFD8',
-        'tomato' : '#FF6347',
-        'turquoise' : '#40E0D0',
-        'violet' : '#EE82EE',
-        'wheat' : '#F5DEB3',
-        'white' : '#FFFFFF',
-        'whitesmoke' : '#F5F5F5',
-        'yellow' : '#FFFF00',
-        'yellowgreen' : '#9ACD32'
-    };
-
     function hue2rgb(m1, m2, h) {
         if (h < 0) {
             h += 1;
@@ -246,9 +96,9 @@ define([
         lightness = defaultValue(lightness, 0.0);
         alpha = defaultValue(alpha, 1.0);
 
-        var r = lightness;
-        var g = lightness;
-        var b = lightness;
+        var red = lightness;
+        var green = lightness;
+        var blue = lightness;
 
         if (saturation !== 0) {
             var m2;
@@ -259,12 +109,12 @@ define([
             }
 
             var m1 = 2.0 * lightness - m2;
-            r = hue2rgb(m1, m2, hue + 1 / 3);
-            g = hue2rgb(m1, m2, hue);
-            b = hue2rgb(m1, m2, hue - 1 / 3);
+            red = hue2rgb(m1, m2, hue + 1 / 3);
+            green = hue2rgb(m1, m2, hue);
+            blue = hue2rgb(m1, m2, hue - 1 / 3);
         }
 
-        return new Color(r, g, b, alpha);
+        return new Color(red, green, blue, alpha);
     };
 
     //#rgb
@@ -296,9 +146,9 @@ define([
             throw new DeveloperError('color is required');
         }
 
-        var namedColor = cssColorNames[color.toLowerCase()];
+        var namedColor = Color[color.toUpperCase()];
         if (typeof namedColor !== 'undefined') {
-            return Color.fromCssColorString(namedColor);
+            return namedColor.clone();
         }
 
         var matches = rgbMatcher.exec(color);
@@ -456,10 +306,10 @@ define([
      * @see <a href="http://www.w3.org/TR/css3-color/#rgba-color">CSS RGBA color values</a>
      */
     Color.prototype.toCssColorString = function() {
-        var r = Color.floatToByte(this.red);
-        var g = Color.floatToByte(this.green);
-        var b = Color.floatToByte(this.blue);
-        return 'rgba(' + r + ',' + g + ',' + b + ',' + this.alpha + ')';
+        var red = Color.floatToByte(this.red);
+        var green = Color.floatToByte(this.green);
+        var blue = Color.floatToByte(this.blue);
+        return 'rgba(' + red + ',' + green + ',' + blue + ',' + this.alpha + ')';
     };
 
     /**
