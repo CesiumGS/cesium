d07db008358893d5e4935713e5fd46691a7b4026
Richard Page <rcpage3@gmail.com>
2013-01-24 11:57:56 -0500

    Unit-Testing EOP (handling UT1 over Leap Second)
    Adding a test for handling UT1 interpolation over a leap second as well as
    unit-testing the interpolation capabilities of the rest of the data.

--- Specs/Core/EarthOrientationParametersSpec.js
+++ Specs/Core/EarthOrientationParametersSpec.js
@@ -123,7 +123,7 @@ defineSuite(['Core/EarthOrientationParameters',
             return y1 + dx * (y2 - y1);
         }
 
-        it('data interpolates correctly under normal circumstances', function() {
+        it('interpolates data correctly under normal circumstances', function() {
             var eopDescription = {
                 url : undefined,
                 data : {
@@ -164,7 +164,60 @@ defineSuite(['Core/EarthOrientationParameters',
             x0 = eopDescription.data.samples[1 * nColumns + 5];
             x1 = eopDescription.data.samples[2 * nColumns + 5];
             expected = linearInterp(dt, x0, x1);
-            expect(result.yPoleOffset).toEqualEpsilon(expected, 1e-22);
+            expect(result.yPoleOffset).toEqualEpsilon(expected, 1.0e-22);
+            x0 = eopDescription.data.samples[1*nColumns + 6];
+            x1 = eopDescription.data.samples[2*nColumns + 6];
+            expected = linearInterp(dt, x0, x1);
+            expect(result.ut1MinusUtc).toEqualEpsilon(expected, 1.0e-15);
+        });
+
+        it('interpolates UT1 correctly over a leap second', function() {
+            var eopDescription = {
+                    url : undefined,
+                    data : {
+                        "columnNames":['dateIso8601',
+                                       'modifiedJulianDateUtc',
+                                       'xPoleWanderRadians',
+                                       'yPoleWanderRadians',
+                                       'xCelestialPoleOffsetRadians',
+                                       'yCelestialPoleOffsetRadians',
+                                       'ut1MinusUtcSeconds',
+                                       'taiMinusUtcSeconds',
+                                       'lengthOfDayCorrectionSeconds'],
+                        "samples":[
+                                    "2008-12-30T00:00:00Z",54830.0,-4.6702101901281595e-8,6.989752766028624e-7,-7.80550026586353e-10,-1.052045688007693e-9,-0.5911461,33.0,5.7e-4,
+                                    "2008-12-31T00:00:00Z",54831.0,-6.507169227852191e-8,7.02873178598983e-7,-7.80550026586353e-10,-1.8907733563271904e-9,-0.59187,33.0,8.303e-4,
+                                    "2009-01-01T00:00:00Z",54832.0,-8.25443773457096e-8,7.089575902969077e-7,-8.047907106418297e-10,-2.0604581447155277e-9,0.4071533,34.0,0.0010604,
+                                    "2009-01-02T00:00:00Z",54833.0,-9.884381330461219e-8,7.147559619229779e-7,-8.435758051305926e-10,-1.8228994409718553e-9,0.4059739,34.0,0.0012279,
+                                    "2009-01-03T00:00:00Z",54834.0,-1.1269009203710055e-7,7.226826656091187e-7,-9.066015836748322e-10,-1.5029224114395617e-9,0.404674,34.0,0.0013014
+                                    ]
+                    }
+            };
+
+            var eop = new EarthOrientationParameters(eopDescription);
+            var dateAtLeapSecond = JulianDate.fromIso8601("2009-01-01T00:00:00Z");
+            var dateSlightlyBefore = dateAtLeapSecond.addSeconds(-1.0);
+            var dateSlightlyAfter = dateAtLeapSecond.addSeconds(1.0);
+            var nColumns = eopDescription.data.columnNames.length;
+            var x0 = eopDescription.data.samples[1*nColumns + 6];
+            var x1 = eopDescription.data.samples[2*nColumns + 6];
+            var x2 = eopDescription.data.samples[3*nColumns + 6];
+            var t0 = JulianDate.fromIso8601(eopDescription.data.samples[nColumns]);
+            var t1 = JulianDate.fromIso8601(eopDescription.data.samples[2*nColumns]);
+            var dt = t0.getSecondsDifference(dateSlightlyBefore) / (86400.0 + 1);
+            // Adjust for leap second when interpolating
+            var expectedBefore = linearInterp(dt, x0, (x1 - 1));
+            var resultBefore = eop.compute(dateSlightlyBefore);
+            expect(resultBefore.ut1MinusUtc).toEqualEpsilon(expectedBefore, 1.0e-15);
+            var expectedAt = eopDescription.data.samples[2*nColumns + 6];
+            var resultAt = eop.compute(dateAtLeapSecond);
+            expect(resultAt.ut1MinusUtc).toEqualEpsilon(expectedAt, 1.0e-15);
+            dt = t1.getSecondsDifference(dateSlightlyAfter) / (86400.0);
+            var expectedAfter = linearInterp(dt, x1, x2);
+            var resultAfter = eop.compute(dateSlightlyAfter);
+            expect(resultAfter.ut1MinusUtc).toEqualEpsilon(expectedAfter, 1.0e-15);
+            // Check to make sure the values are (correctly) discontinuous
+            expect(Math.abs(resultBefore.ut1MinusUtc - resultAfter.ut1MinusUtc) > 0.5).toEqual(true);
         });
     });
 });
