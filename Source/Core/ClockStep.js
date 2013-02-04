ef33d25969d634faca22492c83383dca9cabf735
Ed Mackey <elm19087@gmail.com>
2013-02-04 13:44:10 -0500

    Restore docs for SYSTEM_CLOCK_TIME.

--- Source/Core/ClockStep.js
+++ Source/Core/ClockStep.js
@@ -26,6 +26,13 @@ define([
          * time elapsed since the previous call multiplied by {@link Clock#multiplier}.
          */
         SYSTEM_CLOCK_MULTIPLIER : new Enumeration(1, 'SYSTEM_CLOCK_MULTIPLIER'),
+
+        /**
+         * {@link Clock#tick} sets the clock to the current system time, unless
+         * the resulting time would fall out of range of {@link ClockRange}, at
+         * which point this mode reverts to SYSTEM_CLOCK_MULTIPLIER.  To prevent this,
+         * use {@link ClockRange.UNBOUNDED}.
+         */
         SYSTEM_CLOCK_TIME: new Enumeration(2, 'SYSTEM_CLOCK_TIME')
     };
 
