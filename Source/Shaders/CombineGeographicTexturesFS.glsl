a0a5004d6e8a209e1fae84dd85d46073353e01b6
Kevin Ring <kevin@kotachrome.com>
2013-02-21 17:10:40 -0500

    Remove workaround for old Canary bug.
    It no longer appears to be in v24.0.1312.57 and likely never made it into
    a stable version.

--- Source/Shaders/CombineGeographicTexturesFS.glsl
+++ Source/Shaders/CombineGeographicTexturesFS.glsl
@@ -7,23 +7,17 @@ varying vec2 v_textureCoordinates;
 
 void main()
 {
-    // The clamp below works around an apparent bug in Chrome Canary v23.0.1241.0
-    // where the fragment shader sees textures coordinates < 0.0 and > 1.0 for the
-    // fragments on the edges of tiles even though the vertex shader is outputting
-    // coordinates strictly in the 0-1 range.
-    vec2 geographicUV = clamp(v_textureCoordinates, 0.0, 1.0);
-
-    if (geographicUV.s < u_textureCoordinateExtent.s ||
-        geographicUV.s > u_textureCoordinateExtent.p ||
-        geographicUV.t < u_textureCoordinateExtent.t ||
-        geographicUV.t > u_textureCoordinateExtent.q)
+    if (v_textureCoordinates.s < u_textureCoordinateExtent.s ||
+        v_textureCoordinates.s > u_textureCoordinateExtent.p ||
+        v_textureCoordinates.t < u_textureCoordinateExtent.t ||
+        v_textureCoordinates.t > u_textureCoordinateExtent.q)
     {
         discard;
     }
 
     vec2 translation = u_textureTranslationAndScale.xy;
     vec2 scale = u_textureTranslationAndScale.zw;
-    geographicUV = geographicUV * scale + translation;
+    vec2 geographicUV = v_textureCoordinates * scale + translation;
 
     gl_FragColor = texture2D(u_texture, geographicUV);
 }
