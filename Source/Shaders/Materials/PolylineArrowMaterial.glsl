1ee20fb58f53f9cfa9f7f96982a15c42f73b224e
Dan Bagnell <daniel.p.bagnell@gmail.com>
2013-03-20 17:28:16 -0400

    Update polyline arrow material shader.

--- Source/Shaders/Materials/PolylineArrowMaterial.glsl
+++ Source/Shaders/Materials/PolylineArrowMaterial.glsl
@@ -1,4 +1,4 @@
-#extension GL_OES_standard_derivatives : enable // TODO check for support
+#extension GL_OES_standard_derivatives : enable // TODO Is there an alternative if standard derivatives aren't supported?
 
 uniform vec4 color;
 
@@ -16,7 +16,7 @@ czm_material czm_getMaterial(czm_materialInput materialInput)
     
     vec2 st = materialInput.st;
     
-    float base = 1.0 - dFdx(st.s) * 10.0;
+    float base = 1.0 - abs(dFdx(st.s)) * 10.0;
     vec2 center = vec2(1.0, 0.5);
     float ptOnUpperLine = getPointOnLine(vec2(base, 1.0), center, st.s);
     float ptOnLowerLine = getPointOnLine(vec2(base, 0.0), center, st.s);
