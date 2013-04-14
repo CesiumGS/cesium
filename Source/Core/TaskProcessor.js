72148feb80a86cf1e9e18f91de1a48362152ade0
Scott Hunter <scott.k.hunter@gmail.com>
2013-04-13 23:59:47 -0400

    Fix Worker creation when loading AMD modules cross-origin too.

--- Source/Core/TaskProcessor.js
+++ Source/Core/TaskProcessor.js
@@ -76,7 +76,9 @@ define([
         };
 
         if (typeof require.toUrl !== 'undefined') {
-            bootstrapMessage.loaderConfig.baseUrl = '..';
+            var resolvedBootstrapperUrl = new Uri(buildModuleUrl('Workers/cesiumWorkerBootstrapper.js')).resolve(new Uri(document.location.href));
+            var baseUrl = new Uri('..').resolve(resolvedBootstrapperUrl).toString();
+            bootstrapMessage.loaderConfig.baseUrl = baseUrl;
         } else {
             var workersUrl = new Uri(buildModuleUrl('Workers')).resolve(new Uri(document.location.href)).toString();
             bootstrapMessage.loaderConfig.paths = {
