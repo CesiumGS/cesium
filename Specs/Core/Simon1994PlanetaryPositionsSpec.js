fab8f1c3c8d7f0dc37cd4da8bbee016f6b89ce70
hpinkos <pinkos.hannah@gmail.com>
2013-04-17 17:08:31 -0400

    dummy test to run computer barycenter

--- Specs/Core/Simon1994PlanetaryPositionsSpec.js
+++ Specs/Core/Simon1994PlanetaryPositionsSpec.js
@@ -13,9 +13,15 @@ function(PlanetaryPositions,
     /*global jasmine,describe,xdescribe,it,xit,expect,beforeEach,afterEach,beforeAll,afterAll,spyOn,runs,waits,waitsFor*/
 
     // All exact Julian Dates found using NASA's Time Conversion Tool: http://ssd.jpl.nasa.gov/tc.cgi
-    it('runs', function() {
+    it('runs moon', function() {
         var currentDate = JulianDate.fromTotalDays(2436912.5, TimeStandard.TAI);
         var moonPosition = PlanetaryPositions.ComputeMoon(currentDate);
+        expect(true).toEqual(true);
+    });
+
+    it('runs barycenter', function() {
+        var currentDate = JulianDate.fromTotalDays(2436912.5, TimeStandard.TAI);
+        var barycenter = PlanetaryPositions.ComputeEarthMoonBarycenter(currentDate);
         expect(true).toEqual(true);
     });
 });
