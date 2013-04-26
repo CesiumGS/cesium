06e4f7960d457bdfb343db434b6894590d94a296
Kevin Ring <kevin@kotachrome.com>
2013-04-26 09:58:12 -0400

    Tweaks to match blog.

--- Source/Core/EllipsoidalOccluder.js
+++ Source/Core/EllipsoidalOccluder.js
@@ -20,7 +20,8 @@ define([
     /**
      * Determine whether or not other objects are visible or hidden behind the visible horizon defined by
      * an {@link Ellipsoid} and a camera position.  The ellipsoid is assumed to be located at the
-     * origin of the coordinate system.
+     * origin of the coordinate system.  This class uses the algorithm described in the
+     * <a href="http://cesium.agi.com/2013/04/25/Horizon-culling/">Horizon Culling</a> blog post.
      *
      * @alias EllipsoidalOccluder
      *
@@ -74,6 +75,7 @@ define([
      * @param {Cartesian3} cameraPosition The new position of the camera.
      */
     EllipsoidalOccluder.prototype.setCameraPosition = function(cameraPosition) {
+        // See http://cesium.agi.com/2013/04/25/Horizon-culling/
         var ellipsoid = this._ellipsoid;
         var cv = ellipsoid.transformPositionToScaledSpace(cameraPosition, this._cameraPositionInScaledSpace);
         var vhMagnitudeSquared = Cartesian3.magnitudeSquared(cv) - 1.0;
@@ -138,12 +140,14 @@ define([
      * occluder.isScaledSpacePointVisible(scaledSpacePoint); //returns true
      */
     EllipsoidalOccluder.prototype.isScaledSpacePointVisible = function(occludeeScaledSpacePosition) {
-        // See http://cesium.agi.com/2013/04/22/Horizon-culling/
+        // See http://cesium.agi.com/2013/04/25/Horizon-culling/
         var cv = this._cameraPositionInScaledSpace;
         var vhMagnitudeSquared = this._distanceToLimbInScaledSpaceSquared;
         var vt = Cartesian3.subtract(occludeeScaledSpacePosition, cv, scratchCartesian);
         var vtDotVc = -vt.dot(cv);
-        return vtDotVc <= vhMagnitudeSquared || vtDotVc * vtDotVc / vt.magnitudeSquared() <= vhMagnitudeSquared;
+        var isOccluded = vtDotVc > vhMagnitudeSquared &&
+                         vtDotVc * vtDotVc / vt.magnitudeSquared() > vhMagnitudeSquared;
+        return !isOccluded;
     };
 
     return EllipsoidalOccluder;
