d9f0660b586f9286adebc6a7cfc04d59fc4f403e
Dan Bagnell <daniel.p.bagnell@gmail.com>
2013-05-03 14:46:59 -0400

    Update sun fragment shader.

--- Source/Shaders/SunFS.glsl
+++ Source/Shaders/SunFS.glsl
@@ -2,26 +2,26 @@ varying vec2 v_textureCoordinates;
 
 void main()
 {
-    #define DIRECTIONS_SIZE 4
+    #define DIRECTIONS_SIZE 10
     vec2 rayDirections[DIRECTIONS_SIZE];
-    rayDirections[0] = vec2(1.0, 0.0);
-    rayDirections[1] = vec2(0.0, 1.0);
-    rayDirections[2] = vec2(0.5, 0.8660254037844386);
-    rayDirections[3] = vec2(-0.5, 0.8660254037844386);
+    for (int i = 0; i < DIRECTIONS_SIZE; i++)
+    {
+        float angle = czm_pi * float(i) / float(DIRECTIONS_SIZE);
+        rayDirections[i] = vec2(cos(angle), sin(angle));
+    }
     
     vec4 color = vec4(1.0, 1.0, 0.0, 1.0);
     vec2 st = v_textureCoordinates;
     
-    float b = smoothstep(0.04, 0.4, length(v_textureCoordinates - vec2(0.5)));
+    float b = smoothstep(0.03, 0.3, length(v_textureCoordinates - vec2(0.5)));
     color.ba = mix(vec2(1.0), vec2(0.0), b);
     
     float distFromCenter = length((st * 2.0) - 1.0);
-    float verticalColor = mix(1.0, 0.0, distFromCenter);
-    
-    float width = mix(0.02, 0.0, distFromCenter);
+    float width = mix(0.015, 0.0, distFromCenter);
     
     for (int i = 0; i < DIRECTIONS_SIZE; i++)
     {
+        float verticalColor = mix(mod(float(i), 2.0) + 0.5, 0.0, distFromCenter);
         vec2 direction = rayDirections[i];
         float distToLine = length(((vec2(0.5) - st) - (dot(vec2(0.5) - st, direction) * direction)));
         float horizontalColor = clamp(width - distToLine, 0.0, width) / width;
