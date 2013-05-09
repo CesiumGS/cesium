e4828ca8e5872c667bae6bd691ebd0b82e1261ea
Patrick Cozzi <pjcozzi@gmail.com>
2013-05-09 10:28:20 -0400

    Start of vertex format

--- Source/Core/BoxGeometry.js
+++ Source/Core/BoxGeometry.js
@@ -21,6 +21,62 @@ define([
         GeometryIndices) {
     "use strict";
 
+// TODO: move elsewhere
+
+    /**
+     * DOC_TBA
+     *
+     * @alias GeometryVertexFormat
+     * @constructor
+     */
+    var GeometryVertexFormat = function(options) {
+        options = defaultValue(options, defaultValue.EMPTY_OBJECT);
+
+        /**
+         * DOC_TBA
+         */
+        this.position = defaultValue(options.position, true);
+
+        /**
+         * DOC_TBA
+         */
+        this.normal = defaultValue(options.normal, true);
+
+        /**
+         * DOC_TBA
+         */
+        this.textureCoordinates = defaultValue(options.textureCoordinates, true);
+
+        /**
+         * DOC_TBA
+         */
+        this.binormal = defaultValue(options.binormal, false);
+
+        /**
+         * DOC_TBA
+         */
+        this.tangent = defaultValue(options.tangent, false);
+    };
+
+ // TODO: freezeObject
+
+    /**
+     * DOC_TBA
+     */
+    GeometryVertexFormat.DEFAULT = new GeometryVertexFormat();
+
+    /**
+     * DOC_TBA
+     */
+    GeometryVertexFormat.POSITION_ONLY = new GeometryVertexFormat({
+        normal : false,
+        textureCoordinates : false
+    });
+
+/////////////////////////////////////////////////////////////////
+
+
+
     /**
      * DOC_TBA
      *
@@ -50,50 +106,63 @@ define([
             maximumCorner = corner;
         }
 
-        /**
-         * DOC_TBA
-         */
-        this.attributes = {
-            position : new GeometryAttribute({
-                componentDatatype : ComponentDatatype.FLOAT,
-                componentsPerAttribute : 3,
-                values : [
-                    // 8 corner points.
-                    minimumCorner.x, minimumCorner.y, minimumCorner.z,
-                    maximumCorner.x, minimumCorner.y, minimumCorner.z,
-                    maximumCorner.x, maximumCorner.y, minimumCorner.z,
-                    minimumCorner.x, maximumCorner.y, minimumCorner.z,
-                    minimumCorner.x, minimumCorner.y, maximumCorner.z,
-                    maximumCorner.x, minimumCorner.y, maximumCorner.z,
-                    maximumCorner.x, maximumCorner.y, maximumCorner.z,
-                    minimumCorner.x, maximumCorner.y, maximumCorner.z
-                ]
-            })
-        };
+        var vertexFormat = defaultValue(options.vertexFormat, GeometryVertexFormat.DEFAULT);
+
+        var attributes;
+        var indexLists;
+
+//        if (vertexFormat !== GeometryVertexFormat.POSITION_ONLY) {
+//        } else {
+            // Positions only - no need to duplicate corner points
+            attributes = {
+                position : new GeometryAttribute({
+                    componentDatatype : ComponentDatatype.FLOAT,
+                    componentsPerAttribute : 3,
+                    values : [
+                        // 8 corner points.
+                        minimumCorner.x, minimumCorner.y, minimumCorner.z,
+                        maximumCorner.x, minimumCorner.y, minimumCorner.z,
+                        maximumCorner.x, maximumCorner.y, minimumCorner.z,
+                        minimumCorner.x, maximumCorner.y, minimumCorner.z,
+                        minimumCorner.x, minimumCorner.y, maximumCorner.z,
+                        maximumCorner.x, minimumCorner.y, maximumCorner.z,
+                        maximumCorner.x, maximumCorner.y, maximumCorner.z,
+                        minimumCorner.x, maximumCorner.y, maximumCorner.z
+                    ]
+                })
+            };
+
+            indexLists = [
+                new GeometryIndices({
+                    // 12 triangles:  6 faces, 2 triangles each.
+                    primitiveType : PrimitiveType.TRIANGLES,
+                    values : [
+                        4, 5, 6, // Top: plane z = corner.Z
+                        4, 6, 7,
+                        1, 0, 3, // Bottom: plane z = -corner.Z
+                        1, 3, 2,
+                        1, 6, 5, // Side: plane x = corner.X
+                        1, 2, 6,
+                        2, 3, 7, // Side: plane y = corner.Y
+                        2, 7, 6,
+                        3, 0, 4, // Side: plane x = -corner.X
+                        3, 4, 7,
+                        0, 1, 5, // Side: plane y = -corner.Y
+                        0, 5, 4
+                    ]
+                })
+            ];
+//        }
 
         /**
          * DOC_TBA
          */
-        this.indexLists = [
-            new GeometryIndices({
-                // 12 triangles:  6 faces, 2 triangles each.
-                primitiveType : PrimitiveType.TRIANGLES,
-                values : [
-                    4, 5, 6, // Top: plane z = corner.Z
-                    4, 6, 7,
-                    1, 0, 3, // Bottom: plane z = -corner.Z
-                    1, 3, 2,
-                    1, 6, 5, // Side: plane x = corner.X
-                    1, 2, 6,
-                    2, 3, 7, // Side: plane y = corner.Y
-                    2, 7, 6,
-                    3, 0, 4, // Side: plane x = -corner.X
-                    3, 4, 7,
-                    0, 1, 5, // Side: plane y = -corner.Y
-                    0, 5, 4
-                ]
-            })
-        ];
+        this.attributes = attributes;
+
+        /**
+         * DOC_TBA
+         */
+        this.indexLists = indexLists;
 
         /**
          * DOC_TBA
