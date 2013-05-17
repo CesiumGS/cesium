fae9bbb45dd2255110159fd2bb8ea9020236a104
Dan Bagnell <daniel.p.bagnell@gmail.com>
2013-05-16 20:12:38 -0400

    Start moving EllipsoidGeometry to use a VertexFormat.

--- Source/Core/EllipsoidGeometry.js
+++ Source/Core/EllipsoidGeometry.js
@@ -9,7 +9,8 @@ define([
         './PrimitiveType',
         './BoundingSphere',
         './GeometryAttribute',
-        './GeometryIndices'
+        './GeometryIndices',
+        './VertexFormat'
     ], function(
         defaultValue,
         DeveloperError,
@@ -20,7 +21,8 @@ define([
         PrimitiveType,
         BoundingSphere,
         GeometryAttribute,
-        GeometryIndices) {
+        GeometryIndices,
+        VertexFormat) {
     "use strict";
 
     /**
@@ -37,83 +39,14 @@ define([
         var ellipsoid = defaultValue(options.ellipsoid, Ellipsoid.UNIT_SPHERE);
         var numberOfPartitions = defaultValue(options.numberOfPartitions, 32);
 
+        var vertexFormat = defaultValue(options.vertexFormat, VertexFormat.DEFAULT);
+
         if (numberOfPartitions <= 0) {
             throw new DeveloperError('options.numberOfPartitions must be greater than zero.');
         }
 
         var positions = [];
         var indices = [];
-
-        function addEdgePositions(i0, i1) {
-            var indices = [];
-            indices[0] = i0;
-            indices[2 + (numberOfPartitions - 1) - 1] = i1;
-
-            var origin = positions[i0];
-            var direction = positions[i1].subtract(positions[i0]);
-
-            for ( var i = 1; i < numberOfPartitions; ++i) {
-                var delta = i / numberOfPartitions;
-
-                indices[i] = positions.length;
-                positions.push(origin.add(direction.multiplyByScalar(delta)));
-            }
-
-            return indices;
-        }
-
-        function addFaceTriangles(leftBottomToTop, bottomLeftToRight, rightBottomToTop, topLeftToRight) {
-            var origin = positions[bottomLeftToRight[0]];
-            var x = positions[bottomLeftToRight[bottomLeftToRight.length - 1]].subtract(origin);
-            var y = positions[topLeftToRight[0]].subtract(origin);
-
-            var bottomIndicesBuffer = [];
-            var topIndicesBuffer = [];
-
-            var bottomIndices = bottomLeftToRight;
-            var topIndices = topIndicesBuffer;
-
-            for ( var j = 1; j <= numberOfPartitions; ++j) {
-                if (j !== numberOfPartitions) {
-                    if (j !== 1) {
-                        //
-                        // This copy could be avoided by ping ponging buffers.
-                        //
-                        bottomIndicesBuffer = topIndicesBuffer.slice(0);
-                        bottomIndices = bottomIndicesBuffer;
-                    }
-
-                    topIndicesBuffer[0] = leftBottomToTop[j];
-                    topIndicesBuffer[numberOfPartitions] = rightBottomToTop[j];
-
-                    var deltaY = j / numberOfPartitions;
-                    var offsetY = y.multiplyByScalar(deltaY);
-
-                    for ( var i = 1; i < numberOfPartitions; ++i) {
-                        var deltaX = i / numberOfPartitions;
-                        var offsetX = x.multiplyByScalar(deltaX);
-
-                        topIndicesBuffer[i] = positions.length;
-                        positions.push(origin.add(offsetX).add(offsetY));
-                    }
-                } else {
-                    if (j !== 1) {
-                        bottomIndices = topIndicesBuffer;
-                    }
-                    topIndices = topLeftToRight;
-                }
-
-                for ( var k = 0; k < numberOfPartitions; ++k) {
-                    indices.push(bottomIndices[k]);
-                    indices.push(bottomIndices[k + 1]);
-                    indices.push(topIndices[k + 1]);
-
-                    indices.push(bottomIndices[k]);
-                    indices.push(topIndices[k + 1]);
-                    indices.push(topIndices[k]);
-                }
-            }
-        }
 
         //
         // Initial cube.  In the plane, z = -1:
@@ -145,52 +78,62 @@ define([
         // 4 -> 5, 5 -> 6, 6 -> 7, 7 -> 4.  Plane z = 1
         // 0 -> 4, 1 -> 5, 2 -> 6, 3 -> 7.  From plane z = -1 to plane z - 1
         //
-        var edge0to1 = addEdgePositions(0, 1);
-        var edge1to2 = addEdgePositions(1, 2);
-        var edge2to3 = addEdgePositions(2, 3);
-        var edge3to0 = addEdgePositions(3, 0);
+        var edge0to1 = addEdgePositions(0, 1, numberOfPartitions, positions);
+        var edge1to2 = addEdgePositions(1, 2, numberOfPartitions, positions);
+        var edge2to3 = addEdgePositions(2, 3, numberOfPartitions, positions);
+        var edge3to0 = addEdgePositions(3, 0, numberOfPartitions, positions);
 
-        var edge4to5 = addEdgePositions(4, 5);
-        var edge5to6 = addEdgePositions(5, 6);
-        var edge6to7 = addEdgePositions(6, 7);
-        var edge7to4 = addEdgePositions(7, 4);
+        var edge4to5 = addEdgePositions(4, 5, numberOfPartitions, positions);
+        var edge5to6 = addEdgePositions(5, 6, numberOfPartitions, positions);
+        var edge6to7 = addEdgePositions(6, 7, numberOfPartitions, positions);
+        var edge7to4 = addEdgePositions(7, 4, numberOfPartitions, positions);
 
-        var edge0to4 = addEdgePositions(0, 4);
-        var edge1to5 = addEdgePositions(1, 5);
-        var edge2to6 = addEdgePositions(2, 6);
-        var edge3to7 = addEdgePositions(3, 7);
+        var edge0to4 = addEdgePositions(0, 4, numberOfPartitions, positions);
+        var edge1to5 = addEdgePositions(1, 5, numberOfPartitions, positions);
+        var edge2to6 = addEdgePositions(2, 6, numberOfPartitions, positions);
+        var edge3to7 = addEdgePositions(3, 7, numberOfPartitions, positions);
 
-        addFaceTriangles(edge0to4, edge0to1, edge1to5, edge4to5); // Q3 Face
-        addFaceTriangles(edge1to5, edge1to2, edge2to6, edge5to6); // Q4 Face
-        addFaceTriangles(edge2to6, edge2to3, edge3to7, edge6to7); // Q1 Face
-        addFaceTriangles(edge3to7, edge3to0, edge0to4, edge7to4); // Q2 Face
-        addFaceTriangles(edge7to4.slice(0).reverse(), edge4to5, edge5to6, edge6to7.slice(0).reverse()); // Plane z = 1
-        addFaceTriangles(edge1to2, edge0to1.slice(0).reverse(), edge3to0.slice(0).reverse(), edge2to3); // Plane z = -1
+        // Q3 Face
+        addFaceTriangles(edge0to4, edge0to1, edge1to5, edge4to5, numberOfPartitions, positions, indices);
+        // Q4 Face
+        addFaceTriangles(edge1to5, edge1to2, edge2to6, edge5to6, numberOfPartitions, positions, indices);
+        // Q1 Face
+        addFaceTriangles(edge2to6, edge2to3, edge3to7, edge6to7, numberOfPartitions, positions, indices);
+        // Q2 Face
+        addFaceTriangles(edge3to7, edge3to0, edge0to4, edge7to4, numberOfPartitions, positions, indices);
+        // Plane z = 1
+        addFaceTriangles(edge7to4.slice(0).reverse(), edge4to5, edge5to6, edge6to7.slice(0).reverse(), numberOfPartitions, positions, indices);
+        // Plane z = -1
+        addFaceTriangles(edge1to2, edge0to1.slice(0).reverse(), edge3to0.slice(0).reverse(), edge2to3, numberOfPartitions, positions, indices);
 
-        // Expand cube into ellipsoid and flatten values
-        var radii = ellipsoid.getRadii();
-        var length = positions.length;
-        var q = 0;
-        var flattenedPositions = new Array(length * 3);
-        for ( var i = 0; i < length; ++i) {
-            var item = positions[i];
-            Cartesian3.normalize(item, item);
-            Cartesian3.multiplyComponents(item, radii, item);
-            flattenedPositions[q++] = item.x;
-            flattenedPositions[q++] = item.y;
-            flattenedPositions[q++] = item.z;
+        var attributes = {};
+
+        if (vertexFormat.position) {
+            // Expand cube into ellipsoid and flatten values
+            var radii = ellipsoid.getRadii();
+            var length = positions.length;
+            var q = 0;
+            var flattenedPositions = new Array(length * 3);
+            for ( var i = 0; i < length; ++i) {
+                var item = positions[i];
+                Cartesian3.normalize(item, item);
+                Cartesian3.multiplyComponents(item, radii, item);
+                flattenedPositions[q++] = item.x;
+                flattenedPositions[q++] = item.y;
+                flattenedPositions[q++] = item.z;
+            }
+
+            attributes.position = new GeometryAttribute({
+                componentDatatype : ComponentDatatype.FLOAT,
+                componentsPerAttribute : 3,
+                values : flattenedPositions
+            });
         }
 
         /**
          * DOC_TBA
          */
-        this.attributes = {
-            position : new GeometryAttribute({
-                componentDatatype : ComponentDatatype.FLOAT,
-                componentsPerAttribute : 3,
-                values : flattenedPositions
-            })
-        };
+        this.attributes = attributes;
 
         /**
          * DOC_TBA
@@ -217,6 +160,77 @@ define([
          */
         this.pickData = options.pickData;
     };
+
+    function addEdgePositions(i0, i1, numberOfPartitions, positions) {
+        var indices = [];
+        indices[0] = i0;
+        indices[2 + (numberOfPartitions - 1) - 1] = i1;
+
+        var origin = positions[i0];
+        var direction = positions[i1].subtract(positions[i0]);
+
+        for ( var i = 1; i < numberOfPartitions; ++i) {
+            var delta = i / numberOfPartitions;
+
+            indices[i] = positions.length;
+            positions.push(origin.add(direction.multiplyByScalar(delta)));
+        }
+
+        return indices;
+    }
+
+    function addFaceTriangles(leftBottomToTop, bottomLeftToRight, rightBottomToTop, topLeftToRight, numberOfPartitions, positions, indices) {
+        var origin = positions[bottomLeftToRight[0]];
+        var x = positions[bottomLeftToRight[bottomLeftToRight.length - 1]].subtract(origin);
+        var y = positions[topLeftToRight[0]].subtract(origin);
+
+        var bottomIndicesBuffer = [];
+        var topIndicesBuffer = [];
+
+        var bottomIndices = bottomLeftToRight;
+        var topIndices = topIndicesBuffer;
+
+        for ( var j = 1; j <= numberOfPartitions; ++j) {
+            if (j !== numberOfPartitions) {
+                if (j !== 1) {
+                    //
+                    // This copy could be avoided by ping ponging buffers.
+                    //
+                    bottomIndicesBuffer = topIndicesBuffer.slice(0);
+                    bottomIndices = bottomIndicesBuffer;
+                }
+
+                topIndicesBuffer[0] = leftBottomToTop[j];
+                topIndicesBuffer[numberOfPartitions] = rightBottomToTop[j];
+
+                var deltaY = j / numberOfPartitions;
+                var offsetY = y.multiplyByScalar(deltaY);
+
+                for ( var i = 1; i < numberOfPartitions; ++i) {
+                    var deltaX = i / numberOfPartitions;
+                    var offsetX = x.multiplyByScalar(deltaX);
+
+                    topIndicesBuffer[i] = positions.length;
+                    positions.push(origin.add(offsetX).add(offsetY));
+                }
+            } else {
+                if (j !== 1) {
+                    bottomIndices = topIndicesBuffer;
+                }
+                topIndices = topLeftToRight;
+            }
+
+            for ( var k = 0; k < numberOfPartitions; ++k) {
+                indices.push(bottomIndices[k]);
+                indices.push(bottomIndices[k + 1]);
+                indices.push(topIndices[k + 1]);
+
+                indices.push(bottomIndices[k]);
+                indices.push(topIndices[k + 1]);
+                indices.push(topIndices[k]);
+            }
+        }
+    }
 
     return EllipsoidGeometry;
 });
