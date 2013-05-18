d45ad97e23af8bf491fe1fc2532dfc6679a535fa
YouLu <steven_lu860612@hotmail.com>
2013-05-17 19:16:13 -0700

    Solve Mac bugs

--- Source/Scene/CentralBody.js
+++ Source/Scene/CentralBody.js
@@ -669,6 +669,7 @@ define([
             }
 
             this._surfaceShaderSet.baseVertexShaderString =
+                 (hasWaterMask ? '#define XXXX\n' : '') +
                  CentralBodyVS + '\n' +
                  getPositionMode + '\n' +
                  get2DYPositionFraction;
