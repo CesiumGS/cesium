99aaa12220bc8221a2313c9d69e768e4b5377a3e
Dan Bagnell <daniel.p.bagnell@gmail.com>
2013-05-31 14:41:23 -0400

    Modify WallGeometry to follow the template of other geometry types.

--- Source/Core/WallGeometry.js
+++ Source/Core/WallGeometry.js
@@ -1,45 +1,54 @@
 /*global define*/
 define([
-        './DeveloperError',
-        './Ellipsoid',
-        './Matrix4',
-        './ComponentDatatype',
-        './PrimitiveType',
         './defaultValue',
         './BoundingSphere',
+        './Cartesian3',
+        './Cartographic',
+        './ComponentDatatype',
+        './DeveloperError',
+        './Ellipsoid',
         './GeometryAttribute',
-        './GeometryIndices'
+        './GeometryIndices',
+        './Matrix4',
+        './PrimitiveType',
+        './VertexFormat'
     ], function(
-        DeveloperError,
-        Ellipsoid,
-        Matrix4,
-        ComponentDatatype,
-        PrimitiveType,
         defaultValue,
         BoundingSphere,
+        Cartesian3,
+        Cartographic,
+        ComponentDatatype,
+        DeveloperError,
+        Ellipsoid,
         GeometryAttribute,
-        GeometryIndices) {
+        GeometryIndices,
+        Matrix4,
+        PrimitiveType,
+        VertexFormat) {
     "use strict";
+
+    var scratchCartographic = new Cartographic();
+    var scratchCartesian3Position = new Cartesian3();
 
     /**
      * Creates a wall, which is similar to a KML line string. A wall is defined by a series of points,
-     * which extrude down to the ground. Optionally they can extrude downwards to a specified height.
+     * which extrude down to the ground. Optionally, they can extrude downwards to a specified height.
      * The points in the wall can be offset by supplied terrain elevation data.
      *
      * @alias WallGeometry
      * @constructor
      *
-     * @param {Array} positions an array of Cartesian objects, which are the points of the wall
-     * @param {string} altitudeMode either 'absolute' or 'relativeToGround'. 'absolute' means the height
-     *        is treated from the WGS84 ellipsoid. 'relativeToGround' means they are treated
-     *        relative to the supplied terrain data
-     * @param {Array} [terrain] required if altitudeMode is 'relativeToGround'. has to denote the same points
-     *        as in positions, with the ground elevation reflecting the terrain elevation
-     * @param {Number} [top] optional, the top of the wall. if specified, the top of the wall is treated as this
-     *        height, and the information in the positions array is disregarded
-     * @param {Number} [bottom] optional, the bottom of the wall. if specified, the bottom of the wall is treated as
-     *        this height. otherwise its treated as 'ground' (the WGS84 ellipsoid height 0)
-     * @param {Ellipsoid} [ellipsoid=Ellipsoid.WGS84] ellispoid for coordinate manipulation
+     * @param {Array} positions An array of Cartesian objects, which are the points of the wall.
+     * @param {String} altitudeMode 'absolute' means the height is treated from the WGS84 ellipsoid.
+     *        'relativeToGround' means they are treated relative to the supplied terrain data.
+     * @param {Array} [terrain] Required if <code>altitudeMode</code> is 'relativeToGround'.
+     *        Has to denote the same points as in positions, with the ground elevation reflecting the terrain elevation.
+     * @param {Number} [top] The top of the wall. If specified, the top of the wall is treated as this
+     *        height, and the information in the positions array is disregarded.
+     * @param {Number} [bottom] The bottom of the wall. If specified, the bottom of the wall is treated as
+     *        this height. Otherwise, its treated as 'ground' (the WGS84 ellipsoid height 0).
+     * @param {Ellipsoid} [ellipsoid=Ellipsoid.WGS84] The ellipsoid for coordinate manipulation
+     * @param {VertexFormat} [options.vertexFormat=VertexFormat.DEFAULT] The vertex attributes to be computed.
      * @param {Matrix4} [options.modelMatrix] The model matrix for this geometry.
      * @param {Color} [options.color] The color of the geometry when a per-geometry color appearance is used.
      * @param {DOC_TBA} [options.pickData] DOC_TBA
@@ -60,86 +69,87 @@ define([
      *  ];
      *
      *  // create a wall that spans from ground level to 10000 meters
-     *  var wall1 = new Cesium.WallGeometry({
+     *  var wall = new Cesium.WallGeometry({
      *      altitudeMode : 'absolute',
-     *      positions    : ellipsoid.cartographicArrayToCartesianArray(positions),
-     *      pickData     : 'wall1'
+     *      positions    : ellipsoid.cartographicArrayToCartesianArray(positions)
      *  });
      *
      */
     var WallGeometry = function(options) {
         options = defaultValue(options, defaultValue.EMPTY_OBJECT);
 
-        var positions;
+        var wallPositions = options.positions;
+        var altitudeMode = options.altitudeMode;
+        var terrain = options.terrain;
+        var top = options.top;
+        var bottom = options.bottom;
+        var ellipsoid = defaultValue(options.ellipsoid, Ellipsoid.WGS84);
+        var vertexFormat = defaultValue(options.vertexFormat, VertexFormat.DEFAULT);
 
-        if (typeof options.positions !== 'undefined') {
-            positions = options.positions;
-        } else {
+        if (typeof wallPositions === 'undefined') {
             throw new DeveloperError('positions is required.');
         }
 
-        var attributes = {};
-        var indexLists = [];
-
-        if (options.altitudeMode === 'relativeToGround' && typeof options.terrain === 'undefined') {
+        if (altitudeMode === 'relativeToGround' && typeof terrain === 'undefined') {
             throw new DeveloperError('No terrain supplied when required.');
         }
-        if (typeof options.terrain !== 'undefined' && options.terrain.length !== positions.length) {
+
+        if (typeof terrain !== 'undefined' && terrain.length !== wallPositions.length) {
             throw new DeveloperError('Coordinates and terrain points don\'t match in number');
         }
 
-        var ellipsoid = defaultValue(options.ellipsoid, Ellipsoid.WGS84);
-
-        attributes.position = new GeometryAttribute({
-            componentDatatype : ComponentDatatype.FLOAT,
-            componentsPerAttribute : 3,
-            values : new Array(positions.length * 6)
-        });
+        var i;
+        var j;
 
         // add lower and upper points one after the other, lower
         // points being even and upper points being odd
-        var origHeight;
-        var c;
-        var values = attributes.position.values;
-        for (var i = 0, j = 0; i < positions.length; ++i) {
-            c = ellipsoid.cartesianToCartographic(positions[i]);
-            origHeight = c.height;
+        var positions = new Array(wallPositions.length * 6);
+        for (i = 0, j = 0; i < wallPositions.length; ++i) {
+            var c = ellipsoid.cartesianToCartographic(wallPositions[i], scratchCartographic);
+            var origHeight = c.height;
             c.height = 0.0;
-            if (options.bottom !== undefined) {
-                c.height = options.bottom;
-            }
-            if (options.terrain !== undefined && options.bottom !== undefined &&
-                !isNaN(options.terrain[i].height)) {
 
-                c.height += options.terrain[i].height;
+            var terrainHeight = 0.0;
+            if (terrain !== undefined) {
+                var h = terrain[i].height;
+                if (!isNaN(h)) {
+                    terrainHeight = h;
+                }
             }
-            var v = ellipsoid.cartographicToCartesian(c);
+
+            if (bottom !== undefined) {
+                c.height = bottom;
+            } else {
+                c.height += terrainHeight;
+            }
+
+            var v = ellipsoid.cartographicToCartesian(c, scratchCartesian3Position);
 
             // insert the lower point
-            values[j++] = v.x;
-            values[j++] = v.y;
-            values[j++] = v.z;
+            positions[j++] = v.x;
+            positions[j++] = v.y;
+            positions[j++] = v.z;
 
             // get the original height back, or set the top value
-            c.height    = options.top === undefined ? origHeight : options.top;
-            if (options.terrain !== undefined && !isNaN(options.terrain[i].height)) {
-                c.height += options.terrain[i].height;
-            }
-            v = ellipsoid.cartographicToCartesian(c);
+            c.height = (top === undefined) ? origHeight : top;
+            c.height += terrainHeight;
+            v = ellipsoid.cartographicToCartesian(c, scratchCartesian3Position);
 
             // insert the upper point
-            values[j++] = v.x;
-            values[j++] = v.y;
-            values[j++] = v.z;
+            positions[j++] = v.x;
+            positions[j++] = v.y;
+            positions[j++] = v.z;
         }
 
-        var noPoints1 = attributes.position.values.length / 3 - 2;
+        var attributes = {};
 
-        indexLists.push(
-            new GeometryIndices({
-                primitiveType : PrimitiveType.TRIANGLES,
-                values : new Array(noPoints1 * 3)
-        }));
+        if (vertexFormat.position) {
+            attributes.position = new GeometryAttribute({
+                componentDatatype : ComponentDatatype.FLOAT,
+                componentsPerAttribute : 3,
+                values : positions
+            });
+        }
 
 
         // prepare the side walls, two triangles for each wall
@@ -156,17 +166,20 @@ define([
         //    C (i)    D (i+2) F
         //
 
-        var indexes = indexLists[0].values;
-        for (i = 0, j = 0; i < noPoints1; i += 2) {
+        var size = positions.length / 3 - 2;
+        var indices = new Array(size * 3);
+
+        j = 0;
+        for (i = 0; i < size; i += 2) {
             // first do A C B
-            indexes[j++] = i + 1;
-            indexes[j++] = i;
-            indexes[j++] = i + 3;
+            indices[j++] = i + 1;
+            indices[j++] = i;
+            indices[j++] = i + 3;
 
             // now do B C D
-            indexes[j++] = i + 3;
-            indexes[j++] = i;
-            indexes[j++] = i + 2;
+            indices[j++] = i + 3;
+            indices[j++] = i;
+            indices[j++] = i + 2;
         }
 
         /**
@@ -182,14 +195,19 @@ define([
          *
          * @type Array
          */
-        this.indexLists = indexLists;
+        this.indexLists = [
+            new GeometryIndices({
+                primitiveType : PrimitiveType.TRIANGLES,
+                values : indices
+            })
+        ];
 
         /**
          * A tight-fitting bounding sphere that encloses the vertices of the geometry.
          *
          * @type BoundingSphere
          */
-        this.boundingSphere = new BoundingSphere.fromVertices(attributes.position.values);
+        this.boundingSphere = new BoundingSphere.fromVertices(positions);
 
         /**
          * The 4x4 transformation matrix that transforms the geometry from model to world coordinates.
