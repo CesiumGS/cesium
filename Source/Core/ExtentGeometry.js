47742583412383129d5fdf6313ed2cffc756b5d3
hpinkos <pinkos.hannah@gmail.com>
2013-06-04 11:07:21 -0400

    initial extruded extent

--- Source/Core/ExtentGeometry.js
+++ Source/Core/ExtentGeometry.js
@@ -57,6 +57,11 @@ define([
     var normal = new Cartesian3();
     var tangent = new Cartesian3();
     var binormal = new Cartesian3();
+    var extrudedPosition = new Cartesian3();
+    var extrudedNormal = new Cartesian3();
+    var extrudedTangent = new Cartesian3();
+    var extrudedBinormal = new Cartesian3();
+
 
     /**
      * Creates geometry for a cartographic extent on an ellipsoid centered at the origin.
@@ -68,6 +73,11 @@ define([
      * @param {Number} [options.rotation=0.0] The rotation of the extent in radians. A positive rotation is counter-clockwise.
      * @param {Matrix4} [options.modelMatrix] The model matrix for this geometry.
      * @param {Color} [options.color] The color of the geometry when a per-geometry color appearance is used.
+     * @param {Object} [options.extrudedOptions] Extruded options
+     * @param {Number} [options.extrudedOptions.minHeight] Lower height of extruded extent
+     * @param {Number} [options.extrudedOptions.maxHeight] Upper height of extruded extent
+     * @param {Boolean} [options.extrudedOptions.closeTop=true] Render top of extrusion
+     * @param {Number} [options.extrudedOptions.closeBottom=true] Render bottom of extrusion
      * @param {DOC_TBA} [options.pickData] DOC_TBA
      *
      * @exception {DeveloperError} <code>options.extent</code> is required and must have north, south, east and west attributes.
@@ -96,12 +106,117 @@ define([
      */
     var ExtentGeometry = function(options) {
         options = defaultValue(options, defaultValue.EMPTY_OBJECT);
+        var vertexFormat = defaultValue(options.vertexFormat, VertexFormat.DEFAULT);
+        var attr;
 
+        if (typeof options.extrudedOptions !== 'undefined') {
+            attr = constructExtrudedExtent(options, vertexFormat);
+        } else {
+            attr = constructExtent(options, vertexFormat, false);
+        }
+
+
+
+
+        var attributes = {};
+
+        if (vertexFormat.position) {
+            attributes.position = new GeometryAttribute({
+                componentDatatype : ComponentDatatype.FLOAT,
+                componentsPerAttribute : 3,
+                values : attr.positions
+            });
+        }
+
+        if (vertexFormat.st) {
+            attributes.st = new GeometryAttribute({
+                componentDatatype : ComponentDatatype.FLOAT,
+                componentsPerAttribute : 2,
+                values : attr.textureCoordinates
+            });
+        }
+
+        if (vertexFormat.normal) {
+            attributes.normal = new GeometryAttribute({
+                componentDatatype : ComponentDatatype.FLOAT,
+                componentsPerAttribute : 3,
+                values : attr.normals
+            });
+        }
+
+        if (vertexFormat.tangent) {
+            attributes.tangent = new GeometryAttribute({
+                componentDatatype : ComponentDatatype.FLOAT,
+                componentsPerAttribute : 3,
+                values : attr.tangents
+            });
+        }
+
+        if (vertexFormat.binormal) {
+            attributes.binormal = new GeometryAttribute({
+                componentDatatype : ComponentDatatype.FLOAT,
+                componentsPerAttribute : 3,
+                values : attr.binormals
+            });
+        }
+
+        /**
+         * An object containing {@link GeometryAttribute} properties named after each of the
+         * <code>true</code> values of the {@link VertexFormat} option.
+         *
+         * @type Object
+         */
+        this.attributes = attributes;
+
+        /**
+         * An array of {@link GeometryIndices} defining primitives.
+         *
+         * @type Array
+         */
+        this.indexLists = [
+            new GeometryIndices({
+                primitiveType : PrimitiveType.TRIANGLES,
+                values : attr.indices
+            })
+        ];
+
+        /**
+         * A tight-fitting bounding sphere that encloses the vertices of the geometry.
+         *
+         * @type BoundingSphere
+         */
+      //  this.boundingSphere = BoundingSphere.fromExtent3D(extent, ellipsoid, surfaceHeight);
+
+        /**
+         * The 4x4 transformation matrix that transforms the geometry from model to world coordinates.
+         * When this is the identity matrix, the geometry is drawn in world coordinates, i.e., Earth's WGS84 coordinates.
+         * Local reference frames can be used by providing a different transformation matrix, like that returned
+         * by {@link Transforms.eastNorthUpToFixedFrame}.
+         *
+         * @type Matrix4
+         *
+         * @see Transforms.eastNorthUpToFixedFrame
+         */
+        this.modelMatrix = defaultValue(options.modelMatrix, Matrix4.IDENTITY.clone());
+
+        /**
+         * The color of the geometry when a per-geometry color appearance is used.
+         *
+         * @type Color
+         */
+        this.color = options.color;
+
+        /**
+         * DOC_TBA
+         */
+        this.pickData = options.pickData;
+    };
+
+    function constructExtent(options, vertexFormat, extrude){
         var extent = options.extent;
         if (typeof extent === 'undefined') {
             throw new DeveloperError('extent is required.');
         }
-
         extent.validate();
 
         var granularity = defaultValue(options.granularity, 0.1);
@@ -117,6 +232,7 @@ define([
         var radiiSquaredZ = radiiSquared.z;
 
         var surfaceHeight = defaultValue(options.surfaceHeight, 0.0);
+        var extrudedMinHeight = extrude ? options.extrudedOptions.minHeight : 0;
         var rotation = defaultValue(options.rotation, 0.0);
 
         var cos = Math.cos;
@@ -155,9 +271,6 @@ define([
             }
         }
 
-        var vertexFormat = defaultValue(options.vertexFormat, VertexFormat.DEFAULT);
-        var attributes = {};
-
         var positionIndex = 0;
         var stIndex = 0;
         var normalIndex = 0;
@@ -165,11 +278,13 @@ define([
         var binormalIndex = 0;
 
         var size = width * height;
-        var positions = (vertexFormat.position) ? new Array(size * 3) : undefined;
-        var textureCoordinates = (vertexFormat.st) ? new Array(size * 2) : undefined;
-        var normals = (vertexFormat.normal) ? new Array(size * 3) : undefined;
-        var tangents = (vertexFormat.tangent) ? new Array(size * 3) : undefined;
-        var binormals = (vertexFormat.binormal) ? new Array(size * 3) : undefined;
+        var threeSize = size * 3;
+        var twoSize = size * 2;
+        var positions = (vertexFormat.position) ? new Array(threeSize * ((extrude)? 2 : 1)) : undefined;
+        var textureCoordinates = (vertexFormat.st) ? new Array(twoSize * ((extrude)? 2 : 1)) : undefined;
+        var normals = (vertexFormat.normal) ? new Array(threeSize * ((extrude)? 2 : 1)) : undefined;
+        var tangents = (vertexFormat.tangent) ? new Array(threeSize * ((extrude)? 2 : 1)) : undefined;
+        var binormals = (vertexFormat.binormal) ? new Array(threeSize * ((extrude)? 2 : 1)) : undefined;
 
         for ( var row = 0; row < height; ++row) {
             for ( var col = 0; col < width; ++col) {
@@ -196,21 +311,46 @@ define([
                 position.y = rSurfaceY + nY * surfaceHeight;
                 position.z = rSurfaceZ + nZ * surfaceHeight;
 
+                if (extrude) {
+                    extrudedPosition.x = rSurfaceX + nX * extrudedMinHeight;
+                    extrudedPosition.y = rSurfaceY + nY * extrudedMinHeight;
+                    extrudedPosition.z = rSurfaceZ + nZ * extrudedMinHeight;
+                }
+
                 if (vertexFormat.position) {
+                    if (extrude) {
+                        positions[positionIndex + threeSize ] = extrudedPosition.x;
+                        positions[positionIndex + 1 + threeSize] = extrudedPosition.y;
+                        positions[positionIndex + 2 + threeSize] = extrudedPosition.z;
+                    }
+
                     positions[positionIndex++] = position.x;
                     positions[positionIndex++] = position.y;
                     positions[positionIndex++] = position.z;
                 }
 
                 if (vertexFormat.st) {
+                    if (extrude) {
+                        textureCoordinates[stIndex + twoSize] = (longitude - extent.west) * lonScalar;
+                        textureCoordinates[stIndex + 1 + twoSize] = (latitude - extent.south) * latScalar;
+                    }
+
                     textureCoordinates[stIndex++] = (longitude - extent.west) * lonScalar;
                     textureCoordinates[stIndex++] = (latitude - extent.south) * latScalar;
                 }
 
                 if (vertexFormat.normal || vertexFormat.tangent || vertexFormat.binormal) {
                     ellipsoid.geodeticSurfaceNormal(position, normal);
+                    if (extrude) {
+                        Cartesian3.negate(normal, extrudedNormal);
+                    }
 
                     if (vertexFormat.normal) {
+                        if (extrude) {
+                            normals[normalIndex + threeSize] = extrudedNormal.x;
+                            normals[normalIndex + 1 + threeSize] = extrudedNormal.y;
+                            normals[normalIndex + 2 + threeSize] = extrudedNormal.z;
+                        }
                         normals[normalIndex++] = normal.x;
                         normals[normalIndex++] = normal.y;
                         normals[normalIndex++] = normal.z;
@@ -218,6 +358,12 @@ define([
 
                     if (vertexFormat.tangent) {
                         Cartesian3.cross(Cartesian3.UNIT_Z, normal, tangent);
+                        if (extrude) {
+                            Cartesian3.cross(Cartesian3.UNIT_Z, extrudedNormal, extrudedTangent);
+                            tangents[tangentIndex + threeSize] = extrudedTangent.x;
+                            tangents[tangentIndex + 1 + threeSize] = extrudedTangent.y;
+                            tangents[tangentIndex + 2 + threeSize] = extrudedTangent.z;
+                        }
 
                         tangents[tangentIndex++] = tangent.x;
                         tangents[tangentIndex++] = tangent.y;
@@ -227,6 +373,13 @@ define([
                     if (vertexFormat.binormal) {
                         Cartesian3.cross(Cartesian3.UNIT_Z, normal, tangent);
                         Cartesian3.cross(normal, tangent, binormal);
+                        if (extrude) {
+                            Cartesian3.cross(Cartesian3.UNIT_Z, extrudedNormal, extrudedTangent);
+                            Cartesian3.cross(extrudedNormal, extrudedTangent, extrudedBinormal);
+                            binormals[binormalIndex + threeSize] = extrudedBinormal.x;
+                            binormals[binormalIndex + 1 + threeSize] = extrudedBinormal.y;
+                            binormals[binormalIndex + 2 + threeSize] = extrudedBinormal.z;
+                        }
 
                         binormals[binormalIndex++] = binormal.x;
                         binormals[binormalIndex++] = binormal.y;
@@ -245,6 +398,14 @@ define([
                 var lowerLeft = upperLeft + width;
                 var lowerRight = lowerLeft + 1;
                 var upperRight = upperLeft + 1;
+                if (extrude) {
+                    indices[indicesIndex++] = upperRight + size;
+                    indices[indicesIndex++] = lowerLeft + size;
+                    indices[indicesIndex++] = upperLeft + size;
+                    indices[indicesIndex++] = lowerRight + size;
+                    indices[indicesIndex++] = lowerLeft + size;
+                    indices[indicesIndex++] = upperRight + size;
+                }
 
                 indices[indicesIndex++] = upperLeft;
                 indices[indicesIndex++] = lowerLeft;
@@ -257,98 +418,95 @@ define([
             }
             ++index;
         }
+        var attributes = {
+                indices: indices,
+                binormals: binormals,
+                tangents: tangents,
+                normals: normals,
+                textureCoordinates: textureCoordinates,
+                positions: positions
+        };
 
-        if (vertexFormat.position) {
-            attributes.position = new GeometryAttribute({
-                componentDatatype : ComponentDatatype.FLOAT,
-                componentsPerAttribute : 3,
-                values : positions
-            });
+        return attributes;
+    }
+
+    function constructExtrudedExtent(options, vertexFormat) {
+        var extrudedOptions = options.extrudedOptions;
+        var minHeight = extrudedOptions.minHeight;
+        var maxHeight = extrudedOptions.maxHeight;
+        if (minHeight > maxHeight) {
+            throw new DeveloperError('options.extrudedOptions.maxHeight cannot be less than options.extrudedOptions.minHeight');
         }
 
-        if (vertexFormat.st) {
-            attributes.st = new GeometryAttribute({
-                componentDatatype : ComponentDatatype.FLOAT,
-                componentsPerAttribute : 2,
-                values : textureCoordinates
-            });
+        options.surfaceHeight = maxHeight;
+        var attr = constructExtent(options, vertexFormat, true);
+        var extent = options.extent;
+        var granularity = defaultValue(options.granularity, 0.1);
+        var width = Math.ceil((extent.east - extent.west) / granularity) + 1;
+        var height = Math.ceil((extent.north - extent.south) / granularity) + 1;
+        var size = width * height;
+        var indices = attr.indices;
+        var upperLeft;
+        var lowerLeft;
+        var lowerRight;
+        var upperRight;
+        var indicesIndex = indices.length;
+        var i = 0;
+
+        for (i = width - 1; i > 0; i--) { // north
+            upperLeft = i;
+            lowerLeft = upperLeft + size;
+            lowerRight = lowerLeft - 1;
+            upperRight = upperLeft - 1;
+            indices[indicesIndex++] = upperLeft;
+            indices[indicesIndex++] = lowerLeft;
+            indices[indicesIndex++] = upperRight;
+            indices[indicesIndex++] = upperRight;
+            indices[indicesIndex++] = lowerLeft;
+            indices[indicesIndex++] = lowerRight;
         }
 
-        if (vertexFormat.normal) {
-            attributes.normal = new GeometryAttribute({
-                componentDatatype : ComponentDatatype.FLOAT,
-                componentsPerAttribute : 3,
-                values : normals
-            });
+        for (i = 0; i < size - width; i += width) { // west
+            upperLeft = i;
+            lowerLeft = upperLeft + size;
+            lowerRight = lowerLeft + width;
+            upperRight = upperLeft + width;
+            indices[indicesIndex++] = upperLeft;
+            indices[indicesIndex++] = lowerLeft;
+            indices[indicesIndex++] = upperRight;
+            indices[indicesIndex++] = upperRight;
+            indices[indicesIndex++] = lowerLeft;
+            indices[indicesIndex++] = lowerRight;
         }
 
-        if (vertexFormat.tangent) {
-            attributes.tangent = new GeometryAttribute({
-                componentDatatype : ComponentDatatype.FLOAT,
-                componentsPerAttribute : 3,
-                values : tangents
-            });
+        for (i = size - 1; i > width - 1; i -= width) { // east
+            upperLeft = i;
+            lowerLeft = upperLeft + size;
+            lowerRight = lowerLeft - width;
+            upperRight = upperLeft - width;
+            indices[indicesIndex++] = upperLeft;
+            indices[indicesIndex++] = lowerLeft;
+            indices[indicesIndex++] = upperRight;
+            indices[indicesIndex++] = upperRight;
+            indices[indicesIndex++] = lowerLeft;
+            indices[indicesIndex++] = lowerRight;
         }
 
-        if (vertexFormat.binormal) {
-            attributes.binormal = new GeometryAttribute({
-                componentDatatype : ComponentDatatype.FLOAT,
-                componentsPerAttribute : 3,
-                values : binormals
-            });
+        for (i = size - width; i < size-1; i++) { // south
+            upperLeft = i;
+            lowerLeft = upperLeft + size;
+            lowerRight = lowerLeft + 1;
+            upperRight = upperLeft + 1;
+            indices[indicesIndex++] = upperLeft;
+            indices[indicesIndex++] = lowerLeft;
+            indices[indicesIndex++] = upperRight;
+            indices[indicesIndex++] = upperRight;
+            indices[indicesIndex++] = lowerLeft;
+            indices[indicesIndex++] = lowerRight;
         }
 
-        /**
-         * An object containing {@link GeometryAttribute} properties named after each of the
-         * <code>true</code> values of the {@link VertexFormat} option.
-         *
-         * @type Object
-         */
-        this.attributes = attributes;
-
-        /**
-         * An array of {@link GeometryIndices} defining primitives.
-         *
-         * @type Array
-         */
-        this.indexLists = [
-            new GeometryIndices({
-                primitiveType : PrimitiveType.TRIANGLES,
-                values : indices
-            })
-        ];
-
-        /**
-         * A tight-fitting bounding sphere that encloses the vertices of the geometry.
-         *
-         * @type BoundingSphere
-         */
-        this.boundingSphere = BoundingSphere.fromExtent3D(extent, ellipsoid, surfaceHeight);
-
-        /**
-         * The 4x4 transformation matrix that transforms the geometry from model to world coordinates.
-         * When this is the identity matrix, the geometry is drawn in world coordinates, i.e., Earth's WGS84 coordinates.
-         * Local reference frames can be used by providing a different transformation matrix, like that returned
-         * by {@link Transforms.eastNorthUpToFixedFrame}.
-         *
-         * @type Matrix4
-         *
-         * @see Transforms.eastNorthUpToFixedFrame
-         */
-        this.modelMatrix = defaultValue(options.modelMatrix, Matrix4.IDENTITY.clone());
-
-        /**
-         * The color of the geometry when a per-geometry color appearance is used.
-         *
-         * @type Color
-         */
-        this.color = options.color;
-
-        /**
-         * DOC_TBA
-         */
-        this.pickData = options.pickData;
-    };
+        return attr;
+    }
 
     return ExtentGeometry;
 });
