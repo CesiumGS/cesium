a26ed293db854191e9a0a8397d791327e0065216
Scott Hunter <shunter@agi.com>
2013-06-18 15:21:16 -0400

    Minor cleanup.

--- Specs/createCanvas.js
+++ Specs/createCanvas.js
@@ -12,12 +12,12 @@ define([
         height = defaultValue(height, 1);
 
         var canvas = document.createElement('canvas');
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
-        canvas.innerHTML = 'To view this web page, upgrade your browser; it does not support the HTML5 canvas element.';
+        canvas.width = width;
+        canvas.height = height;
+
         document.body.appendChild(canvas);
 
-        if ((canvas.width !== canvas.clientWidth) || (canvas.height !== canvas.clientHeight)) {
+        if (canvas.width !== canvas.clientWidth || canvas.height !== canvas.clientHeight) {
             throw new RuntimeError('Canvas width and height do not match client width and height.  Perhaps the browser is not at 100% zoom?');
         }
 
