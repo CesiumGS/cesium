1b829cc6367fd45f570e76ba0930502302934655
Dan Bagnell <daniel.p.bagnell@gmail.com>
2013-06-19 21:22:37 -0400

    Update appearance shaders.

--- Source/Shaders/Appearances/PerInstanceFlatColorAppearanceVS.glsl
+++ Source/Shaders/Appearances/PerInstanceFlatColorAppearanceVS.glsl
@@ -1,12 +1,29 @@
 attribute vec3 position3DHigh;
 attribute vec3 position3DLow;
+attribute vec3 position2DHigh;
+attribute vec3 position2DLow;
 attribute vec4 color;
 
 varying vec4 v_color;
 
 void main() 
 {
-    vec4 p = czm_translateRelativeToEye(position3DHigh, position3DLow);   
+    vec4 p;
+    if (czm_morphTime == 1.0)
+    {
+        p = czm_translateRelativeToEye(position3DHigh, position3DLow);
+    }
+    else if (czm_morphTime == 0.0)
+    {
+        p = czm_translateRelativeToEye(position2DHigh.zxy, position2DLow.zxy);
+    }
+    else
+    {
+        p = czm_columbusViewMorph(
+            czm_translateRelativeToEye(position2DHigh.zxy, position2DLow.zxy),
+            czm_translateRelativeToEye(position3DHigh, position3DLow), 
+            czm_morphTime);
+    }
 
     v_color = color;
     
--- Source/Shaders/Appearances/TexturedMaterialAppearanceVS.glsl
+++ Source/Shaders/Appearances/TexturedMaterialAppearanceVS.glsl
@@ -1,5 +1,7 @@
 attribute vec3 position3DHigh;
 attribute vec3 position3DLow;
+attribute vec3 position2DHigh;
+attribute vec3 position2DLow;
 attribute vec3 normal;
 attribute vec2 st;
 
@@ -9,7 +11,22 @@ varying vec2 v_st;
 
 void main() 
 {
-    vec4 p = czm_translateRelativeToEye(position3DHigh, position3DLow);   
+    vec4 p;
+    if (czm_morphTime == 1.0)
+    {
+        p = czm_translateRelativeToEye(position3DHigh, position3DLow);
+    }
+    else if (czm_morphTime == 0.0)
+    {
+        p = czm_translateRelativeToEye(position2DHigh.zxy, position2DLow.zxy);
+    }
+    else
+    {
+        p = czm_columbusViewMorph(
+            czm_translateRelativeToEye(position2DHigh.zxy, position2DLow.zxy),
+            czm_translateRelativeToEye(position3DHigh, position3DLow), 
+            czm_morphTime);
+    } 
 
     v_positionEC = (czm_modelViewRelativeToEye * p).xyz;      // position in eye coordinates
     v_normalEC = czm_normal * normal;                         // normal in eye coordinates
