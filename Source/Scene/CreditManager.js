837f9ac9f9254e1f6a2a640b65e7a9aa075d3b10
hpinkos <pinkos.hannah@gmail.com>
2013-06-27 17:20:09 -0400

    separate images and text credits

--- Source/Scene/CreditManager.js
+++ Source/Scene/CreditManager.js
@@ -9,134 +9,172 @@ define([
      * DOC_TBA
      */
 
-    var interpunct;
     var delimeter = ' • ';
-    function createFirstInterpunct() {
-        interpunct = document.createElement('span');
-        interpunct.id = 'interpunct';
-        interpunct.appendChild(document.createTextNode(delimeter));
-        interpunct.style.padding = "3px";
-    }
 
     var CreditManager = function(container) {
-        this.visibleCredits = {};
-        this.countDefaultCredits = 0;
-        this.hasVisibleCredits = false;
+        var imageContainer = document.createElement('span');
+        var textContainer = document.createElement('span');
+        textContainer.style["padding-right"] = "5px";
+        container.appendChild(imageContainer);
+        container.appendChild(textContainer);
+
+        this.visibleImageCredits = {};
         this.container = container;
-        createFirstInterpunct();
+        this.textContainer = textContainer;
+        this.imageContainer = imageContainer;
+        this.defaultTextCredits = [];
+        this.visibleTextCredits = [];
+        this.previousCredits = [];
     };
 
     CreditManager.prototype.addDefaultCredit = function(credit) {
         if (typeof credit !== 'undefined') {
-            if (this.countDefaultCredits === 0) {
-                this.container.appendChild(interpunct);
+            if (typeof credit.image === 'undefined') {
+                this.defaultTextCredits.push(credit);
+                addTextCredits(this);
+            } else {
+                addImageCredit(credit, this.imageContainer);
             }
-            this.countDefaultCredits++;
-            showCredit(credit, this.container);
         }
     };
 
     CreditManager.prototype.removeDefaultCredit = function(credit) {
         if (typeof credit !== 'undefined') {
-            this.countDefaultCredits--;
-            if (this.countDefaultCredits === 0) {
-                this.container.removeChild(interpunct);
+            if (typeof credit.image === 'undefined') {
+                var index = this.defaultTextCredits.indexOf(credit);
+                if (index !== -1) {
+                    this.defaultTextCredits.splice(index, 1);
+                    addTextCredits(this);
+                }
+            } else {
+                removeImageCredit(credit, this.imageContainer);
             }
-            hideCredit(credit, this.container);
         }
     };
 
     CreditManager.prototype.showCredits = function(credits) {
-        var newVisible = [];
+        var previousCredits = this.previousCredits;
+        if (previousCredits.length !== credits.length) {
+            processCredits(this, credits);
+            this.previousCredits = credits;
+        } else {
+            for (var i = 0; i < credits.length; i++) {
+                if (credits[i] !== this.previousCredits[i]) {
+                    processCredits(this, credits);
+                    this.previousCredits = credits;
+                    break;
+                }
+            }
+        }
+    };
+
+    function processCredits(creditManager, credits) {
+        var newImages = [];
+        var newText = [];
         var stillVisible = {};
         var i;
-        var undefinedCredits = 0;
 
         for (i = 0; i < credits.length; i++) {
             var credit = credits[i];
             if (typeof credit !== 'undefined') {
-                if (this.visibleCredits.hasOwnProperty(credit.name)) {
+                if (typeof credit.image === 'undefined'){
+                    newText.push(credit);
+                } else if (!creditManager.visibleImageCredits.hasOwnProperty(credit.name)) {
+                    newImages.push(credit);
                     stillVisible[credit.name] = credit;
                 } else {
-                    newVisible.push(credit);
-                }
-            } else {
-                undefinedCredits++;
-            }
-        }
-
-        if (this.countDefaultCredits === 0) {
-            if (credits.length === undefinedCredits) {
-                if (this.hasVisibleCredits) {
-                    this.container.removeChild(interpunct);
-                    this.hasVisibleCredits = false;
-                }
-            }
-
-            if (newVisible.length > 0) {
-                if (!this.hasVisibleCredits) {
-                    this.container.appendChild(interpunct);
-                    this.hasVisibleCredits = true;
+                    stillVisible[credit.name] = credit;
                 }
             }
         }
 
-        for (i = 0; i < newVisible.length; i++) {
-            showCredit(newVisible[i], this.container);
-            stillVisible[newVisible[i].name] = newVisible[i];
+        for (i = 0; i < newImages.length; i++) {
+            var c = newImages[i];
+            addImageCredit(c, creditManager.imageContainer);
         }
+
+        creditManager.visibleTextCredits = newText;
+        addTextCredits(creditManager);
+
 
         for (var a in stillVisible) {
             if (stillVisible.hasOwnProperty(a)) {
-                delete this.visibleCredits[a];
+                delete creditManager.visibleImageCredits[a];
             }
         }
 
-        for (var b in this.visibleCredits) {
-            if (this.visibleCredits.hasOwnProperty(b)) {
-                hideCredit(this.visibleCredits[b], this.container);
+        for (var b in creditManager.visibleImageCredits) {
+            if (creditManager.visibleImageCredits.hasOwnProperty(b)) {
+                removeImageCredit(creditManager.visibleImageCredits[b], creditManager.imageContainer);
             }
         }
-        this.visibleCredits = stillVisible;
-    };
 
-    function showCredit(credit, container) {
-        if (typeof credit !== 'undefined') {
-            var name = credit.name;
-            if (typeof credit !== 'undefined') {
-                var span = document.createElement('span');
-                span.id = name;
-                var content;
-                if (typeof credit.image !== 'undefined') {
-                    content = document.createElement('img');
-                    content.className = "credit-image";
-                    content.src = credit.image;
-                    content.style["vertical-align"] = "bottom";
-                    if (typeof credit.text !== 'undefined') {
-                        content.alt = credit.text;
-                        content.title = credit.text;
-                    }
-                } else if (typeof credit.text !== 'undefined') {
-                    content = document.createTextNode(credit.text);
-                } else {
-                    content = document.createTextNode(credit.link);
-                }
-                if (typeof credit.link !== 'undefined') {
-                    var link = document.createElement('a');
-                    link.appendChild(content);
-                    link.href = credit.link;
-                    link.target = "_blank";
-                    span.appendChild(link);
-                } else {
-                    span.appendChild(content);
-                }
-                span.appendChild(document.createTextNode(delimeter));
-                container.appendChild(span);
+        creditManager.visibleImageCredits = stillVisible;
+    }
+
+    function addTextCredits(creditManager) {
+        var container = creditManager.textContainer;
+        var replacementContainer = document.createElement('span');
+        var textCredits = creditManager.defaultTextCredits.concat(creditManager.visibleTextCredits);
+        for (var i = 0; i < textCredits.length; i++) {
+            var credit = textCredits[i];
+            var str;
+            if (i !== 0) {
+                str = delimeter;
+            } else {
+                str = '';
             }
+            if (typeof credit.text !== 'undefined') {
+                str += credit.text;
+            } else {
+                str += credit.link;
+            }
+            var text = document.createTextNode(str);
+            var span = document.createElement('span');
+            if (typeof credit.link !== 'undefined') {
+                var link = document.createElement('a');
+                link.appendChild(text);
+                link.href = credit.link;
+                link.target = "_blank";
+
+                span.appendChild(link);
+            } else {
+                span.appendChild(text);
+            }
+            replacementContainer.appendChild(span);
+        }
+        if (container.innerHTML !== replacementContainer.innerHTML) {
+            container.innerHTML = replacementContainer.innerHTML;
         }
     }
 
-    function hideCredit(credit, container) {
+    function addImageCredit(credit, container) {
+        var name = credit.name;
+        var span = document.createElement('span');
+        span.id = name;
+        var content = document.createElement('img');
+        content.className = "credit-image";
+        content.src = credit.image;
+        content.style["vertical-align"] = "bottom";
+        if (typeof credit.text !== 'undefined') {
+            content.alt = credit.text;
+            content.title = credit.text;
+        }
+
+        if (typeof credit.link !== 'undefined') {
+            var link = document.createElement('a');
+            link.appendChild(content);
+            link.href = credit.link;
+            link.target = "_blank";
+            span.appendChild(link);
+        } else {
+            span.appendChild(content);
+        }
+
+        container.appendChild(span);
+    }
+
+    function removeImageCredit(credit, container) {
         if (typeof credit !== 'undefined') {
             var name = credit.name;
             if (typeof credit !== 'undefined') {
