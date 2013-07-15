0b5da60fc87312068c81829ac1a931a90a21b936
hpinkos <pinkos.hannah@gmail.com>
2013-07-15 14:19:29 -0400

    clone options

--- Source/Core/SphereGeometry.js
+++ Source/Core/SphereGeometry.js
@@ -10,12 +10,14 @@ define([
     "use strict";
 
     /**
+     * A {@link Geometry} that represents vertices and indices for an ellipse on the ellipsoid.
+     *
      * Creates vertices and indices for an sphere centered at the origin.
      *
      * @alias SphereGeometry
      * @constructor
      *
-     * @param {radius} [options.radius=1] The radius of the sphere.
+     * @param {Number} [options.radius=1.0] The radius of the sphere.
      * @param {Number} [options.numberOfPartitions=32] The number of times to partition the sphere in a plane formed by two radii in a single quadrant.
      * @param {VertexFormat} [options.vertexFormat=VertexFormat.DEFAULT] The vertex attributes to be computed.
      *
@@ -23,15 +25,21 @@ define([
      *
      * @example
      * var sphere = new SphereGeometry({
-     *   radius : 100,
+     *   radius : 100.0,
      *   vertexFormat : VertexFormat.POSITION_ONLY
      * });
      */
     var SphereGeometry = function(options) {
-        var radius = defaultValue(options.radius, 1);
+        var radius = defaultValue(options.radius, 1.0);
         var radii = new Cartesian3(radius, radius, radius);
         options.radii = radii;
-        var ellipsoidGeometry = new EllipsoidGeometry(options);
+        var ellipsoidOptions = {
+                radii: radii,
+                numberOfPartitions: options.numberOfPartitions,
+                vertexFormat: options.vertexFormat
+        };
+
+        var ellipsoidGeometry = new EllipsoidGeometry(ellipsoidOptions);
 
         /**
          * An object containing {@link GeometryAttribute} properties named after each of the
