0969c55ed35bb7cdb53c2006950638be43d961f9
Ed Mackey <elm19087@gmail.com>
2013-08-27 11:32:58 -0400

    Added get/setGlowFactor to address a feature request in #769.

--- Source/Shaders/SunFS.glsl
+++ Source/Shaders/SunFS.glsl
@@ -1,7 +1,7 @@
 varying vec2 v_textureCoordinates;
 varying float v_radiusTS;           // The radius in texture space
 
-const float glowLengthTS = 0.3;
+uniform float u_glowLengthTS;
 
 vec2 rotate(vec2 p, vec2 direction)
 {
@@ -12,7 +12,7 @@ vec4 addBurst(vec2 position, vec2 direction)
 {
     vec2 rotatedPosition = rotate(position, direction) * vec2(25.0, 0.75);
     float radius = length(rotatedPosition);
-    float burst = 1.0 - smoothstep(0.0, glowLengthTS + v_radiusTS, radius);
+    float burst = 1.0 - smoothstep(0.0, u_glowLengthTS + v_radiusTS, radius);
 
     return vec4(burst);
 }
@@ -24,7 +24,7 @@ void main()
     float surface = step(radius, v_radiusTS);
     vec4 color = vec4(1.0, 1.0, surface + 0.2, surface);
 
-    float glow = 1.0 - smoothstep(0.0, glowLengthTS + v_radiusTS, radius);
+    float glow = 1.0 - smoothstep(0.0, u_glowLengthTS + v_radiusTS, radius);
     color.ba += mix(vec2(0.0), vec2(1.0), glow) * 0.75;
 
     // The following loop has been manually unrolled for speed, to
