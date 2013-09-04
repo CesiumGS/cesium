23ba244fcf65aa26783dc074fa0c0e86933744f9
Greg Beatty <gbeatty@agi.com>
2013-09-04 11:46:20 -0400

    test cleanup

--- Specs/Renderer/ShaderProgramSpec.js
+++ Specs/Renderer/ShaderProgramSpec.js
@@ -34,38 +34,40 @@ defineSuite([
     var sp;
     var va;
 
+    var injectedTestFunctions = {
+            czm_circularDependency1 : 'void czm_circularDependency1() { czm_circularDependency2(); }',
+            czm_circularDependency2 : 'void czm_circularDependency2() { czm_circularDependency1(); }',
+            czm_testFunction3 : 'void czm_testFunction3(vec4 color) { czm_testFunction2(color); }',
+            czm_testFunction2 : 'void czm_testFunction2(vec4 color) { czm_testFunction1(color); }',
+            czm_testFunction1 : 'void czm_testFunction1(vec4 color) { gl_FragColor = color; }',
+            czm_testDiamondDependency1 : 'vec4 czm_testDiamondDependency1(vec4 color) { return czm_testAddAlpha(color); }',
+            czm_testDiamondDependency2 : 'vec4 czm_testDiamondDependency2(vec4 color) { return czm_testAddAlpha(color); }',
+            czm_testAddAlpha : 'vec4 czm_testAddAlpha(vec4 color) { color.a = clamp(color.a + 0.1, 0.0, 1.0); return color; }',
+            czm_testAddRed : 'vec4 czm_testAddRed(vec4 color) { color.r = clamp(color.r + 0.1, 0.0, 1.0); return color; }',
+            czm_testAddGreen : 'vec4 czm_testAddGreen(vec4 color) { color.g = clamp(color.g + 0.1, 0.0, 1.0); return color; }',
+            czm_testAddRedGreenAlpha : 'vec4 czm_testAddRedGreenAlpha(vec4 color) { color = czm_testAddRed(color); color = czm_testAddGreen(color); color = czm_testAddAlpha(color); return color; }',
+            czm_testFunction4 : 'void czm_testFunction4(vec4 color) { color = czm_testAddAlpha(color); color = czm_testAddRedGreenAlpha(color); czm_testFunction3(color); }',
+            czm_testFunctionWithComment : '/**\n czm_circularDependency1()  \n*/\nvoid czm_testFunctionWithComment(vec4 color) { czm_testFunction1(color); }'
+    };
+
     beforeAll(function() {
         context = createContext();
 
-        CzmBuiltins.czm_circularDependency1 = 'void czm_circularDependency1() { czm_circularDependency2(); }';
-        CzmBuiltins.czm_circularDependency2 = 'void czm_circularDependency2() { czm_circularDependency1(); }';
-        CzmBuiltins.czm_testFunction3 = 'void czm_testFunction3(vec4 color) { czm_testFunction2(color); }';
-        CzmBuiltins.czm_testFunction2 = 'void czm_testFunction2(vec4 color) { czm_testFunction1(color); }';
-        CzmBuiltins.czm_testFunction1 = 'void czm_testFunction1(vec4 color) { gl_FragColor = color; }';
-        CzmBuiltins.czm_testDiamondDependency1 = 'vec4 czm_testDiamondDependency1(vec4 color) { return czm_testAddAlpha(color); }';
-        CzmBuiltins.czm_testDiamondDependency2 = 'vec4 czm_testDiamondDependency2(vec4 color) { return czm_testAddAlpha(color); }';
-        CzmBuiltins.czm_testAddAlpha = 'vec4 czm_testAddAlpha(vec4 color) { color.a = clamp(color.a + 0.1, 0.0, 1.0); return color; }';
-        CzmBuiltins.czm_testAddRed = 'vec4 czm_testAddRed(vec4 color) { color.r = clamp(color.r + 0.1, 0.0, 1.0); return color; }';
-        CzmBuiltins.czm_testAddGreen = 'vec4 czm_testAddGreen(vec4 color) { color.g = clamp(color.g + 0.1, 0.0, 1.0); return color; }';
-        CzmBuiltins.czm_testAddRedGreenAlpha = 'vec4 czm_testAddRedGreenAlpha(vec4 color) { color = czm_testAddRed(color); color = czm_testAddGreen(color); color = czm_testAddAlpha(color); return color; }';
-        CzmBuiltins.czm_testFunction4 = 'void czm_testFunction4(vec4 color) { color = czm_testAddAlpha(color); color = czm_testAddRedGreenAlpha(color); czm_testFunction3(color); }';
+        for(var functionName in injectedTestFunctions) {
+            if(injectedTestFunctions.hasOwnProperty(functionName)) {
+                CzmBuiltins[functionName] = injectedTestFunctions[functionName];
+            }
+        }
     });
 
     afterAll(function() {
         destroyContext(context);
 
-        delete CzmBuiltins.czm_circularDependency1;
-        delete CzmBuiltins.czm_circularDependency2;
-        delete CzmBuiltins.czm_testFunction3;
-        delete CzmBuiltins.czm_testFunction2;
-        delete CzmBuiltins.czm_testFunction1;
-        delete CzmBuiltins.czm_testDiamondDependency1;
-        delete CzmBuiltins.czm_testDiamondDependency2;
-        delete CzmBuiltins.czm_testAddAlpha;
-        delete CzmBuiltins.czm_testAddRed;
-        delete CzmBuiltins.czm_testAddGreen;
-        delete CzmBuiltins.czm_testAddRedGreenAlpha;
-        delete CzmBuiltins.czm_testFunction4;
+        for(var functionName in injectedTestFunctions) {
+            if(injectedTestFunctions.hasOwnProperty(functionName)) {
+                delete CzmBuiltins[functionName];
+            }
+        }
     });
 
     it('has vertex and fragment shader source', function() {
@@ -651,6 +653,32 @@ defineSuite([
             '  color = czm_testDiamondDependency1(color); \n' +
             '  color = czm_testDiamondDependency2(color); \n' +
             '  czm_testFunction4(color); \n' +
+            '}';
+        sp = context.createShaderProgram(vs, fs);
+
+        va = context.createVertexArray([{
+            index : sp.getVertexAttributes().position.index,
+            vertexBuffer : context.createVertexBuffer(new Float32Array([0, 0, 0, 1]), BufferUsage.STATIC_DRAW),
+            componentsPerAttribute : 4
+        }]);
+
+        ClearCommand.ALL.execute(context);
+        expect(context.readPixels()).toEqual([0, 0, 0, 0]);
+
+        context.draw({
+            primitiveType : PrimitiveType.POINTS,
+            shaderProgram : sp,
+            vertexArray : va
+        });
+        expect(context.readPixels()).toEqual([255, 255, 255, 255]);
+    });
+
+    it('doc comment with reference to another function', function() {
+        var vs = 'attribute vec4 position; void main() { gl_PointSize = 1.0; gl_Position = position; }';
+        var fs =
+            'void main() { \n' +
+            '  vec4 color = vec4(1.0, 1.0, 1.0, 1.0); \n' +
+            '  czm_testFunctionWithComment(color); \n' +
             '}';
         sp = context.createShaderProgram(vs, fs);
 
