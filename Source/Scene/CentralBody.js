CesiumGS/cesium

commit b76935654a236921130ea124bab7387314c2ff90

Updates to work with Cartesian2/3/4 without prototype add/subtract

Source/Scene/CentralBody.js
@@ -279,10 +279,10 @@ define([
         var northOffset = nUnit.multiplyByScalar(scalar);
 
         // A conservative measure for the longitudes would be to use the min/max longitudes of the bounding frustum.
-        var upperLeft = radii.multiplyComponents(center.add(northOffset).subtract(eastOffset));
-        var upperRight = radii.multiplyComponents(center.add(northOffset).add(eastOffset));
-        var lowerLeft = radii.multiplyComponents(center.subtract(northOffset).subtract(eastOffset));
-        var lowerRight = radii.multiplyComponents(center.subtract(northOffset).add(eastOffset));
+        var upperLeft  = Cartesian3.subtract(radii.multiplyComponents(Cartesian3.add(center, northOffset)), eastOffset);
+        var upperRight = Cartesian3.add(radii.multiplyComponents(Cartesian3.add(center, northOffset)), eastOffset);
+        var lowerLeft  = Cartesian3.subtract(radii.multiplyComponents(Cartesian3.subtract(center, northOffset)), eastOffset);
+        var lowerRight = Cartesian3.add(radii.multiplyComponents(Cartesian3.subtract(center, northOffset)), eastOffset);
 
         depthQuadScratch[0] = upperLeft.x;
         depthQuadScratch[1] = upperLeft.y;

Source/Core/BoundingRectangle.js
@@ -3,6 +3,7 @@ define([
         './defaultValue',
         './defined',
         './DeveloperError',
+        './Cartesian2',
         './Cartographic',
         './GeographicProjection',
         './Intersect'
@@ -10,6 +11,7 @@ define([
         defaultValue,
         defined,
         DeveloperError,
+        Cartesian2,
         Cartographic,
         GeographicProjection,
         Intersect) {
@@ -135,7 +137,7 @@ define([
         var lowerLeft = projection.project(extent.getSouthwest(fromExtentLowerLeft));
         var upperRight = projection.project(extent.getNortheast(fromExtentUpperRight));
 
-        upperRight.subtract(lowerLeft, upperRight);
+        Cartesian2.subtract(upperRight, lowerLeft, upperRight);
 
         result.x = lowerLeft.x;
         result.y = lowerLeft.y;

Source/Core/CatmullRomSpline.js
@@ -60,10 +60,7 @@ define([
             var controlPoint1 = Cartesian3.clone(controlPoints[1].point);
             var controlPoint2 = Cartesian3.clone(controlPoints[2].point);
 
-            this._ti = controlPoint1
-                           .multiplyByScalar(2.0)
-                           .subtract(controlPoint2)
-                           .subtract(controlPoint0)
+            this._ti = Cartesian3.subtract(Cartesian3.subtract(controlPoint1.multiplyByScalar(2.0), controlPoint2), controlPoint0)
                            .multiplyByScalar(0.5);
         }
 
@@ -76,9 +73,7 @@ define([
             var controlPointn1 = Cartesian3.clone(controlPoints[n - 1].point);
             var controlPointn2 = Cartesian3.clone(controlPoints[n - 2].point);
 
-            this._to = controlPointn0
-                           .subtract(controlPointn1.multiplyByScalar(2.0))
-                           .add(controlPointn2)
+            this._to = Cartesian3.add(Cartesian3.subtract(controlPointn0, controlPointn1.multiplyByScalar(2.0)), controlPointn2)
                            .multiplyByScalar(0.5);
         }
     };
@@ -206,12 +201,12 @@ define([
             p0 = this._points[0].point;
             p1 = this._points[1].point;
             p2 = this._ti;
-            p3 = this._points[2].point.subtract(p0).multiplyByScalar(0.5);
+            p3 = Cartesian3.subtract(this._points[2].point, p0).multiplyByScalar(0.5);
             coefs = HermiteSpline.hermiteCoefficientMatrix.multiplyByPoint(timeVec);
         } else if (i === this._points.length - 2) {
             p0 = this._points[i].point;
             p1 = this._points[i + 1].point;
-            p2 = p1.subtract(this._points[i - 1].point).multiplyByScalar(0.5);
+            p2 = Cartesian3.subtract(p1, this._points[i - 1].point).multiplyByScalar(0.5);
             p3 = this._to;
             coefs = HermiteSpline.hermiteCoefficientMatrix.multiplyByPoint(timeVec);
         } else {
@@ -226,7 +221,7 @@ define([
         p2 = p2.multiplyByScalar(coefs.z);
         p3 = p3.multiplyByScalar(coefs.w);
 
-        return p0.add(p1.add(p2.add(p3)));
+        return Cartesian3.add(Cartesian3.add(Cartesian3.add(p0, p1), p2), p3);
     };
 
     return CatmullRomSpline;

Source/Core/EllipseGeometry.js
@@ -102,7 +102,7 @@ define([
             if (vertexFormat.st) {
                 var rotatedPoint = Matrix3.multiplyByVector(textureMatrix, position, scratchCartesian2);
                 var projectedPoint = projection.project(ellipsoid.cartesianToCartographic(rotatedPoint, scratchCartographic), scratchCartesian3);
-                projectedPoint = Cartesian3.subtract(projectedPoint, projectedCenter, projectedPoint);
+                Cartesian3.subtract(projectedPoint, projectedCenter, projectedPoint);
 
                 texCoordScratch.x = (projectedPoint.x + semiMajorAxis) / (2.0 * semiMajorAxis);
                 texCoordScratch.y = (projectedPoint.y + semiMajorAxis) / (2.0 * semiMajorAxis);
@@ -350,7 +350,7 @@ define([
             if (vertexFormat.st) {
                 var rotatedPoint = Matrix3.multiplyByVector(textureMatrix, position, scratchCartesian2);
                 var projectedPoint = projection.project(ellipsoid.cartesianToCartographic(rotatedPoint, scratchCartographic), scratchCartesian3);
-                projectedPoint = Cartesian3.subtract(projectedPoint, projectedCenter, projectedPoint);
+                Cartesian3.subtract(projectedPoint, projectedCenter, projectedPoint);
 
                 texCoordScratch.x = (projectedPoint.x + semiMajorAxis) / (2.0 * semiMajorAxis);
                 texCoordScratch.y = (projectedPoint.y + semiMajorAxis) / (2.0 * semiMajorAxis);
@@ -384,8 +384,8 @@ define([
 
                 binormal = normal.clone(binormal);
                 var next = Cartesian3.fromArray(positions, (i + 3) % length, scratchCartesian4);
-                next = next.subtract(position, next);
-                var bottom = extrudedPosition.subtract(position, scratchCartesian3);
+                Cartesian3.subtract(next, position, next);
+                var bottom = Cartesian3.subtract(extrudedPosition, position, scratchCartesian3);
 
                 normal = bottom.cross(next, normal).normalize(normal);
 

Source/Core/EllipsoidTangentPlane.js
@@ -121,7 +121,7 @@ define([
         var intersectionPoint = IntersectionTests.rayPlane(ray, this._plane, projectPointOntoPlaneCartesian3);
 
         if (defined(intersectionPoint)) {
-            var v = intersectionPoint.subtract(this._origin, intersectionPoint);
+            var v = Cartesian2.subtract(intersectionPoint, this._origin, intersectionPoint);
             var x = this._xAxis.dot(v);
             var y = this._yAxis.dot(v);
 

Source/Core/ExtentGeometry.js
@@ -250,8 +250,8 @@ define([
                 var p1 = Cartesian3.fromArray(positions, i + 3, v1Scratch);
                 if (recomputeNormal) {
                     var p2 = Cartesian3.fromArray(positions, i + bottomOffset, v2Scratch);
-                    p1 = p1.subtract(p, p1);
-                    p2 = p2.subtract(p, p2);
+                    Cartesian3.subtract(p1, p, p1);
+                    Cartesian3.subtract(p2, p, p2);
                     normal = p2.cross(p1, normal).normalize(normal);
                     recomputeNormal = false;
                 }
@@ -740,10 +740,10 @@ define([
             proj.project(nwCartographic, nw);
             proj.project(centerCartographic, center);
 
-            nw.subtract(center, nw);
+            Cartesian3.subtract(nw, center, nw);
             Matrix2.fromRotation(rotation, rotationMatrix);
             rotationMatrix.multiplyByVector(nw, nw);
-            nw.add(center, nw);
+            Cartesian3.add(nw, center, nw);
             proj.unproject(nw, nwCartographic);
 
             var latitude = nwCartographic.latitude;

Source/Core/ExtentOutlineGeometry.js
@@ -358,10 +358,10 @@ define([
             proj.project(nwCartographic, nw);
             proj.project(centerCartographic, center);
 
-            nw.subtract(center, nw);
+            Cartesian3.subtract(nw, center, nw);
             Matrix2.fromRotation(rotation, rotationMatrix);
             rotationMatrix.multiplyByVector(nw, nw);
-            nw.add(center, nw);
+            Cartesian3.add(nw, center, nw);
             proj.unproject(nw, nwCartographic);
 
             var latitude = nwCartographic.latitude;

commit e33c5223ade7a85ca70b633327844fa05e9d2ff1

Fixed occlusion issue introduced in b769356

Source/Scene/CentralBody.js
@@ -279,10 +279,10 @@ define([
         var northOffset = Cartesian3.multiplyByScalar(nUnit, scalar);
 
         // A conservative measure for the longitudes would be to use the min/max longitudes of the bounding frustum.
-        var upperLeft  = Cartesian3.subtract(Cartesian3.multiplyComponents(radii, Cartesian3.add(center, northOffset)), eastOffset);
-        var upperRight = Cartesian3.add(Cartesian3.multiplyComponents(radii, Cartesian3.add(center, northOffset)), eastOffset);
-        var lowerLeft  = Cartesian3.subtract(Cartesian3.multiplyComponents(radii, Cartesian3.subtract(center, northOffset)), eastOffset);
-        var lowerRight = Cartesian3.add(Cartesian3.multiplyComponents(radii, Cartesian3.subtract(center, northOffset)), eastOffset);
+        var upperLeft = Cartesian3.multiplyComponents(radii, Cartesian3.subtract(Cartesian3.add(center, northOffset), eastOffset));
+        var upperRight = Cartesian3.multiplyComponents(radii, Cartesian3.add(Cartesian3.add(center, northOffset), eastOffset));
+        var lowerLeft = Cartesian3.multiplyComponents(radii, Cartesian3.subtract(Cartesian3.subtract(center, northOffset), eastOffset));
+        var lowerRight = Cartesian3.multiplyComponents(radii, Cartesian3.add(Cartesian3.subtract(center, northOffset), eastOffset));
 
         depthQuadScratch[0] = upperLeft.x;
         depthQuadScratch[1] = upperLeft.y;