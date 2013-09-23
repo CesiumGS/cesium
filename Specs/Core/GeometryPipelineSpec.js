eb7dfd64fa48c576001bcd20d485361227a89962
hpinkos <pinkos.hannah@gmail.com>
2013-09-23 15:38:44 -0400

    cleanup

--- Specs/Core/GeometryPipelineSpec.js
+++ Specs/Core/GeometryPipelineSpec.js
@@ -812,50 +812,6 @@ defineSuite([
         expect(transformed.modelMatrix).toEqual(Matrix4.IDENTITY);
     });
 
-    it('transformToWorldCoordinates with uniform scale', function() {
-        var instance = new GeometryInstance({
-            geometry : new Geometry({
-                attributes : {
-                    position : new GeometryAttribute({
-                        componentDatatype : ComponentDatatype.FLOAT,
-                        componentsPerAttribute : 3,
-                        values : [
-                            0.0, 0.0, 0.0,
-                            1.0, 0.0, 0.0,
-                            0.0, 1.0, 0.0
-                        ]
-                    }),
-                    normal : new GeometryAttribute({
-                        componentDatatype : ComponentDatatype.FLOAT,
-                        componentsPerAttribute : 3,
-                        values : [
-                            0.0, 0.0, 1.0,
-                            0.0, 0.0, 1.0,
-                            0.0, 0.0, 1.0
-                        ]
-                    })
-                },
-                indices : [0, 1, 2],
-                primitiveType : PrimitiveType.TRIANGLES,
-                boundingSphere : new BoundingSphere(new Cartesian3(0.5, 0.5, 0.0), 1.0)
-            }),
-            modelMatrix : Matrix4.multiplyByUniformScale(new Matrix4(0.0, 0.0, 1.0, 0.0,
-                                      1.0, 0.0, 0.0, 0.0,
-                                      0.0, 1.0, 0.0, 0.0,
-                                      0.0, 0.0, 0.0, 1.0), 2)
-        });
-
-        var transformed = GeometryPipeline.transformToWorldCoordinates(instance);
-        var transformedPositions = [0.0, 0.0, 0.0, 0.0, 2.0, 0.0, 0.0, 0.0, 2.0];
-        var transformedNormals = [1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0];
-
-        expect(transformed.geometry.attributes.position.values).toEqual(transformedPositions);
-        expect(transformed.geometry.attributes.normal.values).toEqual(transformedNormals);
-        expect(transformed.geometry.boundingSphere).toEqual(new BoundingSphere(new Cartesian3(0.0, 1.0, 1.0), 2.0));
-        expect(transformed.modelMatrix).toEqual(Matrix4.IDENTITY);
-    });
-
-
     it('transformToWorldCoordinates with non-uniform scale', function() {
         var instance = new GeometryInstance({
             geometry : new Geometry({
