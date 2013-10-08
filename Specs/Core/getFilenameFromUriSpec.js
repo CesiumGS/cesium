6d1f31674c314682c0bb0bbe6aa1ce72b1716d86
Scott Hunter <shunter@agi.com>
2013-10-08 15:16:37 -0400

    Add another test case.

--- Specs/Core/getFilenameFromUriSpec.js
+++ Specs/Core/getFilenameFromUriSpec.js
@@ -6,6 +6,9 @@ defineSuite(['Core/getFilenameFromUri'], function(getFilenameFromUri) {
     it('works as expected', function() {
         var result = getFilenameFromUri('http://www.mysite.com/awesome?makeitawesome=true');
         expect(result).toEqual('awesome');
+
+        result = getFilenameFromUri('http://www.mysite.com/somefolder/awesome.png#makeitawesome');
+        expect(result).toEqual('awesome.png');
     });
 
     it('throws with undefined parameter', function() {
