425e8f9a17ef7d4e8d78157272e5bba064af9715
Gabor SEBESTYEN <segabor@gmail.com>
2013-10-09 17:06:12 +0200

    Aviation widget - basic layout

--- Apps/AviationWidget/main.js
+++ Apps/AviationWidget/main.js
@@ -21,6 +21,13 @@
     tick();
     **/
 
+    var AlwaysTrue = {
+        getValue: function(t) {return true;}
+    };
+    var AlwaysFalse = {
+        getValue: function(t) {return false;}
+    };
+
     /**
      * @param {String} ID of Canvas element
      * @param {Object} TBD {routeURL: {String}, cockpitURL: {String}}
@@ -34,6 +41,8 @@
         var _hasCockpitModel = false;
 
         var _viewerOpts = Cesium.defaultValue(options.viewer, Cesium.defaultValue.EMPTY_OBJECT);
+
+        var _toolbar;
 
         /**
          * Initialize widget
@@ -55,32 +64,84 @@
 
             // -- Load KMLs -- //
             var i;
-            /* for (i=0; i<kmlResources.length; i++) {
-                ds.loadUrl(kmlResources[i]).then(function() {
-                    that.viewer.dataSources.add(ds);
-                });
-            } */
+
             // 3D KML
             var tourKml = options.routeURL;
 
             if (tourKml !== 'undefined') {
-                var ds = new Cesium.KmlDataSource();
-                ds.loadUrl(tourKml).then(function() {
-                    that.viewer.dataSources.add(ds);
-                    _hasTourModel = true;
+                that._tourDataSource = new Cesium.KmlDataSource();
+                that._tourDataSource.loadUrl(tourKml).then(function() {
+                    that.viewer.dataSources.add(that._tourDataSource);
                     console.log('route loaded ...');
                 });
             }
             // Cockpit KML
             var cockpitKml = options.cockpitURL;
             if (cockpitKml !== 'undefined') {
-                var ds2 = new Cesium.KmlDataSource();
-                ds2.loadUrl(cockpitKml).then(function() {
-                    that.viewer.dataSources.add(ds2);
-                    _hasCockpitModel = true;
+                that._cockpitDataSource = new Cesium.KmlDataSource();
+                that._cockpitDataSource.loadUrl(cockpitKml).then(function() {
+                    that.viewer.dataSources.add(that._cockpitDataSource);
                     console.log('cockpit loaded ...');
                 });
             }
+
+
+            // -- add toolbar -- //
+            _toolbar = document.createElement('div');
+            _toolbar.setAttribute('id', 'awToolbar');
+
+
+            that.viewer._element.parentNode.insertBefore(_toolbar, that.viewer._element.nextSibling);
+
+            /* var _sel = document.createElement('select');
+            _toolbar.appendChild(_sel); */
+
+            var _it, _obj;
+            if (that._tourDataSource) {
+                _it = document.createElement('input');
+                _it.setAttribute('type', 'checkbox');
+                _it.setAttribute('name', 'gxaviation');
+                _it.checked = true;
+                _toolbar.appendChild(_it);
+
+                _it.onchange = function(ev) {
+                    var coll = that._tourDataSource.getDynamicObjectCollection().getObjects();
+                    var i;
+                    var flag = ev.target.checked;
+                    for (i=0; i<coll.length; i++) {
+                        // the wall
+                        if (typeof coll[i].wall !== 'undefined') {
+                            coll[i].wall._show = flag ? AlwaysTrue : AlwaysFalse;
+                        }
+                        // the polyline running on top of the wall
+                        if (typeof coll[i]._polyline !== 'undefined') {
+                            coll[i]._polyline._show = flag ? AlwaysTrue : AlwaysFalse;
+                        }
+                    }
+                };
+
+                _obj = document.createTextNode('Tour');
+                _toolbar.appendChild(_obj);
+            }
+            if (that._cockpitDataSource) {
+                _it = document.createElement('input');
+                _it.setAttribute('type', 'checkbox');
+                _it.setAttribute('name', 'gxaviation');
+                _it.checked = true;
+                _toolbar.appendChild(_it);
+
+                _it.onchange = function(ev) {
+                    if (ev.target.checked) {
+                        that.viewer.dataSources.add(that._cockpitDataSource);
+                    } else {
+                        that.viewer.dataSources.remove(that._cockpitDataSource);
+                    }
+                };
+
+                _obj = document.createTextNode('Cockpit');
+                _toolbar.appendChild(_obj);
+            }
+
 
 
             // -- Create buttons -- //
