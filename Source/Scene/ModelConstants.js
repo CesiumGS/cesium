3438790117d74930e9a14c002170c918fdf7642b
Patrick Cozzi <pjcozzi@gmail.com>
2013-11-26 13:54:54 -0500

    Remove unused model constants

--- Source/Scene/ModelConstants.js
+++ Source/Scene/ModelConstants.js
@@ -6,14 +6,6 @@ define(function() {
      * @private
      */
     return {
-        POINTS : 0x0000,
-        LINES : 0x0001,
-        LINE_LOOP : 0x0002,
-        LINE_STRIP : 0x0003,
-        TRIANGLES : 0x0004,
-        TRIANGLE_STRIP : 0x0005,
-        TRIANGLE_FAN : 0x0006,
-
         BYTE : 0x1400,
         UNSIGNED_BYTE : 0x1401,
         SHORT : 0x1402,
