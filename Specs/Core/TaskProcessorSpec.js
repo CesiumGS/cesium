53a2eff2d9bbbecba3b39ed594943b91b2f35976
Scott Hunter <shunter@agi.com>
2013-12-02 16:20:31 -0500

    Override worker loader config in specs, so they work even with the combined Cesium.js.

--- Specs/Core/TaskProcessorSpec.js
+++ Specs/Core/TaskProcessorSpec.js
@@ -1,9 +1,11 @@
 /*global defineSuite*/
 defineSuite([
         'Core/TaskProcessor',
+        'require',
         'Specs/waitsForPromise'
     ], function(
         TaskProcessor,
+        require,
         waitsForPromise) {
     "use strict";
     /*global jasmine,describe,xdescribe,it,xit,expect,beforeEach,afterEach,beforeAll,afterAll,spyOn,runs,waits,waitsFor*/
@@ -12,10 +14,22 @@ defineSuite([
 
     beforeEach(function() {
         TaskProcessor._workerModulePrefix = '../Specs/TestWorkers/';
+
+        function absolutize(url) {
+            var a = document.createElement('a');
+            a.href = url;
+            a.href = a.href; // IE only absolutizes href on get, not set
+            return a.href;
+        }
+
+        TaskProcessor._loaderConfig = {
+            baseUrl : absolutize(require.toUrl('Specs/../Source'))
+        };
     });
 
     afterEach(function() {
         TaskProcessor._workerModulePrefix = TaskProcessor._defaultWorkerModulePrefix;
+        TaskProcessor._loaderConfig = undefined;
 
         if (taskProcessor && !taskProcessor.isDestroyed()) {
             taskProcessor = taskProcessor.destroy();
