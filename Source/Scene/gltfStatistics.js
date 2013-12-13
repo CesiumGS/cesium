ce7bd94953a265e6ae62db54d265bfac2e947248
Patrick Cozzi <pjcozzi@gmail.com>
2013-12-13 08:46:11 -0500

    More stats

--- Source/Scene/gltfStatistics.js
+++ Source/Scene/gltfStatistics.js
@@ -44,7 +44,7 @@ define([
         var accessors = gltf.accessors;
 
         if (!defined(meshes) || !defined(accessors)) {
-            return 0;
+            return undefined;
         }
 
         var statistics = {};
@@ -88,6 +88,111 @@ define([
         return statistics;
     }
 
+    function getSceneStatistics(gltf, meshStatistics) {
+        var scenes = gltf.scenes;
+        var nodes = gltf.nodes;
+
+        if (!defined(scenes) || !defined(nodes)) {
+            return undefined;
+        }
+
+        // TODO: skin stats
+        var numberOfNodes = 0;
+        var numberOfMeshNodes = 0;
+        var numberOfCameraNodes = 0;
+        var numberOfLightNodes = 0;
+        var numberOfTransformNodes = 0;
+
+        var numberOfMeshes = 0;     // instanced.  The same mesh may be referenced by several nodes.
+        var numberOfPrimitives = 0; // instanced.  Basically number of draw calls.
+        var numberOfTriangles = 0;  // instanced.  Total triangles rendered.  Total in meshes will be less than or equal to.
+
+        var stack = [];
+        var scene = scenes[gltf.scene];
+        var sceneNodes = scene.nodes;
+        var length = sceneNodes.length;
+
+        for (var i = 0; i < length; ++i) {
+            stack.push(nodes[sceneNodes[i]]);
+
+            while (stack.length > 0) {
+                var n = stack.pop();
+                ++numberOfNodes;
+
+                var meshes = n.meshes;
+
+                if (defined(meshes)) {
+                    ++numberOfMeshNodes;
+
+                    var meshesLength = meshes.length;
+                    numberOfMeshes += meshesLength;
+
+                    for (var k = 0; k < meshesLength; ++k) {
+                        var mesh = gltf.meshes[meshes[k]];
+                        numberOfPrimitives += mesh.primitives.length;
+                        numberOfTriangles += meshStatistics[mesh.name].numberOfTriangles;
+                    }
+                } else if (defined(n.camerea)) {
+                    ++numberOfCameraNodes;
+                } else if (defined(n.light)) {
+                    ++numberOfLightNodes;
+                } else {
+                    ++numberOfTransformNodes;
+                }
+
+                var children = n.children;
+                var childrenLength = children.length;
+                for (var j = 0; j < childrenLength; ++j) {
+                    stack.push(nodes[children[j]]);
+                }
+            }
+        }
+
+        return {
+            numberOfNodes : numberOfNodes,
+            numberOfMeshNodes : numberOfMeshNodes,
+            numberOfCameraNodes : numberOfCameraNodes,
+            numberOfLightNodes : numberOfLightNodes,
+            numberOfTransformNodes : numberOfTransformNodes,
+
+            numberOfMeshes : numberOfMeshes,
+            numberOfPrimitives : numberOfPrimitives,
+            numberOfTriangles : numberOfTriangles
+        };
+    }
+
+    function getAnimationStatistics(gltf) {
+        var animations = gltf.animations;
+
+        if (!defined(animations)) {
+            return undefined;
+        }
+
+        var numberOfTargetNodes = 0;
+        var targets = {};
+
+        for (var name in animations) {
+            if (animations.hasOwnProperty(name)) {
+                var animation = animations[name];
+                var channels = animation.channels;
+
+                var length = channels.length;
+                for (var i = 0; i < length; ++i) {
+                    var nodeTarget = channels[i].target.id;
+
+                    if (!defined(targets[nodeTarget])) {
+                        targets[nodeTarget] = true;
+                        ++numberOfTargetNodes;
+                    }
+                }
+            }
+        }
+
+        return {
+            numberOfTargetNodes : numberOfTargetNodes
+        };
+    }
+
     /**
      * DOC_TBA
      */
@@ -95,6 +200,8 @@ define([
         if (!defined(gltf)) {
             return undefined;
         }
+
+        var meshStatistics = getMeshStatistics(gltf);
 
         return {
             numberOfPrograms : getNumberOf(gltf.programs),
@@ -105,7 +212,9 @@ define([
             numberOfTotalPrimitives : getNumberOfPrimitives(gltf.meshes),
             numberOfAnimations : getNumberOf(gltf.animations),
 
-            meshStatistics : getMeshStatistics(gltf)
+            meshStatistics : meshStatistics,
+            sceneStatistics : getSceneStatistics(gltf, meshStatistics),
+            animationStatistics : getAnimationStatistics(gltf)
         };
     };
 
