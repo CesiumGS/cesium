a735a48ade2a5d5cc2b77e6d42ac7a9b63c5669b
Chris Cooper <chris.cooper@nicta.com.au>
2014-01-21 10:53:21 +1100

    add build and release scripts

---
+++ build-travail.sh
@@ -0,0 +1,7 @@
+# Build for travail
+
+# Remove old build
+rm -rf ./Build
+
+# Release build
+./Tools/apache-ant-1.8.2/bin/ant release
---
+++ release-travail.sh
@@ -0,0 +1,2 @@
+# Copy across to travail dir
+cp -R ./Build/Cesium/Cesium.js ./Build/Cesium/Assets ./Build/Cesium/Workers ../travail-2/app/assets/javascripts/.
