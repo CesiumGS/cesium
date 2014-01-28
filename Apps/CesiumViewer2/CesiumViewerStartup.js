1b1fccd989c4a343dece1eb0f10498fa8b8d8a5e
Dan Bagnell <daniel.p.bagnell@gmail.com>
2014-01-27 19:54:20 -0500

    Remove test viewer.

--- Apps/CesiumViewer2/CesiumViewerStartup.js
+++
@@ -1,9 +0,0 @@
-/*global require*/
-require({
-    baseUrl : '../../Source',
-    paths : {
-        CesiumViewer : '../Apps/CesiumViewer2',
-        domReady : '../ThirdParty/requirejs-2.1.9/domReady'
-    }
-}, ['CesiumViewer/CesiumViewer'], function() {
-});
