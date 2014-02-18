99af0d811c82c238d6fc792bd8fc14610bfacd1c
Scott Hunter <shunter@agi.com>
2014-02-18 11:10:12 -0500

    Revert structure and clarify that these are "interfaces" on types, not objects.

--- Source/Core/Packable.js
+++ Source/Core/Packable.js
@@ -3,45 +3,43 @@ define(['../Core/DeveloperError'], function(DeveloperError) {
     "use strict";
 
     /**
-     * Base interface for objects which can store their values as packed
-     * elements in an array.
+     * Static interface for types which can store their values as packed
+     * elements in an array.  These methods and properties are expected to be 
+     * defined on a constructor function.
      *
-     * @alias Packable
-     * @constructor
+     * @exports Packable
      *
      * @see PackableForInterpolation
      */
-    var Packable = function() {
+    var Packable = {
         /**
          * The number of elements used to pack the object into an array.
          * @Type {Number}
          */
-        this.packedLength = undefined;
+        packedLength : undefined,
 
-        DeveloperError.throwInstantiationError();
+        /**
+         * Stores the provided instance into the provided array.
+         * @memberof Packable
+         * @function
+         *
+         * @param {Object} value The value to pack.
+         * @param {Array} array The array to pack into.
+         * @param {Number} [startingIndex=0] The index into the array at which to start packing the elements.
+         */
+        pack : DeveloperError.throwInstantiationError,
+
+        /**
+         * Retrieves an instance from a packed array.
+         * @memberof Packable
+         * @function
+         *
+         * @param {Array} array The packed array.
+         * @param {Number} [startingIndex=0] The starting index of the element to be unpacked.
+         * @param {Object} [result] The object into which to store the result.
+         */
+        unpack : DeveloperError.throwInstantiationError
     };
-
-    /**
-     * Stores the provided instance into the provided array.
-     * @memberof Packable
-     * @function
-     *
-     * @param {Object} value The value to pack.
-     * @param {Array} array The array to pack into.
-     * @param {Number} [startingIndex=0] The index into the array at which to start packing the elements.
-     */
-    Packable.prototype.pack = DeveloperError.throwInstantiationError;
-
-    /**
-     * Retrieves an instance from a packed array.
-     * @memberof Packable
-     * @function
-     *
-     * @param {Array} array The packed array.
-     * @param {Number} [startingIndex=0] The starting index of the element to be unpacked.
-     * @param {Object} [result] The object into which to store the result.
-     */
-    Packable.prototype.unpack = DeveloperError.throwInstantiationError;
 
     return Packable;
 });
--- Source/Core/PackableForInterpolation.js
+++ Source/Core/PackableForInterpolation.js
@@ -3,48 +3,46 @@ define(['../Core/DeveloperError'], function(DeveloperError) {
     "use strict";
 
     /**
-     * Interface for {@link Packable} objects which are interpolated in a
-     * different representation than their packed value.
+     * Static interface for {@link Packable} types which are interpolated in a
+     * different representation than their packed value.  These methods and
+     * properties are expected to be defined on a constructor function.
      *
-     * @alias PackableForInterpolation
-     * @constructor
+     * @exports PackableForInterpolation
      *
      * @see Packable
      */
-    var PackableForInterpolation = function() {
+    var PackableForInterpolation = {
         /**
          * The number of elements used to store the object into an array in its interpolatable form.
          * @Type {Number}
          */
-        this.packedInterpolationLength = undefined;
+        packedInterpolationLength : undefined,
 
-        DeveloperError.throwInstantiationError();
+        /**
+         * Converts a packed array into a form suitable for interpolation.
+         * @memberof PackableForInterpolation
+         * @function
+         *
+         * @param {Array} packedArray The packed array.
+         * @param {Number} [startingIndex=0] The index of the first element to be converted.
+         * @param {Number} [lastIndex=packedArray.length] The index of the last element to be converted.
+         * @param {Array} [result] The object into which to store the result.
+         */
+        convertPackedArrayForInterpolation : DeveloperError.throwInstantiationError,
+
+        /**
+         * Retrieves an instance from a packed array converted with {@link convertPackedArrayForInterpolation}.
+         * @memberof PackableForInterpolation
+         * @function
+         *
+         * @param {Array} array The original packed array.
+         * @param {Array} sourceArray The converted array.
+         * @param {Number} [startingIndex=0] The startingIndex used to convert the array.
+         * @param {Number} [lastIndex=packedArray.length] The lastIndex used to convert the array.
+         * @param {Object} [result] The object into which to store the result.
+         */
+        unpackInterpolationResult : DeveloperError.throwInstantiationError
     };
-
-    /**
-     * Converts a packed array into a form suitable for interpolation.
-     * @memberof PackableForInterpolation
-     * @function
-     *
-     * @param {Array} packedArray The packed array.
-     * @param {Number} [startingIndex=0] The index of the first element to be converted.
-     * @param {Number} [lastIndex=packedArray.length] The index of the last element to be converted.
-     * @param {Array} [result] The object into which to store the result.
-     */
-    PackableForInterpolation.prototype.convertPackedArrayForInterpolation = DeveloperError.throwInstantiationError;
-
-    /**
-     * Retrieves an instance from a packed array converted with {@link convertPackedArrayForInterpolation}.
-     * @memberof PackableForInterpolation
-     * @function
-     *
-     * @param {Array} array The original packed array.
-     * @param {Array} sourceArray The converted array.
-     * @param {Number} [startingIndex=0] The startingIndex used to convert the array.
-     * @param {Number} [lastIndex=packedArray.length] The lastIndex used to convert the array.
-     * @param {Object} [result] The object into which to store the result.
-     */
-    PackableForInterpolation.prototype.unpackInterpolationResult = DeveloperError.throwInstantiationError;
 
     return PackableForInterpolation;
 });
