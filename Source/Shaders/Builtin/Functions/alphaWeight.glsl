aad09aa3a7f6a13a99aa405465a608548a43380a
Dan Bagnell <daniel.p.bagnell@gmail.com>
2014-03-12 16:48:24 -0400

    Change weight function yet again. This looks better with outlined geometry.

--- Source/Shaders/Builtin/Functions/alphaWeight.glsl
+++ Source/Shaders/Builtin/Functions/alphaWeight.glsl
@@ -5,5 +5,5 @@ float czm_alphaWeight(float z, float a)
 {
     // See Weighted Blended Order-Independent Transparency for examples of different weighting functions:
     // http://jcgt.org/published/0002/02/09/
-    return clamp(0.5 / 1e-5 + pow(abs(z) / 200.0, 6.0), 1e-2, 3e-3);
+    return pow(a + 0.01, 4.0) + max(1e-2, min(3.0 * 1e3, 100.0 / (1e-5 + pow(abs(z) / 10.0, 3.0) + pow(abs(z) / 200.0, 6.0))));
 }
