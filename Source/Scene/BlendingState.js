c2cd8b1e7b889d16d15dd4ff05c5d7112822fd71
Patrick Cozzi <pjcozzi@gmail.com>
2014-05-21 15:54:40 -0400

    Fix BlendingState

--- Source/Scene/BlendingState.js
+++ Source/Scene/BlendingState.js
@@ -1,10 +1,10 @@
 /*global define*/
 define([
-        '../Core/defineProperties',
+        '../Core/freezeObject',
         './BlendEquation',
         './BlendFunction'
     ], function(
-        defineProperties,
+        freezeObject,
         BlendEquation,
         BlendFunction) {
     "use strict";
@@ -19,11 +19,24 @@ define([
      *
      * @exports BlendingState
      */
-    var BlendingState = function() {
-        this._DISABLED = {
+    var BlendingState = {
+        /**
+         * Blending is disabled.
+         *
+         * @type {Object}
+         * @readonly
+         */
+        DISABLED : freezeObject({
             enabled : false
-        };
-        this._ALPHA_BLEND = {
+        }),
+
+        /**
+         * Blending is enabled using alpha blending, <code>source(source.alpha) + destination(1 - source.alpha)</code>.
+         *
+         * @type {Object}
+         * @readonly
+         */
+        ALPHA_BLEND : freezeObject({
             enabled : true,
             equationRgb : BlendEquation.ADD,
             equationAlpha : BlendEquation.ADD,
@@ -31,8 +44,15 @@ define([
             functionSourceAlpha : BlendFunction.SOURCE_ALPHA,
             functionDestinationRgb : BlendFunction.ONE_MINUS_SOURCE_ALPHA,
             functionDestinationAlpha : BlendFunction.ONE_MINUS_SOURCE_ALPHA
-        };
-        this._PRE_MULTIPLIED_ALPHA_BLEND = {
+        }),
+
+        /**
+         * Blending is enabled using alpha blending with premultiplied alpha, <code>source + destination(1 - source.alpha)</code>.
+         *
+         * @type {Object}
+         * @readonly
+         */
+        PRE_MULTIPLIED_ALPHA_BLEND : freezeObject({
             enabled : true,
             equationRgb : BlendEquation.ADD,
             equationAlpha : BlendEquation.ADD,
@@ -40,8 +60,15 @@ define([
             functionSourceAlpha : BlendFunction.ONE,
             functionDestinationRgb : BlendFunction.ONE_MINUS_SOURCE_ALPHA,
             functionDestinationAlpha : BlendFunction.ONE_MINUS_SOURCE_ALPHA
-        };
-        this._ADDITIVE_BLEND = {
+        }),
+
+        /**
+         * Blending is enabled using additive blending, <code>source(source.alpha) + destination</code>.
+         *
+         * @type {Object}
+         * @readonly
+         */
+        ADDITIVE_BLEND : freezeObject({
             enabled : true,
             equationRgb : BlendEquation.ADD,
             equationAlpha : BlendEquation.ADD,
@@ -49,66 +76,8 @@ define([
             functionSourceAlpha : BlendFunction.SOURCE_ALPHA,
             functionDestinationRgb : BlendFunction.ONE,
             functionDestinationAlpha : BlendFunction.ONE
-        };
+        })
     };
-
-    defineProperties(BlendingState.prototype, {
-        /**
-         * Blending is disabled.
-         *
-         * @memberof BlendingState.prototype
-         *
-         * @type {Object}
-         * @readonly
-         */
-        DISABLED : {
-            get : function() {
-                return this._DISABLED;
-            }
-        },
-
-        /**
-         * Blending is enabled using alpha blending, <code>source(source.alpha) + destination(1 - source.alpha)</code>.
-         *
-         * @memberof BlendingState.prototype
-         *
-         * @type {Object}
-         * @readonly
-         */
-        ALPHA_BLEND : {
-            get : function() {
-                return this._ALPHA_BLEND;
-            }
-        },
-
-        /**
-         * Blending is enabled using alpha blending with premultiplied alpha, <code>source + destination(1 - source.alpha)</code>.
-         *
-         * @memberof BlendingState.prototype
-         *
-         * @type {Object}
-         * @readonly
-         */
-        PRE_MULTIPLIED_ALPHA_BLEND : {
-            get : function() {
-                return this._PRE_MULTIPLIED_ALPHA_BLEND;
-            }
-        },
-
-        /**
-         * Blending is enabled using additive blending, <code>source(source.alpha) + destination</code>.
-         *
-         * @memberof BlendingState.prototype
-         *
-         * @type {Object}
-         * @readonly
-         */
-        ADDITIVE_BLEND : {
-            get : function() {
-                return this._ADDITIVE_BLEND;
-            }
-        }
-    });
 
     return BlendingState;
 });
