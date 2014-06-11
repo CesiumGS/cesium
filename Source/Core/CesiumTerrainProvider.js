aa6e393cdfa4a50ab8a8c5891b5043e6bdfcdf0a
Alex Wood <abwood@gmail.com>
2014-06-11 16:18:53 -0400

    Simple change to support any quantized-mesh-1.X version.  Minor version numbers indicate non-breaking changes and therefore should be supported by the CesiumTerrainProvider

--- Source/Core/CesiumTerrainProvider.js
+++ Source/Core/CesiumTerrainProvider.js
@@ -123,7 +123,7 @@ define([
                         isBigEndian : false
                     };
                 that._hasWaterMask = true;
-            } else if (data.format === 'quantized-mesh-1.0') {
+            } else if (data.format.substring(0, 17) === 'quantized-mesh-1.') {
                 that._hasWaterMask = false;
             } else {
                 message = 'The tile format "' + data.format + '" is invalid or not supported.';
