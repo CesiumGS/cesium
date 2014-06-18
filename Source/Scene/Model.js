CesiumGS/cesium

commit 895987af3603429f73af4a7a31a82930ca44150d

Only set active uniforms.  Most useful for debugging since generated shaders SHOULD not have inactive uniforms

Source/Scene/Model.js
@@ -816,13 +816,15 @@ define([
     function createUniformMaps(model, context) {
         var loadResources = model._loadResources;
 
-        if (!loadResources.finishedTextureCreation()) {
+        if (!loadResources.finishedTextureCreation() || !loadResources.finishedProgramCreation()) {
             return;
         }
 
         var name;
-        var materials = model.gltf.materials;
-        var techniques = model.gltf.techniques;
+        var gltf = model.gltf;
+        var materials = gltf.materials;
+        var techniques = gltf.techniques;
+        var programs = gltf.programs;
 
         for (name in materials) {
             if (materials.hasOwnProperty(name)) {
@@ -835,6 +837,8 @@ define([
                 var uniforms = instanceProgram.uniforms;
 
                 var parameterValues = {};
+
+// TODO: handle parameter.source, e.g., light
 
                 // Uniform parameters for this pass
                 for (name in uniforms) {
@@ -856,16 +860,21 @@ define([
                 for (var i = 0; i < length; ++i) {
                     var instanceParam = instanceParameters[i];
                     var parameterValue = parameterValues[instanceParam.parameter];
-
                     parameterValue.func = gltfUniformFunctions[parameters[instanceParam.parameter].type](instanceParam.value, model, context);
                 }
+
+                var activeUniforms = programs[instanceProgram.program].czm.program.getAllUniforms();
 
                 // Create uniform map
                 var uniformMap = {};
                 for (name in parameterValues) {
                     if (parameterValues.hasOwnProperty(name)) {
                         var pv = parameterValues[name];
-                        uniformMap[pv.uniformName] = pv.func;
+
+                        // Only add active uniforms
+                        if (defined(activeUniforms[pv.uniformName])) {
+                            uniformMap[pv.uniformName] = pv.func;
+                        }
                     }
                 }
 

commit b21be5b4e152425896b92662b6a5746520999e8a

Fix subtle model picking bug introduced in 895987af3603429f73af4a7a31a82930ca44150d and exposed in IE 11. If the shader program for the color command had uniforms optimized out that were not also optimized out in the pick command, the uniform map for the pick command did not contain the uniforms.

Source/Scene/Model.js
@@ -1619,41 +1619,38 @@ define([
                 var uniformMap = {};
                 var uniformValues = {};
                 var jointMatrixUniformName;
-
+//
                 // Uniform parameters for this pass
                 for (var name in uniforms) {
                     if (uniforms.hasOwnProperty(name)) {
-                        // Only add active uniforms
-                        if (defined(activeUniforms[name])) {
-                            var parameterName = uniforms[name];
-                            var parameter = parameters[parameterName];
+                        var parameterName = uniforms[name];
+                        var parameter = parameters[parameterName];
 
-                            // GLTF_SPEC: In this implementation, material parameters with a
-                            // semantic or targeted via a source (for animation) are not
-                            // targetable for material animations.  Is this too strict?
-                            //
-                            // https://github.com/KhronosGroup/glTF/issues/142
+                        // GLTF_SPEC: In this implementation, material parameters with a
+                        // semantic or targeted via a source (for animation) are not
+                        // targetable for material animations.  Is this too strict?
+                        //
+                        // https://github.com/KhronosGroup/glTF/issues/142
 
-                            if (defined(instanceParameters[parameterName])) {
-                                // Parameter overrides by the instance technique
-                                var uv = gltfUniformFunctions[parameter.type](instanceParameters[parameterName], model);
-                                uniformMap[name] = uv.func;
-                                uniformValues[parameterName] = uv;
-                            } else if (defined(parameter.semantic)) {
-                                if (parameter.semantic !== 'JOINT_MATRIX') {
-                                    // Map glTF semantic to Cesium automatic uniform
-                                    uniformMap[name] = gltfSemanticUniforms[parameter.semantic](context.uniformState);
-                                } else {
-                                    jointMatrixUniformName = name;
-                                }
-                            } else if (defined(parameter.source)) {
-                                uniformMap[name] = getUniformFunctionFromSource(parameter.source, model);
-                            } else if (defined(parameter.value)) {
-                                // Technique value that isn't overridden by a material
-                                var uv2 = gltfUniformFunctions[parameter.type](parameter.value, model);
-                                uniformMap[name] = uv2.func;
-                                uniformValues[parameterName] = uv2;
+                        if (defined(instanceParameters[parameterName])) {
+                            // Parameter overrides by the instance technique
+                            var uv = gltfUniformFunctions[parameter.type](instanceParameters[parameterName], model);
+                            uniformMap[name] = uv.func;
+                            uniformValues[parameterName] = uv;
+                        } else if (defined(parameter.semantic)) {
+                            if (parameter.semantic !== 'JOINT_MATRIX') {
+                                // Map glTF semantic to Cesium automatic uniform
+                                uniformMap[name] = gltfSemanticUniforms[parameter.semantic](context.uniformState);
+                            } else {
+                                jointMatrixUniformName = name;
                             }
+                        } else if (defined(parameter.source)) {
+                            uniformMap[name] = getUniformFunctionFromSource(parameter.source, model);
+                        } else if (defined(parameter.value)) {
+                            // Technique value that isn't overridden by a material
+                            var uv2 = gltfUniformFunctions[parameter.type](parameter.value, model);
+                            uniformMap[name] = uv2.func;
+                            uniformValues[parameterName] = uv2;
                         }
                     }
                 }