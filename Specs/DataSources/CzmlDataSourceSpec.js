369adf4956b8ac4ebb587db164eabb61d2ac4d4b
Matthew Amato <matt.amato@gmail.com>
2014-07-10 11:37:39 -0400

    Allow CZML polylines and paths to use materials.
    We already allowed this via DataSources, but CZML itself had not yet been udpated.

--- Specs/DataSources/CzmlDataSourceSpec.js
+++ Specs/DataSources/CzmlDataSourceSpec.js
@@ -1387,15 +1387,19 @@ defineSuite([
     it('CZML adds data for infinite path.', function() {
         var pathPacket = {
             path : {
-                color : {
-                    rgbaf : [0.1, 0.1, 0.1, 0.1]
+                material : {
+                    polylineOutline : {
+                        color : {
+                            rgbaf : [0.1, 0.1, 0.1, 0.1]
+                        },
+                        outlineColor : {
+                            rgbaf : [0.2, 0.2, 0.2, 0.2]
+                        },
+                        outlineWidth : 1.0
+                    }
                 },
                 width : 1.0,
                 resolution : 23.0,
-                outlineColor : {
-                    rgbaf : [0.2, 0.2, 0.2, 0.2]
-                },
-                outlineWidth : 1.0,
                 leadTime : 2.0,
                 trailTime : 3.0,
                 show : true
@@ -1411,7 +1415,7 @@ defineSuite([
         expect(entity.path.width.getValue(Iso8601.MINIMUM_VALUE)).toEqual(pathPacket.path.width);
         expect(entity.path.resolution.getValue(Iso8601.MINIMUM_VALUE)).toEqual(pathPacket.path.resolution);
         expect(entity.path.material.outlineColor.getValue(Iso8601.MINIMUM_VALUE)).toEqual(new Color(0.2, 0.2, 0.2, 0.2));
-        expect(entity.path.material.outlineWidth.getValue(Iso8601.MINIMUM_VALUE)).toEqual(pathPacket.path.outlineWidth);
+        expect(entity.path.material.outlineWidth.getValue(Iso8601.MINIMUM_VALUE)).toEqual(1.0);
         expect(entity.path.leadTime.getValue(Iso8601.MINIMUM_VALUE)).toEqual(pathPacket.path.leadTime);
         expect(entity.path.trailTime.getValue(Iso8601.MINIMUM_VALUE)).toEqual(pathPacket.path.trailTime);
         expect(entity.path.show.getValue(Iso8601.MINIMUM_VALUE)).toEqual(true);
@@ -1421,15 +1425,19 @@ defineSuite([
         var pathPacket = {
             path : {
                 interval : '2000-01-01/2001-01-01',
-                color : {
-                    rgbaf : [0.1, 0.1, 0.1, 0.1]
+                material : {
+                    polylineOutline : {
+                        color : {
+                            rgbaf : [0.1, 0.1, 0.1, 0.1]
+                        },
+                        outlineColor : {
+                            rgbaf : [0.2, 0.2, 0.2, 0.2]
+                        },
+                        outlineWidth : 1.0
+                    }
                 },
                 width : 1.0,
                 resolution : 23.0,
-                outlineColor : {
-                    rgbaf : [0.2, 0.2, 0.2, 0.2]
-                },
-                outlineWidth : 1.0,
                 leadTime : 2.0,
                 trailTime : 3.0,
                 show : true
@@ -1453,7 +1461,7 @@ defineSuite([
         expect(entity.path.show.getValue(validTime)).toEqual(true);
         expect(entity.path.material.getValue(validTime).color).toEqual(new Color(0.1, 0.1, 0.1, 0.1));
         expect(entity.path.material.getValue(validTime).outlineColor).toEqual(new Color(0.2, 0.2, 0.2, 0.2));
-        expect(entity.path.material.getValue(validTime).outlineWidth).toEqual(pathPacket.path.outlineWidth);
+        expect(entity.path.material.getValue(validTime).outlineWidth).toEqual(1.0);
 
         expect(entity.path.material.getValue(invalidTime)).toBeUndefined();
         expect(entity.path.width.getValue(invalidTime)).toBeUndefined();
@@ -1594,14 +1602,18 @@ defineSuite([
     it('CZML adds data for infinite polyline.', function() {
         var polylinePacket = {
             polyline : {
-                color : {
-                    rgbaf : [0.1, 0.1, 0.1, 0.1]
+                material : {
+                    polylineOutline : {
+                        color : {
+                            rgbaf : [0.1, 0.1, 0.1, 0.1]
+                        },
+                        outlineColor : {
+                            rgbaf : [0.2, 0.2, 0.2, 0.2]
+                        },
+                        outlineWidth : 1.0
+                    }
                 },
                 width : 1.0,
-                outlineColor : {
-                    rgbaf : [0.2, 0.2, 0.2, 0.2]
-                },
-                outlineWidth : 1.0,
                 show : true
             }
         };
@@ -1614,7 +1626,7 @@ defineSuite([
         expect(entity.polyline.material.color.getValue(Iso8601.MINIMUM_VALUE)).toEqual(new Color(0.1, 0.1, 0.1, 0.1));
         expect(entity.polyline.width.getValue(Iso8601.MINIMUM_VALUE)).toEqual(polylinePacket.polyline.width);
         expect(entity.polyline.material.outlineColor.getValue(Iso8601.MINIMUM_VALUE)).toEqual(new Color(0.2, 0.2, 0.2, 0.2));
-        expect(entity.polyline.material.outlineWidth.getValue(Iso8601.MINIMUM_VALUE)).toEqual(polylinePacket.polyline.outlineWidth);
+        expect(entity.polyline.material.outlineWidth.getValue(Iso8601.MINIMUM_VALUE)).toEqual(1.0);
         expect(entity.polyline.show.getValue(Iso8601.MINIMUM_VALUE)).toEqual(true);
     });
 
@@ -1622,14 +1634,18 @@ defineSuite([
         var polylinePacket = {
             polyline : {
                 interval : '2000-01-01/2001-01-01',
-                color : {
-                    rgbaf : [0.1, 0.1, 0.1, 0.1]
+                material : {
+                    polylineOutline : {
+                        color : {
+                            rgbaf : [0.1, 0.1, 0.1, 0.1]
+                        },
+                        outlineColor : {
+                            rgbaf : [0.2, 0.2, 0.2, 0.2]
+                        },
+                        outlineWidth : 1.0
+                    }
                 },
                 width : 1.0,
-                outlineColor : {
-                    rgbaf : [0.2, 0.2, 0.2, 0.2]
-                },
-                outlineWidth : 1.0,
                 show : true
             }
         };
@@ -1647,7 +1663,7 @@ defineSuite([
         expect(entity.polyline.material.getValue(validTime).color).toEqual(new Color(0.1, 0.1, 0.1, 0.1));
         expect(entity.polyline.width.getValue(validTime)).toEqual(polylinePacket.polyline.width);
         expect(entity.polyline.material.getValue(validTime).outlineColor).toEqual(new Color(0.2, 0.2, 0.2, 0.2));
-        expect(entity.polyline.material.getValue(validTime).outlineWidth).toEqual(polylinePacket.polyline.outlineWidth);
+        expect(entity.polyline.material.getValue(validTime).outlineWidth).toEqual(1.0);
         expect(entity.polyline.show.getValue(validTime)).toEqual(true);
 
         expect(entity.polyline.material.getValue(invalidTime)).toBeUndefined();
