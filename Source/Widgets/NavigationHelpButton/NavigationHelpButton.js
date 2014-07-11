c034d79ba5e074fa08bd841a90a3e64b1b9ce3f1
hpinkos <pinkos.hannah@gmail.com>
2014-07-11 14:16:53 -0400

    touch icons, text

--- Source/Widgets/NavigationHelpButton/NavigationHelpButton.js
+++ Source/Widgets/NavigationHelpButton/NavigationHelpButton.js
@@ -108,26 +108,31 @@ cesiumSvgPath: { path: _svgPath, width: 32, height: 32 }');
         touchInstructions.innerHTML = '\
             <table>\
                 <tr>\
-                    <td><img src="' + buildModuleUrl('Widgets/Images/NavigationHelp/MouseLeft.svg') + '" width="48" height="48" /></td>\
+                    <td><img src="' + buildModuleUrl('Widgets/Images/NavigationHelp/TouchDrag.svg') + '" width="70" height="48" /></td>\
                     <td>\
                         <div class="cesium-navigation-help-pan">Pan view</div>\
-                        <div class="cesium-navigation-help-details">Touch stuff</div>\
+                        <div class="cesium-navigation-help-details">One finger drag</div>\
                     </td>\
                 </tr>\
                 <tr>\
-                    <td><img src="' + buildModuleUrl('Widgets/Images/NavigationHelp/MouseRight.svg') + '" width="48" height="48" /></td>\
+                    <td><img src="' + buildModuleUrl('Widgets/Images/NavigationHelp/TouchZoom.svg') + '" width="70" height="48" /></td>\
                     <td>\
                         <div class="cesium-navigation-help-zoom">Zoom view</div>\
-                        <div class="cesium-navigation-help-details">Touch stuff</div>\
-                        <div class="cesium-navigation-help-details">Touch stuff</div>\
+                        <div class="cesium-navigation-help-details">Two finger pinch</div>\
                     </td>\
                 </tr>\
                 <tr>\
-                    <td><img src="' + buildModuleUrl('Widgets/Images/NavigationHelp/MouseMiddle.svg') + '" width="48" height="48" /></td>\
+                    <td><img src="' + buildModuleUrl('Widgets/Images/NavigationHelp/TouchTilt.svg') + '" width="70" height="48" /></td>\
                     <td>\
-                        <div class="cesium-navigation-help-rotate">Rotate view</div>\
-                        <div class="cesium-navigation-help-details">Touch stuff or</div>\
-                        <div class="cesium-navigation-help-details">Touch stuff</div>\
+                        <div class="cesium-navigation-help-rotate">Tilt view</div>\
+                        <div class="cesium-navigation-help-details">Two finger drag, same direction</div>\
+                    </td>\
+                </tr>\
+                <tr>\
+                    <td><img src="' + buildModuleUrl('Widgets/Images/NavigationHelp/TouchRotate.svg') + '" width="70" height="48" /></td>\
+                    <td>\
+                        <div class="cesium-navigation-help-tilt">Rotate view</div>\
+                        <div class="cesium-navigation-help-details">Two finger drag, opposite direction</div>\
                     </td>\
                 </tr>\
             </table>';
