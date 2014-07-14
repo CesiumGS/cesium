5a12300cb6e76e8025b084e95705d1bcf1cf2c65
Patrick Cozzi <pjcozzi@gmail.com>
2014-07-14 16:19:02 -0400

    Reorder followSurface

--- Source/Core/PolylineGeometry.js
+++ Source/Core/PolylineGeometry.js
@@ -82,9 +82,9 @@ define([
      * @param {Number} [options.width=1.0] The width in pixels.
      * @param {Color[]} [options.colors] An Array of {@link Color} defining the per vertex or per segment colors.
      * @param {Boolean} [options.colorsPerVertex=false] A boolean that determines whether the colors will be flat across each segment of the line or interpolated across the vertices.
+     * @param {Boolean} [options.followSurface=true] A boolean that determines whether positions will be adjusted to the surface of the ellipsoid via a great arc.
      * @param {Number} [options.granularity=CesiumMath.RADIANS_PER_DEGREE] The distance, in radians, between each latitude and longitude if options.followSurface=true. Determines the number of positions in the buffer.
      * @param {Ellipsoid} [options.ellipsoid=Ellipsoid.WGS84] The ellipsoid to be used as a reference.
-     * @param {Boolean} [options.followSurface=true] A boolean that determines whether positions will be adjusted to the surface of the ellipsoid via a great arc.
      *
      * @exception {DeveloperError} At least two positions are required.
      * @exception {DeveloperError} width must be greater than or equal to one.
@@ -128,9 +128,9 @@ define([
         this._width = width;
         this._perVertex = perVertex;
         this._vertexFormat = defaultValue(options.vertexFormat, VertexFormat.DEFAULT);
+        this._followSurface = defaultValue(options.followSurface, true);
         this._granularity = defaultValue(options.granularity, CesiumMath.RADIANS_PER_DEGREE);
         this._ellipsoid = defaultValue(options.ellipsoid, Ellipsoid.WGS84);
-        this._followSurface = defaultValue(options.followSurface, true);
         this._workerName = 'createPolylineGeometry';
     };
 
@@ -149,9 +149,9 @@ define([
         var vertexFormat = polylineGeometry._vertexFormat;
         var colors = polylineGeometry._colors;
         var perVertex = polylineGeometry._perVertex;
+        var followSurface = polylineGeometry._followSurface;
         var granularity = polylineGeometry._granularity;
         var ellipsoid = polylineGeometry._ellipsoid;
-        var followSurface = polylineGeometry._followSurface;
 
         var i;
         var j;
