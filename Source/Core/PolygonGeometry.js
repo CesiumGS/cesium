7971a3f64d1505cdf6944bf7561178d3592b6f63
Pat Mahoney <pmahoney@agi.com>
2014-08-26 09:15:41 -0400

    Fix sample code base on feedback.

--- Source/Core/PolygonGeometry.js
+++ Source/Core/PolygonGeometry.js
@@ -544,7 +544,7 @@ define([
      *
      * // 3. create extruded polygon
      * var extrudedPolygon = new Cesium.PolygonGeometry({
-	 *   polygonHierarchy : {
+     *   polygonHierarchy : {
      *     positions : Cesium.Cartesian3.fromDegreesArray([
      *       -72.0, 40.0,
      *       -70.0, 35.0,
@@ -553,7 +553,7 @@ define([
      *       -68.0, 40.0
      *     ]),
      *     extrudedHeight: 300000
-	 *   } 
+     *   } 
      * });
      * var geometry = Cesium.PolygonGeometry.createGeometry(extrudedPolygon);
      */
