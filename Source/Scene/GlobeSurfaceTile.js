b32264507ea94e5ac19d595cf3603764b51c656a
Kevin Ring <kevin@kotachrome.com>
2014-10-24 16:25:41 +1100

    Pass stride in pickTriangle function, too.

--- Source/Scene/GlobeSurfaceTile.js
+++ Source/Scene/GlobeSurfaceTile.js
@@ -236,6 +236,7 @@ define([
         }
 
         var vertices = mesh.vertices;
+        var stride = mesh.stride;
         var indices = mesh.indices;
 
         var length = indices.length;
@@ -244,9 +245,9 @@ define([
             var i1 = indices[i + 1];
             var i2 = indices[i + 2];
 
-            var v0 = getPosition(this, scene, vertices, i0, scratchV0);
-            var v1 = getPosition(this, scene, vertices, i1, scratchV1);
-            var v2 = getPosition(this, scene, vertices, i2, scratchV2);
+            var v0 = getPosition(this, scene, vertices, stride, i0, scratchV0);
+            var v1 = getPosition(this, scene, vertices, stride, i1, scratchV1);
+            var v2 = getPosition(this, scene, vertices, stride, i2, scratchV2);
 
             var intersection = IntersectionTests.rayTriangle(ray, v0, v1, v2, cullBackFaces, scratchResult);
             if (defined(intersection)) {
