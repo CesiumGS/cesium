0f6ab04948aa6d9258c29dd73ee0aadfa7d53492
Matthew Amato <matt.amato@gmail.com>
2014-11-14 13:40:56 -0500

    Fix initial geometry outline color.

--- Source/DataSources/RectangleGeometryUpdater.js
+++ Source/DataSources/RectangleGeometryUpdater.js
@@ -356,13 +356,14 @@ define([
 
         var entity = this._entity;
         var isAvailable = entity.isAvailable(time);
+        var outlineColor = Property.getValueOrDefault(this._outlineColorProperty, time, Color.BLACK);
 
         return new GeometryInstance({
             id : entity,
             geometry : new RectangleOutlineGeometry(this._options),
             attributes : {
                 show : new ShowGeometryInstanceAttribute(isAvailable && this._showProperty.getValue(time) && this._showOutlineProperty.getValue(time)),
-                color : ColorGeometryInstanceAttribute.fromColor(isAvailable ? this._outlineColorProperty.getValue(time) : Color.BLACK)
+                color : ColorGeometryInstanceAttribute.fromColor(outlineColor)
             }
         });
     };
