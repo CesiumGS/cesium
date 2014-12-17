f08da9af204dbfecb60463bfeb084451d7191f98
Matthew Amato <matt.amato@gmail.com>
2014-12-16 23:45:53 -0500

    Add Entity support for box geometry
    Still needs cleanup and tests.

---
+++ Source/DataSources/BoxGraphics.js
@@ -0,0 +1,160 @@
+/*global define*/
+define([
+        '../Core/defaultValue',
+        '../Core/defined',
+        '../Core/defineProperties',
+        '../Core/DeveloperError',
+        '../Core/Event',
+        './createPropertyDescriptor'
+    ], function(
+        defaultValue,
+        defined,
+        defineProperties,
+        DeveloperError,
+        Event,
+        createPropertyDescriptor) {
+    "use strict";
+
+    /**
+     * An optionally time-dynamic box.
+     *
+     * @alias BoxGraphics
+     * @constructor
+     */
+    var BoxGraphics = function() {
+        this._minimumCorner = undefined;
+        this._minimumCornerSubscription = undefined;
+        this._maximumCorner = undefined;
+        this._maximumCornerSubscription = undefined;
+        this._show = undefined;
+        this._showSubscription = undefined;
+        this._fill = undefined;
+        this._fillSubscription = undefined;
+        this._material = undefined;
+        this._materialSubscription = undefined;
+        this._outline = undefined;
+        this._outlineSubscription = undefined;
+        this._outlineColor = undefined;
+        this._outlineColorSubscription = undefined;
+        this._outlineWidth = undefined;
+        this._outlineWidthSubscription = undefined;
+        this._definitionChanged = new Event();
+    };
+
+    defineProperties(BoxGraphics.prototype, {
+        /**
+         * Gets the event that is raised whenever a new property is assigned.
+         * @memberof BoxGraphics.prototype
+         *
+         * @type {Event}
+         * @readonly
+         */
+        definitionChanged : {
+            get : function() {
+                return this._definitionChanged;
+            }
+        },
+
+        /**
+         * Gets or sets the boolean {@link Property} specifying the box's visibility.
+         * @memberof BoxGraphics.prototype
+         * @type {Property}
+         */
+        show : createPropertyDescriptor('show'),
+
+        /**
+         * Gets or sets the {@link Cartesian3} specifying the box's minimum corner.
+         * @memberof BoxGraphics.prototype
+         * @type {Property}
+         */
+        minimumCorner : createPropertyDescriptor('minimumCorner'),
+
+        /**
+         * Gets or sets the {@link Cartesian3} specifying the box's maximum corner.
+         * @memberof BoxGraphics.prototype
+         * @type {Property}
+         */
+        maximumCorner : createPropertyDescriptor('maximumCorner'),
+
+        /**
+         * Gets or sets the {@link MaterialProperty} specifying the appearance of the box.
+         * @memberof BoxGraphics.prototype
+         * @type {MaterialProperty}
+         */
+        material : createPropertyDescriptor('material'),
+
+        /**
+         * Gets or sets the Boolean {@link Property} specifying whether the box should be filled.
+         * @memberof BoxGraphics.prototype
+         * @type {Property}
+         */
+        fill : createPropertyDescriptor('fill'),
+
+        /**
+         * Gets or sets the Boolean {@link Property} specifying whether the box should be outlined.
+         * @memberof BoxGraphics.prototype
+         * @type {Property}
+         */
+        outline : createPropertyDescriptor('outline'),
+
+        /**
+         * Gets or sets the Color {@link Property} specifying whether the color of the outline.
+         * @memberof BoxGraphics.prototype
+         * @type {Property}
+         */
+        outlineColor : createPropertyDescriptor('outlineColor'),
+
+        /**
+         * Gets or sets the Number {@link Property} specifying the width of the outline.
+         * @memberof BoxGraphics.prototype
+         * @type {Property}
+         */
+        outlineWidth : createPropertyDescriptor('outlineWidth')
+    });
+
+    /**
+     * Duplicates a BoxGraphics instance.
+     *
+     * @param {BoxGraphics} [result] The object onto which to store the result.
+     * @returns {BoxGraphics} The modified result parameter or a new instance if one was not provided.
+     */
+    BoxGraphics.prototype.clone = function(result) {
+        if (!defined(result)) {
+            result = new BoxGraphics();
+        }
+        result.minimumCorner = this.minimumCorner;
+        result.maximumCorner = this.maximumCorner;
+        result.show = this.show;
+        result.material = this.material;
+        result.fill = this.fill;
+        result.outline = this.outline;
+        result.outlineColor = this.outlineColor;
+        result.outlineWidth = this.outlineWidth;
+        return result;
+    };
+
+    /**
+     * Assigns each unassigned property on this object to the value
+     * of the same property on the provided source object.
+     *
+     * @param {BoxGraphics} source The object to be merged into this object.
+     */
+    BoxGraphics.prototype.merge = function(source) {
+        //>>includeStart('debug', pragmas.debug);
+        if (!defined(source)) {
+            throw new DeveloperError('source is required.');
+        }
+        //>>includeEnd('debug');
+
+        this.minimumCorner = defaultValue(this.minimumCorner, source.minimumCorner);
+        this.maximumCorner = defaultValue(this.maximumCorner, source.maximumCorner);
+        this.show = defaultValue(this.show, source.show);
+        this.material = defaultValue(this.material, source.material);
+        this.fill = defaultValue(this.fill, source.fill);
+        this.outline = defaultValue(this.outline, source.outline);
+        this.outlineColor = defaultValue(this.outlineColor, source.outlineColor);
+        this.outlineWidth = defaultValue(this.outlineWidth, source.outlineWidth);
+    };
+
+    return BoxGraphics;
+});
