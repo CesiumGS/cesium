5ce5b1ec4ba329f88ecada9be15f1f4da92ffe1d
Dan Bagnell <daniel.p.bagnell@gmail.com>
2015-01-21 14:00:33 -0500

    Tweak rotation order. Roll needs to be applied first or pitch and roll are the same rotation when the heading is 0.0 or 180.0 degrees.

--- Source/Scene/Camera.js
+++ Source/Scene/Camera.js
@@ -884,10 +884,6 @@ define([
         var pitch = scene2D ? -CesiumMath.PI_OVER_TWO : defaultValue(options.pitch, this.pitch);
         var roll = scene2D ? 0.0 : defaultValue(options.roll, this.roll);
 
-        if (sceneCV) {
-            roll = -roll;
-        }
-
         var cartesian = options.position;
         var cartographic = options.positionCartographic;
 
