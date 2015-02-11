a208f5289b39e644ace72959696333f1aca03bec
Alex Wood <abwood@gmail.com>
2015-02-11 17:33:29 -0500

    Scrubbed smallterrain from doc. Also removed smallterrain from sandcastle and baselayerpicker.

--- Source/Widgets/BaseLayerPicker/createDefaultTerrainProviderViewModels.js
+++ Source/Widgets/BaseLayerPicker/createDefaultTerrainProviderViewModels.js
@@ -32,19 +32,9 @@ define([
             tooltip : 'High-resolution, mesh-based terrain for the entire globe. Free for use on the Internet. Closed-network options are available.\nhttp://www.agi.com',
             creationFunction : function() {
                 return new CesiumTerrainProvider({
-                    url : '//cesiumjs.org/stk-terrain/tilesets/world/tiles'
-                });
-            }
-        }));
-
-        providerViewModels.push(new ProviderViewModel({
-            name : 'Small Terrain heightmaps with water',
-            iconUrl : buildModuleUrl('Widgets/Images/TerrainProviders/STK.png'),
-            tooltip : 'Medium-resolution, heightmap-based terrain for the entire globe. This tileset also includes a water mask. Free for use on the Internet.\nhttp://www.agi.com',
-            creationFunction : function() {
-                return new CesiumTerrainProvider({
-                    url : '//cesiumjs.org/smallterrain',
-                    credit : 'Terrain data courtesy Analytical Graphics, Inc.'
+                    url : '//cesiumjs.org/stk-terrain/world',
+                    requestWaterMask : true,
+                    requestVertexNormals : true
                 });
             }
         }));
