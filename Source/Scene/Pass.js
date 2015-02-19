fe4d6b89e779c357d6bdc00c46c6c28bc549ecc5
Dan Bagnell <daniel.p.bagnell@gmail.com>
2015-02-19 16:31:20 -0500

    Add comment about the order of passes.

--- Source/Scene/Pass.js
+++ Source/Scene/Pass.js
@@ -13,6 +13,9 @@ define([
     var Pass = {
         GLOBE : 0,
         OPAQUE : 1,
+        // Commands are executed in order by pass up to the translucent pass.
+        // Translucent geometry needs special handling (sorting/OIT). Overlays
+        // are also special (they're executed last, they're not sorted by frustum).
         TRANSLUCENT : 2,
         OVERLAY : 3,
         NUMBER_OF_PASSES : 4
