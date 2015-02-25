647fdd622e6e24e2b73644bda110ed395ac4925d
Matthew Amato <matt.amato@gmail.com>
2015-02-24 23:43:40 -0500

    Clean up CSS/HTML handling
    Work around astrange issue in Chrome with the frame height.

--- Source/Widgets/InfoBox/InfoBox.js
+++ Source/Widgets/InfoBox/InfoBox.js
@@ -69,16 +69,12 @@ click: function () { closeClicked.raiseEvent(this); }');
         closeElement.innerHTML = '&times;';
         infoElement.appendChild(closeElement);
 
-        var infoBodyElement = document.createElement('div');
-        infoBodyElement.className = 'cesium-infoBox-body';
-        infoElement.appendChild(infoBodyElement);
-
         var frame = document.createElement('iframe');
         frame.className = 'cesium-infoBox-iframe';
         frame.setAttribute('data-bind', 'style : { maxHeight : maxHeightOffset(40) }');
         frame.setAttribute('sandbox', 'allow-same-origin allow-popups allow-pointer-lock allow-forms'); // allow-scripts allow-top-navigation
         frame.setAttribute('allowfullscreen', true);
-        infoBodyElement.appendChild(frame);
+        infoElement.appendChild(frame);
 
         var viewModel = new InfoBoxViewModel();
         knockout.applyBindings(viewModel, infoElement);
@@ -112,8 +108,11 @@ click: function () { closeClicked.raiseEvent(this); }');
             that._processedDescriptionSubscription = subscribeAndEvaluate(viewModel, 'processedDescription', function(value) {
                 frameContent.innerHTML = value;
                 frame.style.height = frameContent.getBoundingClientRect().height + 'px';
-            });
 
+                //Chrome seems to get the height wrong above and requires us to meaure the body
+                //after setting the frame. Other browsers work without the below line.
+                frame.style.height = frameDocument.body.getBoundingClientRect().height + 'px';
+            });
         });
 
         //Chrome does not send the load event unless we explicitly set a src
