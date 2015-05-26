b65ab25d2bf0bc8adde00e73e2cff0196341441f
andre-nunes <andre.garcia.nunes@gmail.com>
2015-05-26 14:00:08 +0100

    Add empty GPX data source and spec

---
+++ Source/DataSources/GpxDataSource.js
@@ -0,0 +1,18 @@
+/*global define*/
+define(['../Core/DeveloperError'
+        ], function(
+                DeveloperError) {
+    "use strict";
+
+    /**
+     * A {@link DataSource} which processes GPX.
+     * @alias GpxDataSource
+     * @constructor
+     */
+    var GpxDataSource = function(){
+
+    };
+
+
+    return GpxDataSource;
+});
---
+++ Specs/DataSources/GpxDataSourceSpec.js
@@ -0,0 +1,10 @@
+/*global defineSuite*/
+defineSuite(['DataSources/GpxDataSource'
+            ], function(
+                    GpxDataSource) {
+    "use strict";
+    /*global jasmine,describe,xdescribe,it,xit,expect,beforeEach,afterEach,beforeAll,afterAll,spyOn,runs,waits,waitsFor*/
+
+    it('A new empty test', function() {
+    });
+});
