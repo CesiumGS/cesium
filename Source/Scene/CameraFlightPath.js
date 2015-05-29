4abb5edac13d4a8a7d9250ed56a91b4dd9c28a09
Dan Bagnell <daniel.p.bagnell@gmail.com>
2015-05-29 18:42:59 -0400

    New camera flights in 3D and Columbus view. Orientation is a WIP.

--- Source/Scene/CameraFlightPath.js
+++ Source/Scene/CameraFlightPath.js
@@ -91,144 +91,78 @@ define([
         return Math.max(dx, dy);
     }
 
-    var scratchCart = new Cartesian3();
-    var scratchCart2 = new Cartesian3();
-    var scratchCart3 = new Cartesian3();
-    var scratchCart4 = new Cartesian3();
-    var rotMatrixScratch = new Matrix3();
-    function createPath3D(camera, ellipsoid, start, up, right, end, duration) {
-        // get minimum altitude from which the whole ellipsoid is visible
-        var radius = ellipsoid.maximumRadius;
-        var frustum = camera.frustum;
-        var maxStartAlt = getAltitude(frustum, radius, radius);
+    var scratchStart = new Cartesian3();
 
-        var dot = Cartesian3.dot(Cartesian3.normalize(start, scratchCart), Cartesian3.normalize(end, scratchCart2));
+    function createUpdateCV(scene, duration, destination, heading, pitch, roll) {
+        var camera = scene.camera;
 
-        var points;
-        var altitude;
-        var incrementPercentage;
-        if (Cartesian3.magnitude(start) > maxStartAlt) {
-            altitude = radius + 0.6 * (maxStartAlt - radius);
-            incrementPercentage = 0.35;
-        } else {
-            var diff = Cartesian3.subtract(start, end, scratchCart);
-            altitude = Cartesian3.magnitude(Cartesian3.add(Cartesian3.multiplyByScalar(diff, 0.5, scratchCart2), end, scratchCart2));
-            var verticalDistance = Cartesian3.magnitude(Cartesian3.multiplyByScalar(up, Cartesian3.dot(diff, up), scratchCart2));
-            var horizontalDistance = Cartesian3.magnitude(Cartesian3.multiplyByScalar(right, Cartesian3.dot(diff, right), scratchCart2));
-            altitude += getAltitude(frustum, verticalDistance, horizontalDistance);
-            incrementPercentage = CesiumMath.clamp(dot + 1.0, 0.25, 0.5);
+        var start = Cartesian3.clone(camera.position, scratchStart);
+        var startHeading = camera.heading;
+        var startPitch = camera.pitch;
+        var startRoll = camera.roll;
+
+        if (destination.z <= 0.0) {
+            destination.z = start.z;
         }
 
-        var aboveEnd = Cartesian3.multiplyByScalar(Cartesian3.normalize(end, scratchCart2), altitude, scratchCart2);
-        var afterStart = Cartesian3.multiplyByScalar(Cartesian3.normalize(start, scratchCart), altitude, scratchCart);
+        var update = function(value) {
+            var time = value.time / duration;
 
-        var axis, angle, rotation;
-        var middle = new Cartesian3();
-        if (Cartesian3.magnitude(end) > maxStartAlt && dot > 0.75) {
-            middle = Cartesian3.add(Cartesian3.multiplyByScalar(Cartesian3.subtract(start, end, middle), 0.5, middle), end, middle);
-            points = [ start, middle, end ];
-        } else if (Cartesian3.magnitude(start) > maxStartAlt && dot > 0) {
-            middle = Cartesian3.add(Cartesian3.multiplyByScalar(Cartesian3.subtract(start, aboveEnd, middle), 0.5, middle), aboveEnd, middle);
-            points = [ start, middle, end ];
-        } else {
-            points = [ start ];
+            Cartesian3.lerp(start, destination, time, camera.position);
+        };
 
-            angle = CesiumMath.acosClamped(Cartesian3.dot(Cartesian3.normalize(afterStart, scratchCart3), Cartesian3.normalize(aboveEnd, scratchCart4)));
-            axis = Cartesian3.cross(aboveEnd, afterStart, scratchCart3);
-            if (Cartesian3.equalsEpsilon(axis, Cartesian3.ZERO, CesiumMath.EPSILON6)) {
-                axis = Cartesian3.UNIT_Z;
-            }
-
-            var increment = incrementPercentage * angle;
-            var startCondition = angle - increment;
-            for ( var i = startCondition; i > 0.0; i = i - increment) {
-                rotation = Matrix3.fromQuaternion(Quaternion.fromAxisAngle(axis, i), rotMatrixScratch);
-                points.push(Matrix3.multiplyByVector(rotation, aboveEnd, new Cartesian3()));
-            }
-
-            points.push(end);
-        }
-
-        var times = new Array(points.length);
-        var scalar = duration / (points.length - 1);
-        for ( var k = 0; k < points.length; ++k) {
-            times[k] = k * scalar;
-        }
-
-        return HermiteSpline.createNaturalCubic({
-            points : points,
-            times : times
-        });
-    }
-
-    var direction3D = new Cartesian3();
-    var right3D = new Cartesian3();
-    var up3D = new Cartesian3();
-    var quat3D = new Quaternion();
-
-    function createOrientations3D(path, startDirection, startUp, endDirection, endUp) {
-        var points = path.points;
-        var orientations = new Array(points.length);
-        orientations[0] = createQuaternion(startDirection, startUp);
-
-        var point;
-        var length = points.length - 1;
-        for (var i = 1; i < length; ++i) {
-            point = points[i];
-            Cartesian3.normalize(Cartesian3.negate(point, direction3D), direction3D);
-            Cartesian3.normalize(Cartesian3.cross(direction3D, Cartesian3.UNIT_Z, right3D), right3D);
-            Cartesian3.cross(right3D, direction3D, up3D);
-            orientations[i] = createQuaternion(direction3D, up3D, quat3D);
-        }
-
-        point = points[length];
-        if (defined(endDirection) && defined(endUp)) {
-            orientations[length] = createQuaternion(endDirection, endUp);
-        } else {
-            Cartesian3.normalize(Cartesian3.negate(point, direction3D), direction3D);
-            Cartesian3.normalize(Cartesian3.cross(direction3D, Cartesian3.UNIT_Z, right3D), right3D);
-            Cartesian3.cross(right3D, direction3D, up3D);
-            orientations[length] = createQuaternion(direction3D, up3D, quat3D);
-        }
-
-        return new QuaternionSpline({
-            points : orientations,
-            times : path.times
-        });
+        return update;
     }
 
     var scratchStartPosition = new Cartesian3();
-    var scratchStartDirection = new Cartesian3();
-    var scratchStartUp = new Cartesian3();
-    var scratchStartRight = new Cartesian3();
+    var scratchStartCart = new Cartographic();
+    var scratchEndCart = new Cartographic();
+    var scratchCurrentPositionCart = new Cartographic();
     var currentFrame = new Matrix4();
 
-    function createUpdate3D(scene, destination, duration, direction, up) {
+    function createUpdate3D(scene, duration, destination, heading, pitch, roll) {
         var camera = scene.camera;
-        var ellipsoid = scene.mapProjection.ellipsoid;
+        var projection = scene.mapProjection;
+        var ellipsoid = projection.ellipsoid;
 
-        var start = camera.cameraToWorldCoordinatesPoint(camera.position, scratchStartPosition);
-        var startDirection = camera.cameraToWorldCoordinatesVector(camera.direction, scratchStartDirection);
-        var startUp = camera.cameraToWorldCoordinatesVector(camera.up, scratchStartUp);
-        var startRight = Cartesian3.cross(startDirection, startUp, scratchStartRight);
+        var startCart = Cartographic.clone(camera.positionCartographic, scratchStartCart);
+        var startHeading = camera.heading;
+        var startPitch = camera.pitch;
+        var startRoll = camera.roll;
 
-        var path = createPath3D(camera, ellipsoid, start, startUp, startRight, destination, duration);
-        var orientations = createOrientations3D(path, startDirection, startUp, direction, up);
+        if (CesiumMath.equalsEpsilon(startHeading, CesiumMath.TWO_PI, CesiumMath.EPSILON11)) {
+            startHeading = 0.0;
+        }
+
+        var destCart = ellipsoid.cartesianToCartographic(destination, scratchEndCart);
+        if (destCart.height <= 0.0) {
+            destCart.height = startCart.height;
+        }
+
+        startCart.longitude = CesiumMath.zeroToTwoPi(startCart.longitude);
+        destCart.longitude = CesiumMath.zeroToTwoPi(destCart.longitude);
+
+        var diff = startCart.longitude - destCart.longitude;
+        if (diff < -CesiumMath.PI) {
+            startCart.longitude += CesiumMath.TWO_PI;
+        } else if (diff > CesiumMath.PI) {
+            destCart.longitude += CesiumMath.TWO_PI;
+        }
 
         var update = function(value) {
-            var time = value.time;
-            var orientation = orientations.evaluate(time);
-            Matrix3.fromQuaternion(orientation, rotMatrix);
+            var time = value.time / duration;
 
-            Matrix4.clone(camera.transform, currentFrame);
-            camera._setTransform(Matrix4.IDENTITY);
+            var position = scratchCurrentPositionCart;
+            position.longitude = CesiumMath.lerp(startCart.longitude, destCart.longitude, time);
+            position.latitude = CesiumMath.lerp(startCart.latitude, destCart.latitude, time);
+            position.height = CesiumMath.lerp(startCart.height, destCart.height, time);
 
-            camera.position = path.evaluate(time, camera.position);
-            camera.right = Matrix3.getRow(rotMatrix, 0, camera.right);
-            camera.up = Matrix3.getRow(rotMatrix, 1, camera.up);
-            camera.direction = Cartesian3.negate(Matrix3.getRow(rotMatrix, 2, camera.direction), camera.direction);
-
-            camera._setTransform(currentFrame);
+            camera.setView({
+                positionCartographic : position,
+                //heading : CesiumMath.lerp(startHeading, heading, time),
+                pitch : CesiumMath.lerp(startPitch, pitch, time),
+                //roll : CesiumMath.lerp(startRoll, roll, time)
+            });
         };
 
         return update;
@@ -327,32 +261,6 @@ define([
         });
     }
 
-    function createUpdateCV(scene, destination, duration, direction, up) {
-        var camera = scene.camera;
-        var ellipsoid = scene.mapProjection.ellipsoid;
-
-        var path = createPath2D(camera, ellipsoid, Cartesian3.clone(camera.position), destination, duration);
-        var orientations = createOrientations2D(camera, path, direction, up);
-
-        var update = function(value) {
-            var time = value.time;
-            var orientation = orientations.evaluate(time);
-            Matrix3.fromQuaternion(orientation, rotMatrix);
-
-            Matrix4.clone(camera.transform, currentFrame);
-            camera._setTransform(Matrix4.IDENTITY);
-
-            camera.position = path.evaluate(time, camera.position);
-            camera.right = Matrix3.getRow(rotMatrix, 0, camera.right);
-            camera.up = Matrix3.getRow(rotMatrix, 1, camera.up);
-            camera.direction = Cartesian3.negate(Matrix3.getRow(rotMatrix, 2, camera.direction), camera.direction);
-
-            camera._setTransform(currentFrame);
-        };
-
-        return update;
-    }
-
     function createUpdate2D(scene, destination, duration, direction, up) {
         var camera = scene.camera;
         var ellipsoid = scene.mapProjection.ellipsoid;
@@ -403,15 +311,15 @@ define([
         var direction = options.direction;
         var up = options.up;
 
+        var projection = scene.mapProjection;
+        var ellipsoid = projection.ellipsoid;
+
         //>>includeStart('debug', pragmas.debug);
         if (!defined(scene)) {
             throw new DeveloperError('scene is required.');
         }
         if (!defined(destination)) {
             throw new DeveloperError('destination is required.');
-        }
-        if ((defined(direction) && !defined(up)) || (defined(up) && !defined(direction))) {
-            throw new DeveloperError('If either direction or up is given, then both are required.');
         }
         //>>includeEnd('debug');
 
@@ -426,8 +334,6 @@ define([
         var convert = defaultValue(options.convert, true);
 
         if (convert && scene.mode !== SceneMode.SCENE3D) {
-            var projection = scene.mapProjection;
-            var ellipsoid = projection.ellipsoid;
             ellipsoid.cartesianToCartographic(destination, scratchCartographic);
             destination = projection.project(scratchCartographic, scratchDestination);
         }
@@ -528,18 +434,30 @@ define([
             };
         }
 
+        var heading;
+        var pitch;
+        var roll;
+
         var update;
         if (scene.mode === SceneMode.SCENE3D) {
-            update = createUpdate3D(scene, destination, duration, direction, up);
-        } else if (scene.mode === SceneMode.SCENE2D) {
-            update = createUpdate2D(scene, destination, duration, direction, up);
+            heading = defaultValue(heading, 0.0);
+            pitch = defaultValue(pitch, CesiumMath.toRadians(-89.9));//-CesiumMath.PI_OVER_TWO);
+            roll = defaultValue(roll, 0.0);
+
+            update = createUpdate3D(scene, duration, destination, heading, pitch, roll);
+        } else if (scene.mode === SceneMode.COLUMBUS_VIEW) {
+            heading = defaultValue(heading, 0.0);
+            pitch = defaultValue(pitch, -CesiumMath.PI_OVER_TWO);
+            roll = defaultValue(roll, 0.0);
+
+            update = createUpdateCV(scene, duration, destination, heading, pitch, roll);
         } else {
-            update = createUpdateCV(scene, destination, duration, direction, up);
+            update = createUpdate2D(scene, destination, duration, direction, up);
         }
 
         return {
             duration : duration,
-            easingFunction : EasingFunction.SINUSOIDAL_IN_OUT,
+            easingFunction : EasingFunction.QUINTIC_OUT,
             startObject : {
                 time : 0.0
             },
