690bca30720fbc1916b0bbc564df68df924a1838
Kevin Ring <kevin@kotachrome.com>
2015-05-29 14:12:21 +1000

    Simplify the code.

--- Source/Scene/Camera.js
+++ Source/Scene/Camera.js
@@ -1662,7 +1662,6 @@ define([
     var viewRectangle3DNorthWest = new Cartesian3();
     var viewRectangle3DSouthEast = new Cartesian3();
     var viewRectangle3DCenter = new Cartesian3();
-    var viewRectangle3DCenter2 = new Cartesian3();
     var defaultRF = {direction: new Cartesian3(), right: new Cartesian3(), up: new Cartesian3()};
     function rectangleCameraPosition3D (camera, rectangle, ellipsoid, result, positionOnly) {
         if (!defined(result)) {
@@ -1694,14 +1693,10 @@ define([
         cart.latitude = north;
         var northWest = ellipsoid.cartographicToCartesian(cart, viewRectangle3DNorthWest);
 
-        var center1 = Cartesian3.add(northEast, southWest, viewRectangle3DCenter);
-        Cartesian3.multiplyByScalar(center1, 0.5, center1);
-
-        var center2 = Cartesian3.add(northWest, southEast, viewRectangle3DCenter2);
-        Cartesian3.multiplyByScalar(center2, 0.5, center2);
-
-        var center = Cartesian3.add(center1, center2, viewRectangle3DCenter);
-        Cartesian3.multiplyByScalar(center, 0.5, center);
+        var center = Cartesian3.add(northEast, southWest, viewRectangle3DCenter);
+        center = Cartesian3.add(center, northWest, center);
+        center = Cartesian3.add(center, southEast, center);
+        Cartesian3.multiplyByScalar(center, 0.25, center);
 
         var mag = Cartesian3.magnitude(center);
         if (mag < CesiumMath.EPSILON6) {
