bbbcc48eb9c397dceb82639c79b9a66ca1d8944a
andre-nunes <andre.garcia.nunes@gmail.com>
2015-07-29 00:33:19 +0100

    Add tests

--- Specs/DataSources/GpxDataSourceSpec.js
+++ Specs/DataSources/GpxDataSourceSpec.js
@@ -1,15 +1,21 @@
 /*global defineSuite*/
 defineSuite(['DataSources/GpxDataSource',
+             'Core/Cartesian3',
              'DataSources/EntityCollection',
              'Core/loadXML',
-             'Core/Event'
+             'Core/Event',
+             'Core/Iso8601'
             ], function(
                     GpxDataSource,
+                    Cartesian3,
                     EntityCollection,
                     loadXML,
-                    Event) {
+                    Event,
+                    Iso8601) {
     "use strict";
     /*global jasmine,describe,xdescribe,it,xit,expect,beforeEach,afterEach,beforeAll,afterAll,spyOn,runs,waits,waitsFor*/
+
+    var parser = new DOMParser();
 
     it('default constructor has expected values', function() {
         var dataSource = new GpxDataSource();
@@ -34,6 +40,21 @@ defineSuite(['DataSources/GpxDataSource',
         return dataSource.load('Data/GPX/simple.gpx').then(function(source) {
             expect(source).toBe(dataSource);
             expect(source.entities.values.length).toEqual(1);
+        });
+    });
+
+    it('Waypoint: handles simple waypoint', function() {
+        var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+                <wpt lon="38.737125" lat="-9.139242">\
+                    <ele>0.0</ele>\
+                    <name>Position 1</name>\
+                </wpt>\
+            </gpx>';
+        return GpxDataSource.load(parser.parseFromString(gpx, "text/xml")).then(function(dataSource) {
+            var entities = dataSource.entities.values;
+            expect(entities.length).toEqual(1);
+            expect(entities[0].position.getValue(Iso8601.MINIMUM_VALUE)).toEqual(Cartesian3.fromDegrees(38.737125, -9.139242, undefined));
         });
     });
 
