de82e5d0f62f46a7e143513c476dccd5d84facf8
Aditya Raisinghani <aditya.ajeet@gmail.com>
2015-08-11 00:42:06 +0530

    Modify build tool to create new index file.

--- Tools/buildTasks/createGalleryList.js
+++ Tools/buildTasks/createGalleryList.js
@@ -7,23 +7,37 @@ load(project.getProperty('tasksDirectory') + '/shared.js'); /*global forEachFile
 
 var demos = [];
 var output = attributes.get('output');
-
 forEachFile('demos', function(relativePath, file) {
     "use strict";
 
     var demo = relativePath.substring(0, relativePath.lastIndexOf('.')).replace('\\', '/');
+    var demoFile = readFileContents(file);
+    var descriptionIndex = demoFile.indexOf('<meta name="description" ');
+    var i = descriptionIndex;
+    while(demoFile.charAt(i) != '>'){
+        i++;
+    }
+    var descriptionEnd = i;
+    var description = demoFile.substring(descriptionIndex+33, descriptionEnd);
+    var desc = description.substring(description.indexOf('\"')+1, description.lastIndexOf('\"'));
+    var labelIndex = demoFile.indexOf('<meta name="cesium-sandcastle-labels" ');
+    i = labelIndex;
+    while(demoFile.charAt(i) != '>'){
+        i++;
+    }
+    var labelEnd = i;
+    var label = demoFile.substring(labelIndex+46, labelEnd);
+    var labelText = label.substring(label.indexOf('\"')+1, label.lastIndexOf('\"'));
+    java.lang.System.out.println(labelText);
     var demoObject = {
         name : String(demo),
-        date : file.lastModified()
+        date : file.lastModified(),
+        description: desc,
+        labels: labelText
     };
-
-    if (new File(new File(output).getParent(), demo + '.jpg').exists()) {
-        demoObject.img = demo + '.jpg';
-    }
 
     demos.push(JSON.stringify(demoObject, null, 2));
 });
-
 var contents = '\
 // This file is automatically rebuilt by the Cesium build process.\n\
 var gallery_demos = [' + demos.join(', ') + '];';
