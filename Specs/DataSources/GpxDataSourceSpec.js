8c3867de5ad93849439fb8110685fc0e64e15a92
andre-nunes <andre.garcia.nunes@gmail.com>
2015-08-21 00:36:51 +0100

    Add tests

--- Specs/DataSources/GpxDataSourceSpec.js
+++ Specs/DataSources/GpxDataSourceSpec.js
@@ -1,23 +1,29 @@
 /*global defineSuite*/
 defineSuite(['DataSources/GpxDataSource',
              'Core/Cartesian3',
+             'Core/Color',
              'Core/DeveloperError',
              'Core/RequestErrorEvent',
              'Core/RuntimeError',
              'DataSources/EntityCollection',
              'Core/loadXML',
              'Core/Event',
-             'Core/Iso8601'
+             'Core/Iso8601',
+             'Core/JulianDate',
+             'Core/Math'
             ], function(
                     GpxDataSource,
                     Cartesian3,
+                    Color,
                     DeveloperError,
                     RequestErrorEvent,
                     RuntimeError,
                     EntityCollection,
                     loadXML,
                     Event,
-                    Iso8601) {
+                    Iso8601,
+                    JulianDate,
+                    CesiumMath) {
     "use strict";
     /*global jasmine,describe,xdescribe,it,xit,expect,beforeEach,afterEach,beforeAll,afterAll,spyOn,runs,waits,waitsFor*/
 
@@ -144,9 +150,9 @@ defineSuite(['DataSources/GpxDataSource',
         });
     });
 
-    it('Complex Type: name', function() {
+    it('Waypoint: sets name', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lat="1" lon="2">\
                     <name>Test</name>\
                 </wpt>\
@@ -162,7 +168,7 @@ defineSuite(['DataSources/GpxDataSource',
 
     it('Waypoint: throws with invalid coordinates', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lat="hello" lon="world">\
                 </wpt>\
             </gpx>';
@@ -173,7 +179,7 @@ defineSuite(['DataSources/GpxDataSource',
 
     it('Waypoint: throws when no coordinates are given', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt>\
                 </wpt>\
             </gpx>';
@@ -184,7 +190,7 @@ defineSuite(['DataSources/GpxDataSource',
 
     it('Waypoint: handles simple waypoint', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lon="38.737125" lat="-9.139242">\
                     <name>Position 1</name>\
                 </wpt>\
@@ -197,9 +203,26 @@ defineSuite(['DataSources/GpxDataSource',
         });
     });
 
+    it('Waypoint: uses default billboard style', function() {
+        var BILLBOARD_SIZE = 32;
+        var DEFAULT_ICON = '../../../Build/Cesium/Assets/Textures/maki/marker.png';
+        var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
+                <wpt lon="38.737125" lat="-9.139242">\
+                    <name>Position 1</name>\
+                </wpt>\
+            </gpx>';
+        return GpxDataSource.load(parser.parseFromString(gpx, "text/xml")).then(function(dataSource) {
+            var entities = dataSource.entities.values;
+            expect(entities[0].billboard.height.getValue()).toEqual(BILLBOARD_SIZE);
+            expect(entities[0].billboard.width.getValue()).toEqual(BILLBOARD_SIZE);
+            expect(entities[0].billboard.image.getValue()).toEqual(DEFAULT_ICON);
+        });
+    });
+
     it('Waypoint: handles simple waypoint with elevation', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lon="1" lat="2">\
                     <ele>3</ele>\
                     <name>Position 1</name>\
@@ -214,7 +237,7 @@ defineSuite(['DataSources/GpxDataSource',
 
     it('Waypoint: handles multiple waypoints', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lon="1" lat="2">\
                     <name>Position 1</name>\
                 </wpt>\
@@ -236,7 +259,7 @@ defineSuite(['DataSources/GpxDataSource',
 
     it('Description: handles desc', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lon="1" lat="2">\
                     <desc>The Description</desc>\
                 </wpt>\
@@ -257,7 +280,7 @@ defineSuite(['DataSources/GpxDataSource',
 
     it('Description: handles time', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lon="1" lat="2">\
                     <time>2015-08-17T00:06Z</time>\
                 </wpt>\
@@ -278,7 +301,7 @@ defineSuite(['DataSources/GpxDataSource',
 
     it('Description: handles comment', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lon="1" lat="2">\
                     <cmt>The comment</cmt>\
                 </wpt>\
@@ -299,7 +322,7 @@ defineSuite(['DataSources/GpxDataSource',
 
     it('Description: handles source', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lon="1" lat="2">\
                     <src>The source</src>\
                 </wpt>\
@@ -320,7 +343,7 @@ defineSuite(['DataSources/GpxDataSource',
 
     it('Description: handles gps number', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lon="1" lat="2">\
                     <number>The number</number>\
                 </wpt>\
@@ -341,7 +364,7 @@ defineSuite(['DataSources/GpxDataSource',
 
     it('Description: handles type', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lon="1" lat="2">\
                     <type>The type</type>\
                 </wpt>\
@@ -362,7 +385,7 @@ defineSuite(['DataSources/GpxDataSource',
 
     it('Description: handles multiple fields', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <wpt lon="1" lat="2">\
                     <cmt>The comment</cmt>\
                     <desc>The description</desc>\
@@ -383,27 +406,64 @@ defineSuite(['DataSources/GpxDataSource',
         });
     });
 
+    it('Description: handles route description', function() {
+        var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
+                <rte>\
+                    <cmt>The comment</cmt>\
+                    <desc>The description</desc>\
+                    <type>The type</type>\
+                    <rtept lon="1" lat="2">\
+                        <ele>1</ele>\
+                        <name>Position 1</name>\
+                    </rtept>\
+                    <rtept lon="3" lat="4">\
+                        <ele>1</ele>\
+                        <name>Position 2</name>\
+                    </rtept>\
+                    <rtept lon="5" lat="6">\
+                        <ele>1</ele>\
+                        <name>Position 3</name>\
+                    </rtept>\
+                    <rtept lon="7" lat="8">\
+                        <ele>1</ele>\
+                        <name>Position 4</name>\
+                    </rtept>\
+                </rte>\
+            </gpx>';
+        return GpxDataSource.load(parser.parseFromString(gpx, "text/xml")).then(function(dataSource) {
+            var entity = dataSource.entities.values[0];
 
+            var element = document.createElement('div');
+            element.innerHTML = entity.description.getValue();
+
+            var div = element.firstChild;
+            expect(div.style['word-wrap']).toEqual('break-word');
+            expect(div.style['background-color']).toEqual('rgb(255, 255, 255)');
+            expect(div.style.color).toEqual('rgb(0, 0, 0)');
+            expect(div.textContent).toEqual('Comment: The commentDescription: The descriptionType: The type');
+        });
+    });
 
     it('Route: handles simple route', function() {
         var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
-            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="RouteConverter">\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
                 <rte>\
                     <name>Test Route</name>\
-                    <rtept lon="9.860624216140083" lat="54.9328621088893">\
-                        <ele>0.0</ele>\
+                    <rtept lon="1" lat="2">\
+                        <ele>1</ele>\
                         <name>Position 1</name>\
                     </rtept>\
-                    <rtept lon="9.86092208681491" lat="54.93293237320851">\
-                        <ele>0.0</ele>\
+                    <rtept lon="3" lat="4">\
+                        <ele>1</ele>\
                         <name>Position 2</name>\
                     </rtept>\
-                    <rtept lon="9.86187816543752" lat="54.93327743521187">\
-                        <ele>0.0</ele>\
+                    <rtept lon="5" lat="6">\
+                        <ele>1</ele>\
                         <name>Position 3</name>\
                     </rtept>\
-                    <rtept lon="9.862439849679859" lat="54.93342326167919">\
-                        <ele>0.0</ele>\
+                    <rtept lon="7" lat="8">\
+                        <ele>1</ele>\
                         <name>Position 4</name>\
                     </rtept>\
                 </rte>\
@@ -411,7 +471,152 @@ defineSuite(['DataSources/GpxDataSource',
         return GpxDataSource.load(parser.parseFromString(gpx, "text/xml")).then(function(dataSource) {
             var entities = dataSource.entities.values;
             expect(entities.length).toEqual(5); //1 for the route and 4 routepoints
+            expect(entities[1].position.getValue(Iso8601.MINIMUM_VALUE)).toEqual(Cartesian3.fromDegrees(1, 2, 1));
+            expect(entities[2].position.getValue(Iso8601.MINIMUM_VALUE)).toEqual(Cartesian3.fromDegrees(3, 4, 1));
+            expect(entities[3].position.getValue(Iso8601.MINIMUM_VALUE)).toEqual(Cartesian3.fromDegrees(5, 6, 1));
+            expect(entities[4].position.getValue(Iso8601.MINIMUM_VALUE)).toEqual(Cartesian3.fromDegrees(7, 8, 1));
         });
     });
 
+    it('Track: handles simple track', function() {
+        var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
+                <trk>\
+                <name>Test Track</name>\
+                    <trkseg>\
+                        <trkpt lon="1" lat="2">\
+                            <ele>1.0</ele>\
+                            <name>Position 1</name>\
+                        </trkpt>\
+                        <trkpt lon="3" lat="4">\
+                            <ele>1.0</ele>\
+                            <name>Position 2</name>\
+                            </trkpt>\
+                    </trkseg>\
+                </trk>\
+            </gpx>';
+        return GpxDataSource.load(parser.parseFromString(gpx, "text/xml")).then(function(dataSource) {
+            var entities = dataSource.entities.values;
+            expect(entities.length).toEqual(1);
+
+            var entity = entities[0];
+            expect(entity.path).toBeUndefined();
+            expect(entity.polyline).toBeDefined();
+
+            var positions = entity.polyline.positions.getValue(Iso8601.MINIMUM_VALUE);
+            expect(positions).toEqual([Cartesian3.fromDegrees(1, 2, 1), Cartesian3.fromDegrees(3, 4, 1)]);
+        });
+    });
+
+    it('Track: uses default polyline style', function() {
+        var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
+                <trk>\
+                <name>Test Track</name>\
+                    <trkseg>\
+                        <trkpt lon="1" lat="2">\
+                            <ele>1.0</ele>\
+                            <name>Position 1</name>\
+                        </trkpt>\
+                        <trkpt lon="3" lat="4">\
+                            <ele>1.0</ele>\
+                            <name>Position 2</name>\
+                            </trkpt>\
+                    </trkseg>\
+                </trk>\
+            </gpx>';
+        return GpxDataSource.load(parser.parseFromString(gpx, "text/xml")).then(function(dataSource) {
+            var entities = dataSource.entities.values;
+            expect(entities.length).toEqual(1);
+
+            var entity = entities[0];
+            expect(entity.path).toBeUndefined();
+            expect(entity.polyline).toBeDefined();
+            expect(entity.polyline.width.getValue()).toEqual(4);
+            expect(entity.polyline.material.getValue()).toBeDefined();
+            expect(entity.polyline.material.color.getValue()).toEqual(Color.RED);
+            expect(entity.polyline.material.outlineWidth.getValue()).toEqual(2);
+            expect(entity.polyline.material.outlineColor.getValue()).toEqual(Color.BLACK);
+        });
+    });
+
+    it('Track: handles time-dynamic track', function() {
+        var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
+                <trk>\
+                <name>Test Track</name>\
+                    <trkseg>\
+                        <trkpt lon="1" lat="2">\
+                            <ele>1.0</ele>\
+                            <name>Position 1</name>\
+                            <time>2000-01-01T00:00:00Z</time>\
+                        </trkpt>\
+                        <trkpt lon="3" lat="4">\
+                            <ele>1.0</ele>\
+                            <name>Position 2</name>\
+                            <time>2000-01-01T00:00:01Z</time>\
+                        </trkpt>\
+                        <trkpt lon="5" lat="6">\
+                            <ele>1.0</ele>\
+                            <name>Position 3</name>\
+                            <time>2000-01-01T00:00:02Z</time>\
+                        </trkpt>\
+                    </trkseg>\
+                </trk>\
+            </gpx>';
+        return GpxDataSource.load(parser.parseFromString(gpx, "text/xml")).then(function(dataSource) {
+            var time1 = JulianDate.fromIso8601('2000-01-01T00:00:00Z');
+            var time2 = JulianDate.fromIso8601('2000-01-01T00:00:01Z');
+            var time3 = JulianDate.fromIso8601('2000-01-01T00:00:02Z');
+
+            var entity = dataSource.entities.values[0];
+            expect(entity.position.getValue(time1)).toEqual(Cartesian3.fromDegrees(1, 2, 1));
+            expect(entity.position.getValue(time2)).toEqual(Cartesian3.fromDegrees(3, 4, 1));
+            expect(entity.position.getValue(time3)).toEqual(Cartesian3.fromDegrees(5, 6, 1));
+            expect(entity.polyline).toBeUndefined();
+
+            expect(entity.availability.start).toEqual(time1);
+            expect(entity.availability.stop).toEqual(time3);
+        });
+    });
+
+    it('Track: time-dynamic uses default path style', function() {
+        var gpx = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\
+            <gpx xmlns="http://www.topografix.com/GPX/1/1" version="1.1" creator="andre">\
+                <trk>\
+                <name>Test Track</name>\
+                    <trkseg>\
+                        <trkpt lon="1" lat="2">\
+                            <ele>1.0</ele>\
+                            <name>Position 1</name>\
+                            <time>2000-01-01T00:00:00Z</time>\
+                        </trkpt>\
+                        <trkpt lon="3" lat="4">\
+                            <ele>1.0</ele>\
+                            <name>Position 2</name>\
+                            <time>2000-01-01T00:00:01Z</time>\
+                        </trkpt>\
+                        <trkpt lon="5" lat="6">\
+                            <ele>1.0</ele>\
+                            <name>Position 3</name>\
+                            <time>2000-01-01T00:00:02Z</time>\
+                        </trkpt>\
+                    </trkseg>\
+                </trk>\
+            </gpx>';
+        return GpxDataSource.load(parser.parseFromString(gpx, "text/xml")).then(function(dataSource) {
+            var entities = dataSource.entities.values;
+            expect(entities.length).toEqual(1);
+
+            var entity = entities[0];
+            expect(entity.polyline).toBeUndefined();
+            expect(entity.path).toBeDefined();
+            expect(entity.path.leadTime.getValue()).toEqual(0);
+            expect(entity.path.width.getValue()).toEqual(4);
+            expect(entity.path.material.getValue()).toBeDefined();
+            expect(entity.path.material.color.getValue()).toEqual(Color.RED);
+            expect(entity.path.material.outlineWidth.getValue()).toEqual(2);
+            expect(entity.path.material.outlineColor.getValue()).toEqual(Color.BLACK);
+        });
+    });
 });
