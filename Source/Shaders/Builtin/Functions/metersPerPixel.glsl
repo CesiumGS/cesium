926ecf4831c9ccd40091dd225b74de6de837421a
Patrick Cozzi <pjcozzi@gmail.com>
2015-09-24 10:28:38 -0400

    Fix whitespace

--- Source/Shaders/Builtin/Functions/metersPerPixel.glsl
+++ Source/Shaders/Builtin/Functions/metersPerPixel.glsl
@@ -12,23 +12,24 @@ float czm_metersPerPixel(vec4 positionEC)
 {
     float width = czm_viewport.z;
     float height = czm_viewport.w;
-    float pixelWidth, pixelHeight;
+    float pixelWidth;
+    float pixelHeight;
     
     if (czm_sceneMode == czm_sceneMode2D)
     {
-	    float frustumWidth = czm_frustumRight - czm_frustumLeft;
-	    float frustumHeight = czm_frustumTop - czm_frustumBottom;
-	    pixelWidth = frustumWidth / width;
-	    pixelHeight = frustumHeight / height;
+        float frustumWidth = czm_frustumRight - czm_frustumLeft;
+        float frustumHeight = czm_frustumTop - czm_frustumBottom;
+        pixelWidth = frustumWidth / width;
+        pixelHeight = frustumHeight / height;
     }
     else
     {
         float distanceToPixel = -positionEC.z;
-	    float inverseNear = 1.0 / czm_currentFrustum.x;
-	    float tanTheta = czm_frustumTop * inverseNear;
-	    pixelHeight = 2.0 * distanceToPixel * tanTheta / height;
-	    tanTheta = czm_frustumRight * inverseNear;
-	    pixelWidth = 2.0 * distanceToPixel * tanTheta / width;
+        float inverseNear = 1.0 / czm_currentFrustum.x;
+        float tanTheta = czm_frustumTop * inverseNear;
+        pixelHeight = 2.0 * distanceToPixel * tanTheta / height;
+        tanTheta = czm_frustumRight * inverseNear;
+        pixelWidth = 2.0 * distanceToPixel * tanTheta / width;
     }
 
     return max(pixelWidth, pixelHeight);
