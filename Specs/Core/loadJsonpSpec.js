c51e84a2ea47944af7ee3758e45831a4562c086a
ggetz <ggetz881@gmail.com>
2015-10-15 18:10:39 -0400

    Updated unit test

--- Specs/Core/loadJsonpSpec.js
+++ Specs/Core/loadJsonpSpec.js
@@ -1,9 +1,11 @@
 /*global defineSuite*/
 defineSuite([
     'Core/loadJsonp',
+    'Core/DefaultProxy',
     'Core/RequestErrorEvent'
 ], function(
     loadJsonp,
+    DefaultProxy,
     RequestErrorEvent) {
     "use strict";
     /*global jasmine,describe,xdescribe,it,xit,expect,beforeEach,afterEach,beforeAll,afterAll,spyOn*/
@@ -16,25 +18,57 @@ defineSuite([
 
     it('returns a promise that resolves when the request loads', function() {
         var testUrl = 'http://example.invalid/testuri';
-        var promise = loadJsonp(testUrl).then(function(data){
-            expect(data).toEqual({
-                good : 'data'
-            });
-        }).otherwise(function(error) {
-
+        spyOn(loadJsonp, 'loadAndExecuteScript').and.callFake(function (url, name, deffered){
+            expect(url).toContain(testUrl);
+            expect(name).toContain('loadJsonp');
+            expect(deffered).toBeDefined();
         });
-
-        expect(promise).toBeDefined();
+        loadJsonp(testUrl);
     });
 
     it('returns a promise that rejects when the request errors', function() {
         var testUrl = 'http://example.invalid/testuri';
-        var promise = loadJsonp(testUrl).then(function(data){
-
-        }).otherwise(function(error) {
+        return loadJsonp(testUrl).otherwise(function(error) {
             expect(error).toBeDefined();
         });
-
-        expect(promise).toBeDefined();
     });
+
+    it('Appends parameters specified in options', function() {
+        var testUrl = 'test';
+        var options = {
+            parameters: {
+                isTest : 'true',
+                myNum : 8
+            }
+        }
+        spyOn(loadJsonp, 'loadAndExecuteScript').and.callFake(function(url, functionName, deferred) {
+            expect(url).toContain('isTest=true&myNum=8');
+        });
+        loadJsonp(testUrl, options);
+    });
+
+    it('Uses callback name specified in options', function() {
+        var testUrl = 'test';
+        var options = {
+            callbackParameterName : 'testCallback'
+        }
+        spyOn(loadJsonp, 'loadAndExecuteScript').and.callFake(function(url, functionName, deferred) {
+            expect(url).toContain('testCallback=loadJsonp');
+        });
+        loadJsonp(testUrl, options);
+    });
+
+    it('Uses proxy url is proxy is specified', function() {
+        var testUrl = 'test';
+        var testProxy = '/proxy/';
+        var options = {
+            proxy: new DefaultProxy(testProxy)
+        }
+        spyOn(loadJsonp, 'loadAndExecuteScript').and.callFake(function(url, functionName, deferred) {
+            expect(url).toStartWith(testProxy + '?' + testUrl);
+        });
+        loadJsonp(testUrl, options);
+    });
+
+
 });
