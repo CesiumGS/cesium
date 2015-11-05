4dd9b3d061849e12eb75d3477a922ed27970ae39
Matthew Amato <matt.amato@gmail.com>
2015-11-04 21:53:38 -0500

    All Entity.parent to be set to undefined.
    This should have always worked, but we were missing a `defined` check.
    
    Reported via the [forum](https://groups.google.com/d/msg/cesium-dev/danz-dtw8Jg/Id6DBZwaEQAJ)

--- Source/DataSources/Entity.js
+++ Source/DataSources/Entity.js
@@ -297,7 +297,9 @@ define([
                 }
 
                 this._parent = value;
-                value._children.push(this);
+                if (defined(value)) {
+                    value._children.push(this);
+                }
 
                 var isShowing = this.isShowing;
 
