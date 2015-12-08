cb4eaf4cf822a70022fe5aaf0d9a891645bf7828
Sean Lilley <lilleyse@gmail.com>
2015-12-08 13:27:56 -0500

    ContentProvider spec

---
+++ Specs/Scene/Cesium3DTileContentProviderSpec.js
@@ -0,0 +1,26 @@
+/*global defineSuite*/
+defineSuite([
+        'Scene/Cesium3DTileContentProvider'
+    ], function(
+        Cesium3DTileContentProvider) {
+    "use strict";
+
+    it('throws', function() {
+        var content = new Cesium3DTileContentProvider();
+        expect(function() {
+            content.request();
+        }).toThrowDeveloperError();
+        expect(function() {
+            content.initialize();
+        }).toThrowDeveloperError();
+        expect(function() {
+            content.update();
+        }).toThrowDeveloperError();
+        expect(function() {
+            content.isDestroyed();
+        }).toThrowDeveloperError();
+        expect(function() {
+            content.destroy();
+        }).toThrowDeveloperError();
+    });
+});
