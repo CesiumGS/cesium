c9f826381352cebae97b1e12a807de4ebc669266
Dan Bagnell <daniel.p.bagnell@gmail.com>
2015-12-09 17:44:36 -0500

    Fix tests.

--- Source/Renderer/Context.js
+++ Source/Renderer/Context.js
@@ -357,6 +357,9 @@ define([
         var ps = new PassState(this);
         var rs = RenderState.fromCache();
 
+        // default to the whole drawing buffer
+        ps.viewport = undefined;
+
         this._defaultPassState = ps;
         this._defaultRenderState = rs;
         this._defaultTexture = undefined;
