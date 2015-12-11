1253098984a74651cbbbc588e5fdb58318092a57
Erik Andersson <erik.andersson@vricon.com>
2015-12-11 09:13:30 +0100

    added comments and developer exceptions

--- Source/Core/BoundingSphere.js
+++ Source/Core/BoundingSphere.js
@@ -1109,7 +1109,23 @@ define([
                 left.radius === right.radius);
     };
 
+    /**
+     * Computes the distance between a sphere and the camera attached to frameState.
+     * @param {BoundingSphere} [sphere] The bounding sphere.
+     * @param {FrameState} [frameState] The frameState to which the camera is attached.
+     * @returns {Number} The distance between the camera and the bounding sphere in meters. Returns 0 if the camera is inside the bounding volume.
+     *
+     * @private
+     */
     BoundingSphere.distanceToCamera = function(sphere, frameState) {
+        //>>includeStart('debug', pragmas.debug);
+        if (!defined(sphere)) {
+            throw new DeveloperError('sphere is required.');
+        }
+        if (!defined(frameState)) {
+            throw new DeveloperError('sphere is required.');
+        }
+        //>>includeEnd('debug');
         return Math.max(0.0, Cartesian3.distance(sphere.center, frameState.camera.positionWC) - sphere.radius);
     };
 
@@ -1190,6 +1206,11 @@ define([
     };
 
 
+    /**
+     * Computes the distance between this sphere and the camera attached to frameState.
+     * @param {FrameState} [frameState] The frameState to which the camera is attached.
+     * @returns {Number} The distance between the camera and the bounding sphere in meters. Returns 0 if the camera is inside the bounding volume.
+     */
     BoundingSphere.prototype.distanceToCamera = function(frameState) {
         return BoundingSphere.distanceToCamera(this, frameState);
     };
