897814ae407cb6e8af16e16e374c31f5ec687d40
Amay Agrawal <amayagrawal22@gmail.com>
2015-12-24 03:10:32 +0530

    Update PolylineVolumeOutlineGeometry.js
    @function not needed here

--- Source/Core/PolylineVolumeOutlineGeometry.js
+++ Source/Core/PolylineVolumeOutlineGeometry.js
@@ -163,7 +163,6 @@ define([
 
     /**
      * Stores the provided instance into the provided array.
-     * @function
      *
      * @param {PolylineVolumeOutlineGeometry} value The value to pack.
      * @param {Number[]} array The array to pack into.
