edafaa767fabc57220f70b9ecec5156aeda7f92e
Amay Agrawal <amayagrawal22@gmail.com>
2015-12-24 02:40:53 +0530

    Update CircleOutlineGeometry.js
    @function not needed here

--- Source/Core/CircleOutlineGeometry.js
+++ Source/Core/CircleOutlineGeometry.js
@@ -81,7 +81,6 @@ define([
 
     /**
      * Stores the provided instance into the provided array.
-     * @function
      *
      * @param {CircleOutlineGeometry} value The value to pack.
      * @param {Number[]} array The array to pack into.
