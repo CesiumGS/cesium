546f9bd799c469d43a5d5b3d871652b2b01fe894
Amay Agrawal <amayagrawal22@gmail.com>
2015-12-24 03:14:00 +0530

    Update SphereGeometry.js
    @function not needed here

--- Source/Core/SphereGeometry.js
+++ Source/Core/SphereGeometry.js
@@ -63,7 +63,6 @@ define([
 
     /**
      * Stores the provided instance into the provided array.
-     * @function
      *
      * @param {SphereGeometry} value The value to pack.
      * @param {Number[]} array The array to pack into.
