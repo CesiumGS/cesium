48a949748af3a91d80484d6b519a9da0914946dc
Amay Agrawal <amayagrawal22@gmail.com>
2015-12-30 01:27:10 +0530

    Update loadText.js
    @see after @example

--- Source/Core/loadText.js
+++ Source/Core/loadText.js
@@ -17,9 +17,6 @@ define([
      * @param {Object} [headers] HTTP headers to send with the request.
      * @returns {Promise.<String>} a promise that will resolve to the requested data when loaded.
      *
-     * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest|XMLHttpRequest}
-     * @see {@link http://www.w3.org/TR/cors/|Cross-Origin Resource Sharing}
-     * @see {@link http://wiki.commonjs.org/wiki/Promises/A|CommonJS Promises/A}
      *
      * @example
      * // load text from a URL, setting a custom header
@@ -30,6 +27,9 @@ define([
      * }).otherwise(function(error) {
      *     // an error occurred
      * });
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest|XMLHttpRequest}
+     * @see {@link http://www.w3.org/TR/cors/|Cross-Origin Resource Sharing}
+     * @see {@link http://wiki.commonjs.org/wiki/Promises/A|CommonJS Promises/A}
      */
     function loadText(url, headers) {
         return loadWithXhr({
