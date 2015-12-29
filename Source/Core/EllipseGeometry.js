22f8f5034227fb14cf6a3a45fd127398eb75d687
Amay Agrawal <amayagrawal22@gmail.com>
2015-12-30 00:47:11 +0530

    Update EllipseGeometry.js
    @see after @example

--- Source/Core/EllipseGeometry.js
+++ Source/Core/EllipseGeometry.js
@@ -629,7 +629,6 @@ define([
      * @exception {DeveloperError} semiMajorAxis must be greater than or equal to the semiMinorAxis.
      * @exception {DeveloperError} granularity must be greater than zero.
      *
-     * @see EllipseGeometry.createGeometry
      *
      * @example
      * // Create an ellipse.
@@ -640,6 +639,7 @@ define([
      *   rotation : Cesium.Math.toRadians(60.0)
      * });
      * var geometry = Cesium.EllipseGeometry.createGeometry(ellipse);
+     * @see EllipseGeometry.createGeometry
      */
     function EllipseGeometry(options) {
         options = defaultValue(options, defaultValue.EMPTY_OBJECT);
