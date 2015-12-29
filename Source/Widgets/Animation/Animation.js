a5fd50e32a5fb038b71bc85baf018b3922b04222
Amay Agrawal <amayagrawal22@gmail.com>
2015-12-30 05:27:56 +0530

    Update Animation.js
    @see after @example

--- Source/Widgets/Animation/Animation.js
+++ Source/Widgets/Animation/Animation.js
@@ -312,8 +312,6 @@ define([
      *
      * @exception {DeveloperError} Element with id "container" does not exist in the document.
      *
-     * @see AnimationViewModel
-     * @see Clock
      *
      * @example
      * // In HTML head, include a link to Animation.css stylesheet,
@@ -329,6 +327,8 @@ define([
      *     Cesium.requestAnimationFrame(tick);
      * }
      * Cesium.requestAnimationFrame(tick);
+     * @see AnimationViewModel
+     * @see Clock
      */
     function Animation(container, viewModel) {
         //>>includeStart('debug', pragmas.debug);
