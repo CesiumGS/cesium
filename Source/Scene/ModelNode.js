851dfe311e32bf240c7603ce7294c6cd9090e148
Amay Agrawal <amayagrawal22@gmail.com>
2016-01-01 04:10:39 +0530

    Update ModelNode.js
    @see after @example

--- Source/Scene/ModelNode.js
+++ Source/Scene/ModelNode.js
@@ -25,6 +25,7 @@ define([
      * @example
      * var node = model.getNode('LOD3sp');
      * node.matrix = Cesium.Matrix4.fromScale(new Cesium.Cartesian3(5.0, 1.0, 1.0), node.matrix);
+     * 
      * @see Model#getNode
      */
     function ModelNode(model, node, runtimeNode, id, matrix) {
