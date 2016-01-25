d975882518b8b86b15b5dbf557e308ad4434b6d5
Amay Agrawal <amayagrawal22@gmail.com>
2016-01-26 03:43:34 +0530

    Update Geometry.js

--- Source/Core/Geometry.js
+++ Source/Core/Geometry.js
@@ -40,8 +40,6 @@ define([
      * @see BoxGeometry
      * @see EllipsoidGeometry
      *
-     * @demo {@link http://cesiumjs.org/Cesium/Apps/Sandcastle/index.html?src=Geometry%20and%20Appearances.html|Geometry and Appearances Demo}
-     *
      * @example
      * // Create geometry with a position attribute and indexed lines.
      * var positions = new Float64Array([
