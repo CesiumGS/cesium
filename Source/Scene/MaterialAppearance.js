c88c341b1f4e10cb99bc95a62bc381cc1ac1f589
Amay Agrawal <amayagrawal22@gmail.com>
2016-01-26 03:52:31 +0530

    Update MaterialAppearance.js

--- Source/Scene/MaterialAppearance.js
+++ Source/Scene/MaterialAppearance.js
@@ -48,7 +48,6 @@ define([
      * @param {RenderState} [options.renderState] Optional render state to override the default render state.
      *
      * @see {@link https://github.com/AnalyticalGraphicsInc/cesium/wiki/Fabric|Fabric}
-     * @demo {@link http://cesiumjs.org/Cesium/Apps/Sandcastle/index.html?src=Materials.html|Cesium Sandcastle Material Appearance Demo}
      *
      * @example
      * var primitive = new Cesium.Primitive({
