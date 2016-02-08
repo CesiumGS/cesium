535e63445dda24d227f6f79c69226f1c969d2939
Patrick Cozzi <pjcozzi@gmail.com>
2016-02-08 11:33:17 -0500

    Allow property and literal operands on both sides of an expression

--- Source/Scene/BooleanExpression.js
+++ Source/Scene/BooleanExpression.js
@@ -1,7 +1,9 @@
 /*global define*/
 define([
+       '../Core/defined',
        '../Core/defineProperties'
     ], function(
+        defined,
         defineProperties) {
     "use strict";
 
@@ -10,10 +12,11 @@ define([
     /**
      * DOC_TBA
      * <p>
-     * Creates an expression of the form: [propertyName] [operator] [operand].  For example:
-     *     Height > 100
-     *     id !== 10
-     *     state === 'Pennsylvania'
+     * Creates an expression of the form: [propertyName|literal] [operator] [propertyName|literal].  For example:
+     *     {Height} > 100
+     *     {id} !== 10
+     *     {Height} === {id}
+     *     {state} === 'Pennsylvania'
      * </p>
      * <p>
      * Do not construct this directly; instead use {@link Cesium3DTileStyle}.
@@ -22,9 +25,15 @@ define([
     function BooleanExpression(styleEngine, jsonExpression) {
         this._styleEngine = styleEngine;
 
-        this._propertyName = jsonExpression.propertyName;
+        var leftOperand = jsonExpression.leftOperand;
+        this._leftOperand = leftOperand;
+        this._leftPropertyName = getPropertyName(leftOperand);  // Cached property name if left operand is a property
+
         this._operator = jsonExpression.operator;
-        this._operand = jsonExpression.operand;
+
+        var rightOperand = jsonExpression.rightOperand;
+        this._rightOperand = rightOperand;
+        this._rightPropertyName = getPropertyName(rightOperand);  // Cached property name if right operand is a property
 
         /**
          * DOC_TBA
@@ -40,13 +49,15 @@ define([
         /**
          * DOC_TBA
          */
-        propertyName : {
+        leftOperand : {
             get : function() {
-                return this._propertyName;
+                return this._leftOperand;
             },
             set : function(value) {
-                if (this._propertyName !== value) {
-                    this._propertyName = value;
+                if (this._leftOperand !== value) {
+                    this._leftOperand = value;
+                    this._leftPropertyName = getPropertyName(value);
+                    setEvaluateFunction(this);
                     this._styleEngine.makeDirty();
                 }
             }
@@ -71,44 +82,117 @@ define([
         /**
          * DOC_TBA
          */
-        operand : {
+        rightOperand : {
             get : function() {
-                return this._operand;
+                return this._rightOperand;
             },
             set : function(value) {
-                if (this._operand !== value) {
-                    this._operand = value;
+                if (this._rightOperand !== value) {
+                    this._rightOperand = value;
+                    this._rightPropertyName = getPropertyName(value);
+                    setEvaluateFunction(this);
                     this._styleEngine.makeDirty();
                 }
             }
         }
     });
 
-    function setEvaluateFunction(booleanExpression) {
+    function getPropertyName(operand) {
+        if (typeof operand !== 'string') {
+            return undefined;
+        }
+
+        if (operand.length < 2) {
+            return undefined;
+        }
+
+        // Strings that should be substituted with a property value are enclosed in {}.
+        if ((operand.charAt(0) !== '{') || (operand.charAt(operand.length - 1) !== '}')) {
+            return undefined;
+        }
+
+        return operand.substring(1, operand.length - 1);
+    }
+
+    function setEvaluateFunction(expression) {
         var evaluate;
-        var operator = booleanExpression._operator;
+        var operator = expression._operator;
+
+        var leftOperandIsProperty = defined(expression._leftPropertyName);
+        var leftOperandIsLiteral = !leftOperandIsProperty;
+
+        var rightOperandIsProperty = defined(expression._rightPropertyName);
+        var rightOperandIsLiteral = !rightOperandIsProperty;
 
         if (operator === 'true') {
-            evaluate = booleanExpression._true;
+            evaluate = expression._true;
         } else if (operator === 'false') {
-            evaluate = booleanExpression._false;
+            evaluate = expression._false;
         } else if (operator === '>') {
-            evaluate = booleanExpression._greater;
+            if (leftOperandIsLiteral && rightOperandIsLiteral) {
+                evaluate = expression._greaterLiteralLiteral;
+            } else if (leftOperandIsLiteral && rightOperandIsProperty) {
+                evaluate = expression._greaterLiteralProperty;
+            } else if (leftOperandIsProperty && rightOperandIsLiteral) {
+                evaluate = expression._greaterPropertyLiteral;
+            } else {
+                evaluate = expression._greaterPropertyProperty;
+            }
         } else if (operator === '>=') {
-            evaluate = booleanExpression._greaterEqual;
+            if (leftOperandIsLiteral && rightOperandIsLiteral) {
+                evaluate = expression._greaterEqualLiteralLiteral;
+            } else if (leftOperandIsLiteral && rightOperandIsProperty) {
+                evaluate = expression._greaterEqualLiteralProperty;
+            } else if (leftOperandIsProperty && rightOperandIsLiteral) {
+                evaluate = expression._greaterEqualPropertyLiteral;
+            } else {
+                evaluate = expression._greaterEqualPropertyProperty;
+            }
         } else if (operator === '<') {
-            evaluate = booleanExpression._less;
+            if (leftOperandIsLiteral && rightOperandIsLiteral) {
+                evaluate = expression._lessLiteralLiteral;
+            } else if (leftOperandIsLiteral && rightOperandIsProperty) {
+                evaluate = expression._lessLiteralProperty;
+            } else if (leftOperandIsProperty && rightOperandIsLiteral) {
+                evaluate = expression._lessPropertyLiteral;
+            } else {
+                evaluate = expression._lessPropertyProperty;
+            }
         } else if (operator === '<=') {
-            evaluate = booleanExpression._lessEqual;
+            if (leftOperandIsLiteral && rightOperandIsLiteral) {
+                evaluate = expression._lessEqualLiteralLiteral;
+            } else if (leftOperandIsLiteral && rightOperandIsProperty) {
+                evaluate = expression._lessEqualLiteralProperty;
+            } else if (leftOperandIsProperty && rightOperandIsLiteral) {
+                evaluate = expression._lessEqualPropertyLiteral;
+            } else {
+                evaluate = expression._lessEqualPropertyProperty;
+            }
         } else if (operator === '===') {
-            evaluate = booleanExpression._equal;
+            if (leftOperandIsLiteral && rightOperandIsLiteral) {
+                evaluate = expression._equalLiteralLiteral;
+            } else if (leftOperandIsLiteral && rightOperandIsProperty) {
+                evaluate = expression._equalLiteralProperty;
+            } else if (leftOperandIsProperty && rightOperandIsLiteral) {
+                evaluate = expression._equalPropertyLiteral;
+            } else {
+                evaluate = expression._equalPropertyProperty;
+            }
         } else if (operator === '!==') {
-            evaluate = booleanExpression._notEqual;
+            if (leftOperandIsLiteral && rightOperandIsLiteral) {
+                evaluate = expression._notEqualLiteralLiteral;
+            } else if (leftOperandIsLiteral && rightOperandIsProperty) {
+                evaluate = expression._notEqualLiteralProperty;
+            } else if (leftOperandIsProperty && rightOperandIsLiteral) {
+                evaluate = expression._notEqualPropertyLiteral;
+            } else {
+                evaluate = expression._notEqualPropertyProperty;
+            }
         } else {
             evaluate = undefined;
         }
 
-        booleanExpression.evaluate = evaluate;
+        expression.evaluate = evaluate;
     }
 
     BooleanExpression.prototype._true = function(feature) {
@@ -119,28 +203,112 @@ define([
         return false;
     };
 
-    BooleanExpression.prototype._greater = function(feature) {
-        return feature.getProperty(this._propertyName) > this._operand;
+    ///////////////////////////////////////////////////////////////////////////
+
+    BooleanExpression.prototype._greaterLiteralLiteral = function(feature) {
+        return this._leftOperand > this._rightOperand;
     };
 
-    BooleanExpression.prototype._greaterEqual = function(feature) {
-        return feature.getProperty(this._propertyName) >= this._operand;
+    BooleanExpression.prototype._greaterLiteralProperty = function(feature) {
+        return this._leftOperand > feature.getProperty(this._rightPropertyName);
     };
 
-    BooleanExpression.prototype._less = function(feature) {
-        return feature.getProperty(this._propertyName) < this._operand;
+    BooleanExpression.prototype._greaterPropertyLiteral = function(feature) {
+        return feature.getProperty(this._leftPropertyName) > this._rightOperand;
     };
 
-    BooleanExpression.prototype._lessEqual = function(feature) {
-        return feature.getProperty(this._propertyName) <= this._operand;
+    BooleanExpression.prototype._greaterPropertyProperty = function(feature) {
+        return feature.getProperty(this._leftPropertyName) > feature.getProperty(this._rightPropertyName);
     };
 
-    BooleanExpression.prototype._equal = function(feature) {
-        return feature.getProperty(this._propertyName) === this._operand;
+    ///////////////////////////////////////////////////////////////////////////
+
+    BooleanExpression.prototype._greaterEqualLiteralLiteral = function(feature) {
+        return this._leftOperand >= this._rightOperand;
     };
 
-    BooleanExpression.prototype._notEqual = function(feature) {
-        return feature.getProperty(this._propertyName) !== this._operand;
+    BooleanExpression.prototype._greaterEqualLiteralProperty = function(feature) {
+        return this._leftOperand >= feature.getProperty(this._rightPropertyName);
+    };
+
+    BooleanExpression.prototype._greaterEqualPropertyLiteral = function(feature) {
+        return feature.getProperty(this._leftPropertyName) >= this._rightOperand;
+    };
+
+    BooleanExpression.prototype._greaterEqualPropertyProperty = function(feature) {
+        return feature.getProperty(this._leftPropertyName) >= feature.getProperty(this._rightPropertyName);
+    };
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    BooleanExpression.prototype._lessLiteralLiteral = function(feature) {
+        return this._leftOperand < this._rightOperand;
+    };
+
+    BooleanExpression.prototype._lessLiteralProperty = function(feature) {
+        return this._leftOperand < feature.getProperty(this._rightPropertyName);
+    };
+
+    BooleanExpression.prototype._lessPropertyLiteral = function(feature) {
+        return feature.getProperty(this._leftPropertyName) < this._rightOperand;
+    };
+
+    BooleanExpression.prototype._lessPropertyProperty = function(feature) {
+        return feature.getProperty(this._leftPropertyName) < feature.getProperty(this._rightPropertyName);
+    };
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    BooleanExpression.prototype._lessEqualLiteralLiteral = function(feature) {
+        return this._leftOperand <= this._rightOperand;
+    };
+
+    BooleanExpression.prototype._lessEqualLiteralProperty = function(feature) {
+        return this._leftOperand <= feature.getProperty(this._rightPropertyName);
+    };
+
+    BooleanExpression.prototype._lessEqualPropertyLiteral = function(feature) {
+        return feature.getProperty(this._leftPropertyName) <= this._rightOperand;
+    };
+
+    BooleanExpression.prototype._lessEqualPropertyProperty = function(feature) {
+        return feature.getProperty(this._leftPropertyName) <= feature.getProperty(this._rightPropertyName);
+    };
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    BooleanExpression.prototype._equalLiteralLiteral = function(feature) {
+        return this._leftOperand === this._rightOperand;
+    };
+
+    BooleanExpression.prototype._equalLiteralProperty = function(feature) {
+        return this._leftOperand === feature.getProperty(this._rightPropertyName);
+    };
+
+    BooleanExpression.prototype._equalPropertyLiteral = function(feature) {
+        return feature.getProperty(this._leftPropertyName) === this._rightOperand;
+    };
+
+    BooleanExpression.prototype._equalPropertyProperty = function(feature) {
+        return feature.getProperty(this._leftPropertyName) === feature.getProperty(this._rightPropertyName);
+    };
+
+    ///////////////////////////////////////////////////////////////////////////
+
+    BooleanExpression.prototype._notEqualLiteralLiteral = function(feature) {
+        return this._leftOperand !== this._rightOperand;
+    };
+
+    BooleanExpression.prototype._notEqualLiteralProperty = function(feature) {
+        return this._leftOperand !== feature.getProperty(this._rightPropertyName);
+    };
+
+    BooleanExpression.prototype._notEqualPropertyLiteral = function(feature) {
+        return feature.getProperty(this._leftPropertyName) !== this._rightOperand;
+    };
+
+    BooleanExpression.prototype._notEqualPropertyProperty = function(feature) {
+        return feature.getProperty(this._leftPropertyName) !== feature.getProperty(this._rightPropertyName);
     };
 
     return BooleanExpression;
