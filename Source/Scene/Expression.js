3849907f3363adb36b2e865f15d30b7b1c67f5e1
ggetz <ggetz881@gmail.com>
2016-02-15 14:35:51 -0500

    Added evaluation of literal types and specs

--- Source/Scene/Expression.js
+++ Source/Scene/Expression.js
@@ -1,8 +1,12 @@
 /*global define*/
 define([
+       '../Core/Color',
+       '../Core/defined',
        '../Core/defineProperties',
-       '../ThirdParty/jsep',
+       '../ThirdParty/jsep'
     ], function(
+        Color,
+        defined,
         defineProperties,
         jsep) {
     "use strict";
@@ -13,14 +17,41 @@ define([
     function Expression(styleEngine, expression) {
         this._styleEngine = styleEngine;
 
-        console.log(jsep(expression));
+        this._ast = jsep(expression);
+        this._runtimeAST = undefined;
+
+        // create "compiled" AST
+        createRuntimeAst(this);
+    }
+
+    function createRuntimeAst(expression) {
+        console.log(expression._ast);
+        if (expression._ast.type === 'Literal') {
+            expression._runtimeAST = expression._ast;
+
+            var value = expression._ast.value;
+
+            //check if the string is a color, if so turn it into a cesium color
+            if (typeof(value) === 'string') {
+                var c = Color.fromCssColorString(value);
+                if (defined(c)) {
+                    expression._runtimeAST.value = c;
+                }
+            }
+        }
+        console.log(expression._runtimeAST);
     }
 
     defineProperties(Expression.prototype, {
     });
 
     Expression.prototype.evaluate = function(feature) {
-        return true;
+        // if it's a literal, we're done, return value
+        if (this._runtimeAST.type === 'Literal') {
+            return this._runtimeAST.value;
+        }
+
+        return undefined;
     };
 
     return Expression;
--- Specs/Scene/ExpressionSpec.js
+++ Specs/Scene/ExpressionSpec.js
@@ -1,8 +1,10 @@
 /*global defineSuite*/
 defineSuite([
-        'Scene/Expression'
+        'Scene/Expression',
+        'Core/Color'
     ], function(
-        Expression) {
+        Expression,
+        Color) {
     'use strict';
 
     function MockStyleEngine() {
@@ -11,8 +13,32 @@ defineSuite([
     MockStyleEngine.prototype.makeDirty = function() {
     };
 
-    it('evalutes', function() {
+    it('evaluates literal boolean', function() {
         var expression = new Expression(new MockStyleEngine(), 'true');
         expect(expression.evaluate(undefined)).toEqual(true);
+
+        expression = new Expression(new MockStyleEngine(), 'false');
+        expect(expression.evaluate(undefined)).toEqual(false);
+    });
+
+    it('evaluates literal number', function() {
+        var expression = new Expression(new MockStyleEngine(), '1');
+        expect(expression.evaluate(undefined)).toEqual(1);
+
+        expression = new Expression(new MockStyleEngine(), '0');
+        expect(expression.evaluate(undefined)).toEqual(0);
+    });
+
+    it('evaluates literal string', function() {
+        var expression = new Expression(new MockStyleEngine(), '\'hello\'');
+        expect(expression.evaluate(undefined)).toEqual('hello');
+    });
+
+    it('evaluates literal color', function() {
+        var expression = new Expression(new MockStyleEngine(), '\'#ffffff\'');
+        expect(expression.evaluate(undefined)).toEqual(Color.WHITE);
+
+        expression = new Expression(new MockStyleEngine(), '\'white\'');
+        expect(expression.evaluate(undefined)).toEqual(Color.WHITE);
     });
 });
