433d098f16173dddcc7126c9d964e0503991918d
ggetz <ggetz881@gmail.com>
2016-02-23 10:17:23 -0500

    Added array expressions

--- Source/Scene/ExpressionNodeType.js
+++ Source/Scene/ExpressionNodeType.js
@@ -14,12 +14,13 @@ define([
         BINARY : 2,
         CONDITIONAL : 3,
         MEMBER : 4,
-        LITERAL_NULL : 5,
-        LITERAL_BOOLEAN : 6,
-        LITERAL_NUMBER : 7,
-        LITERAL_STRING: 8,
-        LITERAL_COLOR: 9,
-        VARIABLE_IN_STRING : 10
+        ARRAY : 5,
+        LITERAL_NULL : 6,
+        LITERAL_BOOLEAN : 7,
+        LITERAL_NUMBER : 8,
+        LITERAL_STRING: 9,
+        LITERAL_COLOR: 10,
+        VARIABLE_IN_STRING : 11
     };
 
     return freezeObject(ExpressionNodeType);
