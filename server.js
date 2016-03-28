6cd0209a53fd5b077efd3ac3541a3f5e42bfac7b
Premkumar Jayaseelan <jpremkumar@live.com>
2016-03-28 16:40:51 +0530

    Updated the port number in server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 
     var yargs = require('yargs').options({
         'port' : {
-            'default' : 8080,
+            'default' : 8088,
             'description' : 'Port to listen on.'
         },
         'public' : {
