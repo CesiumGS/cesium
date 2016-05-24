a527e59047b9377ca669821c7147f5ef1c6d1972
JoshuaStorm <jsbecker@princeton.edu>
2016-05-24 15:51:39 -0400

    Added boundingVolume getter documentation.

--- Source/Scene/Cesium3DTileset.js
+++ Source/Scene/Cesium3DTileset.js
@@ -572,7 +572,17 @@ define([
         },
 
         /**
+         * The tileset's bounding volume. As previously accessed by tileset._root.contentBoundingVolume.
          *
+         * @memberof Cesium3DTileset.prototype
+         *
+         * @type {TileBoundingVolume}
+         * @readonly
+         *
+         * @exception {DeveloperError} The tileset is not loaded.  Use Cesium3DTileset.readyPromise or wait for Cesium3DTileset.ready to be true.
+         *
+         * @example
+         * console.log('Bounding volume of tileset: ' + tileset.boundingVolume);
          */
         boundingVolume : {
             get : function() {
