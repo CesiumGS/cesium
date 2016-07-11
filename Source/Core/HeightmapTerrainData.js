ce7b6b45423e7f9fbbe086b421b014daefcd8b89
Chris Cooper <chris@propelleraero.com.au>
2016-07-11 14:55:22 +1000

    guard against negative height samples

--- Source/Core/HeightmapTerrainData.js
+++ Source/Core/HeightmapTerrainData.js
@@ -350,7 +350,7 @@ define([
             for (var i = 0; i < width; ++i) {
                 var longitude = CesiumMath.lerp(destinationRectangle.west, destinationRectangle.east, i / (width - 1));
                 var heightSample = interpolateMeshHeight(buffer, encoding, heightOffset, heightScale, skirtHeight, sourceRectangle, width, height, longitude, latitude, exaggeration);
-                setHeight(heights, elementsPerHeight, elementMultiplier, divisor, stride, isBigEndian, j * width + i, heightSample);
+                setHeight(heights, elementsPerHeight, elementMultiplier, divisor, stride, isBigEndian, j * width + i, Math.max(heightSample, 0));
             }
         }
 
