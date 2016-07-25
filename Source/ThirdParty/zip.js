b47d18293e0e897d456aab6ff83369fac44582f1
Alex Gilleran <alex.gilleran@nicta.com.au>
2016-07-25 12:43:06 +1000

    Resolved bad path for inflate/deflate.

--- Source/ThirdParty/zip.js
+++ Source/ThirdParty/zip.js
@@ -402,7 +402,7 @@ define([
 		}
 
 		if (obj.zip.useWebWorkers) {
-			worker = require('./' + INFLATE_JS)();
+			worker = require('worker!./Workers/' + INFLATE_JS)();
 			launchWorkerProcess(worker, reader, writer, offset, size, oninflateappend, onprogress, oninflateend, onreaderror, onwriteerror);
 		} else
 			launchProcess(new obj.zip.Inflater(), reader, writer, offset, size, oninflateappend, onprogress, oninflateend, onreaderror, onwriteerror);
@@ -427,7 +427,7 @@ define([
 		}
 
 		if (obj.zip.useWebWorkers) {
-			worker = require('./' + DEFLATE_JS)();
+			worker = require('worker!./Workers/' + DEFLATE_JS)();
 			worker.addEventListener(MESSAGE_EVENT, onmessage, false);
 			worker.postMessage({
 				init : true,
