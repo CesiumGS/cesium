067a13d8754b873afa50b479d387a46f5aaaadd0
Tom Fili <tfili@agi.com>
2016-08-01 17:06:13 -0400

    Fixed typo when creating GroundPrimitives.

--- Source/DataSources/PolygonGeometryUpdater.js
+++ Source/DataSources/PolygonGeometryUpdater.js
@@ -630,7 +630,7 @@ define([
                 }
 
                 this._primitive = groundPrimitives.add(new GroundPrimitive({
-                    geometryInstance : new GeometryInstance({
+                    geometryInstances : new GeometryInstance({
                         id : entity,
                         geometry : new PolygonGeometry(options),
                         attributes: {
