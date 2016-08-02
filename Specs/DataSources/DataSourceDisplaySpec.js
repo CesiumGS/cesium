efe5ae7f16a22a479384e1597c5c475d57d8eae4
Tom Fili <tfili@agi.com>
2016-08-02 10:54:15 -0400

    Fixed DataSourceDisplay tests to already have terrain heights loaded. Added a test that makes sure update returns false till the terrain heights promise resolves.

--- Specs/DataSources/DataSourceDisplaySpec.js
+++ Specs/DataSources/DataSourceDisplaySpec.js
@@ -7,6 +7,7 @@ defineSuite([
         'DataSources/BoundingSphereState',
         'DataSources/DataSourceCollection',
         'DataSources/Entity',
+        'Scene/GroundPrimitive',
         'Specs/createScene',
         'Specs/MockDataSource'
     ], function(
@@ -17,6 +18,7 @@ defineSuite([
         BoundingSphereState,
         DataSourceCollection,
         Entity,
+        GroundPrimitive,
         createScene,
         MockDataSource) {
     'use strict';
@@ -27,6 +29,8 @@ defineSuite([
     beforeAll(function() {
         scene = createScene();
         dataSourceCollection = new DataSourceCollection();
+
+        return GroundPrimitive.initializeTerrainHeights();
     });
 
     afterAll(function() {
@@ -338,5 +342,28 @@ defineSuite([
         expect(function(){
             return display.update();
         }).toThrowDeveloperError();
+    });
+
+    it('verify update returns false till terrain heights are initialized', function() {
+        GroundPrimitive._initialized = false;
+        GroundPrimitive._initPromise = undefined;
+
+        var source1 = new MockDataSource();
+        var source2 = new MockDataSource();
+
+        display = new DataSourceDisplay({
+            scene : scene,
+            dataSourceCollection : dataSourceCollection,
+            visualizersCallback : visualizersCallback
+        });
+        dataSourceCollection.add(source1);
+        dataSourceCollection.add(source2);
+        display.update(Iso8601.MINIMUM_VALUE);
+        expect(display.ready).toBe(false);
+
+        return GroundPrimitive.initializeTerrainHeights().then(function() {
+            display.update(Iso8601.MINIMUM_VALUE);
+            expect(display.ready).toBe(true);
+        });
     });
 }, 'WebGL');
