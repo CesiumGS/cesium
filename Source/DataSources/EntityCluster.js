b89e35258cb8893efba92b22f6c7b338a9f8290f
Dan Bagnell <daniel.p.bagnell@gmail.com>
2016-08-24 11:43:35 -0400

    Add billboard clustering.

--- Source/DataSources/EntityCluster.js
+++ Source/DataSources/EntityCluster.js
@@ -42,7 +42,6 @@ define([
     }
 
     function getLabelBoundingBox(label, coord, pixelRange) {
-        // TODO: create this at label creation time
         var width = 0;
         var height = Number.NEGATIVE_INFINITY;
 
@@ -85,6 +84,44 @@ define([
         return new BoundingRectangle(x, y, width, height);
     }
 
+    function getBillboardBoundingBox(billboard, coord, pixelRange) {
+        var width = billboard.width;
+        var height = billboard.height;
+
+        var scale = billboard.scale;
+        width *= scale;
+        height *= scale;
+
+        var x = coord.x;
+        if (billboard.horizontalOrigin === HorizontalOrigin.RIGHT) {
+            x += width * 0.5;
+        } else if (billboard.horizontalOrigin === HorizontalOrigin.LEFT) {
+            x -= width * 0.5;
+        }
+
+        var y = coord.y;
+        if (billboard.verticalOrigin === VerticalOrigin.TOP) {
+            y -= height;
+        } else if (billboard.verticalOrigin === VerticalOrigin.CENTER) {
+            y -= height * 0.5;
+        }
+
+        x -= pixelRange;
+        y -= pixelRange;
+        width += pixelRange * 0.5;
+        height += pixelRange * 0.5;
+
+        return new BoundingRectangle(x, y, width, height);
+    }
+
+    function getBoundingBox(item, coord, pixelRange) {
+        if (defined(item._glyphs)) {
+            return getLabelBoundingBox(item, coord, pixelRange);
+        }
+
+        return getBillboardBoundingBox(item, coord, pixelRange);
+    }
+
     function cloneLabel(label) {
         return {
             text : label.text,
@@ -107,6 +144,73 @@ define([
         };
     }
 
+    function cloneBillboard(billboard) {
+        return {
+            show : billboard.show,
+            position : billboard.position,
+            heightReference : billboard.heightReference,
+            pixelOffset : billboard.pixelOffset,
+            scaleByDistance : billboard.scaleByDistance,
+            translucencyByDistance : billboard.translucencyByDistance,
+            pixelOffsetScaleByDistance : billboard.pixelOffsetScaleByDistance,
+            eyeOffset : billboard.eyeOffset,
+            horizontalOrigin : billboard.horizontalOrigin,
+            verticalOrigin : billboard.verticalOrigin,
+            scale : billboard.scale,
+            color : billboard.color,
+            rotation : billboard.rotation,
+            alignedAxis : billboard.alignedAxis,
+            width : billboard.width,
+            height : billboard.height,
+            sizeInMeters : billboard.sizeInMeters,
+            id : billboard.id,
+            pickPrimitive : billboard.pickPrimitive,
+            image : billboard.image
+        };
+    }
+
+    function addNonClusteredItem(item, entityCluster) {
+        if (item._glyphs) {
+            entityCluster._clusterLabelCollection.add(cloneLabel(item));
+        } else {
+            entityCluster._clusterBillboardCollection.add(cloneBillboard(item));
+        }
+    }
+
+    function addCluster(position, numPoints, ids, entityCluster) {
+        entityCluster._clusterLabelCollection.add({
+            text : '' + numPoints,
+            position : position,
+            id : ids
+        });
+    }
+
+    function getScreenSpacePositions(collection, points, scene, occluder) {
+        if (!defined(collection)) {
+            return;
+        }
+
+        var length = collection.length;
+        for (var i = 0; i < length; ++i) {
+            var item = collection.get(i);
+            if (!item.show || !occluder.isPointVisible(item.position)) {
+                continue;
+            }
+
+            var coord = item.computeScreenSpacePosition(scene);
+            if (!defined(coord) || coord.x < 0.0 || coord.x > scene.drawingBufferWidth || coord.y < 0.0 || coord.y > scene.drawingBufferHeight) {
+                continue;
+            }
+
+            points.push({
+                index : i,
+                collection : collection,
+                clustered : false,
+                coord : coord
+            });
+        }
+    }
+
     function createDeclutterCallback(entityCluster) {
         return function(amount) {
             if (defined(amount) && amount < 0.05) {
@@ -116,10 +220,29 @@ define([
             var scene = entityCluster._scene;
 
             var labelCollection = entityCluster._labelCollection;
-            var clusteredLabelCollection = entityCluster._clusteredLabelCollection;
+            var billboardCollection = entityCluster._billboardCollection;
 
-            if (!defined(labelCollection)) {
+            if (!defined(labelCollection) && !defined(billboardCollection)) {
                 return;
+            }
+
+            var clusteredLabelCollection = entityCluster._clusterLabelCollection;
+            var clusteredBillboardCollection = entityCluster._clusterBillboardCollection;
+
+            if (defined(clusteredLabelCollection)) {
+                clusteredLabelCollection.removeAll();
+            } else {
+                clusteredLabelCollection = entityCluster._clusterLabelCollection = new LabelCollection({
+                    scene : scene
+                });
+            }
+
+            if (defined(clusteredBillboardCollection)) {
+                clusteredBillboardCollection.removeAll();
+            } else {
+                clusteredBillboardCollection = entityCluster._clusterBillboardCollection = new BillboardCollection({
+                    scene : scene
+                });
             }
 
             var pixelRange = entityCluster._pixelRange;
@@ -130,43 +253,17 @@ define([
             var previousHeight = entityCluster._previousHeight;
             var currentHeight = scene.camera.positionCartographic.height;
 
-            if (defined(clusteredLabelCollection)) {
-                clusteredLabelCollection.removeAll();
-            } else {
-                clusteredLabelCollection = new LabelCollection({
-                    scene : scene
-                });
-            }
-
             var ellipsoid = scene.mapProjection.ellipsoid;
             var cameraPosition = scene.camera.positionWC;
             var occluder = new EllipsoidalOccluder(ellipsoid, cameraPosition);
 
-            var i;
-            var label;
-            var coord;
-            var length = labelCollection.length;
             var points = [];
+            getScreenSpacePositions(labelCollection, points, scene, occluder);
+            getScreenSpacePositions(billboardCollection, points, scene, occluder);
 
-            for (i = 0; i < length; ++i) {
-                label = labelCollection.get(i);
-                if (!label.show || !occluder.isPointVisible(label.position)) {
-                    continue;
-                }
-
-                coord = label.computeScreenSpacePosition(scene);
-                if (!defined(coord) || coord.x < 0.0 || coord.x > scene.drawingBufferWidth || coord.y < 0.0 || coord.y > scene.drawingBufferHeight) {
-                    continue;
-                }
-
-                points.push({
-                    labelIndex : i,
-                    clustered : false,
-                    coord : coord
-                });
-            }
-
+            var i;
             var j;
+            var length;
             var bbox;
             var neighbors;
             var neighborLength;
@@ -174,6 +271,9 @@ define([
             var neighborPoint;
             var ids;
             var numPoints;
+
+            var collection;
+            var collectionIndex;
 
             var index = kdbush(points, getX, getY, 64, Int32Array);
 
@@ -186,7 +286,7 @@ define([
                         continue;
                     }
 
-                    coord = Billboard._computeScreenSpacePosition(Matrix4.IDENTITY, cluster.position, Cartesian3.ZERO, Cartesian2.ZERO, scene);
+                    var coord = Billboard._computeScreenSpacePosition(Matrix4.IDENTITY, cluster.position, Cartesian3.ZERO, Cartesian2.ZERO, scene);
                     if (!defined(coord) || coord.x < 0.0 || coord.x > scene.drawingBufferWidth || coord.y < 0.0 || coord.y > scene.drawingBufferHeight) {
                         continue;
                     }
@@ -203,17 +303,15 @@ define([
                             neighborPoint.clustered = true;
                             ++numPoints;
 
-                            ids.push(labelCollection.get(neighborPoint.labelIndex));
+                            collection = neighborPoint.collection;
+                            collectionIndex = neighborPoint.index;
+                            ids.push(collection.get(collectionIndex));
                         }
                     }
 
                     if (numPoints > 1) {
+                        addCluster(cluster.position, numPoints, ids, entityCluster);
                         newClusters.push(cluster);
-                        clusteredLabelCollection.add({
-                            text : '' + numPoints,
-                            position : cluster.position,
-                            id : ids
-                        });
                     }
                 }
             }
@@ -227,13 +325,20 @@ define([
 
                 point.clustered = true;
 
-                label = labelCollection.get(point.labelIndex);
-                bbox = getLabelBoundingBox(label, point.coord, pixelRange);
+                collection = point.collection;
+                collectionIndex = point.index;
 
-                neighbors = index.within(bbox.x, bbox.y, bbox.width);
+                var item = collection.get(collectionIndex);
+                bbox = getBoundingBox(item, point.coord, pixelRange);
+
+                var x = bbox.x + bbox.width * 0.5;
+                var y = bbox.y + bbox.height * 0.5;
+                var radius = Math.max(bbox.width, bbox.height) * 0.5;
+
+                neighbors = index.within(x, y, radius);
                 neighborLength = neighbors.length;
 
-                var clusterPosition = Cartesian3.clone(label.position);
+                var clusterPosition = Cartesian3.clone(item.position);
                 numPoints = 1;
                 ids = [];
 
@@ -243,25 +348,23 @@ define([
                     if (!neighborPoint.clustered) {
                         neighborPoint.clustered = true;
 
-                        var neighborLabel = labelCollection.get(neighborPoint.labelIndex);
-                        Cartesian3.add(clusterPosition, neighborLabel.position, clusterPosition);
-                        BoundingRectangle.union(bbox, getLabelBoundingBox(neighborLabel, neighborPoint.coord, pixelRange), bbox);
+                        var neighborItem = neighborPoint.collection.get(neighborPoint.index);
+                        var neighborBBox = getBoundingBox(neighborItem, neighborPoint.coord, pixelRange);
+
+                        Cartesian3.add(neighborItem.position, clusterPosition, clusterPosition);
+
+                        BoundingRectangle.union(bbox, neighborBBox, bbox);
                         ++numPoints;
 
-                        ids.push(labelCollection.get(neighborPoint.labelIndex));
+                        ids.push(neighborItem);
                     }
                 }
 
                 if (numPoints === 1) {
-                    clusteredLabelCollection.add(cloneLabel(label));
+                    addNonClusteredItem(item, entityCluster);
                 } else {
                     var position = Cartesian3.multiplyByScalar(clusterPosition, 1.0 / numPoints, clusterPosition);
-                    clusteredLabelCollection.add({
-                        text : '' + numPoints,
-                        position : position,
-                        id : ids
-                    });
-
+                    addCluster(position, numPoints, ids, entityCluster);
                     newClusters.push({
                         position : position,
                         radius : Math.max(bbox.width, bbox.height) * 0.5
@@ -271,10 +374,14 @@ define([
 
             if (clusteredLabelCollection.length === 0) {
                 clusteredLabelCollection.destroy();
-                clusteredLabelCollection = undefined;
+                entityCluster._clusterLabelCollection = undefined;
             }
 
-            entityCluster._clusteredLabelCollection = clusteredLabelCollection;
+            if (clusteredBillboardCollection.length === 0) {
+                clusteredBillboardCollection.destroy();
+                entityCluster._clusterBillboardCollection = undefined;
+            }
+
             entityCluster._previousClusters = newClusters;
             entityCluster._previousHeight = currentHeight;
         };
@@ -282,13 +389,14 @@ define([
 
     function EntityCluster(options) {
         this._scene = options.scene;
-        this._pixelRange = defaultValue(options.pixelRange, 5);
+        this._pixelRange = defaultValue(options.pixelRange, 80);
 
         this._labelCollection = undefined;
-        this._clusteredLabelCollection = undefined;
-
         this._billboardCollection = undefined;
-        this._clusteredBillboardCollection = undefined;
+
+        this._clusterBillboardCollection = undefined;
+        this._clusterLabelCollection = undefined;
+        //this._clusterPointCollection = undefined;
 
         this._unusedLabelIndices = [];
         this._unusedBillboardIndices = [];
@@ -384,20 +492,16 @@ define([
     };
 
     EntityCluster.prototype.update = function(frameState) {
-        if (defined(this._labelCollection)) {
-            if (!defined(this._clusteredLabelCollection)) {
-                this._labelCollection.update(frameState);
-            } else {
-                this._clusteredLabelCollection.update(frameState);
-            }
+        if (defined(this._clusterLabelCollection)) {
+            this._clusterLabelCollection.update(frameState);
+        } else if (defined(this._labelCollection)) {
+            this._labelCollection.update(frameState);
         }
 
-        if (defined(this._billboardCollection)) {
-            if (!defined(this._clusteredBillboardCollection)) {
-                this._billboardCollection.update(frameState);
-            } else {
-                this._clusteredBillboardCollection.update(frameState);
-            }
+        if (defined(this._clusterBillboardCollection)) {
+            this._clusterBillboardCollection.update(frameState);
+        } else if (defined(this._billboardCollection)) {
+            this._billboardCollection.update(frameState);
         }
     };
 
@@ -407,9 +511,11 @@ define([
 
     EntityCluster.prototype.destroy = function() {
         this._labelCollection = this._labelCollection && this._labelCollection.destroy();
-        this._clusteredLabelCollection = this._clusteredLabelCollection && this._clusteredLabelCollection.destroy();
         this._billboardCollection = this._billboardCollection && this._billboardCollection.destroy();
-        this._clusteredBillboardCollection = this._clusteredBillboardCollection && this._clusteredBillboardCollection.destroy();
+
+        this._clusterLabelCollection = this._clusterLabelCollection && this._clusterLabelCollection.destroy();
+        this._clusterBillboardCollection = this._clusterBillboardCollection && this._clusterBillboardCollection.destroy();
+
         this._removeEventListener();
         return destroyObject(this);
     };
