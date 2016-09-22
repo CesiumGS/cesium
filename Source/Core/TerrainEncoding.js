783ea9ab8055691a094a2c56cb8418c4df0882db
Kevin Ring <kevin@kotachrome.com>
2016-09-22 12:27:20 +1000

    Fix formatting better.

--- Source/Core/TerrainEncoding.js
+++ Source/Core/TerrainEncoding.js
@@ -342,19 +342,19 @@ define([
             stride = (numCompressed0 + numCompressed1) * sizeInBytes;
 
             return [{
-                index: attributes.compressed0,
-                vertexBuffer: buffer,
-                componentDatatype: datatype,
-                componentsPerAttribute: numCompressed0,
-                offsetInBytes: 0,
-                strideInBytes: stride
+                index : attributes.compressed0,
+                vertexBuffer : buffer,
+                componentDatatype : datatype,
+                componentsPerAttribute : numCompressed0,
+                offsetInBytes : 0,
+                strideInBytes : stride
             }, {
-                index: attributes.compressed1,
-                vertexBuffer: buffer,
-                componentDatatype: datatype,
-                componentsPerAttribute: numCompressed1,
-                offsetInBytes: numCompressed0 * sizeInBytes,
-                strideInBytes: stride
+                index : attributes.compressed1,
+                vertexBuffer : buffer,
+                componentDatatype : datatype,
+                componentsPerAttribute : numCompressed1,
+                offsetInBytes : numCompressed0 * sizeInBytes,
+                strideInBytes : stride
             }];
         } else {
             return [{
