9b5856cb7107214593509aa1d1a7fb4b49599b1d
Tom Fili <tfili@agi.com>
2016-09-23 12:10:26 -0400

    Added options for background color and border color to writeTextToCanvas.

--- Source/Core/writeTextToCanvas.js
+++ Source/Core/writeTextToCanvas.js
@@ -27,7 +27,10 @@ define([
      * @param {Boolean} [options.stroke=false] Whether to stroke the text.
      * @param {Color} [options.fillColor=Color.WHITE] The fill color.
      * @param {Color} [options.strokeColor=Color.BLACK] The stroke color.
-     * @param {Color} [options.strokeWidth=1] The stroke width.
+     * @param {Number} [options.strokeWidth=1] The stroke width.
+     * @param {Color} [options.backgroundColor=Color.TRANSPARENT] The background color of the canvas.
+     * @param {Number} [options.borderWidth=0] The pixel size of the border to add around the text.
+     * @param {Number} [options.borderColor=Color.BLACK] The color of the border.
      * @returns {Canvas} A new canvas with the given text drawn into it.  The dimensions object
      *                   from measureText will also be added to the returned canvas. If text is
      *                   blank, returns undefined.
@@ -47,6 +50,9 @@ define([
         var stroke = defaultValue(options.stroke, false);
         var fill = defaultValue(options.fill, true);
         var strokeWidth = defaultValue(options.strokeWidth, 1);
+        var backgroundColor = defaultValue(options.backgroundColor, Color.TRANSPARENT);
+        var borderWidth = defaultValue(options.borderWidth, 0);
+        var halfBorderWidth = borderWidth * 0.5;
 
         var canvas = document.createElement('canvas');
         canvas.width = 1;
@@ -88,10 +94,23 @@ define([
         document.body.removeChild(canvas);
         canvas.style.visibility = '';
 
-        var baseline = dimensions.height - dimensions.ascent;
-        canvas.width = dimensions.computedWidth;
-        canvas.height = dimensions.height;
+        var baseline = dimensions.height - dimensions.ascent + halfBorderWidth;
+        canvas.width = dimensions.computedWidth + borderWidth;
+        canvas.height = dimensions.height + borderWidth;
         var y = canvas.height - baseline;
+
+        // Draw border color to whole canvas
+        if (borderWidth > 0) {
+            var borderColor = defaultValue(options.borderColor, Color.BLACK);
+            context2D.fillStyle = borderColor.toCssColorString();
+            context2D.fillRect(0, 0, canvas.width, canvas.height);
+        }
+
+        // Draw background except to the border area
+        if (backgroundColor !== Color.TRANSPARENT) {
+            context2D.fillStyle = backgroundColor.toCssColorString();
+            context2D.fillRect(halfBorderWidth, halfBorderWidth, canvas.width-borderWidth, canvas.height-borderWidth);
+        }
 
         // Properties must be explicitly set again after changing width and height
         context2D.font = font;
@@ -102,13 +121,13 @@ define([
         if (stroke) {
             var strokeColor = defaultValue(options.strokeColor, Color.BLACK);
             context2D.strokeStyle = strokeColor.toCssColorString();
-            context2D.strokeText(text, 0, y);
+            context2D.strokeText(text, halfBorderWidth, y);
         }
 
         if (fill) {
             var fillColor = defaultValue(options.fillColor, Color.WHITE);
             context2D.fillStyle = fillColor.toCssColorString();
-            context2D.fillText(text, 0, y);
+            context2D.fillText(text, halfBorderWidth, y);
         }
 
         return canvas;
