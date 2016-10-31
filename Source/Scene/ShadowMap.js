9964fc9ae9301903e36b9a393b26b7e912a44f1e
Sean Lilley <lilleyse@gmail.com>
2016-10-31 15:43:27 -0400

    Change url

--- Source/Scene/ShadowMap.js
+++ Source/Scene/ShadowMap.js
@@ -185,7 +185,7 @@ define([
         this._needsUpdate = true;
 
         // In IE11 polygon offset is not functional.
-        // TODO : Also disabled for Chrome temporarily. Re-enable once https://groups.google.com/forum/#!topic/webgl-dev-list/E1dAG65QBhg is resolved.
+        // TODO : Also disabled for Chrome temporarily. Re-enable once https://github.com/AnalyticalGraphicsInc/cesium/issues/4560 is resolved.
         var polygonOffsetSupported = true;
         if (FeatureDetection.isInternetExplorer() || FeatureDetection.isChrome()) {
             polygonOffsetSupported = false;
