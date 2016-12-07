766b21ee5b05d9038e0f6f63feb1bc609e0b7dd6
hpinkos <pinkos.hannah@gmail.com>
2016-12-07 13:48:43 -0500

    handle sandcastle demo not found

--- gulpfile.js
+++ gulpfile.js
@@ -1125,8 +1125,10 @@ function createGalleryList() {
         fileList.push('!Apps/Sandcastle/gallery/development/**/*.html');
     }
 
+    var helloWorld;
     globby.sync(fileList).forEach(function(file) {
         var demo = filePathToModuleId(path.relative('Apps/Sandcastle/gallery', file));
+
         var demoObject = {
             name : demo,
             date : fs.statSync(file).mtime.getTime()
@@ -1137,6 +1139,10 @@ function createGalleryList() {
         }
 
         demoObjects.push(demoObject);
+
+        if (demo === 'Hello World') {
+            helloWorld = demoObject;
+        }
     });
 
     demoObjects.sort(function(a, b) {
@@ -1149,6 +1155,8 @@ function createGalleryList() {
       }
     });
 
+    var helloWorldIndex = demoObjects.indexOf(helloWorld);
+
     var i;
     for (i = 0; i < demoObjects.length; ++i) {
       demoJSONs[i] = JSON.stringify(demoObjects[i], null, 2);
@@ -1156,6 +1164,7 @@ function createGalleryList() {
 
     var contents = '\
 // This file is automatically rebuilt by the Cesium build process.\n\
+var hello_world_index = ' + helloWorldIndex + ';\n\
 var gallery_demos = [' + demoJSONs.join(', ') + '];';
 
     fs.writeFileSync(output, contents);
