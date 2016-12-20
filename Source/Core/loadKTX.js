cd3b02adc547304d1cca3a8f881492536f0e7f1a
Patrick Cozzi <pjcozzi@gmail.com>
2016-12-20 09:02:00 -0500

    Move exported function to the top

--- Source/Core/loadKTX.js
+++ Source/Core/loadKTX.js
@@ -15,6 +15,55 @@ define([
         RuntimeError) {
     'use strict';
 
+    /**
+     * Asynchronously loads and parses the given URL to a KTX file or parses the raw binary data of a KTX file.
+     * Returns a promise that will resolve to an object containing the image buffer, width, height and format once loaded,
+     * or reject if the URL failed to load or failed to parse the data.  The data is loaded
+     * using XMLHttpRequest, which means that in order to make requests to another origin,
+     * the server must have Cross-Origin Resource Sharing (CORS) headers enabled.
+     *
+     * @exports loadKTX
+     *
+     * @param {String|Promise.<String>|ArrayBuffer} urlOrBuffer The URL of the binary data, a promise for the URL, or an ArrayBuffer.
+     * @param {Object} [headers] HTTP headers to send with the requests.
+     * @returns {Promise.<Object>} A promise that will resolve to the requested data when loaded.
+     *
+     *
+     * @example
+     * // load a single URL asynchronously
+     * Cesium.loadKTX('some/url').then(function(ktxData) {
+     *     var width = ktxData.width;
+     *     var height = ktxData.height;
+     *     var format = ktxData.internalFormat;
+     *     var arrayBufferView = ktxData.bufferView;
+     *     // use the data to create a texture
+     * }).otherwise(function(error) {
+     *     // an error occurred
+     * });
+     *
+     * @see {@link https://www.khronos.org/opengles/sdk/tools/KTX/file_format_spec/|KTX file format}
+     * @see {@link http://www.w3.org/TR/cors/|Cross-Origin Resource Sharing}
+     * @see {@link http://wiki.commonjs.org/wiki/Promises/A|CommonJS Promises/A}
+     */
+    function loadKTX(urlOrBuffer, headers) {
+        //>>includeStart('debug', pragmas.debug);
+        if (!defined(urlOrBuffer)) {
+            throw new DeveloperError('urlOrBuffer is required.');
+        }
+        //>>includeEnd('debug');
+
+        var loadPromise;
+        if (urlOrBuffer instanceof ArrayBuffer || ArrayBuffer.isView(urlOrBuffer)) {
+            loadPromise = when.resolve(urlOrBuffer);
+        } else {
+            loadPromise = loadArrayBuffer(urlOrBuffer, headers);
+        }
+
+        return loadPromise.then(function(data) {
+            return parseKTX(data);
+        });
+    }
+
     var fileIdentifier = [0xAB, 0x4B, 0x54, 0x58, 0x20, 0x31, 0x31, 0xBB, 0x0D, 0x0A, 0x1A, 0x0A];
     var endiannessTest = 0x04030201;
 
@@ -143,55 +192,6 @@ define([
             height : pixelHeight,
             internalFormat : glInternalFormat
         };
-    }
-
-    /**
-     * Asynchronously loads and parses the given URL to a KTX file or parses the raw binary data of a KTX file.
-     * Returns a promise that will resolve to an object containing the image buffer, width, height and format once loaded,
-     * or reject if the URL failed to load or failed to parse the data.  The data is loaded
-     * using XMLHttpRequest, which means that in order to make requests to another origin,
-     * the server must have Cross-Origin Resource Sharing (CORS) headers enabled.
-     *
-     * @exports loadKTX
-     *
-     * @param {String|Promise.<String>|ArrayBuffer} urlOrBuffer The URL of the binary data, a promise for the URL, or an ArrayBuffer.
-     * @param {Object} [headers] HTTP headers to send with the requests.
-     * @returns {Promise.<Object>} A promise that will resolve to the requested data when loaded.
-     *
-     *
-     * @example
-     * // load a single URL asynchronously
-     * Cesium.loadKTX('some/url').then(function(ktxData) {
-     *     var width = ktxData.width;
-     *     var height = ktxData.height;
-     *     var format = ktxData.internalFormat;
-     *     var arrayBufferView = ktxData.bufferView;
-     *     // use the data to create a texture
-     * }).otherwise(function(error) {
-     *     // an error occurred
-     * });
-     *
-     * @see {@link https://www.khronos.org/opengles/sdk/tools/KTX/file_format_spec/|KTX file format}
-     * @see {@link http://www.w3.org/TR/cors/|Cross-Origin Resource Sharing}
-     * @see {@link http://wiki.commonjs.org/wiki/Promises/A|CommonJS Promises/A}
-     */
-    function loadKTX(urlOrBuffer, headers) {
-        //>>includeStart('debug', pragmas.debug);
-        if (!defined(urlOrBuffer)) {
-            throw new DeveloperError('urlOrBuffer is required.');
-        }
-        //>>includeEnd('debug');
-
-        var loadPromise;
-        if (urlOrBuffer instanceof ArrayBuffer || ArrayBuffer.isView(urlOrBuffer)) {
-            loadPromise = when.resolve(urlOrBuffer);
-        } else {
-            loadPromise = loadArrayBuffer(urlOrBuffer, headers);
-        }
-
-        return loadPromise.then(function(data) {
-            return parseKTX(data);
-        });
     }
 
     return loadKTX;
