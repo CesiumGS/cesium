ebd16977db25c9716a6c1e3371f8b73b1f817042
Kevin Ring <kevin@kotachrome.com>
2017-01-07 21:05:20 +1100

    Further factoring of DataSourceDisplay.

--- Source/DataSources/DataSourceDisplay.js
+++ Source/DataSources/DataSourceDisplay.js
@@ -1,60 +1,12 @@
 /*global define*/
 define([
-        './DataSourceDisplayWithoutVisualizers'
-        './BillboardVisualizer',
-        './BoxGeometryUpdater',
-        './CorridorGeometryUpdater',
-        './CylinderGeometryUpdater',
-        './EllipseGeometryUpdater',
-        './EllipsoidGeometryUpdater',
-        './GeometryVisualizer',
-        './LabelVisualizer',
-        './ModelVisualizer',
-        './PathVisualizer',
-        './PointVisualizer',
-        './PolygonGeometryUpdater',
-        './PolylineGeometryUpdater',
-        './PolylineVolumeGeometryUpdater',
-        './RectangleGeometryUpdater',
-        './WallGeometryUpdater'
+        './DataSourceDisplayWithoutVisualizers',
+        './createCesiumVisualizers'
     ], function(
         DataSourceDisplay,
-        BillboardVisualizer,
-        BoxGeometryUpdater,
-        CorridorGeometryUpdater,
-        CylinderGeometryUpdater,
-        EllipseGeometryUpdater,
-        EllipsoidGeometryUpdater,
-        GeometryVisualizer,
-        LabelVisualizer,
-        ModelVisualizer,
-        PathVisualizer,
-        PointVisualizer,
-        PolygonGeometryUpdater,
-        PolylineGeometryUpdater,
-        PolylineVolumeGeometryUpdater,
-        RectangleGeometryUpdater,
-        WallGeometryUpdater) {
+        createCesiumVisualizers) {
     'use strict';
 
-    DataSourceDisplay.defaultVisualizersCallback = function(scene, entityCluster, dataSource) {
-        var entities = dataSource.entities;
-        return [new BillboardVisualizer(entityCluster, entities),
-                new GeometryVisualizer(BoxGeometryUpdater, scene, entities),
-                new GeometryVisualizer(CylinderGeometryUpdater, scene, entities),
-                new GeometryVisualizer(CorridorGeometryUpdater, scene, entities),
-                new GeometryVisualizer(EllipseGeometryUpdater, scene, entities),
-                new GeometryVisualizer(EllipsoidGeometryUpdater, scene, entities),
-                new GeometryVisualizer(PolygonGeometryUpdater, scene, entities),
-                new GeometryVisualizer(PolylineGeometryUpdater, scene, entities),
-                new GeometryVisualizer(PolylineVolumeGeometryUpdater, scene, entities),
-                new GeometryVisualizer(RectangleGeometryUpdater, scene, entities),
-                new GeometryVisualizer(WallGeometryUpdater, scene, entities),
-                new LabelVisualizer(entityCluster, entities),
-                new ModelVisualizer(scene, entities),
-                new PointVisualizer(entityCluster, entities),
-                new PathVisualizer(scene, entities)];
-    };
-
+    DataSourceDisplay.defaultVisualizersCallback = createCesiumVisualizers;
     return DataSourceDisplay;
 });
