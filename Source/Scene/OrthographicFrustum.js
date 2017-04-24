6e8c2014758a23b6ea4a9317694f26726177a4c8
hates <ates.hueseyin@gmail.com>
2017-04-24 21:56:04 +0300

    update frustum if block reverted

--- Source/Scene/OrthographicFrustum.js
+++ Source/Scene/OrthographicFrustum.js
@@ -78,6 +78,14 @@ define([
             frustum._near = frustum.near;
             frustum._far = frustum.far;
 
+            var ratio = 1.0 / frustum.aspectRatio;
+            f.right = frustum.width * 0.5;
+            f.left = -f.right;
+            f.top = ratio * f.right;
+            f.bottom = -f.top;
+            f.near = frustum.near;
+            f.far = frustum.far;
+
         }
     }
 
