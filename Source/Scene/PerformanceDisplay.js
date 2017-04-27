0f5a3a1e894891fdd80a301d60aaf24690504186
hpinkos <pinkos.hannah@gmail.com>
2017-04-27 09:31:37 -0400

    average ms

--- Source/Scene/PerformanceDisplay.js
+++ Source/Scene/PerformanceDisplay.js
@@ -46,10 +46,8 @@ define([
 
         this._lastFpsSampleTime = getTimestamp();
         this._lastMsSampleTime = getTimestamp();
-        this._frameCount = 0;
-        this._time = undefined;
-        this._fps = 0;
-        this._frameTime = 0;
+        this._fpsFrameCount = 0;
+        this._msFrameCount = 0;
     }
 
     /**
@@ -57,34 +55,23 @@ define([
      * each call records a frame in the internal buffer and redraws the display.
      */
     PerformanceDisplay.prototype.update = function() {
-        if (!defined(this._time)) {
-            //first update
-            this._lastFpsSampleTime = getTimestamp();
-            this._time = getTimestamp();
-            return;
-        }
-
-        var previousTime = this._time;
         var time = getTimestamp();
-        this._time = time;
 
-        var frameTime = time - previousTime;
-
-        this._frameCount++;
+        this._fpsFrameCount++;
         var fpsElapsedTime = time - this._lastFpsSampleTime;
         if (fpsElapsedTime > 1000) {
-            var fps = this._frameCount * 1000 / fpsElapsedTime | 0;
+            var fps = this._fpsFrameCount * 1000 / fpsElapsedTime | 0;
             this._fpsText.nodeValue = fps + ' FPS';
-            this._fps = fps;
             this._lastFpsSampleTime = time;
-            this._frameCount = 0;
+            this._fpsFrameCount = 0;
         }
 
+        this._msFrameCount++;
         var msElapsedTime = time - this._lastMsSampleTime;
         if (msElapsedTime > 200) {
-            this._msText.nodeValue = frameTime.toFixed(2) + ' MS';
-            this._frameTime = frameTime;
+            this._msText.nodeValue = (msElapsedTime / this._msFrameCount).toFixed(2) + ' MS';
             this._lastMsSampleTime = time;
+            this._msFrameCount = 0;
         }
     };
 
