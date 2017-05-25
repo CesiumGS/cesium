a34bbb65a1945000ae8db8c2f52cc34f01974a48
Sean Lilley <lilleyse@gmail.com>
2017-05-25 10:18:23 -0400

    Remove uneeded section

--- Source/ThirdParty/jsep.js
+++ Source/ThirdParty/jsep.js
@@ -624,26 +624,5 @@ define(function () {
         return this;
     };
 
-    // In desktop environments, have a way to restore the old value for `jsep`
-    if (typeof exports === 'undefined') {
-        var old_jsep = root.jsep;
-        // The star of the show! It's a function!
-        root.jsep = jsep;
-        // And a courteous function willing to move out of the way for other similarly-named objects!
-        jsep.noConflict = function() {
-            if(root.jsep === jsep) {
-                root.jsep = old_jsep;
-            }
-            return jsep;
-        };
-    } else {
-        // In Node.JS environments
-        if (typeof module !== 'undefined' && module.exports) {
-            exports = module.exports = jsep;
-        } else {
-            exports.parse = jsep;
-        }
-    }
-
     return jsep;
 });
