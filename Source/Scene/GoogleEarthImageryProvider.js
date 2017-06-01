eb2bd9c0d524f9f25a9885169acb9c757a041eff
hpinkos <pinkos.hannah@gmail.com>
2017-06-01 12:16:11 -0400

    Move GoogleEarthImageryProvider deprecation to next release

--- Source/Scene/GoogleEarthImageryProvider.js
+++ Source/Scene/GoogleEarthImageryProvider.js
@@ -10,7 +10,7 @@ define([
     /**
      * Provides tiled imagery using the Google Earth Imagery API.
      *
-     * Notes: This imagery provider was deprecated in Cesium 1.34 and replaced with {@link GoogleEarthEnterpriseMapsProvider}.
+     * Notes: This imagery provider was deprecated in Cesium 1.35 and replaced with {@link GoogleEarthEnterpriseMapsProvider}.
      *        These are for use with the 2D Maps API. For 3D Earth API uses, see {@link GoogleEarthEngerpriseImageryProvider}.
      *        GoogleEarthImageryProvider will be removed in Cesium 1.37.
      *
@@ -27,7 +27,7 @@ define([
      * });
      */
     function GoogleEarthImageryProvider(options) {
-        deprecationWarning('GoogleEarthImageryProvider', 'GoogleEarthImageryProvider was deprecated in Cesium 1.34, it will be removed in 1.37. Use GoogleEarthEnterpriseMapsProvider instead.');
+        deprecationWarning('GoogleEarthImageryProvider', 'GoogleEarthImageryProvider was deprecated in Cesium 1.35, it will be removed in 1.37. Use GoogleEarthEnterpriseMapsProvider instead.');
 
         return new GoogleEarthEnterpriseMapsProvider(options);
     }
