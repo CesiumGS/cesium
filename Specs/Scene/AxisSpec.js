80d0dc8faeeb2a5fc6eefee0694ab8e1fbfb752f
Mohamad Moneimne <moneimne@seas.upenn.edu>
2017-06-05 11:38:34 -0400

    Updated files with changes from review

--- Specs/Scene/AxisSpec.js
+++ Specs/Scene/AxisSpec.js
@@ -1,76 +1,45 @@
 /*global defineSuite*/
 defineSuite([
-    'Scene/Axis',
-    'Core/Cartesian4',
-    'Core/Math',
-    'Core/Matrix3',
-    'Core/Matrix4'
-], function(
-    Axis,
-    Cartesian4,
-    CesiumMath,
-    Matrix3,
-    Matrix4) {
+        'Scene/Axis',
+        'Core/Cartesian4',
+        'Core/Math',
+        'Core/Matrix3',
+        'Core/Matrix4'
+    ], function(
+        Axis,
+        Cartesian4,
+        CesiumMath,
+        Matrix3,
+        Matrix4) {
     'use strict';
 
+    function convertUpAxis(upAxis, transformation, expected) {
+        var transformed = Matrix4.multiplyByVector(transformation, upAxis, new Cartesian4());
+        Cartesian4.normalize(transformed, transformed);
+        expect(transformed).toEqualEpsilon(expected, CesiumMath.EPSILON1);
+    }
+
     it('Convert y-up to z-up', function() {
-        var upAxis = Cartesian4.UNIT_Y;
-        var transformation = Axis.Y_UP_TO_Z_UP;
-        var transformed = new Cartesian4();
-        Matrix4.multiplyByVector(transformation, upAxis, transformed);
-        var result = new Cartesian4();
-        Cartesian4.normalize(transformed, result);
-        expect(result).toEqualEpsilon(Cartesian4.UNIT_Z, CesiumMath.EPSILON1);
+        convertUpAxis(Cartesian4.UNIT_Y, Axis.Y_UP_TO_Z_UP, Cartesian4.UNIT_Z);
     });
 
     it('Convert y-up to x-up', function() {
-        var upAxis = Cartesian4.UNIT_Y;
-        var transformation = Axis.Y_UP_TO_X_UP;
-        var transformed = new Cartesian4();
-        Matrix4.multiplyByVector(transformation, upAxis, transformed);
-        var result = new Cartesian4();
-        Cartesian4.normalize(transformed, result);
-        expect(result).toEqualEpsilon(Cartesian4.UNIT_X, CesiumMath.EPSILON1);
+        convertUpAxis(Cartesian4.UNIT_Y, Axis.Y_UP_TO_X_UP, Cartesian4.UNIT_X);
     });
 
     it('Convert z-up to x-up', function() {
-        var upAxis = Cartesian4.UNIT_Z;
-        var transformation = Axis.Z_UP_TO_X_UP;
-        var transformed = new Cartesian4();
-        Matrix4.multiplyByVector(transformation, upAxis, transformed);
-        var result = new Cartesian4();
-        Cartesian4.normalize(transformed, result);
-        expect(result).toEqualEpsilon(Cartesian4.UNIT_X, CesiumMath.EPSILON1);
+        convertUpAxis(Cartesian4.UNIT_Z, Axis.Z_UP_TO_X_UP, Cartesian4.UNIT_X);
     });
 
     it('Convert z-up to y-up', function() {
-        var upAxis = Cartesian4.UNIT_Z;
-        var transformation = Axis.Z_UP_TO_Y_UP;
-        var transformed = new Cartesian4();
-        Matrix4.multiplyByVector(transformation, upAxis, transformed);
-        var result = new Cartesian4();
-        Cartesian4.normalize(transformed, result);
-        expect(result).toEqualEpsilon(Cartesian4.UNIT_Y, CesiumMath.EPSILON1);
+        convertUpAxis(Cartesian4.UNIT_Z, Axis.Z_UP_TO_Y_UP, Cartesian4.UNIT_Y);
     });
 
     it('Convert x-up to y-up', function() {
-        var upAxis = Cartesian4.UNIT_X;
-        var transformation = Axis.X_UP_TO_Y_UP;
-        var transformed = new Cartesian4();
-        Matrix4.multiplyByVector(transformation, upAxis, transformed);
-        var result = new Cartesian4();
-        Cartesian4.normalize(transformed, result);
-        expect(result).toEqualEpsilon(Cartesian4.UNIT_Y, CesiumMath.EPSILON1);
+        convertUpAxis(Cartesian4.UNIT_X, Axis.X_UP_TO_Y_UP, Cartesian4.UNIT_Y);
     });
 
     it('Convert x-up to z-up', function() {
-        var upAxis = Cartesian4.UNIT_X;
-        var transformation = Axis.X_UP_TO_Z_UP;
-        var transformed = new Cartesian4();
-        Matrix4.multiplyByVector(transformation, upAxis, transformed);
-        var result = new Cartesian4();
-        Cartesian4.normalize(transformed, result);
-        expect(result).toEqualEpsilon(Cartesian4.UNIT_Z, CesiumMath.EPSILON1);
+        convertUpAxis(Cartesian4.UNIT_X, Axis.X_UP_TO_Z_UP, Cartesian4.UNIT_Z);
     });
-
-}, 'WebGL');
+});
