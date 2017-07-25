d4cc37537a2bd2af94abf1c2b3896146df314cca
byumjin <byumjin@seas.upenn.edu>
2017-07-25 14:27:26 -0400

    Added stepsize parameter at DOF
    Added stepsize parameter at DOF

--- Source/Scene/PostProcessDepthOfFieldStage.js
+++ Source/Scene/PostProcessDepthOfFieldStage.js
@@ -43,7 +43,8 @@ define([
         this._dofPostProcess = undefined;
 
         this._dofBlurUniformValues = {
-            kernelSize : 1.0
+            kernelSize : 1.0,
+            stepSize : 1.0
         };
 
         this._uniformValues = {
@@ -180,42 +181,179 @@ define([
         var dofBlurXShader =
             'uniform sampler2D u_colorTexture; \n' +
             'uniform float u_kernelSize; \n' +
+            'uniform float u_stepSize; \n' +
+            'precision highp float; \n' +
+
             'varying vec2 v_textureCoordinates; \n' +
+
             'void main(void) \n' +
             '{ \n' +
+
+            'float Blur7Coeff[4]; Blur7Coeff[0] = 0.3125; Blur7Coeff[1] = 0.234375; Blur7Coeff[2] = 0.09375; Blur7Coeff[3] = 0.015625; \n' +
+            'float Blur9Coeff[5]; Blur9Coeff[0] = 0.2734375; Blur9Coeff[1] = 0.21875; Blur9Coeff[2] = 0.109375; Blur9Coeff[3] = 0.03125; Blur9Coeff[4] = 0.00390625;\n' +
+            'float Blur11Coeff[6]; Blur11Coeff[0] = 0.24609375; Blur11Coeff[1] = 0.205078125; Blur11Coeff[2] = 0.1171875; Blur11Coeff[3] = 0.0439453125; Blur11Coeff[4] = 0.009765625; Blur11Coeff[5] = 0.0009765625;\n' +
+            'float Blur13Coeff[7]; Blur13Coeff[0] = 0.2255859375; Blur13Coeff[1] = 0.193359375; Blur13Coeff[2] = 0.120849609375; Blur13Coeff[3] = 0.0537109375; Blur13Coeff[4] = 0.01611328125; Blur13Coeff[5] = 0.0029296875; Blur13Coeff[6] = 0.000244140625;\n' +
+            'float Blur15Coeff[8]; Blur15Coeff[0] = 0.20947265625; Blur15Coeff[1] = 0.18328857421875; Blur15Coeff[2] = 0.1221923828125; Blur15Coeff[3] = 0.06109619140625; Blur15Coeff[4] = 0.022216796875; Blur15Coeff[5] = 0.00555419921875; Blur15Coeff[6] = 0.0008544921875; Blur15Coeff[7] = 0.00006103515625;\n' +
+            'float Blur17Coeff[9]; Blur17Coeff[0] = 0.196380615234375; Blur17Coeff[1] = 0.174560546875; Blur17Coeff[2] = 0.1221923828125; Blur17Coeff[3] = 0.066650390625; Blur17Coeff[4] = 0.02777099609375; Blur17Coeff[5] = 0.008544921875; Blur17Coeff[6] = 0.0018310546875; Blur17Coeff[7] = 0.000244140625; Blur17Coeff[8] = 0.0000152587890625;\n' +
+            'float Blur25Coeff[13]; Blur25Coeff[0] = 0.1611802577972412109375; Blur25Coeff[1] = 0.14878177642822265625; Blur25Coeff[2] = 0.116899967193603515625; Blur25Coeff[3] = 0.07793331146240234375; Blur25Coeff[4] = 0.043837487697601318359375; Blur25Coeff[5] = 0.020629405975341796875; Blur25Coeff[6] = 0.0080225467681884765625;\n' +
+            'Blur25Coeff[7] = 0.002533435821533203125; Blur25Coeff[8] = 0.00063335895538330078125; Blur25Coeff[9] = 0.000120639801025390625; Blur25Coeff[10] = 0.0000164508819580078125; Blur25Coeff[11] = 0.000001430511474609375; Blur25Coeff[12] = 0.000000059604644775390625;\n' +
+
             '    vec4 result = vec4(0.0); \n' +
-            '    vec2 recipalScreenSize = u_kernelSize / czm_viewport.zw; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(-recipalScreenSize.x*4.0, 0.0))*0.00390625; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(-recipalScreenSize.x*3.0, 0.0))*0.03125; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(-recipalScreenSize.x*2.0, 0.0))*0.109375; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(-recipalScreenSize.x, 0.0))*0.21875; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates)*0.2734375; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(recipalScreenSize.x, 0.0))*0.21875; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(recipalScreenSize.x*2.0, 0.0))*0.109375; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(recipalScreenSize.x*3.0, 0.0))*0.03125; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(recipalScreenSize.x*4.0, 0.0))*0.00390625; \n' +           
+            '    vec2 recipalScreenSize = u_stepSize / czm_viewport.zw; \n' +
+            '    if(u_kernelSize < 1.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i <= 3; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(recipalScreenSize.x * float(i), 0.0)) * Blur7Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(-recipalScreenSize.x * float(i), 0.0)) * Blur7Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur7Coeff[0]; \n' +
+            '    } \n' +
+            '    else if(u_kernelSize < 2.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i <= 4; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(recipalScreenSize.x * float(i), 0.0)) * Blur9Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(-recipalScreenSize.x * float(i), 0.0)) * Blur9Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur9Coeff[0]; \n' +
+            '    } \n' +
+            '    else if(u_kernelSize < 3.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i <= 5; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(recipalScreenSize.x * float(i), 0.0)) * Blur11Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(-recipalScreenSize.x * float(i), 0.0)) * Blur11Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur11Coeff[0]; \n' +
+            '    } \n' +
+            '    else if(u_kernelSize < 4.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i<=6; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(recipalScreenSize.x * float(i), 0.0)) * Blur13Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(-recipalScreenSize.x * float(i), 0.0)) * Blur13Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur13Coeff[0]; \n' +
+            '    } \n' +
+            '    else if(u_kernelSize < 5.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i<=7; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(recipalScreenSize.x * float(i), 0.0)) * Blur15Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(-recipalScreenSize.x * float(i), 0.0)) * Blur15Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur15Coeff[0]; \n' +
+            '    } \n' +
+            '    else if(u_kernelSize < 6.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i<=8; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(recipalScreenSize.x * float(i), 0.0)) * Blur17Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(-recipalScreenSize.x * float(i), 0.0)) * Blur17Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur17Coeff[0]; \n' +
+            '    } \n' +     
+            '    else \n' +
+            '    { \n' +
+            '      for(int i=1; i<=12; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(recipalScreenSize.x * float(i), 0.0)) * Blur25Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(-recipalScreenSize.x * float(i), 0.0)) * Blur25Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur25Coeff[0]; \n' +
+            '    } \n' +      
             '    gl_FragColor = result; \n' +
             '} \n';
 
         var dofBlurYShader =
             'uniform sampler2D u_colorTexture; \n' +
             'uniform float u_kernelSize; \n' +
+            'uniform float u_stepSize; \n' +
             'varying vec2 v_textureCoordinates; \n' +
+
+            'precision highp float; \n' +
+
             'void main(void) \n' +
             '{ \n' +
+            
+            'float Blur7Coeff[4]; Blur7Coeff[0] = 0.3125; Blur7Coeff[1] = 0.234375; Blur7Coeff[2] = 0.09375; Blur7Coeff[3] = 0.015625; \n' +
+            'float Blur9Coeff[5]; Blur9Coeff[0] = 0.2734375; Blur9Coeff[1] = 0.21875; Blur9Coeff[2] = 0.109375; Blur9Coeff[3] = 0.03125; Blur9Coeff[4] = 0.00390625;\n' +
+            'float Blur11Coeff[6]; Blur11Coeff[0] = 0.24609375; Blur11Coeff[1] = 0.205078125; Blur11Coeff[2] = 0.1171875; Blur11Coeff[3] = 0.0439453125; Blur11Coeff[4] = 0.009765625; Blur11Coeff[5] = 0.0009765625;\n' +
+            'float Blur13Coeff[7]; Blur13Coeff[0] = 0.2255859375; Blur13Coeff[1] = 0.193359375; Blur13Coeff[2] = 0.120849609375; Blur13Coeff[3] = 0.0537109375; Blur13Coeff[4] = 0.01611328125; Blur13Coeff[5] = 0.0029296875; Blur13Coeff[6] = 0.000244140625;\n' +
+            'float Blur15Coeff[8]; Blur15Coeff[0] = 0.20947265625; Blur15Coeff[1] = 0.18328857421875; Blur15Coeff[2] = 0.1221923828125; Blur15Coeff[3] = 0.06109619140625; Blur15Coeff[4] = 0.022216796875; Blur15Coeff[5] = 0.00555419921875; Blur15Coeff[6] = 0.0008544921875; Blur15Coeff[7] = 0.00006103515625;\n' +
+            'float Blur17Coeff[9]; Blur17Coeff[0] = 0.196380615234375; Blur17Coeff[1] = 0.174560546875; Blur17Coeff[2] = 0.1221923828125; Blur17Coeff[3] = 0.066650390625; Blur17Coeff[4] = 0.02777099609375; Blur17Coeff[5] = 0.008544921875; Blur17Coeff[6] = 0.0018310546875; Blur17Coeff[7] = 0.000244140625; Blur17Coeff[8] = 0.0000152587890625;\n' +
+            'float Blur25Coeff[13]; Blur25Coeff[0] = 0.1611802577972412109375; Blur25Coeff[1] = 0.14878177642822265625; Blur25Coeff[2] = 0.116899967193603515625; Blur25Coeff[3] = 0.07793331146240234375; Blur25Coeff[4] = 0.043837487697601318359375; Blur25Coeff[5] = 0.020629405975341796875; Blur25Coeff[6] = 0.0080225467681884765625;\n' +
+            'Blur25Coeff[7] = 0.002533435821533203125; Blur25Coeff[8] = 0.00063335895538330078125; Blur25Coeff[9] = 0.000120639801025390625; Blur25Coeff[10] = 0.0000164508819580078125; Blur25Coeff[11] = 0.000001430511474609375; Blur25Coeff[12] = 0.000000059604644775390625;\n' +
+
             '    vec4 result = vec4(0.0); \n' +
-            '    vec2 recipalScreenSize = u_kernelSize / czm_viewport.zw; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, -recipalScreenSize.y*4.0))*0.00390625; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, -recipalScreenSize.y*3.0))*0.03125; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, -recipalScreenSize.y*2.0))*0.109375; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, -recipalScreenSize.y))*0.21875; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates)*0.2734375; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, recipalScreenSize.y))*0.21875; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, recipalScreenSize.y*2.0))*0.109375; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, recipalScreenSize.y*3.0))*0.03125; \n' +
-            '    result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, recipalScreenSize.y*4.0))*0.00390625; \n' +           
+            '    vec2 recipalScreenSize = u_stepSize / czm_viewport.zw; \n' +
+            '    if(u_kernelSize < 1.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i <= 3; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, recipalScreenSize.y * float(i))) * Blur7Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, -recipalScreenSize.y * float(i))) * Blur7Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur7Coeff[0]; \n' +
+            '    } \n' +
+            '    else if(u_kernelSize < 2.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i <= 4; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, recipalScreenSize.y * float(i))) * Blur9Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, -recipalScreenSize.y * float(i))) * Blur9Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur9Coeff[0]; \n' +
+            '    } \n' +
+            '    else if(u_kernelSize < 3.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i <= 5; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, recipalScreenSize.y * float(i))) * Blur11Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, -recipalScreenSize.y * float(i))) * Blur11Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur11Coeff[0]; \n' +
+            '    } \n' +
+            '    else if(u_kernelSize < 4.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i<=6; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, recipalScreenSize.y * float(i))) * Blur13Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, -recipalScreenSize.y * float(i))) * Blur13Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur13Coeff[0]; \n' +
+            '    } \n' +
+            '    else if(u_kernelSize < 5.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i<=7; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, recipalScreenSize.y * float(i))) * Blur15Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, -recipalScreenSize.y * float(i))) * Blur15Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur15Coeff[0]; \n' +
+            '    } \n' +
+            '    else if(u_kernelSize < 6.0) \n' +
+            '    { \n' +
+            '      for(int i=1; i<=8; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, recipalScreenSize.y * float(i))) * Blur17Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, -recipalScreenSize.y * float(i))) * Blur17Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur17Coeff[0]; \n' +
+            '    } \n' +        
+            '    else \n' +
+            '    { \n' +
+            '      for(int i=1; i<=12; i++) \n' +
+            '      { \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, recipalScreenSize.y * float(i))) * Blur25Coeff[i]; \n' +
+            '         result += texture2D(u_colorTexture, v_textureCoordinates + vec2(0.0, -recipalScreenSize.y * float(i))) * Blur25Coeff[i]; \n' +
+            '      } \n' +
+            '      result += texture2D(u_colorTexture, v_textureCoordinates) * Blur25Coeff[0]; \n' +
+            '    } \n' +      
             '    gl_FragColor = result; \n' +
-            '} \n';        
+            '} \n';   
+           
 
         var dofBlurXStage = new PostProcessStage({
             fragmentShader : dofBlurXShader,
