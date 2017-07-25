375704e9e5339c5a5aac9a740c8e93c736b5c34e
ggetz <ggetz881@gmail.com>
2017-07-25 16:20:01 -0400

    Updated the cesiumjs.org bing key

--- Source/Core/BingMapsApi.js
+++ Source/Core/BingMapsApi.js
@@ -25,7 +25,7 @@ define([
      *
      * @type {String}
      */
-    BingMapsApi.defaultKey = 'AsarFiDvISunWhi137V7l5Bu80baB73npU98oTyjqKOb7NbrkiuBPZfDxgXTrGtQ'; // For use on cesiumjs.org only
+    BingMapsApi.defaultKey = 'Ahb3XqOqdrFv84q7cYVUwFvDzkr2Wemkhj5j8IczhV4ZkUP3KMm9uuxng0aGzt-Z'; // For use on cesiumjs.org only
 
     var printedBingWarning = false;
     var errorCredit;
