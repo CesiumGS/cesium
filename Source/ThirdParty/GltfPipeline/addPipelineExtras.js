a21416463ab06d95051de7e3b3d1ab97b3828cd7
Sean Lilley <lilleyse@gmail.com>
2017-09-01 09:31:24 -0400

    Small cleanup

--- Source/ThirdParty/GltfPipeline/addPipelineExtras.js
+++ Source/ThirdParty/GltfPipeline/addPipelineExtras.js
@@ -56,10 +56,9 @@ define([
         gltf.extras._pipeline = defaultValue(gltf.extras._pipeline, {});
         gltf.asset = defaultValue(gltf.asset, {});
         gltf.asset.extras = defaultValue(gltf.asset.extras, {});
-        if(defined(gltf.asset.extras) && typeof(gltf.asset.extras) !== 'object'){
-            var tmp = gltf.asset.extras;
+        if (defined(gltf.asset.extras) && typeof(gltf.asset.extras) !== 'object'){
             gltf.asset.extras = {
-                extras: tmp
+                extras : gltf.asset.extras
             };
         }
         gltf.asset.extras._pipeline = defaultValue(gltf.asset.extras._pipeline, {});
