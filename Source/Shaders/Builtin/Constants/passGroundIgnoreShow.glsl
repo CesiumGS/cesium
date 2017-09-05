56424309d7cca5945ab26a5dec6305bc1fe07236
Dan Bagnell <daniel.p.bagnell@gmail.com>
2017-09-05 15:40:37 -0400

    Fix constants.

--- Source/Shaders/Builtin/Constants/passGroundIgnoreShow.glsl
+++
@@ -1,9 +0,0 @@
-/**
- * The automatic GLSL constant for {@link Pass#GOURND_IGNORE_SHOW}
- *
- * @name czm_passGroundIgnoreShow
- * @glslConstant
- *
- * @see czm_pass
- */
-const float czm_passGroundIgnoreShow = 4.0;
