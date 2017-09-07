30d490ef98012bc015ae8b8e02ab8851707adf06
Dan Bagnell <daniel.p.bagnell@gmail.com>
2017-09-07 14:47:44 -0400

    Improve bivariate visibility test by changing the depth test.

--- Source/Scene/Cesium3DTileBatchTable.js
+++ Source/Scene/Cesium3DTileBatchTable.js
@@ -29,6 +29,7 @@ define([
         './BlendingState',
         './Cesium3DTileColorBlendMode',
         './CullFace',
+        './DepthFunction',
         './getBinaryAccessor',
         './StencilFunction',
         './StencilOperation'
@@ -63,6 +64,7 @@ define([
         BlendingState,
         Cesium3DTileColorBlendMode,
         CullFace,
+        DepthFunction,
         getBinaryAccessor,
         StencilFunction,
         StencilOperation) {
@@ -1342,6 +1344,9 @@ define([
             // selection depth to the stencil buffer to prevent ancestor tiles from drawing on top
             derivedCommand = DrawCommand.shallowClone(command);
             var rs = clone(derivedCommand.renderState, true);
+            if (rs.depthTest.enabled && rs.depthTest.func === DepthFunction.LESS) {
+                rs.depthTest.func = DepthFunction.LESS_OR_EQUAL;
+            }
             rs.stencilTest.enabled = true;
             rs.stencilTest.reference = reference;
             rs.stencilTest.frontFunction = StencilFunction.GREATER_OR_EQUAL;
