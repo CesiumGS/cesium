daa095f229e7304a4d1a1fd534c206ffbad9f28c
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2017-10-10 10:52:56 +0200

    Made TextureMagnification/MinificationFilter public and added API-docs.

--- Source/Renderer/TextureMagnificationFilter.js
+++ Source/Renderer/TextureMagnificationFilter.js
@@ -7,12 +7,30 @@ define([
     'use strict';
 
     /**
-     * @private
+     * Enumerates all possible filters used when magnifying WebGL textures, which takes places when zooming
+     * into imagery. Provides the possible values for the {@link ImageryLayer#magnificationFilter} property.
+     *
+     * @alias TextureMagnificationFilter
+     *
+     * @see TextureMinificationFilter
+     * @see ImageryLayer#magnificationFilter
      */
     var TextureMagnificationFilter = {
+        /**
+         * Nearest neighbor sampling of image pixels to texture.
+         */
         NEAREST : WebGLConstants.NEAREST,
+        /**
+         * Bi-linear interpolation of image pixels to texture.
+         */
         LINEAR : WebGLConstants.LINEAR,
 
+        /**
+         * Validates the given <code>textureMinificationFilter</code> with respect to the possible enum values.
+         *
+         * @param textureMagnificationFilter
+         * @returns {boolean} <code>true</code> if <code>textureMagnificationFilter</code> is valid.
+         */
         validate : function(textureMagnificationFilter) {
             return ((textureMagnificationFilter === TextureMagnificationFilter.NEAREST) ||
                     (textureMagnificationFilter === TextureMagnificationFilter.LINEAR));
--- Source/Renderer/TextureMinificationFilter.js
+++ Source/Renderer/TextureMinificationFilter.js
@@ -7,16 +7,46 @@ define([
     'use strict';
 
     /**
-     * @private
+     * Enumerates all possible filters used when minifying WebGL textures, which takes places when zooming
+     * out of imagery. Provides the possible values for the {@link ImageryLayer#minificationFilter} property.
+     *
+     * @alias TextureMinificationFilter
+     *
+     * @see TextureMagnificationFilter
+     * @see ImageryLayer#minificationFilter
      */
     var TextureMinificationFilter = {
+        /**
+         * Nearest neighbor sampling of image pixels to texture.
+         */
         NEAREST : WebGLConstants.NEAREST,
+        /**
+         * Bi-linear interpolation of image pixels to texture.
+         */
         LINEAR : WebGLConstants.LINEAR,
+        /**
+         * WebGL <code>NEAREST_MIPMAP_NEAREST</code> interpolation of image pixels to texture.
+         */
         NEAREST_MIPMAP_NEAREST : WebGLConstants.NEAREST_MIPMAP_NEAREST,
+        /**
+         * WebGL <code>LINEAR_MIPMAP_NEAREST</code> interpolation of image pixels to texture.
+         */
         LINEAR_MIPMAP_NEAREST : WebGLConstants.LINEAR_MIPMAP_NEAREST,
+        /**
+         * WebGL <code>NEAREST_MIPMAP_LINEAR</code> interpolation of image pixels to texture.
+         */
         NEAREST_MIPMAP_LINEAR : WebGLConstants.NEAREST_MIPMAP_LINEAR,
+        /**
+         * WebGL <code>LINEAR_MIPMAP_LINEAR</code> interpolation of image pixels to texture.
+         */
         LINEAR_MIPMAP_LINEAR : WebGLConstants.LINEAR_MIPMAP_LINEAR,
 
+        /**
+         * Validates the given <code>textureMinificationFilter</code> with respect to the possible enum values.
+         *
+         * @param textureMinificationFilter
+         * @returns {boolean} <code>true</code> if <code>textureMinificationFilter</code> is valid.
+         */
         validate : function(textureMinificationFilter) {
             return ((textureMinificationFilter === TextureMinificationFilter.NEAREST) ||
                     (textureMinificationFilter === TextureMinificationFilter.LINEAR) ||
