64106b3e23a52fda1428ab4921e0577ce31a5e6e
JeremyMarzano-ISPA <jmarzano-sub@novetta.com>
2017-11-09 10:03:20 -0500

    Added a function to remove duplicate namespaces in the KML file

--- Source/DataSources/KmlDataSource.js
+++ Source/DataSources/KmlDataSource.js
@@ -282,6 +282,26 @@ define([
         return text;
     }
 
+    function removeDuplicateNamespaces(text) {
+        var index = text.indexOf("xmlns:");
+        var endDeclaration = text.indexOf(">", index);
+        var namespace, startIndex, endIndex;
+
+        while ((index !== -1) && (index < endDeclaration)) {
+            namespace = text.slice(index, text.indexOf("\"", index));
+            startIndex = index;
+            index = text.indexOf(namespace, index + 1);
+            if (index !== -1) {
+                endIndex = text.indexOf("\"", (text.indexOf("\"", index) + 1));
+                text = text.slice(0, index -1) + text.slice(endIndex + 1, text.length);
+            }
+            index = text.indexOf("xmlns:", startIndex + 1);
+        }
+
+        return text;
+    }
+
+
     function loadXmlFromZip(reader, entry, uriResolver, deferred) {
         entry.getData(new zip.TextWriter(), function(text) {
             uriResolver.kml = parser.parseFromString(text, 'application/xml');
@@ -2367,6 +2387,9 @@ define([
 
                             //Insert missing namespaces
                             text = insertNamespaces(text);
+
+                            //Remove Duplicate Namespaces
+                            text = removeDuplicateNamespaces(text);
 
                             //IE raises an exception
                             var kml;
