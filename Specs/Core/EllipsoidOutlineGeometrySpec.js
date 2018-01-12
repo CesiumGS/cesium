96414d7101a2aab5456080d529f85edc87bb20a0
srtrotter <26775896+srtrotter@users.noreply.github.com>
2018-01-12 07:20:57 -0600

    Added unit tests for outlines on partial ellipsoids

--- Specs/Core/EllipsoidOutlineGeometrySpec.js
+++ Specs/Core/EllipsoidOutlineGeometrySpec.js
@@ -61,13 +61,30 @@ defineSuite([
 
     it('computes positions', function() {
         var m = EllipsoidOutlineGeometry.createGeometry(new EllipsoidOutlineGeometry({
-            stackPartitions : 3,
+            stackPartitions: 3,
             slicePartitions: 3,
             subdivisions: 3
         }));
 
         expect(m.attributes.position.values.length).toEqual(14 * 3);
         expect(m.indices.length).toEqual(15 * 2);
+        expect(m.boundingSphere.radius).toEqual(1);
+    });
+
+    it('computes positions for partial ellipsoid', function() {
+        var m = EllipsoidOutlineGeometry.createGeometry(new EllipsoidOutlineGeometry({
+            innerRadii: new Cartesian3(0.5, 0.5, 0.5),
+            minimumClock: CesiumMath.toRadians(90.0),
+            maximumClock: CesiumMath.toRadians(270.0),
+            minimumCone: CesiumMath.toRadians(30.0),
+            maximumCone: CesiumMath.toRadians(120.0),
+            stackPartitions: 3,
+            slicePartitions: 3,
+            subdivisions: 3
+        }));
+
+        expect(m.attributes.position.values.length).toEqual(36 * 3);
+        expect(m.indices.length).toEqual(30 * 2);
         expect(m.boundingSphere.radius).toEqual(1);
     });
 
@@ -90,6 +107,30 @@ defineSuite([
         var ellipsoidOutline5 = new EllipsoidOutlineGeometry({
             radii : new Cartesian3(1000000.0, 500000.0, -10.0)
         });
+        var ellipsoidOutline6 = new EllipsoidOutlineGeometry({
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(0.0, 100000.0, 100000.0)
+        });
+        var ellipsoidOutline7 = new EllipsoidOutlineGeometry({
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(100000.0, 0.0, 100000.0)
+        });
+        var ellipsoidOutline8 = new EllipsoidOutlineGeometry({
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(100000.0, 100000.0, 0.0)
+        });
+        var ellipsoidOutline9 = new EllipsoidOutlineGeometry({
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(-10.0, 100000.0, 100000.0)
+        });
+        var ellipsoidOutline10 = new EllipsoidOutlineGeometry({
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(100000.0, -10.0, 100000.0)
+        });
+        var ellipsoidOutline11 = new EllipsoidOutlineGeometry({
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(100000.0, 100000.0, -10.0)
+        });
 
         var geometry0 = EllipsoidOutlineGeometry.createGeometry(ellipsoidOutline0);
         var geometry1 = EllipsoidOutlineGeometry.createGeometry(ellipsoidOutline1);
@@ -97,6 +138,12 @@ defineSuite([
         var geometry3 = EllipsoidOutlineGeometry.createGeometry(ellipsoidOutline3);
         var geometry4 = EllipsoidOutlineGeometry.createGeometry(ellipsoidOutline4);
         var geometry5 = EllipsoidOutlineGeometry.createGeometry(ellipsoidOutline5);
+        var geometry6 = EllipsoidOutlineGeometry.createGeometry(ellipsoidOutline6);
+        var geometry7 = EllipsoidOutlineGeometry.createGeometry(ellipsoidOutline7);
+        var geometry8 = EllipsoidOutlineGeometry.createGeometry(ellipsoidOutline8);
+        var geometry9 = EllipsoidOutlineGeometry.createGeometry(ellipsoidOutline9);
+        var geometry10 = EllipsoidOutlineGeometry.createGeometry(ellipsoidOutline10);
+        var geometry11 = EllipsoidOutlineGeometry.createGeometry(ellipsoidOutline11);
 
         expect(geometry0).toBeUndefined();
         expect(geometry1).toBeUndefined();
@@ -104,14 +151,31 @@ defineSuite([
         expect(geometry3).toBeUndefined();
         expect(geometry4).toBeUndefined();
         expect(geometry5).toBeUndefined();
+        expect(geometry6).toBeUndefined();
+        expect(geometry7).toBeUndefined();
+        expect(geometry8).toBeUndefined();
+        expect(geometry9).toBeUndefined();
+        expect(geometry10).toBeUndefined();
+        expect(geometry11).toBeUndefined();
     });
 
     var ellipsoidgeometry = new EllipsoidOutlineGeometry({
         radii : new Cartesian3(1.0, 2.0, 3.0),
-        slicePartitions: 3,
-        stackPartitions: 3,
+        innerRadii : new Cartesian3(0.5, 0.6, 0.7),
+        minimumClock : 0.1,
+        maximumClock : 0.2,
+        minimumCone : 0.3,
+        maximumCone : 0.4,
+        slicePartitions : 3,
+        stackPartitions : 3,
         subdivisions: 3
     });
-    var packedInstance = [1.0, 2.0, 3.0, 3.0, 3.0, 3.0];
+    var packedInstance = [
+        1.0, 2.0, 3.0,
+        0.5, 0.6, 0.7,
+        0.1, 0.2,
+        0.3, 0.4,
+        3.0, 3.0, 3.0
+    ];
     createPackableSpecs(EllipsoidOutlineGeometry, ellipsoidgeometry, packedInstance);
 });
