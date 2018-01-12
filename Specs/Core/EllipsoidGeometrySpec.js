c4d640102c19739afd6f9c1bb6298d0b0cfe4ea2
srtrotter <26775896+srtrotter@users.noreply.github.com>
2018-01-12 07:12:30 -0600

    Added unit tests for partial ellipsoids

--- Specs/Core/EllipsoidGeometrySpec.js
+++ Specs/Core/EllipsoidGeometrySpec.js
@@ -51,8 +51,10 @@ defineSuite([
             stackPartitions: 3
         }));
 
-        var numVertices = 16; // 4 rows * 4 positions
-        var numTriangles = 12; //3 top + 3 bottom + 6 around the sides
+        // The vertices are 6x6 because an additional slice and stack are added
+        // and the first and last clock and cone angles are duplicated (3 + 1 + 2 = 6)
+        var numVertices = 36; // 6 rows * 6 positions
+        var numTriangles = 18; // 6 top + 6 bottom + 6 around the sides
         expect(m.attributes.position.values.length).toEqual(numVertices * 3);
         expect(m.indices.length).toEqual(numTriangles * 3);
         expect(m.boundingSphere.radius).toEqual(1);
@@ -65,8 +67,8 @@ defineSuite([
             stackPartitions: 3
         }));
 
-        var numVertices = 16;
-        var numTriangles = 12;
+        var numVertices = 36;
+        var numTriangles = 18;
         expect(m.attributes.position.values.length).toEqual(numVertices * 3);
         expect(m.attributes.st.values.length).toEqual(numVertices * 2);
         expect(m.attributes.normal.values.length).toEqual(numVertices * 3);
@@ -100,7 +102,76 @@ defineSuite([
         }
     });
 
-    it('undefined is returned if the x, y, or z radii are equal or less than zero', function() {
+    it('computes positions with inner surface', function() {
+        var m = EllipsoidGeometry.createGeometry(new EllipsoidGeometry({
+            vertexFormat : VertexFormat.POSITION_ONLY,
+            slicePartitions: 3,
+            stackPartitions: 3,
+            innerRadii: new Cartesian3(0.5, 0.5, 0.5)
+        }));
+
+        var numVertices = 72; // 6 rows * 6 positions * 2 surfaces
+        var numTriangles = 36; // (6 top + 6 bottom + 6 around the sides) * 2 surfaces
+        expect(m.attributes.position.values.length).toEqual(numVertices * 3);
+        expect(m.indices.length).toEqual(numTriangles * 3);
+        expect(m.boundingSphere.radius).toEqual(1);
+    });
+
+    it('computes positions with inner surface and partial clock range', function() {
+        var m = EllipsoidGeometry.createGeometry(new EllipsoidGeometry({
+            vertexFormat : VertexFormat.POSITION_ONLY,
+            slicePartitions: 4,
+            stackPartitions: 4,
+            innerRadii: new Cartesian3(0.5, 0.5, 0.5),
+            minimumClock: CesiumMath.toRadians(90.0),
+            maximumClock: CesiumMath.toRadians(270.0)
+        }));
+
+        var numVertices = 70;
+        var numTriangles = 48;
+        expect(m.attributes.position.values.length).toEqual(numVertices * 3);
+        expect(m.indices.length).toEqual(numTriangles * 3);
+        expect(m.boundingSphere.radius).toEqual(1);
+    });
+
+    it('computes positions with inner surface and partial clock range and open top', function() {
+        var m = EllipsoidGeometry.createGeometry(new EllipsoidGeometry({
+            vertexFormat : VertexFormat.POSITION_ONLY,
+            slicePartitions: 4,
+            stackPartitions: 4,
+            innerRadii: new Cartesian3(0.5, 0.5, 0.5),
+            minimumClock: CesiumMath.toRadians(90.0),
+            maximumClock: CesiumMath.toRadians(270.0),
+            minimumCone: CesiumMath.toRadians(30.0)
+        }));
+
+        var numVertices = 60;
+        var numTriangles = 40;
+        expect(m.attributes.position.values.length).toEqual(numVertices * 3);
+        expect(m.indices.length).toEqual(numTriangles * 3);
+        expect(m.boundingSphere.radius).toEqual(1);
+    });
+
+    it('computes positions with inner surface and partial clock range and open top and bottom', function() {
+        var m = EllipsoidGeometry.createGeometry(new EllipsoidGeometry({
+            vertexFormat : VertexFormat.POSITION_ONLY,
+            slicePartitions: 4,
+            stackPartitions: 4,
+            innerRadii: new Cartesian3(0.5, 0.5, 0.5),
+            minimumClock: CesiumMath.toRadians(90.0),
+            maximumClock: CesiumMath.toRadians(270.0),
+            minimumCone: CesiumMath.toRadians(30.0),
+            maximumCone: CesiumMath.toRadians(120.0)
+        }));
+
+        var numVertices = 50;
+        var numTriangles = 32;
+        expect(m.attributes.position.values.length).toEqual(numVertices * 3);
+        expect(m.indices.length).toEqual(numTriangles * 3);
+        expect(m.boundingSphere.radius).toEqual(1);
+    });
+
+    it('undefined is returned if the x, y, or z radii or innerRadii are equal or less than zero', function() {
         var ellipsoid0 = new EllipsoidGeometry({
             vertexFormat : VertexFormat.POSITION_ONLY,
             radii : new Cartesian3(0.0, 500000.0, 500000.0)
@@ -125,6 +196,36 @@ defineSuite([
             vertexFormat : VertexFormat.POSITION_ONLY,
             radii : new Cartesian3(1000000.0, 500000.0, -10.0)
         });
+        var ellipsoid6 = new EllipsoidGeometry({
+            vertexFormat : VertexFormat.POSITION_ONLY,
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(0.0, 100000.0, 100000.0)
+        });
+        var ellipsoid7 = new EllipsoidGeometry({
+            vertexFormat : VertexFormat.POSITION_ONLY,
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(100000.0, 0.0, 100000.0)
+        });
+        var ellipsoid8 = new EllipsoidGeometry({
+            vertexFormat : VertexFormat.POSITION_ONLY,
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(100000.0, 100000.0, 0.0)
+        });
+        var ellipsoid9 = new EllipsoidGeometry({
+            vertexFormat : VertexFormat.POSITION_ONLY,
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(-10.0, 100000.0, 100000.0)
+        });
+        var ellipsoid10 = new EllipsoidGeometry({
+            vertexFormat : VertexFormat.POSITION_ONLY,
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(100000.0, -10.0, 100000.0)
+        });
+        var ellipsoid11 = new EllipsoidGeometry({
+            vertexFormat : VertexFormat.POSITION_ONLY,
+            radii : new Cartesian3(500000.0, 500000.0, 500000.0),
+            innerRadii : new Cartesian3(100000.0, 100000.0, -10.0)
+        });
 
         var geometry0 = EllipsoidGeometry.createGeometry(ellipsoid0);
         var geometry1 = EllipsoidGeometry.createGeometry(ellipsoid1);
@@ -132,6 +233,12 @@ defineSuite([
         var geometry3 = EllipsoidGeometry.createGeometry(ellipsoid3);
         var geometry4 = EllipsoidGeometry.createGeometry(ellipsoid4);
         var geometry5 = EllipsoidGeometry.createGeometry(ellipsoid5);
+        var geometry6 = EllipsoidGeometry.createGeometry(ellipsoid6);
+        var geometry7 = EllipsoidGeometry.createGeometry(ellipsoid7);
+        var geometry8 = EllipsoidGeometry.createGeometry(ellipsoid8);
+        var geometry9 = EllipsoidGeometry.createGeometry(ellipsoid9);
+        var geometry10 = EllipsoidGeometry.createGeometry(ellipsoid10);
+        var geometry11 = EllipsoidGeometry.createGeometry(ellipsoid11);
 
         expect(geometry0).toBeUndefined();
         expect(geometry1).toBeUndefined();
@@ -139,14 +246,32 @@ defineSuite([
         expect(geometry3).toBeUndefined();
         expect(geometry4).toBeUndefined();
         expect(geometry5).toBeUndefined();
+        expect(geometry6).toBeUndefined();
+        expect(geometry7).toBeUndefined();
+        expect(geometry8).toBeUndefined();
+        expect(geometry9).toBeUndefined();
+        expect(geometry10).toBeUndefined();
+        expect(geometry11).toBeUndefined();
     });
 
     var ellipsoidgeometry = new EllipsoidGeometry({
         vertexFormat : VertexFormat.POSITION_ONLY,
         radii : new Cartesian3(1.0, 2.0, 3.0),
-        slicePartitions: 3,
-        stackPartitions: 3
+        innerRadii : new Cartesian3(0.5, 0.6, 0.7),
+        minimumClock : 0.1,
+        maximumClock : 0.2,
+        minimumCone : 0.3,
+        maximumCone : 0.4,
+        slicePartitions : 3,
+        stackPartitions : 3
     });
-    var packedInstance = [1.0, 2.0, 3.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 3.0, 3.0];
+    var packedInstance = [
+        1.0, 2.0, 3.0,
+        0.5, 0.6, 0.7,
+        1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.1, 0.2,
+        0.3, 0.4,
+        3.0, 3.0
+    ];
     createPackableSpecs(EllipsoidGeometry, ellipsoidgeometry, packedInstance);
 });
