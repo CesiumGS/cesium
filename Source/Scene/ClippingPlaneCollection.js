7ecbbc048c032632460e82c303ff9415c43c369a
Hannah <pinkos.hannah@gmail.com>
2018-03-16 13:48:12 -0400

    whitespace

--- Source/Scene/ClippingPlaneCollection.js
+++ Source/Scene/ClippingPlaneCollection.js
@@ -712,8 +712,6 @@ define([
      * <code>isDestroyed</code> will result in a {@link DeveloperError} exception.  Therefore,
      * assign the return value (<code>undefined</code>) to the object as done in the example.
      *
-     * 
-     *
      * @exception {DeveloperError} This object was destroyed, i.e., destroy() was called.
      *
      *
