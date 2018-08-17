1c9e362ae3515e5619975087a52acf1ca85049df
hpinkos <pinkos.hannah@gmail.com>
2018-08-17 19:21:07 -0400

    next: ambient occlusion [ci skip]

--- Apps/Sandcastle/Sandcastle-header.js
+++ Apps/Sandcastle/Sandcastle-header.js
@@ -63,6 +63,56 @@
             button.textContent = text;
             document.getElementById(toolbarID || 'toolbar').appendChild(button);
         },
+        addSlider : function(text, options, onChange, toolbarID){
+            var div = document.createElement('div');
+            div.stylemargin = '2px 3px';
+            var label = document.createElement('div');
+
+            label.textContent = text;
+            label.style['font-size'] = '13px';
+            label.style.width = '85px';
+            label.style.display = 'inline-block';
+            label.style.color = '#edffff';
+
+            var min = typeof options.minimum === 'number' ? options.minimum : 0.0;
+            var max = typeof options.maximum === 'number' ? options.maximum : 100.0;
+            var step = typeof options.stepSize === 'number' ? options.stepSize : 1.0;
+            var value = typeof options.value === 'number' ? options.value : min;
+
+            var range = document.createElement('input');
+            range.type = 'range';
+            range.min = min;
+            range.max = max;
+            range.step = step;
+            range.value = value;
+
+            var textbox = document.createElement('input');
+            textbox.type = 'number';
+            textbox.min = min;
+            textbox.max = max;
+            textbox.step = step;
+            textbox.value = value;
+            textbox.style['margin-left'] = '3px';
+            textbox.style['width'] = '55px';
+
+            range.oninput = function(){
+                var v = range.value;
+                textbox.value = v;
+                onChange(Number(v));
+            };
+
+            textbox.oninput = function() {
+                var v = textbox.value;
+                range.value = v;
+                onChange(Number(v));
+            };
+
+            div.appendChild(label);
+            div.appendChild(range);
+            div.appendChild(textbox);
+
+            document.getElementById(toolbarID || 'toolbar').appendChild(div);
+        },
         addDefaultToolbarButton : function(text, onclick, toolbarID) {
             window.Sandcastle.addToolbarButton(text, onclick, toolbarID);
             defaultAction = onclick;
