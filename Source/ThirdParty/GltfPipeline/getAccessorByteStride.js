51969fecda79334ed2a8848dea8c7f460c6cb2d3
Hou,Chunlei <omni360@qq.com>
2018-10-10 20:57:00 +0800

    revert thirdparty library

---
+++ Source/ThirdParty/GltfPipeline/getAccessorByteStride.js
@@ -0,0 +1,33 @@
+define([
+        './numberOfComponentsForType',
+        '../../Core/ComponentDatatype',
+        '../../Core/defined'
+    ], function(
+        numberOfComponentsForType,
+        ComponentDatatype,
+        defined) {
+    'use strict';
+
+    /**
+     * Returns the byte stride of the provided accessor.
+     * If the byteStride is 0, it is calculated based on type and componentType
+     *
+     * @param {Object} gltf A javascript object containing a glTF asset.
+     * @param {Object} accessor The accessor.
+     * @returns {Number} The byte stride of the accessor.
+     *
+     * @private
+     */
+    function getAccessorByteStride(gltf, accessor) {
+        var bufferViewId = accessor.bufferView;
+        if (defined(bufferViewId)) {
+            var bufferView = gltf.bufferViews[bufferViewId];
+            if (defined(bufferView.byteStride) && bufferView.byteStride > 0) {
+                return bufferView.byteStride;
+            }
+        }
+        return ComponentDatatype.getSizeInBytes(accessor.componentType) * numberOfComponentsForType(accessor.type);
+    }
+
+    return getAccessorByteStride;
+});
---
+++ Source/ThirdParty/GltfPipeline/removePipelineExtras.js
@@ -0,0 +1,51 @@
+define([
+        './ForEach',
+        '../../Core/defined'
+    ], function(
+        ForEach,
+        defined) {
+    'use strict';
+
+    /**
+     * Iterate through the objects within the glTF and delete their pipeline extras object.
+     *
+     * @param {Object} gltf A javascript object containing a glTF asset.
+     * @returns {Object} glTF with no pipeline extras.
+     *
+     * @private
+     */
+    function removePipelineExtras(gltf) {
+        ForEach.shader(gltf, function(shader) {
+            removeExtras(shader);
+        });
+        ForEach.buffer(gltf, function(buffer) {
+            removeExtras(buffer);
+        });
+        ForEach.image(gltf, function (image) {
+            removeExtras(image);
+            ForEach.compressedImage(image, function(compressedImage) {
+                removeExtras(compressedImage);
+            });
+        });
+
+        removeExtras(gltf);
+
+        return gltf;
+    }
+
+    function removeExtras(object) {
+        if (!defined(object.extras)) {
+            return;
+        }
+
+        if (defined(object.extras._pipeline)) {
+            delete object.extras._pipeline;
+        }
+
+        if (Object.keys(object.extras).length === 0) {
+            delete object.extras;
+        }
+    }
+
+    return removePipelineExtras;
+});
