aaa7d8ad4fa71a2364a1a27ff4863f876a0f538a
Aristeidis Bampakos <bampakoa@gmail.com>
2018-10-31 19:37:51 +0200

    Reverting Intersect

--- Source/Core/Intersect.js
+++ Source/Core/Intersect.js
@@ -1,27 +1,42 @@
 define([
-        './freezeObject'
-    ], function(
-        freezeObject) {
-    'use strict';
+    './freezeObject'
+], function(
+    freezeObject) {
+'use strict';
+
+/**
+ * This enumerated type is used in determining where, relative to the frustum, an
+ * object is located. The object can either be fully contained within the frustum (INSIDE),
+ * partially inside the frustum and partially outside (INTERSECTING), or somwhere entirely
+ * outside of the frustum's 6 planes (OUTSIDE).
+ *
+ * @exports Intersect
+ */
+var Intersect = {
+    /**
+     * Represents that an object is not contained within the frustum.
+     *
+     * @type {Number}
+     * @constant
+     */
+    OUTSIDE : -1,
 
     /**
-     * This enumerated type is used in determining where, relative to the frustum, an
-     * object is located. The object can either be fully contained within the frustum (INSIDE),
-     * partially inside the frustum and partially outside (INTERSECTING), or somwhere entirely
-     * outside of the frustum's 6 planes (OUTSIDE).
+     * Represents that an object intersects one of the frustum's planes.
      *
-     * @exports Intersect
-     * @typedef Intersect
-     * @type {Object}
-     * @property {Number} OUTSIDE Represents that an object is not contained within the frustum.
-     * @property {Number} INTERSECTING Represents that an object intersects one of the frustum's planes.
-     * @property {Number} INSIDE Represents that an object is fully within the frustum.
+     * @type {Number}
+     * @constant
      */
-    var Intersect = {
-        OUTSIDE : -1,
-        INTERSECTING : 0,
-        INSIDE : 1
-    };
+    INTERSECTING : 0,
 
-    return freezeObject(Intersect);
+    /**
+     * Represents that an object is fully within the frustum.
+     *
+     * @type {Number}
+     * @constant
+     */
+    INSIDE : 1
+};
+
+return freezeObject(Intersect);
 });
