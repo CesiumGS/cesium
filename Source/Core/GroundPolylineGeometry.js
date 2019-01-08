bd83b8f3f34bffedb102a523766c1f90766f4605
Chris Cooper <chris@propelleraero.com>
2019-01-08 14:40:32 +1100

    improve the hacks

--- Source/Core/GroundPolylineGeometry.js
+++ Source/Core/GroundPolylineGeometry.js
@@ -70,7 +70,9 @@ define([
     // Ellipsoid height is generally much closer.
     // The initial max height is arbitrary.
     // Both heights are corrected using ApproximateTerrainHeights for computing the actual volume geometry.
-    var WALL_INITIAL_MIN_HEIGHT = 0.0;
+
+    // PROPELLER HACK: Allow rendering of ground primitives with negative WGS84 elevation
+    var WALL_INITIAL_MIN_HEIGHT = -250;
     var WALL_INITIAL_MAX_HEIGHT = 1000.0;
 
     /**
