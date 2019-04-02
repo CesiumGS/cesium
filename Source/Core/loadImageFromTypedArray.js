b53756021805f576255e34e5000de5368842f71e
Shehata <oshehata@agi.com>
2019-04-02 14:50:30 -0400

    Review feedback

--- Source/Core/loadImageFromTypedArray.js
+++ Source/Core/loadImageFromTypedArray.js
@@ -21,7 +21,7 @@ define([
         var uint8Array = options.uint8Array;
         var format = options.format;
         var request = options.request;
-        var flipY = defaultValue(options.flipY, true);
+        var flipY = defaultValue(options.flipY, false);
         //>>includeStart('debug', pragmas.debug);
         Check.typeOf.object('uint8Array', uint8Array);
         Check.typeOf.string('format', format);
