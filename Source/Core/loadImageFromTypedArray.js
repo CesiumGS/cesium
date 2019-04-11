16322780a4cfc813b8829c5e23d0826b0f8ffd39
loshjawrence <loshjawrence@gmail.com>
2019-04-11 10:36:10 -0400

    lint

--- Source/Core/loadImageFromTypedArray.js
+++ Source/Core/loadImageFromTypedArray.js
@@ -20,7 +20,6 @@ define([
     function loadImageFromTypedArray(options) {
         var uint8Array = options.uint8Array;
         var format = options.format;
-        var request = options.request;
         var flipY = defaultValue(options.flipY, false);
         //>>includeStart('debug', pragmas.debug);
         Check.typeOf.object('uint8Array', uint8Array);
