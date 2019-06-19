38bab8880d6097f01e919adbc2209bb651511713
Zhouhai <zhouhai@Zhouhais-iMac.local>
2019-06-19 23:07:08 +0800

    fix: 3dtile dark
    fix: 3dtile dark

--- Source/Shaders/Builtin/Functions/ellipsoidContainsPoint.glsl
+++ Source/Shaders/Builtin/Functions/ellipsoidContainsPoint.glsl
@@ -5,8 +5,8 @@
  * @glslFunction
  *
  */
-bool czm_ellipsoidContainsPoint(czm_ellipsoid ellipsoid, vec3 point)
+bool czm_ellipsoidContainsPoint(vec3 ellipsoid_inverseRadii, vec3 point)
 {
-    vec3 scaled = ellipsoid.inverseRadii * (czm_inverseModelView * vec4(point, 1.0)).xyz;
+    vec3 scaled = ellipsoid_inverseRadii * (czm_inverseModelView * vec4(point, 1.0)).xyz;
     return (dot(scaled, scaled) <= 1.0);
 }
