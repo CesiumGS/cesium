17c8fd0f50e35db7da9e4f48be190eed3ca8c358
Tom Fili <tfili@agi.com>
2019-06-19 13:55:24 -0400

    eslint

--- Specs/DataSources/KmlExporterSpec.js
+++ Specs/DataSources/KmlExporterSpec.js
@@ -9,7 +9,6 @@ defineSuite([
     'Core/Iso8601',
     'Core/JulianDate',
     'Core/Math',
-    'Core/PerspectiveFrustum',
     'Core/PolygonHierarchy',
     'Core/Rectangle',
     'Core/TimeInterval',
@@ -18,7 +17,6 @@ defineSuite([
     'DataSources/Entity',
     'DataSources/EntityCollection',
     'DataSources/ImageMaterialProperty',
-    'DataSources/KmlDataSource',
     'DataSources/PolylineOutlineMaterialProperty',
     'DataSources/SampledPositionProperty',
     'Scene/HeightReference',
@@ -35,7 +33,6 @@ defineSuite([
     Iso8601,
     JulianDate,
     CesiumMath,
-    PerspectiveFrustum,
     PolygonHierarchy,
     Rectangle,
     TimeInterval,
@@ -44,48 +41,12 @@ defineSuite([
     Entity,
     EntityCollection,
     ImageMaterialProperty,
-    KmlDataSource,
     PolylineOutlineMaterialProperty,
     SampledPositionProperty,
     HeightReference,
     HorizontalOrigin,
     VerticalOrigin) {
         'use strict';
-
-        function download(filename, data) {
-            var blob = new Blob([data], { type: 'application/xml' });
-            if (window.navigator.msSaveOrOpenBlob) {
-                window.navigator.msSaveBlob(blob, filename);
-            } else {
-                var elem = window.document.createElement('a');
-                elem.href = window.URL.createObjectURL(blob);
-                elem.download = filename;
-                document.body.appendChild(elem);
-                elem.click();
-                document.body.removeChild(elem);
-            }
-        }
-
-        var options = {
-            camera: {
-                positionWC: new Cartesian3(0.0, 0.0, 0.0),
-                directionWC: new Cartesian3(0.0, 0.0, 1.0),
-                upWC: new Cartesian3(0.0, 1.0, 0.0),
-                pitch: 0.0,
-                heading: 0.0,
-                frustum: new PerspectiveFrustum(),
-                computeViewRectangle: function() {
-                    return Rectangle.MAX_VALUE;
-                },
-                pickEllipsoid: function() {
-                    return undefined;
-                }
-            },
-            canvas: {
-                clientWidth: 512,
-                clientHeight: 512
-            }
-        };
 
         function checkKmlDoc(kmlDoc, properties) {
             var kml = kmlDoc.documentElement;
