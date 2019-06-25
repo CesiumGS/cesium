9d511d664b0324dc20b670c1d9aadc2d68338fc8
Tom Fili <tfili@agi.com>
2019-06-25 11:46:15 -0400

    Updated Doc

--- Source/DataSources/exportKml.js
+++ Source/DataSources/exportKml.js
@@ -128,7 +128,7 @@ define([
         ExternalFileHandler.prototype.model = function(model, time) {
             var modelCallback = this._modelCallback;
             if (!defined(modelCallback)) {
-                throw new RuntimeError('Model callback must be specified if there are models in the entity collection');
+                throw new RuntimeError('Encountered a model entity while exporting to KML, but no model callback was supplied.');
             }
 
             return modelCallback(model, time);
@@ -238,24 +238,25 @@ define([
         };
 
         /**
-         * Exports an EntityCollection as a KML document. Only Point, Billboard, Model, Path, Polygon, Polyline geometries will be exported. If your
-         * document contains a Model, you must specify a modelCallback to handle the conversion as KML doesn't allow for glTF models. There is not
-         * a 1 to 1 mapping of Entity properties to KML Feature properties, so there are some caveats. Entity properties that are time dynamic but
-         * cannot be dynamic in KML are exported with their values at options.time or the beginning of the EntityCollection if not specified. For time-dynamic
-         * properties that are supported in KML, we use the samples if it is a SampledProperty otherwise we sample the value using the options.sampleDuration.
-         * Point, Billboard, Model and Path geometries with time-dynamic positions will be exported as gx:Track Features. Not all Materials are representable
-         * in KML, so for more advanced Materials just the primary color is used. Canvas objects are exported as png images.
+         * Exports an EntityCollection as a KML document. Only Point, Billboard, Model, Path, Polygon, Polyline geometries
+         * will be exported. Note that there is not a 1 to 1 mapping of Entity properties to KML Feature properties. For
+         * example, entity properties that are time dynamic but cannot be dynamic in KML are exported with their values at
+         * options.time or the beginning of the EntityCollection's time interval if not specified. For time-dynamic properties
+         * that are supported in KML, we use the samples if it is a {@link SampledProperty} otherwise we sample the value using
+         * the options.sampleDuration. Point, Billboard, Model and Path geometries with time-dynamic positions will be exported
+         * as gx:Track Features. Not all Materials are representable in KML, so for more advanced Materials just the primary
+         * color is used. Canvas objects are exported as PNG images.
          *
          * @exports exportKml
          *
          * @param {Object} options An object with the following properties:
-         * @param {EntityCollection} options.entities The EntityCollection to export as KML
-         * @param {Ellipsoid} [options.ellipsoid=Ellipsoid.WGS84] The ellipsoid for the output file
-         * @param {exportKml~ModelCallback} [options.modelCallback] A callback that will be called with a ModelGraphics instance and should return the URI to use in the KML. This will throw a RuntimeError if there is a model and this is undefined.
+         * @param {EntityCollection} options.entities The EntityCollection to export as KML.
+         * @param {Ellipsoid} [options.ellipsoid=Ellipsoid.WGS84] The ellipsoid for the output file.
+         * @param {exportKml~ModelCallback} [options.modelCallback] A callback that will be called with a {@link ModelGraphics} instance and should return the URI to use in the KML. Required if a model exists in the entity collection.
          * @param {JulianDate} [options.time=entities.computeAvailability().start] The time value to use to get properties that are not time varying in KML.
          * @param {TimeInterval} [options.defaultAvailability=entities.computeAvailability()] The interval that will be sampled if an entity doesn't have an availability.
          * @param {Number} [options.sampleDuration=60] The number of seconds to sample properties that are varying in KML.
-         * @param {Boolean} [options.kmz] If true KML and external files will be compressed into a kmz file.
+         * @param {Boolean} [options.kmz=false] If true KML and external files will be compressed into a kmz file.
          *
          * @returns {Promise<Object>} A promise that resolved to an object containing the KML string and a dictionary of external file blobs, or a kmz file as a blob if options.kmz is true.
          *
@@ -277,6 +278,7 @@ define([
         function exportKml(options) {
             options = defaultValue(options, defaultValue.EMPTY_OBJECT);
             var entities = options.entities;
+            var kmz = defaultValue(options.kmz, false);
 
             //>>includeStart('debug', pragmas.debug);
             if (!defined(entities)) {
@@ -312,7 +314,7 @@ define([
                 .then(function() {
                     var serializer = new XMLSerializer();
                     var kmlString = serializer.serializeToString(state.kmlDoc);
-                    if (options.kmz) {
+                    if (kmz) {
                         return createKmz(kmlString, externalFileHandler.files);
                     }
 
@@ -1266,7 +1268,7 @@ define([
         }
 
         /**
-         * Function interface for merging interval data.
+         * Since KML does not support glTF models, this callback is required to specify what URL to use for the model in the KML document.
          * @callback exportKml~ModelCallback
          *
          * @param {ModelGraphics} model The ModelGraphics instance for an Entity.
