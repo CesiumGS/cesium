df7d2d1be8e630a9deb909e855d021fecea94031
Zhouhai <zhouhai@Zhouhais-iMac.local>
2019-06-26 10:47:42 +0800

    remove czm_ellipsoid struct
    remove function czm_getWgs84EllipsoidEC
    remove struct czm_ellipsoid
    
    add Constant czm_ellipsoid_radii
    add Constant czm_ellipsoid_inverseRadii

---
+++ Source/Shaders/Builtin/Constants/ellipsoidInverseRadii.glsl
@@ -0,0 +1,7 @@
+/**
+ * 0.1
+ *
+ * @name czm_ellipsoid_inverseradii
+ * @glslConstant
+ */
+const vec3 czm_ellipsoid_inverseRadii = vec3(1.0 / 6378137.0, 1.0 / 6378137.0, 1.0 / 6356752.314245);
---
+++ Source/Shaders/Builtin/Constants/ellipsoidRadii.glsl
@@ -0,0 +1,7 @@
+/**
+ * 0.1
+ *
+ * @name czm_ellipsoid_radii
+ * @glslConstant
+ */
+const vec3 czm_ellipsoid_radii = vec3(6378137.0, 6378137.0, 6356752.314245);
