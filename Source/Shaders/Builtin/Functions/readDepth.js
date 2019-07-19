abd40e77fb6b972b53c4f4636b7427e6b926cce7
njrivera <nrivera@novetta.com>
2019-07-19 14:29:22 -0400

    Revert "Add Build Stuff"

--- Source/Shaders/Builtin/Functions/readDepth.js
+++
@@ -1,9 +0,0 @@
-//This file is automatically rebuilt by the Cesium build process.
-define(function() {
-    'use strict';
-    return "float czm_readDepth(sampler2D depthTexture, vec2 texCoords)\n\
-{\n\
-    return czm_reverseLogDepth(texture2D(depthTexture, texCoords).r);\n\
-}\n\
-";
-});
