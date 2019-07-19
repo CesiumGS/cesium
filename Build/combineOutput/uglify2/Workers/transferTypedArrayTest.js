1641e659ca9fa756c014bde884af7b0cc2457f05
nrivera-Novetta <nrivera@novetta.com>
2019-07-19 15:48:05 +0000

    remove stuff to match normal cesium build

--- Build/combineOutput/uglify2/Workers/transferTypedArrayTest.js
+++
@@ -1,1 +0,0 @@
-"undefined"==typeof self&&(self={}),self.onmessage=function(e){"use strict";var s=e.data.array,a=self.webkitPostMessage||self.postMessage;try{a({array:s},[s.buffer])}catch(e){a({})}};
