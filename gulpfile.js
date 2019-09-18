f9579495f40e91dbfbbeb92841dfa1715fe93595
Aristeidis Bampakos <bampakoa@gmail.com>
2019-09-18 23:51:45 +0300

    Fix arrow method

--- gulpfile.js
+++ gulpfile.js
@@ -278,7 +278,7 @@ function generateDocumentation() {
 gulp.task('generateDocumentation', generateDocumentation);
 
 gulp.task('generateDocumentation-watch', function() {
-    return generateDocumentation().done(() => {
+    return generateDocumentation().done(function() {
         console.log('Listening for changes in documentation...');
         return gulp.watch(sourceFiles, gulp.series('generateDocumentation'));
     });
