1a8e2dd5a74d4dcc731087bf7ec6c6236957d27e
hpinkos <hannah@cesium.com>
2019-09-19 11:04:04 -0400

    update node modules

--- Source/Renderer/Context.js
+++ Source/Renderer/Context.js
@@ -65,9 +65,6 @@ define([
         UniformState,
         VertexArray) {
     'use strict';
-    /*global WebGLRenderingContext*/
-
-    /*global WebGL2RenderingContext*/
 
     function errorToString(gl, error) {
         var message = 'WebGL Error:  ';
