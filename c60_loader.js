095d8967be7f8b7d73847e1a13f25b92146ddc61
Roman <stasis93@yandex.ru>
2019-12-05 20:06:44 +0500

    доп. к "Обновление Cesium до 1.63.1"

---
+++ c60_loader.js
@@ -0,0 +1,30 @@
+// This file loads the unbuilt ES6 version of Cesium
+// into the global scope during local developmnet
+window.CESIUM_BASE_URL = "./Source/";
+import * as Cesium from "./Source/Cesium.js";
+window.Cesium = Cesium;
+
+// Since ES6 modules have no guaranteed load order,
+// only call startup if it's already defined but hasn't been called yet
+//if (!window.startupCalled && typeof window.startup === 'function') {
+    //debugger;
+
+    /*global self*/
+    let scope;
+    if (typeof window !== 'undefined') {
+        window.Cesium = Cesium;
+        scope = window;
+    } else if (typeof self !== 'undefined') {
+        self.Cesium = Cesium;
+        scope = self;
+    } else if(typeof module !== 'undefined') {
+        module.exports = Cesium;
+        scope = module;
+    } else {
+        console.error('Unable to load Cesium.');
+    }
+    if(scope.waitCesium)
+        scope.waitCesium()
+
+    window.startup(Cesium);
+//}
