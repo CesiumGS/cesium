a3e291a2ba55cda7059fe7c6bbb11410162b9901
Sean Lilley <lilleyse@gmail.com>
2020-01-01 19:53:36 -0500

    Only adjust for terrain if screen space camera controller moved camera

--- Source/Scene/ScreenSpaceCameraController.js
+++ Source/Scene/ScreenSpaceCameraController.js
@@ -1137,7 +1137,7 @@ import TweenCollection from './TweenCollection.js';
         controller._rotateRateRangeAdjustment = radius;
 
         var originalPosition = Cartesian3.clone(camera.positionWC, rotateCVCartesian3);
-        camera._adjustHeightForTerrain();
+        adjustHeightForTerrain(controller);
 
         if (!Cartesian3.equals(camera.positionWC, originalPosition)) {
             camera._setTransform(verticalTransform);
@@ -1766,7 +1766,7 @@ import TweenCollection from './TweenCollection.js';
         controller._rotateRateRangeAdjustment = radius;
 
         var originalPosition = Cartesian3.clone(camera.positionWC, tilt3DCartesian3);
-        camera._adjustHeightForTerrain();
+        adjustHeightForTerrain(controller);
 
         if (!Cartesian3.equals(camera.positionWC, originalPosition)) {
             camera._setTransform(verticalTransform);
@@ -1918,11 +1918,77 @@ import TweenCollection from './TweenCollection.js';
         reactToInput(controller, controller.enableLook, controller.lookEventTypes, look3D);
     }
 
+    var scratchAdjustHeightTransform = new Matrix4();
+    var scratchAdjustHeightCartographic = new Cartographic();
+
+    function adjustHeightForTerrain(controller) {
+        if (!controller.enableCollisionDetection) {
+            return;
+        }
+
+        var scene = controller._scene;
+        var mode = scene.mode;
+        var globe = scene.globe;
+
+        if (!defined(globe) || mode === SceneMode.SCENE2D || mode === SceneMode.MORPHING) {
+            return;
+        }
+
+        var camera = scene.camera;
+        var ellipsoid = globe.ellipsoid;
+        var projection = scene.mapProjection;
+
+        var transform;
+        var mag;
+        if (!Matrix4.equals(camera.transform, Matrix4.IDENTITY)) {
+            transform = Matrix4.clone(camera.transform, scratchAdjustHeightTransform);
+            mag = Cartesian3.magnitude(camera.position);
+            camera._setTransform(Matrix4.IDENTITY);
+        }
+
+        var cartographic = scratchAdjustHeightCartographic;
+        if (mode === SceneMode.SCENE3D) {
+            ellipsoid.cartesianToCartographic(camera.position, cartographic);
+        } else {
+            projection.unproject(camera.position, cartographic);
+        }
+
+        var heightUpdated = false;
+        if (cartographic.height < controller._minimumCollisionTerrainHeight) {
+            var height = globe.getHeight(cartographic);
+            if (defined(height)) {
+                height += controller.minimumZoomDistance;
+                if (cartographic.height < height) {
+                    cartographic.height = height;
+                    if (mode === SceneMode.SCENE3D) {
+                        ellipsoid.cartographicToCartesian(cartographic, camera.position);
+                    } else {
+                        projection.project(cartographic, camera.position);
+                    }
+                    heightUpdated = true;
+                }
+            }
+        }
+
+        if (defined(transform)) {
+            camera._setTransform(transform);
+            if (heightUpdated) {
+                Cartesian3.normalize(camera.position, camera.position);
+                Cartesian3.negate(camera.position, camera.direction);
+                Cartesian3.multiplyByScalar(camera.position, Math.max(mag, controller.minimumZoomDistance), camera.position);
+                Cartesian3.normalize(camera.direction, camera.direction);
+                Cartesian3.cross(camera.direction, camera.up, camera.right);
+                Cartesian3.cross(camera.right, camera.direction, camera.up);
+            }
+        }
+    }
+
     /**
      * @private
      */
     ScreenSpaceCameraController.prototype.update = function() {
-        if (!Matrix4.equals(this._scene.camera.transform, Matrix4.IDENTITY)) {
+        var camera = this._scene.camera;
+        if (!Matrix4.equals(camera.transform, Matrix4.IDENTITY)) {
             this._globe = undefined;
             this._ellipsoid = Ellipsoid.UNIT_SPHERE;
         } else {
@@ -1938,6 +2004,9 @@ import TweenCollection from './TweenCollection.js';
         this._rotateFactor = 1.0 / radius;
         this._rotateRateRangeAdjustment = radius;
 
+        var previousPosition = Cartesian3.clone(camera.positionWC);
+        var previousDirection = Cartesian3.clone(camera.directionWC);
+
         var scene = this._scene;
         var mode = scene.mode;
         if (mode === SceneMode.SCENE2D) {
@@ -1948,6 +2017,11 @@ import TweenCollection from './TweenCollection.js';
         } else if (mode === SceneMode.SCENE3D) {
             this._horizontalRotationAxis = undefined;
             update3D(this);
+        }
+
+        var cameraChanged = !Cartesian3.equals(previousPosition, camera.positionWC) || !Cartesian3.equals(previousDirection, camera.directionWC);
+        if (cameraChanged) {
+            adjustHeightForTerrain(this);
         }
 
         this._aggregator.reset();
