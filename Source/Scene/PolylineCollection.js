73d4f95893752909343d573094dc7ba33505be65
Samuel Vargas <sam.vargas@cesium.com>
2020-01-16 11:15:31 -0500

    Remove redundant polyLinesToUpdate synchronization code

--- Source/Scene/PolylineCollection.js
+++ Source/Scene/PolylineCollection.js
@@ -258,12 +258,6 @@ import SceneMode from './SceneMode.js';
     PolylineCollection.prototype.remove = function(polyline) {
         if (this.contains(polyline)) {
             this._polylines[polyline._index] = undefined; // Removed later
-
-            var polylineUpdateIndex = this._polylinesToUpdate.indexOf(polyline);
-            if (polylineUpdateIndex !== -1) {
-                this._polylinesToUpdate.splice(polylineUpdateIndex, 1);
-            }
-
             this._polylinesRemoved = true;
             this._createVertexArray = true;
             this._createBatchTable = true;
