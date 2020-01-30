d6adf52584f6e96fbec36ce06df05c09aa40bec3
Omar Shehata <omar@cesium.com>
2020-01-30 17:35:58 -0500

    Fix lint

--- Source/DataSources/KmlDataSource.js
+++ Source/DataSources/KmlDataSource.js
@@ -1354,7 +1354,7 @@ import WallGraphics from './WallGraphics.js';
                 oneTimeWarning('kml-gx:drawOrder', 'KML - gx:drawOrder is not supported in LineStrings when clampToGround is false');
             }
             if (dataSource._clampToGround && !tessellate) {
-                oneTimeWarning('kml-line-tesselate', 'Ignoring clampToGround for KML lines without the tessellate flag.')
+                oneTimeWarning('kml-line-tesselate', 'Ignoring clampToGround for KML lines without the tessellate flag.');
             }
 
             polyline = defined(polyline) ? polyline.clone() : new PolylineGraphics();
