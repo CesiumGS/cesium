c9eac2476d21c44e32dea24dd5ddc1fab777e5c6
Sean Lilley <lilleyse@gmail.com>
2020-01-31 22:13:43 -0600

    Improved zoom-out behavior while inside globe by finding backwards intersection with an expanded ellipsoid

--- Source/Scene/ScreenSpaceCameraController.js
+++ Source/Scene/ScreenSpaceCameraController.js
@@ -240,6 +240,10 @@ import TweenCollection from './TweenCollection.js';
         this._globe = undefined;
         this._ellipsoid = undefined;
 
+        this._zoomingOnVectorUnderground = false;
+        this._originalEllipsoid = undefined;
+        this._expandedEllipsoid = undefined;
+
         this._aggregator = new CameraEventAggregator(scene.canvas);
 
         this._lastInertiaSpinMovement = undefined;
@@ -534,7 +538,7 @@ import TweenCollection from './TweenCollection.js';
                 }
             } else if (mode === SceneMode.SCENE3D) {
                 var cameraPositionNormal = Cartesian3.normalize(camera.position, scratchCameraPositionNormal);
-                if (scene.frameState.cameraUnderground || camera.positionCartographic.height < 3000.0 && Math.abs(Cartesian3.dot(camera.direction, cameraPositionNormal)) < 0.6) {
+                if (object._zoomingOnVectorUnderground || scene.frameState.cameraUnderground || camera.positionCartographic.height < 3000.0 && Math.abs(Cartesian3.dot(camera.direction, cameraPositionNormal)) < 0.6) {
                     zoomOnVector = true;
                 } else {
                     var canvas = scene.canvas;
@@ -1516,6 +1520,31 @@ import TweenCollection from './TweenCollection.js';
         }
     }
 
+    var scratchBackwardsRay = new Ray();
+
+    function getBackwardsDistance(controller, ray) {
+        var globe = controller._globe;
+        if (!defined(globe)) {
+            return undefined;
+        }
+
+        if (controller._ellipsoid !== controller._originalEllipsoid) {
+            controller._originalEllipsoid = controller._ellipsoid;
+            var radii = controller._ellipsoid.radii;
+            var expansionFactor = 1.002;
+            controller._expandedEllipsoid = new Ellipsoid(radii.x * expansionFactor, radii.y * expansionFactor, radii.z * expansionFactor);
+        }
+
+        var backwardsRay = scratchBackwardsRay;
+        backwardsRay.origin = Cartesian3.clone(ray.origin, backwardsRay.origin);
+        backwardsRay.direction = Cartesian3.negate(ray.direction, backwardsRay.direction);
+        var backwardsIntersection = IntersectionTests.rayEllipsoid(backwardsRay, controller._expandedEllipsoid);
+        if (defined(backwardsIntersection) && backwardsIntersection.start == 0.0) {
+            return backwardsIntersection.stop;
+        }
+        return undefined;
+    }
+
     var zoom3DUnitPosition = new Cartesian3();
     var zoom3DCartographic = new Cartographic();
 
@@ -1552,6 +1581,20 @@ import TweenCollection from './TweenCollection.js';
             distance = Cartesian3.distance(ray.origin, intersection);
         } else {
             distance = height;
+        }
+
+        var zoomingBackwards = movement.endPosition.y < movement.startPosition.y;
+        var sameStartPosition = Cartesian2.equals(startPosition, controller._zoomMouseStart);
+
+        if (!sameStartPosition) {
+            controller._zoomingOnVectorUnderground = scene.frameState.cameraUnderground && defined(intersection);
+        }
+
+        if (controller._zoomingOnVectorUnderground && zoomingBackwards) {
+            var backwardsDistance = getBackwardsDistance(controller, ray);
+            if (defined(backwardsDistance)) {
+                distance = Math.min(distance, backwardsDistance);
+            }
         }
 
         var unitPosition = Cartesian3.normalize(camera.position, zoom3DUnitPosition);
