a309aa62a3590df95103433397eedd57e3965820
Sean Lilley <lilleyse@gmail.com>
2020-02-13 19:33:00 -0500

    Ellipsoid average. bugs when inside the ellipsoid [skip ci]

--- Source/Scene/DepthPlane.js
+++ Source/Scene/DepthPlane.js
@@ -1,11 +1,8 @@
+import BoxGeometry from '../Core/BoxGeometry.js';
 import BoundingSphere from '../Core/BoundingSphere.js';
 import Cartesian3 from '../Core/Cartesian3.js';
-import ComponentDatatype from '../Core/ComponentDatatype.js';
 import defined from '../Core/defined.js';
-import FeatureDetection from '../Core/FeatureDetection.js';
-import Geometry from '../Core/Geometry.js';
-import GeometryAttribute from '../Core/GeometryAttribute.js';
-import PrimitiveType from '../Core/PrimitiveType.js';
+import VertexFormat from '../Core/VertexFormat.js';
 import BufferUsage from '../Renderer/BufferUsage.js';
 import DrawCommand from '../Renderer/DrawCommand.js';
 import Pass from '../Renderer/Pass.js';
@@ -15,7 +12,10 @@ import ShaderSource from '../Renderer/ShaderSource.js';
 import VertexArray from '../Renderer/VertexArray.js';
 import DepthPlaneFS from '../Shaders/DepthPlaneFS.js';
 import DepthPlaneVS from '../Shaders/DepthPlaneVS.js';
+import CullFace from './CullFace.js';
+import DepthFunction from './DepthFunction.js';
 import SceneMode from './SceneMode.js';
+import BlendingState from './BlendingState.js';
 
     /**
      * @private
@@ -27,59 +27,6 @@ import SceneMode from './SceneMode.js';
         this._command = undefined;
         this._mode = undefined;
         this._useLogDepth = false;
-    }
-
-    var depthQuadScratch = FeatureDetection.supportsTypedArrays() ? new Float32Array(12) : [];
-    var scratchCartesian1 = new Cartesian3();
-    var scratchCartesian2 = new Cartesian3();
-    var scratchCartesian3 = new Cartesian3();
-    var scratchCartesian4 = new Cartesian3();
-
-    function computeDepthQuad(ellipsoid, frameState) {
-        var radii = ellipsoid.radii;
-        var p = frameState.camera.positionWC;
-
-        // Find the corresponding position in the scaled space of the ellipsoid.
-        var q = Cartesian3.multiplyComponents(ellipsoid.oneOverRadii, p, scratchCartesian1);
-
-        var qMagnitude = Cartesian3.magnitude(q);
-        var qUnit = Cartesian3.normalize(q, scratchCartesian2);
-
-        // Determine the east and north directions at q.
-        var eUnit = Cartesian3.normalize(Cartesian3.cross(Cartesian3.UNIT_Z, q, scratchCartesian3), scratchCartesian3);
-        var nUnit = Cartesian3.normalize(Cartesian3.cross(qUnit, eUnit, scratchCartesian4), scratchCartesian4);
-
-        // Determine the radius of the 'limb' of the ellipsoid.
-        var wMagnitude = Math.sqrt(Cartesian3.magnitudeSquared(q) - 1.0);
-
-        // Compute the center and offsets.
-        var center = Cartesian3.multiplyByScalar(qUnit, 1.0 / qMagnitude, scratchCartesian1);
-        var scalar = wMagnitude / qMagnitude;
-        var eastOffset = Cartesian3.multiplyByScalar(eUnit, scalar, scratchCartesian2);
-        var northOffset = Cartesian3.multiplyByScalar(nUnit, scalar, scratchCartesian3);
-
-        // A conservative measure for the longitudes would be to use the min/max longitudes of the bounding frustum.
-        var upperLeft = Cartesian3.add(center, northOffset, scratchCartesian4);
-        Cartesian3.subtract(upperLeft, eastOffset, upperLeft);
-        Cartesian3.multiplyComponents(radii, upperLeft, upperLeft);
-        Cartesian3.pack(upperLeft, depthQuadScratch, 0);
-
-        var lowerLeft = Cartesian3.subtract(center, northOffset, scratchCartesian4);
-        Cartesian3.subtract(lowerLeft, eastOffset, lowerLeft);
-        Cartesian3.multiplyComponents(radii, lowerLeft, lowerLeft);
-        Cartesian3.pack(lowerLeft, depthQuadScratch, 3);
-
-        var upperRight = Cartesian3.add(center, northOffset, scratchCartesian4);
-        Cartesian3.add(upperRight, eastOffset, upperRight);
-        Cartesian3.multiplyComponents(radii, upperRight, upperRight);
-        Cartesian3.pack(upperRight, depthQuadScratch, 6);
-
-        var lowerRight = Cartesian3.subtract(center, northOffset, scratchCartesian4);
-        Cartesian3.add(lowerRight, eastOffset, lowerRight);
-        Cartesian3.multiplyComponents(radii, lowerRight, lowerRight);
-        Cartesian3.pack(lowerRight, depthQuadScratch, 9);
-
-        return depthQuadScratch;
     }
 
     DepthPlane.prototype.update = function(frameState) {
@@ -95,17 +42,20 @@ import SceneMode from './SceneMode.js';
         if (!defined(this._command)) {
             this._rs = RenderState.fromCache({ // Write depth, not color
                 cull : {
-                    enabled : true
+                    enabled : true,
+                    face : CullFace.FRONT
                 },
                 depthTest : {
-                    enabled : true
+                    enabled : true,
+                    func : DepthFunction.ALWAYS
                 },
-                colorMask : {
+                /*colorMask : {
                     red : false,
                     green : false,
                     blue : false,
                     alpha : false
-                }
+                }*/
+                blending : BlendingState.ALPHA_BLEND
             });
 
             this._command = new DrawCommand({
@@ -121,6 +71,7 @@ import SceneMode from './SceneMode.js';
 
             var vs = new ShaderSource({
                 sources : [DepthPlaneVS]
+
             });
             var fs = new ShaderSource({
                 sources : [DepthPlaneFS]
@@ -150,22 +101,13 @@ import SceneMode from './SceneMode.js';
             this._command.shaderProgram = this._sp;
         }
 
-        // update depth plane
-        var depthQuad = computeDepthQuad(ellipsoid, frameState);
-
         // depth plane
         if (!defined(this._va)) {
-            var geometry = new Geometry({
-                attributes : {
-                    position : new GeometryAttribute({
-                        componentDatatype : ComponentDatatype.FLOAT,
-                        componentsPerAttribute : 3,
-                        values : depthQuad
-                    })
-                },
-                indices : [0, 1, 2, 2, 1, 3],
-                primitiveType : PrimitiveType.TRIANGLES
-            });
+            var dimensions = Cartesian3.multiplyByScalar(ellipsoid.radii, 2, new Cartesian3());
+            var geometry = BoxGeometry.createGeometry(BoxGeometry.fromDimensions({
+                dimensions : dimensions,
+                vertexFormat : VertexFormat.POSITION_ONLY
+            }));
 
             this._va = VertexArray.fromGeometry({
                 context : context,
@@ -173,12 +115,10 @@ import SceneMode from './SceneMode.js';
                 attributeLocations : {
                     position : 0
                 },
-                bufferUsage : BufferUsage.DYNAMIC_DRAW
+                bufferUsage : BufferUsage.STATIC_DRAW
             });
 
             this._command.vertexArray = this._va;
-        } else {
-            this._va.getAttribute(0).vertexBuffer.copyFromArrayView(depthQuad);
         }
     };
 
--- Source/Shaders/DepthPlaneFS.glsl
+++ Source/Shaders/DepthPlaneFS.glsl
@@ -8,14 +8,28 @@ void main()
     vec3 ellipsoid_center = czm_view[3].xyz;
 
     czm_raySegment intersection = czm_rayEllipsoidIntersectionInterval(ray, ellipsoid_center, czm_ellipsoidInverseRadii);
-    if (!czm_isEmpty(intersection))
+    if (!czm_isEmpty(intersection) && intersection.start != 0.0)
     {
-        gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+        float t = intersection.start * 0.75 + intersection.stop * 0.25;
+
+        vec3 positionEC = czm_pointAlongRay(ray, t);
+        vec4 positionCC = czm_projection * vec4(positionEC, 1.0);
+#ifdef LOG_DEPTH
+        czm_writeLogDepth(1.0 + positionCC.w);
+#else
+        float z = positionCC.z / positionCC.w;
+
+        float n = czm_depthRange.near;
+        float f = czm_depthRange.far;
+
+        gl_FragDepthEXT = (z * (f - n) + f + n) * 0.5;
+#endif
+
+        gl_FragColor = vec4(1.0, 1.0, 1.0, 0.5);
     }
     else
     {
-        discard;
+        gl_FragColor = vec4(1.0, 0.0, 1.0, 0.5);
+        //discard;
     }
-
-    czm_writeLogDepth();
 }
