088e749d5ba598a0579ac3a453000b242dcf05e1
Kevin Ring <kevin@kotachrome.com>
2020-03-18 17:38:24 +1100

    Better solid outline depth biasing.

--- Source/Shaders/Builtin/Functions/adjustSolidOutlineDepth.glsl
+++ Source/Shaders/Builtin/Functions/adjustSolidOutlineDepth.glsl
@@ -12,15 +12,15 @@ czm_ray getPickRay(vec2 screenPosition) {
     float width = czm_viewport.z;
     float height = czm_viewport.w;
     float x = (2.0 / width) * screenPosition.x - 1.0;
-    float y = (2.0 / height) * (height - screenPosition.y) - 1.0;
+    float y = (2.0 / height) * screenPosition.y - 1.0;
 
     float near = czm_currentFrustum.x;
-    vec3 nearCenter = vec3(0.0, 0.0, -1.0) * near;
-    vec3 xDir = vec3(1.0, 0.0, 0.0) * (x * czm_frustumPlanes.w);
-    vec3 yDir = vec3(0.0, 1.0, 0.0) * (y * czm_frustumPlanes.x);
+    vec3 nearCenter = vec3(0.0, 0.0, -near);
+    vec3 xDir = vec3(x * czm_frustumPlanes.w, 0.0, 0.0);
+    vec3 yDir = vec3(0.0, y * czm_frustumPlanes.x, 0.0);
     vec3 direction = normalize(nearCenter + xDir + yDir);
 
-    return czm_ray(vec3(0.0, 0.0, 0.0), direction);
+    return czm_ray(vec3(0.0), direction);
 }
 
 vec3 intersectRayPlane(czm_ray ray, vec3 pointInPlane, vec3 planeNormal) {
@@ -29,7 +29,7 @@ vec3 intersectRayPlane(czm_ray ray, vec3 pointInPlane, vec3 planeNormal) {
     float denominator = dot(planeNormal, direction);
     float planeDistance = -dot(planeNormal, pointInPlane);
 
-    if (abs(denominator) < czm_epsilon7) {
+    if (abs(denominator) < czm_epsilon6) {
         // Ray is parallel to plane.  The ray may be in the polygon's plane.
         discard;
     }
@@ -47,14 +47,17 @@ vec3 intersectRayPlane(czm_ray ray, vec3 pointInPlane, vec3 planeNormal) {
 void czm_adjustSolidOutlineDepth(vec3 positionEye, vec3 normalEye) {
 #if defined(LOG_DEPTH) && defined(GL_EXT_frag_depth)
     // Find ray to diagonally-adjacent pixel (i.e. +1 in both X and Y directions)
+    vec2 currentPixel = gl_FragCoord.xy;
+    czm_ray currentRay = getPickRay(currentPixel);
     vec2 nextPixel = gl_FragCoord.xy + vec2(1.0, 1.0);
-    czm_ray ray = getPickRay(nextPixel);
+    czm_ray nextRay = getPickRay(nextPixel);
 
     // Find intersection of ray with plane created by fragment position and normal.
-    vec3 intersection = intersectRayPlane(ray, positionEye, normalEye);
+    vec3 currentIntersection = intersectRayPlane(currentRay, positionEye, normalEye);
+    vec3 nextIntersection = intersectRayPlane(nextRay, positionEye, normalEye);
 
     // Compute the amount depth changes when we move by one pixel. Expresed in eye coordinates.
-    float deltaDepth = abs(positionEye.z - intersection.z) / sqrt(2.0);
+    float deltaDepth = abs(currentIntersection.z - nextIntersection.z);
 
     czm_writeLogDepth(v_depthFromNearPlusOne - deltaDepth);
     // gl_FragDepthEXT -= 5e-5;
