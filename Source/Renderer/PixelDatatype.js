887faddf0ea32a0ca628a29552085496a1ce976b
Andy <andy132ly@gmail.com>
2020-06-03 14:56:53 +0800

    给PixelDatatype.js添加中文注释

--- Source/Renderer/PixelDatatype.js
+++ Source/Renderer/PixelDatatype.js
@@ -2,25 +2,43 @@ import WebGLConstants from "../Core/WebGLConstants.js";
 
 /**
  * The data type of a pixel.
- *
+ * <br/>定义像素的数据类型
+ * <br/>参考：{@link https://blog.csdn.net/w450468524/article/details/51649065 glTexImage2D 中 format internalformat type 参数的含义}
+ * 、{@link https://stackovrflow.com/questions/34497195/difference-between-format-and-internalformat Difference between format and internalformat}
  * @enum {Number}
+ * @readonly
  * @see PostProcessStage
+ *
+ * @namespace PixelDatatype
  */
 var PixelDatatype = {
+  /**@readonly */
   UNSIGNED_BYTE: WebGLConstants.UNSIGNED_BYTE,
+  /**@readonly */
   UNSIGNED_SHORT: WebGLConstants.UNSIGNED_SHORT,
+  /**@readonly */
   UNSIGNED_INT: WebGLConstants.UNSIGNED_INT,
+  /**@readonly */
   FLOAT: WebGLConstants.FLOAT,
+  /**@readonly */
   HALF_FLOAT: WebGLConstants.HALF_FLOAT_OES,
+  /**@readonly */
   UNSIGNED_INT_24_8: WebGLConstants.UNSIGNED_INT_24_8,
+  /**@readonly */
   UNSIGNED_SHORT_4_4_4_4: WebGLConstants.UNSIGNED_SHORT_4_4_4_4,
+  /**@readonly */
   UNSIGNED_SHORT_5_5_5_1: WebGLConstants.UNSIGNED_SHORT_5_5_5_1,
+  /**@readonly */
   UNSIGNED_SHORT_5_6_5: WebGLConstants.UNSIGNED_SHORT_5_6_5,
 };
 
 /**
-  @private
-*/
+ * 判定是否已从显存传入内存
+ * @param {PixelDatatype} pixelDatatype 像素数据类型
+ * @returns 已传入返回true，否则返回false
+ * @readonly
+ * @private
+ */
 PixelDatatype.isPacked = function (pixelDatatype) {
   return (
     pixelDatatype === PixelDatatype.UNSIGNED_INT_24_8 ||
@@ -31,8 +49,12 @@ PixelDatatype.isPacked = function (pixelDatatype) {
 };
 
 /**
-  @private
-*/
+ * 获取对应像素数据类型的子节大小
+ * @param {PixelDatatype} pixelDatatype 像素数据类型
+ * @returns 返回对应的子节大小
+ * @readonly
+ * @private
+ */
 PixelDatatype.sizeInBytes = function (pixelDatatype) {
   switch (pixelDatatype) {
     case PixelDatatype.UNSIGNED_BYTE:
@@ -51,8 +73,12 @@ PixelDatatype.sizeInBytes = function (pixelDatatype) {
 };
 
 /**
-  @private
-*/
+ * 判定是否为像素数据类型中的一种
+ * @param {PixelDatatype} pixelDatatype 像素数据类型
+ * @returns 是其中一种返回true,否则返回false
+ * @readonly
+ * @private
+ */
 PixelDatatype.validate = function (pixelDatatype) {
   return (
     pixelDatatype === PixelDatatype.UNSIGNED_BYTE ||
