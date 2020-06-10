ba1d82cee51423c978c48acec269cb0fa98bcee4
Andy <andy132ly@gmail.com>
2020-06-10 09:26:10 +0800

    为defined.js添加中文注释

--- Source/Core/defined.js
+++ Source/Core/defined.js
@@ -1,9 +1,9 @@
 /**
  * @function
- *
+ * 判断值是否为<code>undefined</code>或<code>null</code>
  * @param {*} value The object.
  * @returns {Boolean} Returns true if the object is defined, returns false otherwise.
- *
+ * <br/>如果不等于<code>undefined</code>或<code>null</code>，返回true，否则返回false。
  * @example
  * if (Cesium.defined(positions)) {
  *      doSomething();
