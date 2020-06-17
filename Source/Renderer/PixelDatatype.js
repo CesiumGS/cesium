5ae246a5e0801b8652be6fefc63e4ea4869c8654
bao tran <tranthienbao123@gmail.com>
2020-06-17 13:44:15 -0400

    create PixelDatatype.toWebGLConstant() to map it to WebGLConstant for webgl 1 and webgl 2

--- Source/Renderer/PixelDatatype.js
+++ Source/Renderer/PixelDatatype.js
@@ -7,15 +7,43 @@ import WebGLConstants from "../Core/WebGLConstants.js";
  * @see PostProcessStage
  */
 var PixelDatatype = {
-  UNSIGNED_BYTE: WebGLConstants.UNSIGNED_BYTE,
-  UNSIGNED_SHORT: WebGLConstants.UNSIGNED_SHORT,
-  UNSIGNED_INT: WebGLConstants.UNSIGNED_INT,
-  FLOAT: WebGLConstants.FLOAT,
-  HALF_FLOAT: WebGLConstants.HALF_FLOAT_OES,
-  UNSIGNED_INT_24_8: WebGLConstants.UNSIGNED_INT_24_8,
-  UNSIGNED_SHORT_4_4_4_4: WebGLConstants.UNSIGNED_SHORT_4_4_4_4,
-  UNSIGNED_SHORT_5_5_5_1: WebGLConstants.UNSIGNED_SHORT_5_5_5_1,
-  UNSIGNED_SHORT_5_6_5: WebGLConstants.UNSIGNED_SHORT_5_6_5,
+  UNSIGNED_BYTE: 0,
+  UNSIGNED_SHORT: 1,
+  UNSIGNED_INT: 2,
+  FLOAT: 3,
+  HALF_FLOAT: 4,
+  UNSIGNED_INT_24_8: 5,
+  UNSIGNED_SHORT_4_4_4_4: 6,
+  UNSIGNED_SHORT_5_5_5_1: 7,
+  UNSIGNED_SHORT_5_6_5: 8,
+};
+
+/**
+  @private
+*/
+PixelDatatype.toWebGLConstant = function (pixelDatatype, context) {
+  switch (pixelDatatype) {
+    case PixelDatatype.UNSIGNED_BYTE:
+      return WebGLConstants.UNSIGNED_BYTE;
+    case PixelDatatype.UNSIGNED_SHORT:
+      return WebGLConstants.UNSIGNED_SHORT;
+    case PixelDatatype.UNSIGNED_INT:
+      return WebGLConstants.UNSIGNED_INT;
+    case PixelDatatype.FLOAT:
+      return WebGLConstants.FLOAT;
+    case PixelDatatype.HALF_FLOAT:
+      return context.webgl2
+        ? WebGLConstants.HALF_FLOAT
+        : WebGLConstants.HALF_FLOAT_OES;
+    case PixelDatatype.UNSIGNED_INT_24_8:
+      return WebGLConstants.UNSIGNED_INT_24_8;
+    case PixelDatatype.UNSIGNED_SHORT_4_4_4_4:
+      return WebGLConstants.UNSIGNED_SHORT_4_4_4_4;
+    case PixelDatatype.UNSIGNED_SHORT_5_5_5_1:
+      return WebGLConstants.UNSIGNED_SHORT_5_5_5_1;
+    case PixelDatatype.UNSIGNED_SHORT_5_6_5:
+      return PixelDatatype.UNSIGNED_SHORT_5_6_5;
+  }
 };
 
 /**
