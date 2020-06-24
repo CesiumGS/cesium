647f15c9affc78d82a7cb2d76af61f8c28f31d24
ErixenCruz <erixen.cruz@yahoo.com>
2020-06-24 16:37:16 -0400

    Fix glTF animation. Add testing tilesets. Add unit test for instanced tileset.

--- Specs/Scene/Cesium3DTilesetSpec.js
+++ Specs/Scene/Cesium3DTilesetSpec.js
@@ -100,6 +100,8 @@ describe(
       "Data/Cesium3DTiles/Instanced/InstancedWithBatchTable/tileset.json";
     var instancedRedMaterialUrl =
       "Data/Cesium3DTiles/Instanced/InstancedRedMaterial/tileset.json";
+    var instancedAnimationUrl =
+      "Data/Cesium3DTiles/Instanced/InstancedAnimated/tileset.json";
 
     // 1 tile where each feature is a different source color
     var colorsUrl = "Data/Cesium3DTiles/Batched/BatchedColors/tileset.json";
@@ -635,6 +637,29 @@ describe(
         expect(statistics.visited).toEqual(5);
         expect(statistics.numberOfCommands).toEqual(5);
       });
+    });
+
+    it("animates instanced tileset", function () {
+      return Cesium3DTilesTester.loadTileset(scene, instancedAnimationUrl).then(
+        function (tileset) {
+          var renderOptions = {
+            scene: scene,
+            time: new JulianDate(271.828),
+          };
+
+          expect(renderOptions).toRenderAndCall(function (rgba) {
+            var command = scene.frameState.commandList[0];
+            var modelMatrix1 = Matrix4.clone(command.modelMatrix);
+            // Check that the scene changes after .5 sec (it animates)
+            setTimeout(function () {
+              expect(renderOptions).toRenderAndCall(function (rgba) {
+                var modelMatrix2 = Matrix4.clone(command.modelMatrix);
+                expect(modelMatrix1).not.toEqual(modelMatrix2);
+              });
+            }, 500);
+          });
+        }
+      );
     });
 
     it("renders tileset in CV", function () {
