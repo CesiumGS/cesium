d0fd9d209b688bf6119267b85e892df2a7b1285f
bao tran <tranthienbao123@gmail.com>
2020-06-30 10:15:34 -0400

    modify the uniform for debug shader

--- Source/Scene/DebugInspector.js
+++ Source/Scene/DebugInspector.js
@@ -37,42 +37,24 @@ function createDebugFragmentShaderProgram(scene, shaderProgram) {
   var length = targets.length;
 
   var newMain = "";
-  if (scene.debugShowCommands) {
-    newMain += "uniform vec3 debugShowCommandsColor;\n";
-  }
-  if (scene.debugShowFrustums) {
-    newMain += "uniform vec3 debugShowFrustumsColor;\n";
-  }
-
+  newMain += "uniform vec3 debugShowCommandsColor;\n";
+  newMain += "uniform vec3 debugShowFrustumsColor;\n";
   newMain += "void main() \n" + "{ \n" + "    czm_Debug_main(); \n";
 
+  // set debugShowCommandsColor to Color(1.0, 1.0, 1.0, 1.0) to stop rendering scene.debugShowCommands
+  // set debugShowFrustumsColor to Color(1.0, 1.0, 1.0, 1.0) to stop rendering scene.debugShowFrustums
   var i;
-  if (scene.debugShowCommands) {
-    if (length > 0) {
-      for (i = 0; i < length; ++i) {
-        newMain +=
-          "    gl_FragData[" +
-          targets[i] +
-          "].rgb *= debugShowCommandsColor;\n";
-      }
-    } else {
-      newMain += "    gl_FragColor.rgb *= debugShowCommandsColor;\n";
+  if (length > 0) {
+    for (i = 0; i < length; ++i) {
+      newMain +=
+        "    gl_FragData[" + targets[i] + "].rgb *= debugShowCommandsColor;\n";
+      newMain +=
+        "    gl_FragData[" + targets[i] + "].rgb *= debugShowFrustumsColor;\n";
     }
+  } else {
+    newMain += "    gl_FragColor.rgb *= debugShowCommandsColor;\n";
+    newMain += "    gl_FragColor.rgb *= debugShowFrustumsColor;\n";
   }
-
-  if (scene.debugShowFrustums) {
-    if (length > 0) {
-      for (i = 0; i < length; ++i) {
-        newMain +=
-          "    gl_FragData[" +
-          targets[i] +
-          "].rgb *= debugShowFrustumsColor;\n";
-      }
-    } else {
-      newMain += "    gl_FragColor.rgb *= debugShowFrustumsColor;\n";
-    }
-  }
-
   newMain += "}";
 
   fs.sources.push(newMain);
@@ -116,17 +98,25 @@ DebugInspector.prototype.createShowFrustumsCommand = function (scene, command) {
     debugCommand.uniformMap.debugShowCommandsColor = function () {
       return command._debugColor;
     };
+  } else {
+    debugCommand.uniformMap.debugShowCommandsColor = function () {
+      return new Color(1.0, 1.0, 1.0, 1.0);
+    };
   }
 
   if (scene.debugShowFrustums) {
     // Support up to three frustums.  If a command overlaps all
     // three, it's code is not changed.
-    var r = command.debugOverlappingFrustums & (1 << 0);
-    var g = command.debugOverlappingFrustums & (1 << 1);
-    var b = command.debugOverlappingFrustums & (1 << 2);
+    var r = command.debugOverlappingFrustums & (1 << 0) ? 1.0 : 0.0;
+    var g = command.debugOverlappingFrustums & (1 << 1) ? 1.0 : 0.0;
+    var b = command.debugOverlappingFrustums & (1 << 2) ? 1.0 : 0.0;
 
     debugCommand.uniformMap.debugShowFrustumsColor = function () {
-      return new Color(r, g, b);
+      return new Color(r, g, b, 1.0);
+    };
+  } else {
+    debugCommand.uniformMap.debugShowFrustumsColor = function () {
+      return new Color(1.0, 1.0, 1.0, 1.0);
     };
   }
 
