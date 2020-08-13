32e9429b87136d0d88f36bc92f5712c25dfc4f6c
Emma Krantz <emma.krantz@data61.csiro.au>
2020-08-13 10:55:18 +0800

    don't run FXAA on grey fragments

--- Source/Shaders/PostProcessStages/FXAA.glsl
+++ Source/Shaders/PostProcessStages/FXAA.glsl
@@ -6,9 +6,21 @@ const float fxaaQualitySubpix = 0.5;
 const float fxaaQualityEdgeThreshold = 0.125;
 const float fxaaQualityEdgeThresholdMin = 0.0833;
 
+const float fxaaChannelDiffThreshold = 0.12;
+
 void main()
 {
+    // Terrible hack:
+    // Don't run FXAA on grey fragments.
+    // Since most text is black or white, this tends to preserve it while allowing us to AA everything else
+    vec4 initialColor = texture2D(colorTexture, v_textureCoordinates);
+    if (abs(initialColor.r - initialColor.b) + abs(initialColor.r - initialColor.g) + abs(initialColor.g - initialColor.b) < fxaaChannelDiffThreshold) {
+        gl_FragColor = initialColor;
+        return;
+    }
+
     vec2 fxaaQualityRcpFrame = vec2(1.0) / czm_viewport.zw;
+
     vec4 color = FxaaPixelShader(
         v_textureCoordinates,
         colorTexture,
