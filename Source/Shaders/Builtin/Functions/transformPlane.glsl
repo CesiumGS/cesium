bbf2c0c9ebcef012de61d8aa4694038a54080bdb
Bao <tranthienbao123@gmail.com>
2020-09-01 09:47:32 -0400

    fix glsl czm_transformPlane

--- Source/Shaders/Builtin/Functions/transformPlane.glsl
+++ Source/Shaders/Builtin/Functions/transformPlane.glsl
@@ -1,5 +1,5 @@
 vec4 czm_transformPlane(vec4 clippingPlane, mat4 transform) {
     vec4 transformPlane = transform * clippingPlane;
-    vec3 normalMagnitude = length(transformPlane.xyz);
+    float normalMagnitude = length(transformPlane.xyz);
     return transformPlane / normalMagnitude;
 }
