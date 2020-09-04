39e8809690d01848867190f69c97329e0e718065
Bao <tranthienbao123@gmail.com>
2020-09-04 09:46:51 -0400

    add tests for matrix3 and matrix4 transposeInverse

--- Specs/Core/Matrix3Spec.js
+++ Specs/Core/Matrix3Spec.js
@@ -779,6 +779,17 @@ describe("Core/Matrix3", function () {
     expect(result).toEqual(expected);
   });
 
+  it("transposeInverse works", function () {
+    var matrix = new Matrix3(1.0, 5.0, 2.0, 1.0, 1.0, 7.0, 0.0, -3.0, 4.0);
+    var expectedInverse = Matrix3.inverse(matrix, new Matrix3());
+    var expectedTransposeInverse = Matrix3.transpose(
+      expectedInverse,
+      new Matrix3()
+    );
+    var result = Matrix3.transposeInverse(matrix, new Matrix3());
+    expect(result).toEqual(expectedTransposeInverse);
+  });
+
   it("getRotation returns matrix without scale", function () {
     var matrix = new Matrix3(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0);
     var result = new Matrix3();
