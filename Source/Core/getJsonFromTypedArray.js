f94a410b5627cb62c244fe5f8d061ee224a844a6
Ian Lilley <ianlilleyt@gmail.com>
2021-02-01 18:35:21 -0500

    added extra jsdoc

--- Source/Core/getJsonFromTypedArray.js
+++ Source/Core/getJsonFromTypedArray.js
@@ -2,6 +2,10 @@ import getStringFromTypedArray from "./getStringFromTypedArray.js";
 
 /**
  * @private
+ * @function
+ * @param {Uint8Array} uint8Array
+ * @param {Number} byteOffset
+ * @param {Number} byteLength
  */
 function getJsonFromTypedArray(uint8Array, byteOffset, byteLength) {
   return JSON.parse(
