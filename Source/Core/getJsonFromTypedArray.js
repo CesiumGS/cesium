fc2bccc7dddee9d221dff0b2b7a8c3287939e396
Sean Lilley <lilleyse@gmail.com>
2021-02-02 09:11:34 -0500

    Flesh out doc [skip ci]

--- Source/Core/getJsonFromTypedArray.js
+++ Source/Core/getJsonFromTypedArray.js
@@ -1,11 +1,16 @@
 import getStringFromTypedArray from "./getStringFromTypedArray.js";
 
 /**
- * @private
+ * Parses JSON from a Uint8Array.
+ *
  * @function
- * @param {Uint8Array} uint8Array
- * @param {Number} byteOffset
- * @param {Number} byteLength
+ *
+ * @param {Uint8Array} uint8Array The Uint8Array to read from.
+ * @param {Number} [byteOffset=0] The byte offset to start reading from.
+ * @param {Number} [byteLength] The byte length to read. If byteLength is omitted the remainder of the buffer is read.
+ * @returns {Object} An object containing the parsed JSON.
+ *
+ * @private
  */
 function getJsonFromTypedArray(uint8Array, byteOffset, byteLength) {
   return JSON.parse(
