4e35f404073384812c73d00d5f6b8f13d926fe2e
S.H <imdaddyjnj@gmail.com>
2021-02-03 11:02:54 +0900

    Calculate leading only when more than 1 characters

--- Source/ThirdParty/measureText.js
+++ Source/ThirdParty/measureText.js
@@ -103,6 +103,7 @@
 
     // if we're not dealing with white space, we can compute metrics
     if (!isSpace) {
+
         //Calculate leading only when more than 1 characters
         if(textstring.length > 1) {
             // for text lead values, we meaure a multiline text container.
