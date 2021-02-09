12f8b353c8ebe280b2c87e1d380ac231e61af350
Peter Gagliardi <ptrgags@gmail.com>
2021-02-09 14:21:01 -0500

    clean up the code for has3DTilesExtensionSpec

--- Specs/Scene/has3DTilesExtensionSpec.js
+++ Specs/Scene/has3DTilesExtensionSpec.js
@@ -2,37 +2,26 @@ import { has3DTilesExtension } from "../../Source/Cesium.js";
 
 describe("Scene/has3DTilesExtension", function () {
   it("detects the presence of an extension", function () {
-    expect(
-      has3DTilesExtension(
-        {
-          extensions: {
-            "3DTILES_extension": {},
-          },
-        },
-        "3DTILES_extension"
-      )
-    ).toEqual(true);
+    var tile = {
+      extensions: {
+        "3DTILES_extension": {},
+      },
+    };
+    expect(has3DTilesExtension(tile, "3DTILES_extension")).toEqual(true);
   });
 
   it("detects the absence of an extension", function () {
-    expect(has3DTilesExtension({}, "3DTILES_extension")).toEqual(false);
-    expect(
-      has3DTilesExtension(
-        {
-          extensions: {},
-        },
-        "3DTILES_extension"
-      )
-    ).toEqual(false);
-    expect(
-      has3DTilesExtension(
-        {
-          extensions: {
-            "3DTILES_other_extension": {},
-          },
-        },
-        "3DTILES_extension"
-      )
-    ).toEqual(false);
+    var tile = {};
+    expect(has3DTilesExtension(tile, "3DTILES_extension")).toEqual(false);
+    tile = {
+      extensions: {},
+    };
+    expect(has3DTilesExtension(tile, "3DTILES_extension")).toEqual(false);
+    tile = {
+      extensions: {
+        "3DTILES_other_extension": {},
+      },
+    };
+    expect(has3DTilesExtension(tile, "3DTILES_extension")).toEqual(false);
   });
 });
