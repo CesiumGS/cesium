1554453d2038cbb2646291d048fde97161b96fff
Sean Lilley <lilleyse@gmail.com>
2021-02-28 15:36:24 -0500

    Update gltf-pipeline

--- Source/ThirdParty/GltfPipeline/addPipelineExtras.js
+++ Source/ThirdParty/GltfPipeline/addPipelineExtras.js
@@ -1,37 +1,34 @@
-import ForEach from './ForEach.js'
-import defined from '../../Core/defined.js'
+import ForEach from "./ForEach.js"
+import defined from "../../Core/defined.js"
 
-    /**
-     * Adds extras._pipeline to each object that can have extras in the glTF asset.
-     * This stage runs before updateVersion and handles both glTF 1.0 and glTF 2.0 assets.
-     *
-     * @param {Object} gltf A javascript object containing a glTF asset.
-     * @returns {Object} The glTF asset with the added pipeline extras.
-     *
-     * @private
-     */
-    function addPipelineExtras(gltf) {
-        ForEach.shader(gltf, function(shader) {
-            addExtras(shader);
-        });
-        ForEach.buffer(gltf, function(buffer) {
-            addExtras(buffer);
-        });
-        ForEach.image(gltf, function (image) {
-            addExtras(image);
-            ForEach.compressedImage(image, function(compressedImage) {
-                addExtras(compressedImage);
-            });
-        });
+/**
+ * Adds extras._pipeline to each object that can have extras in the glTF asset.
+ * This stage runs before updateVersion and handles both glTF 1.0 and glTF 2.0 assets.
+ *
+ * @param {Object} gltf A javascript object containing a glTF asset.
+ * @returns {Object} The glTF asset with the added pipeline extras.
+ *
+ * @private
+ */
+function addPipelineExtras(gltf) {
+    ForEach.shader(gltf, function(shader) {
+        addExtras(shader);
+    });
+    ForEach.buffer(gltf, function(buffer) {
+        addExtras(buffer);
+    });
+    ForEach.image(gltf, function (image) {
+        addExtras(image);
+    });
 
-        addExtras(gltf);
+    addExtras(gltf);
 
-        return gltf;
-    }
+    return gltf;
+}
 
-    function addExtras(object) {
-        object.extras = defined(object.extras) ? object.extras : {};
-        object.extras._pipeline = defined(object.extras._pipeline) ? object.extras._pipeline : {};
-    }
+function addExtras(object) {
+    object.extras = defined(object.extras) ? object.extras : {};
+    object.extras._pipeline = defined(object.extras._pipeline) ? object.extras._pipeline : {};
+}
 
-    export default addPipelineExtras;
+export default addPipelineExtras;
