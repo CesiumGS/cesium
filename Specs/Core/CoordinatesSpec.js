f336cae519c5ad2770df4c10c638c8d86036e8b4
张瀚 <574771276@qq.com>
2021-03-08 17:40:23 +0800

    add test

---
+++ Specs/Core/CoordinatesSpec.js
@@ -0,0 +1,217 @@
+import { Cartesian3 } from "../../Source/Cesium.js";
+import { Coordinates } from "../../Source/Cesium.js";
+import { Cartographic } from "../../Source/Cesium.js";
+import { Ellipsoid } from "../../Source/Cesium.js";
+import { Math as CesiumMath } from "../../Source/Cesium.js";
+
+describe("Core/Coordinates", function() {
+  var surfaceCartesian = new Cartesian3(
+    -2342541.2318143025,
+    5376884.044708257,
+    2513224.1037388346
+  );
+  var surfaceCoordinates = new Coordinates(
+    113.54126000000002,
+    23.335196000000003,
+    5999.999999999291
+  );
+
+  it("default constructor sets expected properties", function() {
+    var c = new Coordinates();
+    expect(c.longitude).toEqual(0);
+    expect(c.latitude).toEqual(0);
+    expect(c.height).toEqual(0);
+  });
+
+  it("constructor sets expected properties from parameters", function() {
+    var c = new Coordinates(1.0, 2.0, 3.0);
+    expect(c.longitude).toEqual(1);
+    expect(c.latitude).toEqual(2);
+    expect(c.height).toEqual(3);
+  });
+
+  it("toCartesian conversion from Coordinates input to Cartesian3 output", function() {
+    var lon = CesiumMath.toRadians(150);
+    var lat = CesiumMath.toRadians(-40);
+    var height = 100000;
+    var actual = Coordinates.toCartesian(new Coordinates(CesiumMath.toDegrees(lon), CesiumMath.toDegrees(lat), height));
+    var expected = Cartographic.toCartesian(new Cartographic(lon, lat, height));
+    expect(actual).toEqual(expected);
+  });
+
+  it("fromDegrees works without a result parameter", function() {
+    var c = Coordinates.fromDegrees(Math.PI / 2, Math.PI / 4, 100.0);
+    expect(c.longitude).toEqual(Math.PI / 2);
+    expect(c.latitude).toEqual(Math.PI / 4);
+    expect(c.height).toEqual(100.0);
+  });
+
+  it("fromDegrees works with a result parameter", function() {
+    var result = new Coordinates();
+    var c = Coordinates.fromDegrees(Math.PI / 2, Math.PI / 4, 100.0, result);
+    expect(result).toBe(c);
+    expect(c.longitude).toEqual(Math.PI / 2);
+    expect(c.latitude).toEqual(Math.PI / 4);
+    expect(c.height).toEqual(100.0);
+  });
+
+  it("fromDegrees throws without longitude or latitude parameter but defaults altitude", function() {
+    expect(function() {
+      Coordinates.fromDegrees(undefined, 0.0);
+    }).toThrowDeveloperError();
+    expect(function() {
+      Coordinates.fromDegrees(0.0, undefined);
+    }).toThrowDeveloperError();
+    var c = Coordinates.fromDegrees(Math.PI / 2, Math.PI / 4);
+    expect(c.longitude).toEqual(Math.PI / 2);
+    expect(c.latitude).toEqual(Math.PI / 4);
+    expect(c.height).toEqual(0.0);
+  });
+
+  it("fromDegrees works without a result parameter", function() {
+    var c = Coordinates.fromDegrees(90.0, 45.0, 100.0);
+    expect(c.longitude).toEqual(90);
+    expect(c.latitude).toEqual(45);
+    expect(c.height).toEqual(100);
+  });
+
+  it("fromDegrees works with a result parameter", function() {
+    var result = new Coordinates();
+    var c = Coordinates.fromDegrees(90.0, 45.0, 100.0, result);
+    expect(result).toBe(c);
+    expect(c.longitude).toEqual(90.0);
+    expect(c.latitude).toEqual(45);
+    expect(c.height).toEqual(100);
+  });
+
+  it("fromDegrees throws without longitude or latitude parameter but defaults altitude", function() {
+    expect(function() {
+      Coordinates.fromDegrees(undefined, 0.0);
+    }).toThrowDeveloperError();
+    expect(function() {
+      Coordinates.fromDegrees(0.0, undefined);
+    }).toThrowDeveloperError();
+    var c = Coordinates.fromDegrees(90.0, 45.0);
+    expect(c.longitude).toEqual(CesiumMath.toDegrees(Math.PI / 2));
+    expect(c.latitude).toEqual(CesiumMath.toDegrees(Math.PI / 4));
+    expect(c.height).toEqual(0.0);
+  });
+
+  it("fromCartesian works without a result parameter", function() {
+    var ellipsoid = Ellipsoid.WGS84;
+    var c = Coordinates.fromCartesian(surfaceCartesian, ellipsoid);
+    expect(c).toEqualEpsilon(surfaceCoordinates, CesiumMath.EPSILON8);
+  });
+
+  it("fromCartesian works with a result parameter", function() {
+    var ellipsoid = Ellipsoid.WGS84;
+    var result = new Coordinates();
+    var c = Coordinates.fromCartesian(surfaceCartesian, ellipsoid, result);
+    expect(c).toEqualEpsilon(surfaceCoordinates, CesiumMath.EPSILON8);
+    expect(result).toBe(c);
+  });
+
+  it("fromCartesian works without an ellipsoid", function() {
+    var c = Coordinates.fromCartesian(surfaceCartesian);
+    expect(c).toEqualEpsilon(surfaceCoordinates, CesiumMath.EPSILON8);
+  });
+
+  it("fromCartesian throws when there is no cartesian", function() {
+    expect(function() {
+      Coordinates.fromCartesian();
+    }).toThrowDeveloperError();
+  });
+
+  it("fromCartesian works with a value that is above the ellipsoid surface", function() {
+    var coordinates1 = Coordinates.fromRadians(Math.PI / 2, Math.PI / 4, -3000);
+    var cartesian1 = Cartesian3.fromDegrees(
+      coordinates1.longitude,
+      coordinates1.latitude,
+      coordinates1.height
+    );
+    var cartographic2 = Coordinates.fromCartesian(cartesian1);
+
+    expect(cartographic2).toEqualEpsilon(coordinates1, CesiumMath.EPSILON8);
+  });
+
+  it("fromCartesian works with a value that is bellow the ellipsoid surface", function() {
+    var cartographic1 = Coordinates.fromRadians(Math.PI / 2, Math.PI / 4, -3000);
+    var cartesian1 = Cartesian3.fromDegrees(
+      cartographic1.longitude,
+      cartographic1.latitude,
+      cartographic1.height
+    );
+    var cartographic2 = Coordinates.fromCartesian(cartesian1);
+
+    expect(cartographic2).toEqualEpsilon(cartographic1, CesiumMath.EPSILON8);
+  });
+
+  it("clone without a result parameter", function() {
+    var cartographic = new Coordinates(1.0, 2.0, 3.0);
+    var result = cartographic.clone();
+    expect(cartographic).not.toBe(result);
+    expect(cartographic).toEqual(result);
+  });
+
+  it("clone with a result parameter", function() {
+    var cartographic = new Coordinates(1.0, 2.0, 3.0);
+    var result = new Coordinates();
+    var returnedResult = cartographic.clone(result);
+    expect(cartographic).not.toBe(result);
+    expect(result).toBe(returnedResult);
+    expect(cartographic).toEqual(result);
+  });
+
+  it('clone works with "this" result parameter', function() {
+    var cartographic = new Coordinates(1.0, 2.0, 3.0);
+    var returnedResult = cartographic.clone(cartographic);
+    expect(cartographic).toBe(returnedResult);
+  });
+
+  it("equals", function() {
+    var cartographic = new Coordinates(1.0, 2.0, 3.0);
+    expect(cartographic.equals(new Coordinates(1.0, 2.0, 3.0))).toEqual(true);
+    expect(cartographic.equals(new Coordinates(2.0, 2.0, 3.0))).toEqual(false);
+    expect(cartographic.equals(new Coordinates(2.0, 1.0, 3.0))).toEqual(false);
+    expect(cartographic.equals(new Coordinates(1.0, 2.0, 4.0))).toEqual(false);
+    expect(cartographic.equals(undefined)).toEqual(false);
+  });
+
+  it("equalsEpsilon", function() {
+    var cartographic = new Coordinates(1.0, 2.0, 3.0);
+    expect(
+      cartographic.equalsEpsilon(new Coordinates(1.0, 2.0, 3.0), 0.0)
+    ).toEqual(true);
+    expect(
+      cartographic.equalsEpsilon(new Coordinates(1.0, 2.0, 3.0), 1.0)
+    ).toEqual(true);
+    expect(
+      cartographic.equalsEpsilon(new Coordinates(2.0, 2.0, 3.0), 1.0)
+    ).toEqual(true);
+    expect(
+      cartographic.equalsEpsilon(new Coordinates(1.0, 3.0, 3.0), 1.0)
+    ).toEqual(true);
+    expect(
+      cartographic.equalsEpsilon(new Coordinates(1.0, 2.0, 4.0), 1.0)
+    ).toEqual(true);
+    expect(
+      cartographic.equalsEpsilon(new Coordinates(2.0, 2.0, 3.0), 0.99999)
+    ).toEqual(false);
+    expect(
+      cartographic.equalsEpsilon(new Coordinates(1.0, 3.0, 3.0), 0.99999)
+    ).toEqual(false);
+    expect(
+      cartographic.equalsEpsilon(new Coordinates(1.0, 2.0, 4.0), 0.99999)
+    ).toEqual(false);
+    expect(cartographic.equalsEpsilon(undefined, 1)).toEqual(false);
+  });
+
+  it("toString", function() {
+    var cartographic = new Coordinates(1.123, 2.345, 6.789);
+    expect(cartographic.toString()).toEqual("(1.123, 2.345, 6.789)");
+  });
+
+  it("clone returns undefined without cartographic parameter", function() {
+    expect(Coordinates.clone(undefined)).toBeUndefined();
+  });
+});
