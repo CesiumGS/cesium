81b801a49c921551a9d2416cddef5f3931f25f13
Sean Lilley <lilleyse@gmail.com>
2021-03-15 14:02:30 -0400

    Update for classificationType changes

--- Source/Scene/Gltf3DTileContent.js
+++ Source/Scene/Gltf3DTileContent.js
@@ -48,6 +48,10 @@ function Gltf3DTileContent(tileset, tile, resource, gltf) {
   this._batchTable = undefined;
   this._features = undefined;
 
+  this._classificationType = tileset.vectorClassificationOnly
+    ? undefined
+    : tileset.classificationType;
+
   // Populate from gltf when available
   this._batchIdAttributeName = undefined;
   this._diffuseAttributeOrUniformName = {};
@@ -134,7 +138,7 @@ Object.defineProperties(Gltf3DTileContent.prototype, {
 function getVertexShaderCallback(content) {
   return function (vs, programId) {
     var batchTable = content._batchTable;
-    var handleTranslucent = !defined(content._tileset.classificationType);
+    var handleTranslucent = !defined(content._classificationType);
 
     var gltf = content._model.gltf;
     if (defined(gltf)) {
@@ -159,7 +163,7 @@ function getVertexShaderCallback(content) {
 function getFragmentShaderCallback(content) {
   return function (fs, programId) {
     var batchTable = content._batchTable;
-    var handleTranslucent = !defined(content._tileset.classificationType);
+    var handleTranslucent = !defined(content._classificationType);
 
     var gltf = content._model.gltf;
     if (defined(gltf)) {
@@ -169,7 +173,8 @@ function getFragmentShaderCallback(content) {
     }
     var callback = batchTable.getFragmentShaderCallback(
       handleTranslucent,
-      content._diffuseAttributeOrUniformName[programId]
+      content._diffuseAttributeOrUniformName[programId],
+      false
     );
     return defined(callback) ? callback(fs) : fs;
   };
@@ -365,7 +370,7 @@ function initialize(content, gltf) {
   }
 
   var colorChangedCallback;
-  if (defined(tileset.classificationType)) {
+  if (defined(content._classificationType)) {
     colorChangedCallback = createColorChangedCallback(content);
   }
 
@@ -388,7 +393,7 @@ function initialize(content, gltf) {
     primitive: tileset,
   };
 
-  if (!defined(tileset.classificationType)) {
+  if (!defined(content._classificationType)) {
     // PERFORMANCE_IDEA: patch the shader on demand, e.g., the first time show/color changes.
     // The pick shader still needs to be patched.
     content._model = new Model({
@@ -440,7 +445,7 @@ function initialize(content, gltf) {
       ),
       uniformMapLoaded: batchTable.getUniformMapCallback(),
       pickIdLoaded: getPickIdCallback(content),
-      classificationType: tileset.classificationType,
+      classificationType: content._classificationType,
       batchTable: batchTable,
     });
   }
@@ -551,7 +556,7 @@ Gltf3DTileContent.prototype.update = function (tileset, frameState) {
   if (
     commandStart < commandEnd &&
     (frameState.passes.render || frameState.passes.pick) &&
-    !defined(tileset.classificationType)
+    !defined(this._classificationType)
   ) {
     batchTable.addDerivedCommands(frameState, commandStart);
   }
