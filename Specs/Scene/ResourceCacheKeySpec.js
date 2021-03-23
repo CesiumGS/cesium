a20bed35406a0c2232f7d68c5078beecc7255d97
Peter Gagliardi <ptrgags@gmail.com>
2021-03-23 14:18:29 -0400

    Add unit tests for getSchemaCacheKey

--- Specs/Scene/ResourceCacheKeySpec.js
+++ Specs/Scene/ResourceCacheKeySpec.js
@@ -1,7 +1,26 @@
+import { Resource, ResourceCacheKey } from "../../Source/Cesium.js";
+
 describe("ResourceCacheKey", function () {
-  it("getSchemaCacheKey works for external schemas", function () {});
+  var schemaUri = "https://example.com/schema.json";
+  var schemaResource = new Resource({ url: schemaUri });
 
-  it("getSchemaCacheKey works for external schemas", function () {});
+  it("getSchemaCacheKey works for external schemas", function () {
+    var cacheKey = ResourceCacheKey.getSchemaCacheKey({
+      resource: schemaResource,
+    });
 
-  it("getSchemaCacheKey works for internal schemas", function () {});
+    expect(cacheKey).toBe(schemaUri);
+  });
+
+  it("getSchemaCacheKey works for internal schemas", function () {
+    var schema = {
+      classes: {},
+      enums: {},
+    };
+    var cacheKey = ResourceCacheKey.getSchemaCacheKey({
+      schema: schema,
+    });
+
+    expect(cacheKey).toEqual(JSON.stringify(schema));
+  });
 });
