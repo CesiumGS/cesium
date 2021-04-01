c392d8e3c29f1534dd2e7bf53c77728b7f11b51f
Peter Gagliardi <ptrgags@gmail.com>
2021-04-01 08:31:08 -0400

    Fix JSDoc for findMetadataGroup

--- Source/Scene/findMetadataGroup.js
+++ Source/Scene/findMetadataGroup.js
@@ -4,8 +4,12 @@ import has3DTilesExtension from "./has3DTilesExtension.js";
  * Check if a content has a <code>3DTILES_metadata</code> extension, and if so,
  * look up the group from the <code>3DTILES_metadata.groups</code> object.
  *
+ * @function
+ *
  * @param {Cesium3DTileset} tileset The tileset to query for group metadata
  * @param {Object} contentHeader the JSON header for a {@link Cesium3DTileContent}
+ * @return {MetadataGroup} the metadata group, or <code>undefined</code> if not found
+ * @private
  */
 export default function findMetadataGroup(tileset, contentHeader) {
   if (has3DTilesExtension(contentHeader, "3DTILES_metadata")) {
