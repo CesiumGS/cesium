ee9f2f854a0b481107311f74f60d18b2c7b2f3ed
ebogo1 <ebogo@alumni.upenn.edu>
2021-05-22 19:14:25 -0400

    update loadKTX2Spec, first pass

--- Specs/Core/loadKTX2Spec.js
+++ Specs/Core/loadKTX2Spec.js
@@ -1,7 +1,7 @@
 import { loadKTX2 } from "../../Source/Cesium.js";
+import { KTX2Transcoder } from "../../Source/Cesium.js";
 import { PixelFormat } from "../../Source/Cesium.js";
 import { Request } from "../../Source/Cesium.js";
-import { RequestErrorEvent } from "../../Source/Cesium.js";
 import { RequestScheduler } from "../../Source/Cesium.js";
 import { Resource } from "../../Source/Cesium.js";
 import { RuntimeError } from "../../Source/Cesium.js";
@@ -82,160 +82,6 @@ describe("Core/loadKTX2", function () {
     7,
     0,
     0,
-    0,
-    0,
-  ]);
-  var validCompressedMipmap = new Uint8Array([
-    171,
-    75,
-    84,
-    88,
-    32,
-    49,
-    49,
-    187,
-    13,
-    10,
-    26,
-    10,
-    1,
-    2,
-    3,
-    4,
-    0,
-    0,
-    0,
-    0,
-    1,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    100,
-    141,
-    0,
-    0,
-    7,
-    25,
-    0,
-    0,
-    4,
-    0,
-    0,
-    0,
-    4,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    1,
-    0,
-    0,
-    0,
-    3,
-    0,
-    0,
-    0,
-    52,
-    0,
-    0,
-    0,
-    19,
-    0,
-    0,
-    0,
-    67,
-    82,
-    78,
-    76,
-    73,
-    66,
-    95,
-    70,
-    79,
-    85,
-    82,
-    67,
-    67,
-    0,
-    69,
-    84,
-    67,
-    49,
-    0,
-    0,
-    23,
-    0,
-    0,
-    0,
-    75,
-    84,
-    88,
-    111,
-    114,
-    105,
-    101,
-    110,
-    116,
-    97,
-    116,
-    105,
-    111,
-    110,
-    0,
-    83,
-    61,
-    114,
-    44,
-    84,
-    61,
-    100,
-    0,
-    0,
-    8,
-    0,
-    0,
-    0,
-    255,
-    0,
-    0,
-    0,
-    255,
-    255,
-    0,
-    0,
-    8,
-    0,
-    0,
-    0,
-    255,
-    0,
-    0,
-    0,
-    255,
-    255,
-    0,
-    0,
-    8,
-    0,
-    0,
-    0,
-    255,
-    0,
-    0,
-    0,
-    255,
-    255,
     0,
     0,
   ]);
@@ -405,247 +251,13 @@ describe("Core/loadKTX2", function () {
     0,
     255,
   ]);
-  var validUncompressedMipmap = new Uint8Array([
-    171,
-    75,
-    84,
-    88,
-    32,
-    49,
-    49,
-    187,
-    13,
-    10,
-    26,
-    10,
-    1,
-    2,
-    3,
-    4,
-    1,
-    20,
-    0,
-    0,
-    1,
-    0,
-    0,
-    0,
-    7,
-    25,
-    0,
-    0,
-    81,
-    128,
-    0,
-    0,
-    7,
-    25,
-    0,
-    0,
-    4,
-    0,
-    0,
-    0,
-    4,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    1,
-    0,
-    0,
-    0,
-    3,
-    0,
-    0,
-    0,
-    52,
-    0,
-    0,
-    0,
-    19,
-    0,
-    0,
-    0,
-    67,
-    82,
-    78,
-    76,
-    73,
-    66,
-    95,
-    70,
-    79,
-    85,
-    82,
-    67,
-    67,
-    0,
-    82,
-    71,
-    66,
-    120,
-    0,
-    0,
-    23,
-    0,
-    0,
-    0,
-    75,
-    84,
-    88,
-    111,
-    114,
-    105,
-    101,
-    110,
-    116,
-    97,
-    116,
-    105,
-    111,
-    110,
-    0,
-    83,
-    61,
-    114,
-    44,
-    84,
-    61,
-    100,
-    0,
-    0,
-    48,
-    0,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    12,
-    0,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    255,
-    0,
-    0,
-    3,
-    0,
-    0,
-    0,
-    255,
-    0,
-    0,
-    0,
-  ]);
-
-  var fakeXHR;
-
-  beforeEach(function () {
-    fakeXHR = jasmine.createSpyObj("XMLHttpRequest", [
-      "send",
-      "open",
-      "setRequestHeader",
-      "abort",
-      "getAllResponseHeaders",
-    ]);
-    fakeXHR.simulateLoad = function (response) {
-      fakeXHR.simulateHttpResponse(200, response);
-    };
-    fakeXHR.simulateError = function () {
-      fakeXHR.response = "";
-      if (typeof fakeXHR.onerror === "function") {
-        fakeXHR.onerror();
-      }
-    };
-    fakeXHR.simulateHttpResponse = function (statusCode, response) {
-      fakeXHR.status = statusCode;
-      fakeXHR.response = response;
-      if (typeof fakeXHR.onload === "function") {
-        fakeXHR.onload();
-      }
-    };
-
-    spyOn(window, "XMLHttpRequest").and.returnValue(fakeXHR);
-  });
-
   it("throws with no url", function () {
     expect(function () {
       loadKTX2();
     }).toThrowDeveloperError();
   });
 
-  it("creates and sends request without any custom headers", function () {
-    var testUrl = "http://example.invalid/testuri";
-    loadKTX2(testUrl);
-
-    expect(fakeXHR.open).toHaveBeenCalledWith("GET", testUrl, true);
-    expect(fakeXHR.setRequestHeader).not.toHaveBeenCalled();
-    expect(fakeXHR.send).toHaveBeenCalled();
-  });
-
-  it("returns a promise that rejects when the request errors", function () {
+  it("returns a promise that resolves to undefined when there is no data", function () {
     var testUrl = "http://example.invalid/testuri";
     var promise = loadKTX2(testUrl);
 
@@ -664,198 +276,90 @@ describe("Core/loadKTX2", function () {
 
     expect(resolvedValue).toBeUndefined();
     expect(rejectedError).toBeUndefined();
-
-    fakeXHR.simulateError();
-    expect(resolvedValue).toBeUndefined();
-    expect(rejectedError).toBeInstanceOf(RequestErrorEvent);
-    expect(rejectedError.statusCode).toBeUndefined();
-    expect(rejectedError.response).toBeUndefined();
   });
 
-  it("returns a promise that rejects when the request results in an HTTP error code", function () {
-    var testUrl = "http://example.invalid/testuri";
-    var promise = loadKTX2(testUrl);
-
-    expect(promise).toBeDefined();
-
-    var resolvedValue;
-    var rejectedError;
-    promise.then(
-      function (value) {
-        resolvedValue = value;
-      },
-      function (error) {
-        rejectedError = error;
-      }
-    );
-
-    expect(resolvedValue).toBeUndefined();
-    expect(rejectedError).toBeUndefined();
-
-    var error = "some error";
-    fakeXHR.simulateHttpResponse(404, error);
-    expect(resolvedValue).toBeUndefined();
-    expect(rejectedError).toBeInstanceOf(RequestErrorEvent);
-    expect(rejectedError.statusCode).toEqual(404);
-    expect(rejectedError.response).toEqual(error);
-  });
-
-  it("returns a promise that resolves with undefined when the status code is 204", function () {
-    var testUrl = "http://example.invalid/testuri";
-    var promise = loadKTX2(testUrl);
-
-    expect(promise).toBeDefined();
-
-    var resolved = false;
-    var resolvedValue;
-    var rejectedError;
-    promise.then(
-      function (value) {
-        resolved = true;
-        resolvedValue = value;
-      },
-      function (error) {
-        rejectedError = error;
-      }
-    );
-
-    expect(resolvedValue).toBeUndefined();
-    expect(rejectedError).toBeUndefined();
-
-    fakeXHR.simulateHttpResponse(204);
-    expect(resolved).toBe(true);
-    expect(resolvedValue).toBeUndefined();
-    expect(rejectedError).toBeUndefined();
-  });
-
-  it("returns a promise that resolves to an uncompressed texture when the request loads", function () {
-    var testUrl = "http://example.invalid/testuri";
-    var promise = loadKTX2(testUrl);
-
-    expect(promise).toBeDefined();
-
-    var resolvedValue;
-    var rejectedError;
-    promise.then(
-      function (value) {
-        resolvedValue = value;
-      },
-      function (error) {
-        rejectedError = error;
-      }
-    );
-
-    expect(resolvedValue).toBeUndefined();
-    expect(rejectedError).toBeUndefined();
-
-    var response = validUncompressed.buffer;
-    fakeXHR.simulateLoad(response);
-    expect(resolvedValue).toBeDefined();
-    expect(resolvedValue.width).toEqual(4);
-    expect(resolvedValue.height).toEqual(4);
-    expect(
-      PixelFormat.isCompressedFormat(resolvedValue.internalFormat)
-    ).toEqual(false);
-    expect(resolvedValue.bufferView).toBeDefined();
-    expect(rejectedError).toBeUndefined();
+  it("returns a promise that resolves to an uncompressed texture", function () {
+    var resource = Resource.createIfNeeded("./Data/Images/Green4x4.ktx2");
+    var loadPromise = resource.fetchArrayBuffer();
+    return loadPromise.then(function (buffer) {
+      var promise = KTX2Transcoder.transcode(buffer, { pvrtc: true });
+      expect(promise).toBeDefined();
+      return promise.then(function (resolvedValue) {
+        expect(resolvedValue).toBeDefined();
+        expect(resolvedValue.width).toEqual(4);
+        expect(resolvedValue.height).toEqual(4);
+        expect(
+          PixelFormat.isCompressedFormat(resolvedValue.internalFormat)
+        ).toEqual(false);
+        expect(resolvedValue.bufferView).toBeDefined();
+      });
+    });
   });
 
   it("returns a promise that resolves to an uncompressed texture containing all mip levels of the original texture", function () {
-    var testUrl = "http://example.invalid/testuri";
-    var promise = loadKTX2(testUrl);
-
-    expect(promise).toBeDefined();
-
-    var resolvedValue;
-    var rejectedError;
-    promise.then(
-      function (value) {
-        resolvedValue = value;
-      },
-      function (error) {
-        rejectedError = error;
-      }
-    );
-
-    expect(resolvedValue).toBeUndefined();
-    expect(rejectedError).toBeUndefined();
-
-    var response = validUncompressedMipmap.buffer;
-    fakeXHR.simulateLoad(response);
-    expect(resolvedValue).toBeDefined();
-    expect(resolvedValue.length).toEqual(3);
-    expect(resolvedValue[0].width).toEqual(4);
-    expect(resolvedValue[0].height).toEqual(4);
-    expect(
-      PixelFormat.isCompressedFormat(resolvedValue[0].internalFormat)
-    ).toEqual(false);
-    expect(resolvedValue[0].bufferView).toBeDefined();
-    expect(rejectedError).toBeUndefined();
+    var resource = Resource.createIfNeeded("./Data/Images/Green4x4Mipmap.ktx2");
+    var loadPromise = resource.fetchArrayBuffer();
+    return loadPromise.then(function (buffer) {
+      var promise = KTX2Transcoder.transcode(buffer, { pvrtc: true });
+      expect(promise).toBeDefined();
+      return promise.then(function (resolvedValue) {
+        expect(resolvedValue).toBeDefined();
+        expect(resolvedValue.length).toEqual(3);
+        var dims = [4, 2, 1];
+        for (var i = 0; i < resolvedValue.length; ++i) {
+          expect(resolvedValue[i].width).toEqual(dims[i]);
+          expect(resolvedValue[i].height).toEqual(dims[i]);
+          expect(resolvedValue[i].bufferView).toBeDefined();
+        }
+        expect(
+          PixelFormat.isCompressedFormat(resolvedValue.internalFormat)
+        ).toEqual(false);
+      });
+    });
   });
 
-  it("returns a promise that resolves to a compressed texture when the request loads", function () {
-    var testUrl = "http://example.invalid/testuri";
-    var promise = loadKTX2(testUrl);
-
-    expect(promise).toBeDefined();
-
-    var resolvedValue;
-    var rejectedError;
-    promise.then(
-      function (value) {
-        resolvedValue = value;
-      },
-      function (error) {
-        rejectedError = error;
-      }
+  it("returns a promise that resolves to a compressed texture", function () {
+    var resource = Resource.createIfNeeded(
+      "./Data/Images/Green4x4Compressed.ktx2"
     );
-
-    expect(resolvedValue).toBeUndefined();
-    expect(rejectedError).toBeUndefined();
-
-    var response = validCompressed.buffer;
-    fakeXHR.simulateLoad(response);
-    expect(resolvedValue).toBeDefined();
-    expect(resolvedValue.width).toEqual(4);
-    expect(resolvedValue.height).toEqual(4);
-    expect(
-      PixelFormat.isCompressedFormat(resolvedValue.internalFormat)
-    ).toEqual(true);
-    expect(resolvedValue.bufferView).toBeDefined();
-    expect(rejectedError).toBeUndefined();
+    var loadPromise = resource.fetchArrayBuffer();
+    return loadPromise.then(function (buffer) {
+      var promise = KTX2Transcoder.transcode(buffer, { pvrtc: true });
+      expect(promise).toBeDefined();
+      return promise.then(function (resolvedValue) {
+        expect(resolvedValue).toBeDefined();
+        expect(resolvedValue.width).toEqual(4);
+        expect(resolvedValue.height).toEqual(4);
+        expect(
+          PixelFormat.isCompressedFormat(resolvedValue.internalFormat)
+        ).toEqual(true);
+        expect(resolvedValue.bufferView).toBeDefined();
+      });
+    });
   });
 
   it("returns a promise that resolves to a compressed texture containing the all mip levels of the original texture", function () {
-    var testUrl = "http://example.invalid/testuri";
-    var promise = loadKTX2(testUrl);
-
-    expect(promise).toBeDefined();
-
-    var resolvedValue;
-    var rejectedError;
-    promise.then(
-      function (value) {
-        resolvedValue = value;
-      },
-      function (error) {
-        rejectedError = error;
-      }
+    var resource = Resource.createIfNeeded(
+      "./Data/Images/Green4x4CompressedMipmap.ktx2"
     );
-
-    expect(resolvedValue).toBeUndefined();
-    expect(rejectedError).toBeUndefined();
-
-    var response = validCompressedMipmap.buffer;
-    fakeXHR.simulateLoad(response);
-    expect(resolvedValue).toBeDefined();
-    expect(resolvedValue.length).toEqual(3);
-    expect(resolvedValue[0].width).toEqual(4);
-    expect(resolvedValue[0].height).toEqual(4);
-    expect(
-      PixelFormat.isCompressedFormat(resolvedValue[0].internalFormat)
-    ).toEqual(true);
-    expect(resolvedValue[0].bufferView).toBeDefined();
-    expect(rejectedError).toBeUndefined();
+    var loadPromise = resource.fetchArrayBuffer();
+    return loadPromise.then(function (buffer) {
+      var promise = KTX2Transcoder.transcode(buffer, { pvrtc: true });
+      expect(promise).toBeDefined();
+      return promise.then(function (resolvedValue) {
+        expect(resolvedValue).toBeDefined();
+        expect(resolvedValue.length).toEqual(3);
+        var dims = [4, 2, 1];
+        for (var i = 0; i < resolvedValue.length; ++i) {
+          expect(resolvedValue[i].width).toEqual(dims[i]);
+          expect(resolvedValue[i].height).toEqual(dims[i]);
+          expect(resolvedValue[i].bufferView).toBeDefined();
+        }
+        expect(
+          PixelFormat.isCompressedFormat(resolvedValue.internalFormat)
+        ).toEqual(true);
+      });
+    });
   });
 
   it("cannot parse invalid KTX buffer", function () {
