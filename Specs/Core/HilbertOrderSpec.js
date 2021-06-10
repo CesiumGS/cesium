95ef456dac0f88d87297df44243e5d6b5a50aae2
Sanjeet Suhag <suhagsanjeet@gmail.com>
2021-06-09 23:50:57 -0400

    Fixes S2

--- Specs/Core/HilbertOrderSpec.js
+++ Specs/Core/HilbertOrderSpec.js
@@ -47,27 +47,47 @@ describe("Core/HilbertOrder", function () {
   });
 
   it("encode2D works", function () {
-    expect(HilbertOrder.encode2D(1, 0, 0)).toEqual(0);
-    expect(HilbertOrder.encode2D(1, 0, 1)).toEqual(1);
-    expect(HilbertOrder.encode2D(1, 1, 1)).toEqual(2);
-    expect(HilbertOrder.encode2D(1, 1, 0)).toEqual(3);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(1, 0, 0)).toEqual(BigInt(0));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(1, 0, 1)).toEqual(BigInt(1));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(1, 1, 1)).toEqual(BigInt(2));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(1, 1, 0)).toEqual(BigInt(3));
 
-    expect(HilbertOrder.encode2D(2, 0, 0)).toEqual(0);
-    expect(HilbertOrder.encode2D(2, 1, 0)).toEqual(1);
-    expect(HilbertOrder.encode2D(2, 1, 1)).toEqual(2);
-    expect(HilbertOrder.encode2D(2, 0, 1)).toEqual(3);
-    expect(HilbertOrder.encode2D(2, 0, 2)).toEqual(4);
-    expect(HilbertOrder.encode2D(2, 0, 3)).toEqual(5);
-    expect(HilbertOrder.encode2D(2, 1, 3)).toEqual(6);
-    expect(HilbertOrder.encode2D(2, 1, 2)).toEqual(7);
-    expect(HilbertOrder.encode2D(2, 2, 2)).toEqual(8);
-    expect(HilbertOrder.encode2D(2, 2, 3)).toEqual(9);
-    expect(HilbertOrder.encode2D(2, 3, 3)).toEqual(10);
-    expect(HilbertOrder.encode2D(2, 3, 2)).toEqual(11);
-    expect(HilbertOrder.encode2D(2, 3, 1)).toEqual(12);
-    expect(HilbertOrder.encode2D(2, 2, 1)).toEqual(13);
-    expect(HilbertOrder.encode2D(2, 2, 0)).toEqual(14);
-    expect(HilbertOrder.encode2D(2, 3, 0)).toEqual(15);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 0, 0)).toEqual(BigInt(0));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 1, 0)).toEqual(BigInt(1));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 1, 1)).toEqual(BigInt(2));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 0, 1)).toEqual(BigInt(3));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 0, 2)).toEqual(BigInt(4));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 0, 3)).toEqual(BigInt(5));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 1, 3)).toEqual(BigInt(6));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 1, 2)).toEqual(BigInt(7));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 2, 2)).toEqual(BigInt(8));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 2, 3)).toEqual(BigInt(9));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 3, 3)).toEqual(BigInt(10));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 3, 2)).toEqual(BigInt(11));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 3, 1)).toEqual(BigInt(12));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 2, 1)).toEqual(BigInt(13));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 2, 0)).toEqual(BigInt(14));
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.encode2D(2, 3, 0)).toEqual(BigInt(15));
   });
 
   it("decode2D throws for invalid level", function () {
@@ -86,26 +106,45 @@ describe("Core/HilbertOrder", function () {
   });
 
   it("decode2D works", function () {
-    expect(HilbertOrder.decode2D(1, 0)).toEqual([0, 0]);
-    expect(HilbertOrder.decode2D(1, 1)).toEqual([0, 1]);
-    expect(HilbertOrder.decode2D(1, 2)).toEqual([1, 1]);
-    expect(HilbertOrder.decode2D(1, 3)).toEqual([1, 0]);
-
-    expect(HilbertOrder.decode2D(2, 0)).toEqual([0, 0]);
-    expect(HilbertOrder.decode2D(2, 1)).toEqual([1, 0]);
-    expect(HilbertOrder.decode2D(2, 2)).toEqual([1, 1]);
-    expect(HilbertOrder.decode2D(2, 3)).toEqual([0, 1]);
-    expect(HilbertOrder.decode2D(2, 4)).toEqual([0, 2]);
-    expect(HilbertOrder.decode2D(2, 5)).toEqual([0, 3]);
-    expect(HilbertOrder.decode2D(2, 6)).toEqual([1, 3]);
-    expect(HilbertOrder.decode2D(2, 7)).toEqual([1, 2]);
-    expect(HilbertOrder.decode2D(2, 8)).toEqual([2, 2]);
-    expect(HilbertOrder.decode2D(2, 9)).toEqual([2, 3]);
-    expect(HilbertOrder.decode2D(2, 10)).toEqual([3, 3]);
-    expect(HilbertOrder.decode2D(2, 11)).toEqual([3, 2]);
-    expect(HilbertOrder.decode2D(2, 12)).toEqual([3, 1]);
-    expect(HilbertOrder.decode2D(2, 13)).toEqual([2, 1]);
-    expect(HilbertOrder.decode2D(2, 14)).toEqual([2, 0]);
-    expect(HilbertOrder.decode2D(2, 15)).toEqual([3, 0]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(1, BigInt(0))).toEqual([0, 0]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(1, BigInt(1))).toEqual([0, 1]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(1, BigInt(2))).toEqual([1, 1]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(1, BigInt(3))).toEqual([1, 0]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(0))).toEqual([0, 0]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(1))).toEqual([1, 0]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(2))).toEqual([1, 1]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(3))).toEqual([0, 1]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(4))).toEqual([0, 2]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(5))).toEqual([0, 3]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(6))).toEqual([1, 3]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(7))).toEqual([1, 2]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(8))).toEqual([2, 2]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(9))).toEqual([2, 3]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(10))).toEqual([3, 3]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(11))).toEqual([3, 2]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(12))).toEqual([3, 1]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(13))).toEqual([2, 1]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(14))).toEqual([2, 0]);
+    // eslint-disable-next-line no-undef
+    expect(HilbertOrder.decode2D(2, BigInt(15))).toEqual([3, 0]);
   });
 });
--- Specs/Core/S2CellSpec.js
+++ Specs/Core/S2CellSpec.js
@@ -40,6 +40,46 @@ describe("Core/S2Cell", function () {
     }).toThrowDeveloperError();
   });
 
+  it("creates cell from face, position, level", function () {
+    var cell = S2Cell.fromFacePosLevel(0, BigInt(0), 1);
+    expect(S2Cell.getTokenFromId(cell._cellId)).toEqual("04");
+    cell = S2Cell.fromFacePosLevel(BigInt(0), BigInt(1), 1);
+    expect(S2Cell.getTokenFromId(cell._cellId)).toEqual("0c");
+    cell = S2Cell.fromFacePosLevel(BigInt(0), BigInt(2), 1);
+    expect(S2Cell.getTokenFromId(cell._cellId)).toEqual("14");
+    cell = S2Cell.fromFacePosLevel(BigInt(0), BigInt(3), 1);
+    expect(S2Cell.getTokenFromId(cell._cellId)).toEqual("1c");
+
+    cell = S2Cell.fromFacePosLevel(1, BigInt("538969508876688737"), 30);
+    expect(S2Cell.getTokenFromId(cell._cellId)).toEqual("2ef59bd352b93ac3");
+  });
+
+  it("throws for creating cell from invalid face, position, level", function () {
+    expect(function () {
+      S2Cell.fromFacePosLevel(-1, BigInt(0), 1);
+    }).toThrowDeveloperError();
+
+    expect(function () {
+      S2Cell.fromFacePosLevel(6, BigInt(0), 1);
+    }).toThrowDeveloperError();
+
+    expect(function () {
+      S2Cell.fromFacePosLevel(0, BigInt(-1), 1);
+    }).toThrowDeveloperError();
+
+    expect(function () {
+      S2Cell.fromFacePosLevel(0, BigInt(4), 1);
+    }).toThrowDeveloperError();
+
+    expect(function () {
+      S2Cell.fromFacePosLevel(0, BigInt(0), -1);
+    }).toThrowDeveloperError();
+
+    expect(function () {
+      S2Cell.fromFacePosLevel(0, BigInt(0), 31);
+    }).toThrowDeveloperError();
+  });
+
   it("accepts valid token", function () {
     var tokenValidity = S2Cell.isValidToken("1");
     expect(tokenValidity).toBe(true);
