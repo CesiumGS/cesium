4634c36b89cc2b05c012084e7d268184a117d6b0
ebogo1 <ebogo@alumni.upenn.edu>
2021-06-14 14:59:15 -0400

    fix eslint

--- Source/Scene/parseKTX2.js
+++ Source/Scene/parseKTX2.js
@@ -103,12 +103,12 @@ function parseUncompressed(header, result) {
         PixelFormat.componentsLength(internalFormat);
 
       var faceView;
-      if (PixelDatatype.sizeInBytes(datatype) == 1) {
-        var faceView = new Uint8Array(levelBuffer.buffer, faceLength * j);
-      } else if (PixelDatatype.sizeInBytes(datatype) == 2) {
-        var faceView = new Uint16Array(levelBuffer.buffer, faceLength * j);
+      if (PixelDatatype.sizeInBytes(datatype) === 1) {
+        faceView = new Uint8Array(levelBuffer.buffer, faceLength * j);
+      } else if (PixelDatatype.sizeInBytes(datatype) === 2) {
+        faceView = new Uint16Array(levelBuffer.buffer, faceLength * j);
       } else {
-        var faceView = new Float32Array(levelBuffer.buffer, faceLength * j);
+        faceView = new Float32Array(levelBuffer.buffer, faceLength * j);
       }
 
       level[faceOrder[j]] = {
