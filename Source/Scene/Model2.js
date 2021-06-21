513688730c831dd307d6d019d1aed84c037e6bca
Sanjeet Suhag <suhagsanjeet@gmail.com>
2021-06-21 14:58:25 -0400

    Node transform handling, without propagation

--- Source/Scene/Model2.js
+++ Source/Scene/Model2.js
@@ -3,11 +3,12 @@ import Pass from "../Renderer/Pass.js";
 import RenderState from "../Renderer/RenderState.js";
 import when from "../ThirdParty/when.js";
 import BoundingSphere from "../Core/BoundingSphere.js";
-import Cartesian3 from "../Core/Cartesian3.js";
 import ShaderProgram from "../Renderer/ShaderProgram.js";
 import defined from "../Core/defined.js";
 import Matrix4 from "../Core/Matrix4.js";
 import VertexArray from "../Renderer/VertexArray.js";
+import Cartesian3 from "../Core/Cartesian3.js";
+import Quaternion from "../Core/Quaternion.js";
 
 export default function Model(options) {
   this.drawCommand = undefined;
@@ -22,7 +23,9 @@ Model.prototype.update = function (frameState) {
     this.drawCommand = createCommand(this, frameState);
   }
 
-  frameState.commandList.push(this.drawCommand);
+  for (var i = 0; i < this.drawCommand.length; i++) {
+    frameState.commandList.push(this.drawCommand[i]);
+  }
 };
 
 function createCommand(model, frameState) {
@@ -32,40 +35,7 @@ function createCommand(model, frameState) {
     },
   });
 
-  // Hard code first primitive
-  var primitive = model.components.scene.nodes[0].children[0].primitives[0];
-  var positionAttribute = primitive.attributes[1];
-
-  var boundingSphere = BoundingSphere.fromCornerPoints(
-    positionAttribute.min,
-    positionAttribute.max
-  );
-
-  var center = new Cartesian3(
-    1215019.2111447915,
-    -4736339.477299974,
-    4081627.9570209784
-  );
-  boundingSphere.center = center;
-  var modelMatrix = Matrix4.fromTranslation(center);
-
-  var shaderProgram = createShader(frameState.context);
-
-  var vertexArray = createVertexArray(primitive, frameState.context);
-
-  var drawCommand = new DrawCommand({
-    boundingVolume: boundingSphere,
-    modelMatrix: modelMatrix,
-    pass: Pass.OPAQUE,
-    shaderProgram: shaderProgram,
-    renderState: renderState,
-    vertexArray: vertexArray,
-    count: primitive.indices.count,
-    primitiveType: primitive.primitiveType,
-    uniformMap: undefined,
-  });
-
-  return drawCommand;
+  return createModelCommands(model.components, frameState, renderState);
 }
 
 function createShader(context) {
@@ -97,17 +67,106 @@ function createShader(context) {
   });
 }
 
+function getNodeTransform(node) {
+  if (defined(node.matrix)) {
+    return Matrix4.fromColumnMajorArray(node.matrix);
+  }
+
+  return Matrix4.fromTranslationQuaternionRotationScale(
+    defined(node.translation) ? node.translation : new Cartesian3(),
+    defined(node.rotation) ? node.rotation : Quaternion.IDENTITY,
+    defined(node.scale) ? node.scale : new Cartesian3(1, 1, 1)
+  );
+}
+
+function getAttributeBySemantic(primitive, semantic) {
+  var attributes = primitive.attributes;
+  var attributesLength = attributes.length;
+  for (var i = 0; i < attributesLength; ++i) {
+    var attribute = attributes[i];
+    if (attribute.semantic === semantic) {
+      return attribute;
+    }
+  }
+}
+
+function createModelCommands(model, frameState, renderState) {
+  var drawCommands = [];
+
+  var nodes = model.nodes;
+  var nodeStack = [];
+  nodeStack.length = 0;
+
+  var i;
+  for (i = 0; i < nodes.length; i++) {
+    nodeStack.push(nodes[i]);
+  }
+
+  while (nodeStack.length > 0) {
+    var node = nodeStack.pop();
+    var modelMatrix = getNodeTransform(node);
+    var primitives = node.primitives;
+    for (i = 0; i < primitives.length; i++) {
+      var primitive = primitives[i];
+      drawCommands.push(
+        createPrimitiveCommand(
+          primitive,
+          modelMatrix,
+          frameState.context,
+          renderState
+        )
+      );
+    }
+  }
+
+  return drawCommands;
+}
+
+function createPrimitiveCommand(primitive, modelMatrix, context, renderState) {
+  var attributes = primitive.attributes;
+  for (var k = 0; k < attributes.length; k++) {
+    var positionAttribute = getAttributeBySemantic(primitive, "POSITION");
+
+    var boundingSphere = BoundingSphere.fromCornerPoints(
+      positionAttribute.min,
+      positionAttribute.max
+    );
+    boundingSphere.center = Matrix4.getTranslation(
+      modelMatrix,
+      new Cartesian3()
+    );
+
+    var shaderProgram = createShader(context);
+    var vertexArray = createVertexArray(primitive, context);
+
+    return new DrawCommand({
+      boundingVolume: boundingSphere,
+      modelMatrix: modelMatrix,
+      pass: Pass.OPAQUE,
+      shaderProgram: shaderProgram,
+      renderState: renderState,
+      vertexArray: vertexArray,
+      count: primitive.indices.count,
+      primitiveType: primitive.primitiveType,
+      uniformMap: undefined,
+    });
+  }
+}
+
 function createVertexArray(primitive, context) {
+  var positionGltfAttribute = getAttributeBySemantic(primitive, "POSITION");
+  var normalGltfAttribute = getAttributeBySemantic(primitive, "NORMAL");
+
   var positionAttribute = {
     index: 0,
-    vertexBuffer: primitive.attributes[1].buffer,
+    vertexBuffer: positionGltfAttribute.buffer,
     componentsPerAttribute: 3,
-    componentDatatype: primitive.attributes[1].componentDatatype,
+    componentDatatype: positionGltfAttribute.componentDatatype,
   };
 
   var normalAttribute = {
     index: 1,
-    vertexBuffer: primitive.attributes[0].buffer,
+    vertexBuffer: normalGltfAttribute.buffer,
     componentsPerAttribute: 3,
     componentDatatype: primitive.attributes[0].componentDatatype,
   };
