802d71abcd3a650ccae00e0a7945fba666ac7547
Sanjeet Suhag <suhagsanjeet@gmail.com>
2021-07-28 12:33:20 -0400

    Updates shaders

--- Source/Shaders/ModelExperimental/ModelExperimentalVS.glsl
+++ Source/Shaders/ModelExperimental/ModelExperimentalVS.glsl
@@ -4,13 +4,8 @@ void main() {
   vec3 position = vec3(0.0);  
   position = processGeometry(position);
 
-  // TODO: skinning stage
-  // TODO: morph targets stage
-
   gl_Position = czm_modelViewProjection * vec4(position, 1.0);
 
-
-  // handle gl_PointSize
   #ifdef PRIMITIVE_TYPE_POINTS
   processPoints();
   
