7b40a60baebead4bf96a26e35673f6c203d927d8
ebogo1 <ebogo@alumni.upenn.edu>
2021-07-28 14:42:15 -0400

    remove inflate and deflate workers

--- ThirdParty/npm/zip.js
+++ ThirdParty/npm/zip.js
@@ -1,8 +1,2 @@
 import * as zip from "@zip.js/zip.js";
-// zip.configure({
-//   workerScripts: {
-//     deflate: ["../../node_modules/@zip.js/zip.js/dist/z-worker-pako.js", "../../node_modules/@zip.js/zip.js/dist/pako_deflate.min.js"],
-//     inflate: ["../../node_modules/@zip.js/zip.js/dist/z-worker-pako.js", "../../node_modules/@zip.js/zip.js/dist/pako_inflate.min.js"]
-//   }
-// });
 export { zip as default };
