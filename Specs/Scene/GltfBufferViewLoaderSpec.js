a153f3b6d98e461706c75bb8b3a339a450077cd9
Sanjeet Suhag <suhagsanjeet@gmail.com>
2021-07-29 15:10:05 -0400

    Fixes test for meshopt

--- Specs/Scene/GltfBufferViewLoaderSpec.js
+++ Specs/Scene/GltfBufferViewLoaderSpec.js
@@ -39,34 +39,34 @@ describe("Scene/GltfBufferViewLoader", function () {
   };
 
   var meshoptGltfEmbedded = {
-    buffers: [{ byteLength: 108 }, { byteLength: 136 }],
+    buffers: [{ byteLength: 29 }, { byteLength: 360 }],
     bufferViews: [
       {
         buffer: 1,
-        byteOffset: 0,
-        byteLength: 64,
+        byteOffset: 96,
+        byteLength: 192,
         byteStride: 8,
         target: 34962,
         extensions: {
           EXT_meshopt_compression: {
             buffer: 0,
             byteOffset: 0,
-            byteLength: 76,
+            byteLength: 124,
             byteStride: 8,
             mode: "ATTRIBUTES",
-            count: 8,
+            count: 24,
           },
         },
       },
       {
         buffer: 1,
-        byteOffset: 64,
+        byteOffset: 288,
         byteLength: 72,
         target: 34963,
         extensions: {
           EXT_meshopt_compression: {
             buffer: 0,
-            byteOffset: 76,
+            byteOffset: 0,
             byteLength: 29,
             byteStride: 2,
             mode: "TRIANGLES",
@@ -76,15 +76,24 @@ describe("Scene/GltfBufferViewLoader", function () {
       },
     ],
   };
-  var meshoptBufferBase64 =
-    "oAEZhAAAAT/MAAB+fX59fgEGEAAAAQ8wAAB+fX4BAgYAAAEDDwAAfX59AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/z8AAOHwICABMAIwFSAWJBYAdodWZ3iphmWJaJgBaQAAAAAA";
-  var meshoptDecodedPositionBase64 =
-    "AAAAAP8/AAD/PwAA/z8AAAAA/z//PwAA/z8AAAAAAAAAAAAAAAAAAAAA/z8AAAAA/z//P/8/AAD/P/8/AAAAAA==";
-  var meshoptTypedArray = Uint8Array.from(atob(meshoptBufferBase64), function (
-    c
-  ) {
-    return c.charCodeAt(0);
-  });
+
+  function getBase64FromTypedArray(typedArray) {
+    return btoa(String.fromCharCode.apply(null, typedArray));
+  }
+
+  function getTypedArrayFromBase64(base64) {
+    return Uint8Array.from(atob(base64), function (c) {
+      return c.charCodeAt(0);
+    });
+  }
+
+  var fallbackPositionBufferBase64 =
+    "AAD/P/8/AAD/P/8//z8AAAAA/z8AAAAA/z//PwAAAAD/P/8//z8AAAAA/z//PwAA/z8AAP8/AAAAAAAA/z8AAP8//z8AAAAA/z//P/8/AAD/PwAAAAAAAP8/AAD/PwAAAAD/PwAAAAD/P/8/AAAAAAAAAAAAAAAA/z8AAAAAAAAAAP8//z8AAAAA/z8AAAAAAAAAAP8/AAAAAAAAAAAAAAAAAAD/PwAAAAAAAAAAAAD/PwAA/z8AAP8/AAAAAAAA";
+  var meshoptPositionBufferBase64 =
+    "oAUZJkCZgAQAAAU/P8D/fn1+fX59fn1+fX7ADAAAfX4FAAhISEgAAAAFAAzMzH1+fX59zAAAAH59BQhAmYBmZgAABQzA/8B9fn1+fX59//8AAH59fn1+fX59AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP8//z8AAA==";
+  var meshoptPositionTypedArray = getTypedArrayFromBase64(
+    meshoptPositionBufferBase64
+  );
 
   var bufferTypedArray = new Uint8Array([1, 3, 7, 15, 31, 63, 127, 255]);
   var bufferArrayBuffer = bufferTypedArray.buffer;
@@ -262,11 +271,11 @@ describe("Scene/GltfBufferViewLoader", function () {
     });
   });
 
-  it("decodes bufferView with EXT_meshopt_compression", function () {
+  it("decodes positions with EXT_meshopt_compression", function () {
     ResourceCache.loadEmbeddedBuffer({
       parentResource: gltfResource,
       bufferId: 0,
-      typedArray: meshoptTypedArray,
+      typedArray: meshoptPositionTypedArray,
     });
 
     var bufferViewLoader = new GltfBufferViewLoader({
@@ -281,10 +290,10 @@ describe("Scene/GltfBufferViewLoader", function () {
     bufferViewLoader.process({});
 
     return bufferViewLoader.promise.then(function (bufferViewLoader) {
-      var positionBase64 = btoa(
-        String.fromCharCode.apply(null, bufferViewLoader.typedArray)
+      var decodedPositionBase64 = getBase64FromTypedArray(
+        bufferViewLoader.typedArray
       );
-      expect(positionBase64).toEqual(meshoptDecodedPositionBase64);
+      expect(decodedPositionBase64).toEqual(fallbackPositionBufferBase64);
     });
   });
 
