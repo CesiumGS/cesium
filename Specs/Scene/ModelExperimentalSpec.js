71bf348443def21a77c75c5e1c6e66b2a9ac566e
Peter Gagliardi <ptrgags@gmail.com>
2021-07-30 09:23:13 -0400

    Remove unused scene from unit test

--- Specs/Scene/ModelExperimentalSpec.js
+++ Specs/Scene/ModelExperimentalSpec.js
@@ -1,16 +1,9 @@
 import { GltfLoader, Resource } from "../../Source/Cesium.js";
 import { ModelExperimental } from "../../Source/Cesium.js";
-import createScene from "../createScene.js";
 
 describe("Scene/ModelExperimental", function () {
   var boxTexturedGlbUrl =
     "./Data/Models/GltfLoader/BoxTextured/glTF-Binary/BoxTextured.glb";
-
-  var scene;
-
-  beforeAll(function () {
-    scene = createScene();
-  });
 
   it("initializes from Uint8Array", function () {
     spyOn(GltfLoader.prototype, "load");
