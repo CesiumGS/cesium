2f7ebf329973a603e2c433a36cfd77dcef44fdb4
Peter Gagliardi <ptrgags@gmail.com>
2021-08-02 11:00:47 -0400

    Start a documentation sweep

--- Source/Scene/ModelExperimental/ModelExperimentalUtility.js
+++ Source/Scene/ModelExperimental/ModelExperimentalUtility.js
@@ -5,11 +5,22 @@ import Matrix4 from "../../Core/Matrix4.js";
 import Quaternion from "../../Core/Quaternion.js";
 import RuntimeError from "../../Core/RuntimeError.js";
 import Axis from "../Axis.js";
+
 /**
+ * Utility functions for {@link ModelExperimental}.
+ *
  * @private
  */
 export default function ModelExperimentalUtility() {}
 
+/**
+ * Create a function for reporting when a model fails to load
+ *
+ * @param {ModelExperimental} model The model to report about
+ * @param {String} type The type of object to report about
+ * @param {String} path The URI of the model file
+ * @return {Function} An error function that
+ */
 ModelExperimentalUtility.getFailedLoadFunction = function (model, type, path) {
   return function (error) {
     var message = "Failed to load " + type + ": " + path;
@@ -20,6 +31,12 @@ ModelExperimentalUtility.getFailedLoadFunction = function (model, type, path) {
   };
 };
 
+/**
+ * Get a transformation matrix from a node in the model.
+ *
+ * @param {ModelComponents.Node} node The node components
+ * @return {Matrix4} The computed transformation matrix. If no transformation matrix or parameters are specified, this will be the identity matrix.
+ */
 ModelExperimentalUtility.getNodeTransform = function (node) {
   if (defined(node.matrix)) {
     return Matrix4.fromColumnMajorArray(node.matrix);
@@ -32,6 +49,13 @@ ModelExperimentalUtility.getNodeTransform = function (node) {
   );
 };
 
+/**
+ * Find an attribute by semantic such as POSITION or TANGENT.
+ *
+ * @param {ModelComponents.Primitive} primitive The primitive components
+ * @param {VertexAttributeSemantic} semantic The semantic to search for
+ * @return {ModelComponents.Attribute} The selected. attribute, or undefined if not found.
+ */
 ModelExperimentalUtility.getAttributeBySemantic = function (
   primitive,
   semantic
@@ -47,6 +71,13 @@ ModelExperimentalUtility.getAttributeBySemantic = function (
   }
 };
 
+/**
+ * Create a bounding sphere from a primitive's POSITION attribute and model
+ * matrix.
+ *
+ * @param {ModelComponents.Primitive} primitive The primitive components.
+ * @param {Matrix4} modelMatrix The primitive's model matrix.
+ */
 ModelExperimentalUtility.createBoundingSphere = function (
   primitive,
   modelMatrix
@@ -64,6 +95,15 @@ ModelExperimentalUtility.createBoundingSphere = function (
   return boundingSphere;
 };
 
+/**
+ * Model matrices in a model file (e.g. glTF) are typically in a different
+ * coordinate system, such as with y-up instead of z-up. This method adjusts
+ * the matrix so z is up, x is forward.
+ *
+ * @param {Matrix4} modelMatrix The original model matrix. This will be updated in place
+ * @param {Axis} upAxis The original up direction
+ * @param {Axis} forwardAxis The original forward direction
+ */
 ModelExperimentalUtility.correctModelMatrix = function (
   modelMatrix,
   upAxis,
@@ -72,7 +112,7 @@ ModelExperimentalUtility.correctModelMatrix = function (
   if (upAxis === Axis.Y) {
     Matrix4.multiplyTransformation(modelMatrix, Axis.Y_UP_TO_Z_UP, modelMatrix);
   } else if (upAxis === Axis.X) {
-    Matrix4.multiplyTransformation(modelMatrix, Axis.X_UP_TO_Z_UP);
+    Matrix4.multiplyTransformation(modelMatrix, Axis.X_UP_TO_Z_UP, modelMatrix);
   }
 
   if (forwardAxis === Axis.Z) {
