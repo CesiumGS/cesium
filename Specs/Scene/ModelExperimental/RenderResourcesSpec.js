96f32f084b93fe72b735ada5d63f8057bfcb4fc1
Peter Gagliardi <ptrgags@gmail.com>
2021-08-02 17:10:41 -0400

    Get unit tests passing

--- Specs/Scene/ModelExperimental/RenderResourcesSpec.js
+++ Specs/Scene/ModelExperimental/RenderResourcesSpec.js
@@ -1,8 +1,15 @@
 import {
+  AttributeType,
+  Cartesian3,
+  ComponentDatatype,
+  DepthFunction,
+  Math as CesiumMath,
   Matrix4,
   ModelExperimentalSceneNode,
   ModelExperimentalSceneMeshPrimitive,
+  PrimitiveType,
   RenderResources,
+  VertexAttributeSemantic,
 } from "../../../Source/Cesium.js";
 
 describe("Scene/ModelExperimental/RenderResources", function () {
@@ -11,15 +18,6 @@ describe("Scene/ModelExperimental/RenderResources", function () {
   var sceneNode = new ModelExperimentalSceneNode({
     node: mockNode,
     modelMatrix: Matrix4.IDENTITY,
-  });
-
-  var mockPrimitive = {
-    indices: {
-      count: 3000,
-    },
-  };
-  var scenePrimitive = new ModelExperimentalSceneMeshPrimitive({
-    primitive: mockPrimitive,
   });
 
   function checkShaderDefines(shaderBuilder, expectedDefines) {
@@ -100,6 +98,53 @@ describe("Scene/ModelExperimental/RenderResources", function () {
   });
 
   describe("MeshPrimitiveRenderResources", function () {
+    var primitive = {
+      indices: {
+        count: 6,
+      },
+      primitiveType: PrimitiveType.TRIANGLES,
+      attributes: [
+        {
+          semantic: VertexAttributeSemantic.POSITION,
+          buffer: new Float32Array([0, 1, 2, 3, 4, 5]).buffer,
+          type: AttributeType.VEC3,
+          componentDatatype: ComponentDatatype.FLOAT,
+          min: new Cartesian3(-1, -1, -1),
+          max: new Cartesian3(1, 1, 1),
+        },
+      ],
+    };
+
+    var primitiveWithoutIndices = {
+      primitiveType: PrimitiveType.POINTS,
+      attributes: [
+        {
+          semantic: VertexAttributeSemantic.POSITION,
+          buffer: new Float32Array([0, 1, 2, 3, 4, 5, 6, 7, 8]).buffer,
+          type: AttributeType.VEC3,
+          componentDatatype: ComponentDatatype.FLOAT,
+          count: 8,
+          min: new Cartesian3(-2, -2, -2),
+          max: new Cartesian3(2, 2, 2),
+        },
+      ],
+    };
+
+    var scenePrimitive = new ModelExperimentalSceneMeshPrimitive({
+      primitive: primitive,
+    });
+
+    var scenePrimitiveWithoutIndices = new ModelExperimentalSceneMeshPrimitive({
+      primitive: primitiveWithoutIndices,
+    });
+
+    var expectedDepthTest = {
+      depthTest: {
+        enabled: true,
+        func: DepthFunction.LESS_OR_EQUAL,
+      },
+    };
+
     it("throws for undefined nodeRenderResources", function () {
       expect(function () {
         return new RenderResources.MeshPrimitiveRenderResources(
@@ -126,15 +171,109 @@ describe("Scene/ModelExperimental/RenderResources", function () {
     });
 
     it("constructs", function () {
-      fail();
+      var modelResources = new RenderResources.ModelRenderResources(mockModel);
+      var nodeResources = new RenderResources.NodeRenderResources(
+        modelResources,
+        sceneNode
+      );
+      var primitiveResources = new RenderResources.MeshPrimitiveRenderResources(
+        nodeResources,
+        scenePrimitive
+      );
+
+      expect(primitiveResources.count).toBe(6);
+      expect(primitiveResources.indices).toBe(primitive.indices);
+      expect(primitiveResources.primitiveType).toBe(PrimitiveType.TRIANGLES);
+      // The points are in a cube from -1, -1, -1 to 1, 1, 1. The center is
+      // (0, 0, 0). The full diagonal is 2 * sqrt(3), so half is sqrt(3)
+      expect(primitiveResources.boundingSphere.center).toEqualEpsilon(
+        Cartesian3.ZERO,
+        CesiumMath.EPSILON9
+      );
+      expect(primitiveResources.boundingSphere.radius).toEqualEpsilon(
+        Math.sqrt(3),
+        CesiumMath.EPSILON9
+      );
+      expect(primitiveResources.renderStateOptions).toEqual(expectedDepthTest);
+    });
+
+    it("constructs from primitive without indices", function () {
+      var modelResources = new RenderResources.ModelRenderResources(mockModel);
+      var nodeResources = new RenderResources.NodeRenderResources(
+        modelResources,
+        sceneNode
+      );
+      var primitiveResources = new RenderResources.MeshPrimitiveRenderResources(
+        nodeResources,
+        scenePrimitiveWithoutIndices
+      );
+
+      expect(primitiveResources.count).toBe(8);
+      expect(primitiveResources.indices).not.toBeDefined();
+      expect(primitiveResources.primitiveType).toBe(PrimitiveType.POINTS);
+
+      // The points are in a cube from -2, -2, -2 to 2, 2, 2. The center is
+      // (0, 0, 0). The full diagonal is 4 * sqrt(3), so half is 2 * sqrt(3)
+      expect(primitiveResources.boundingSphere.center).toEqualEpsilon(
+        Cartesian3.ZERO,
+        CesiumMath.EPSILON9
+      );
+      expect(primitiveResources.boundingSphere.radius).toEqualEpsilon(
+        2.0 * Math.sqrt(3),
+        CesiumMath.EPSILON9
+      );
+      expect(primitiveResources.renderStateOptions).toEqual(expectedDepthTest);
     });
 
     it("inherits from model render resources", function () {
-      fail();
+      var modelResources = new RenderResources.ModelRenderResources(mockModel);
+      modelResources.shaderBuilder.addDefine("MODEL");
+      var nodeResources = new RenderResources.NodeRenderResources(
+        modelResources,
+        sceneNode
+      );
+      nodeResources.shaderBuilder.addDefine("NODE");
+      var primitiveResources = new RenderResources.MeshPrimitiveRenderResources(
+        nodeResources,
+        scenePrimitive
+      );
+      primitiveResources.shaderBuilder.addDefine("PRIMITIVE");
+
+      expect(primitiveResources.model).toBe(mockModel);
+
+      // The primitive's shader builder should be a clone of the node's
+      expect(primitiveResources.shaderBuilder).not.toBe(
+        modelResources.shaderBuilder
+      );
+      expect(primitiveResources.shaderBuilder).not.toBe(
+        modelResources.shaderBuilder
+      );
+
+      // The defines should cascade through the three levels
+      checkShaderDefines(modelResources.shaderBuilder, ["MODEL"]);
+      checkShaderDefines(nodeResources.shaderBuilder, ["MODEL", "NODE"]);
+      checkShaderDefines(primitiveResources.shaderBuilder, [
+        "MODEL",
+        "NODE",
+        "PRIMITIVE",
+      ]);
     });
 
     it("inherits from node render resources", function () {
-      fail();
+      var modelResources = new RenderResources.ModelRenderResources(mockModel);
+      modelResources.shaderBuilder.addDefine("MODEL");
+      var nodeResources = new RenderResources.NodeRenderResources(
+        modelResources,
+        sceneNode
+      );
+      nodeResources.shaderBuilder.addDefine("NODE");
+      var primitiveResources = new RenderResources.MeshPrimitiveRenderResources(
+        nodeResources,
+        scenePrimitive
+      );
+      expect(primitiveResources.sceneNode).toBe(sceneNode);
+      expect(primitiveResources.modelMatrix).toEqual(sceneNode.modelMatrix);
+      expect(primitiveResources.attributes).toEqual([]);
     });
   });
 });
