3c281a449f65465df6b290d279016b6957765f1b
Peter Gagliardi <ptrgags@gmail.com>
2021-08-03 09:45:59 -0400

    Fix indentation in shaders

--- Source/Shaders/ModelExperimental/ModelExperimentalVS.glsl
+++ Source/Shaders/ModelExperimental/ModelExperimentalVS.glsl
@@ -1,13 +1,14 @@
 precision highp float;
 
-void main() {
-  vec3 position = vec3(0.0);  
-  position = processGeometry(position);
+void main() 
+{
+    vec3 position = vec3(0.0);  
+    position = processGeometry(position);
 
-  gl_Position = czm_modelViewProjection * vec4(position, 1.0);
+    gl_Position = czm_modelViewProjection * vec4(position, 1.0);
 
-  #ifdef PRIMITIVE_TYPE_POINTS
-  processPoints();
-  
-  #endif
+    #ifdef PRIMITIVE_TYPE_POINTS
+    processPoints();
+    
+    #endif
 }
