d7ad65b1debae883074adc3ba4b73f6a0a4da614
Sanjeet Suhag <suhagsanjeet@gmail.com>
2021-08-11 18:15:14 -0400

    Testing something

--- Specs/waitForLoaderProcess.js
+++ Specs/waitForLoaderProcess.js
@@ -2,13 +2,13 @@ import pollToPromise from "./pollToPromise.js";
 
 export default function waitForLoaderProcess(loader, scene) {
   var loaderFinished = false;
-  loader.promise.always(function () {
+  loader.texturesLoadedPromise.always(function () {
     loaderFinished = true;
   });
   return pollToPromise(function () {
     loader.process(scene.frameState);
     return loaderFinished;
   }).then(function () {
-    return loader.promise;
+    return loader.texturesLoadedPromise;
   });
 }
