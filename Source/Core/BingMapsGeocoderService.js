9daaa2d85bf5b90c7f130516c7fa047e3f71d2f9
ebogo1 <ebogo@alumni.upenn.edu>
2021-08-16 15:48:34 -0400

    fix @link spacing

--- Source/Core/BingMapsGeocoderService.js
+++ Source/Core/BingMapsGeocoderService.js
@@ -14,7 +14,7 @@ var url = "https://dev.virtualearth.net/REST/v1/Locations";
  *
  * @param {Object} options Object with the following properties:
  * @param {String} options.key A key to use with the Bing Maps geocoding service
- * @param {String} [options.culture] A Bing Maps { @link https://docs.microsoft.com/en-us/bingmaps/rest-services/common-parameters-and-types/supported-culture-codes|Culture Code} to return results in a specific culture and language.
+ * @param {String} [options.culture] A Bing Maps {@link https://docs.microsoft.com/en-us/bingmaps/rest-services/common-parameters-and-types/supported-culture-codes|Culture Code} to return results in a specific culture and language.
  */
 function BingMapsGeocoderService(options) {
   options = defaultValue(options, defaultValue.EMPTY_OBJECT);
