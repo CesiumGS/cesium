590fecfd8967eccf35f1feb74fcbf50a2af8c73a
Peter Gagliardi <ptrgags@gmail.com>
2021-08-16 12:54:06 -0400

    add UniformType and VaryingType

---
+++ Source/Scene/ModelExperimental/UniformType.js
@@ -0,0 +1,21 @@
+var UniformType = {
+  FLOAT: "float",
+  VEC2: "vec2",
+  VEC3: "vec3",
+  VEC4: "vec4",
+  INT: "int",
+  INT_VEC2: "ivec2",
+  INT_VEC3: "ivec3",
+  INT_VEC4: "ivec4",
+  BOOL: "bool",
+  BOOL_VEC2: "bvec2",
+  BOOL_VEC3: "bvec3",
+  BOOL_VEC4: "bvec4",
+  MAT2: "mat2",
+  MAT3: "mat2",
+  MAT4: "mat4",
+  SAMPLER_2D: "sampler2D",
+  SAMPLER_CUBE: "samplerCube",
+};
+
+export default Object.freeze(UniformType);
---
+++ Source/Scene/ModelExperimental/VaryingType.js
@@ -0,0 +1,11 @@
+var VaryingType = {
+  FLOAT: "float",
+  VEC2: "vec2",
+  VEC3: "vec3",
+  VEC4: "vec4",
+  MAT2: "mat2",
+  MAT3: "mat2",
+  MAT4: "mat4",
+};
+
+export default Object.freeze(VaryingType);
