8678043e453ae807cbec840aa449101b9e859501
njrivera <nrivera@novetta.com>
2021-08-18 18:19:25 +0000

    remove fit

--- Specs/Scene/CameraSpec.js
+++ Specs/Scene/CameraSpec.js
@@ -4062,7 +4062,7 @@ describe("Scene/Camera", function () {
     expect(rect).not.toBeDefined();
   });
 
-  fit("computeViewRectangle when horizon is visible", function () {
+  it("computeViewRectangle when horizon is visible", function () {
     scene.mode = SceneMode.SCENE3D;
 
     // Camera positioned north of Florida and pointing SE
@@ -4082,7 +4082,7 @@ describe("Scene/Camera", function () {
     expect(rect).toEqual(correctResult);
   });
 
-  fit("computeViewRectangle with horizon left of camera", function () {
+  it("computeViewRectangle with horizon left of camera", function () {
     scene.mode = SceneMode.SCENE3D;
 
     // Camera positioned north of Florida and pointing SE
@@ -4106,7 +4106,7 @@ describe("Scene/Camera", function () {
     expect(rect).toEqual(correctResult);
   });
 
-  fit("computeViewRectangle with horizon visible in lower right corner", function () {
+  it("computeViewRectangle with horizon visible in lower right corner", function () {
     scene.mode = SceneMode.SCENE3D;
 
     // Camera positioned north of Florida and pointing SE
