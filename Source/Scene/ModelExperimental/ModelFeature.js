e3130251dda67261ba0e240b6a360d6ce03dd001
Sanjeet Suhag <suhagsanjeet@gmail.com>
2021-08-26 14:57:53 -0400

    Adds ModelFeature and ModelFeatureTable files

---
+++ Source/Scene/ModelExperimental/ModelFeature.js
@@ -0,0 +1,5 @@
+export default function ModelFeature(options) {
+  this._model = options.model;
+  this._featureId = options.featureId;
+  this._content = options.content;
+}
---
+++ Source/Scene/ModelExperimental/ModelFeatureTable.js
@@ -0,0 +1,34 @@
+import BatchTexture from "../BatchTexture.js";
+import ModelFeature from "./ModelFeature.js";
+
+export default function ModelFeatureTable(options) {
+  this._featureTable = options.featureTable;
+  this._features = undefined;
+  this._featuresLength = 0;
+  this._batchTexture = undefined;
+
+  initialize(this);
+}
+
+function initialize(modelFeatureTable) {
+  var featuresLength = modelFeatureTable._featureTable.count;
+  if (featuresLength === 0) {
+    return;
+  }
+
+  var features = new Array(featuresLength);
+  for (var i = 0; i < featuresLength; i++) {
+    features[i] = new ModelFeature({
+      model: modelFeatureTable._model,
+      featureId: i,
+    });
+  }
+
+  modelFeatureTable._features = features;
+  modelFeatureTable._featuresLength = featuresLength;
+  modelFeatureTable._batchTexture = new BatchTexture({
+    featuresLength: featuresLength,
+    content: this,
+    statistics: modelFeatureTable._statistics,
+  });
+}
