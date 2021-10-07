4472b26d8133aba9209889c734f38812811fed0c
Eli Bogomolny <ebogo@seas.upenn.edu>
2021-10-07 00:55:46 -0400

    configure without web workers

--- ThirdParty/npm/zip.js
+++ ThirdParty/npm/zip.js
@@ -1,8 +1,11 @@
 import * as zip from "@zip.js/zip.js/lib/zip-no-worker.js";
+// zip.configure({
+//   workerScripts: {
+//       deflate: ["../../Source/ThirdParty/Workers/z-worker-pako.js", "./pako_deflate.min.js"],
+//       inflate: ["../../Source/ThirdParty/Workers/z-worker-pako.js", "./pako_inflate.min.js"]
+//   }
+// });
 zip.configure({
-  workerScripts: {
-      deflate: ["../../Source/ThirdParty/Workers/z-worker-pako.js", "./pako_deflate.min.js"],
-      inflate: ["../../Source/ThirdParty/Workers/z-worker-pako.js", "./pako_inflate.min.js"]
-  }
+  useWebWorkers: false
 });
 export { zip as default };
