d790df7437f5b7cfab5adc9cdbdfa5811b286e22
Peter Gagliardi <ptrgags@gmail.com>
2021-10-19 14:59:42 -0400

    Update bounding sphere semantic to be an array

--- Specs/Scene/Cesium3DTileFeatureSpec.js
+++ Specs/Scene/Cesium3DTileFeatureSpec.js
@@ -1,6 +1,5 @@
 import {
   Cartesian3,
-  Cartesian4,
   Cesium3DTileFeature,
   Ellipsoid,
   HeadingPitchRange,
@@ -126,16 +125,22 @@ describe(
 
       it("getPropertyInherited returns tile property by semantic", function () {
         var feature = new Cesium3DTileFeature(childContents["ll.b3dm"], 0);
-        expect(feature.getPropertyInherited("COLOR")).toEqual(
-          new Cartesian4(255, 255, 0, 1.0)
-        );
+        expect(feature.getPropertyInherited("COLOR")).toEqual([
+          255,
+          255,
+          0,
+          1.0,
+        ]);
       });
 
       it("getPropertyInherited returns tile property", function () {
         var feature = new Cesium3DTileFeature(childContents["ll.b3dm"], 0);
-        expect(feature.getPropertyInherited("color")).toEqual(
-          new Cartesian4(255, 255, 0, 1.0)
-        );
+        expect(feature.getPropertyInherited("color")).toEqual([
+          255,
+          255,
+          0,
+          1.0,
+        ]);
         expect(feature.getPropertyInherited("population")).toBe(50);
       });
 
@@ -157,9 +162,12 @@ describe(
 
       it("getPropertyInherited returns tileset property by semantic", function () {
         var feature = new Cesium3DTileFeature(parentContent, 0);
-        expect(feature.getPropertyInherited("COLOR")).toEqual(
-          new Cartesian4(255, 0, 255, 1.0)
-        );
+        expect(feature.getPropertyInherited("COLOR")).toEqual([
+          255,
+          0,
+          255,
+          1.0,
+        ]);
         expect(feature.getPropertyInherited("DATE_ISO_8601")).toBe(
           "2021-04-07"
         );
@@ -168,16 +176,17 @@ describe(
 
       it("getPropertyInherited returns tileset property", function () {
         var feature = new Cesium3DTileFeature(parentContent, 0);
-        expect(feature.getPropertyInherited("color")).toEqual(
-          new Cartesian4(255, 0, 255, 1.0)
-        );
-        expect(feature.getPropertyInherited("centerCartographic")).toEqual(
-          new Cartesian3(
-            -1.3196816996258511,
-            0.6988767486400521,
-            45.78600543644279
-          )
-        );
+        expect(feature.getPropertyInherited("color")).toEqual([
+          255,
+          0,
+          255,
+          1.0,
+        ]);
+        expect(feature.getPropertyInherited("centerCartographic")).toEqual([
+          -1.3196816996258511,
+          0.6988767486400521,
+          45.78600543644279,
+        ]);
         expect(feature.getPropertyInherited("date")).toBe("2021-04-07");
         expect(feature.getPropertyInherited("author")).toBe("Cesium");
         expect(feature.getPropertyInherited("tileCount")).toBe(5);
@@ -187,9 +196,12 @@ describe(
         // tile metadata is more specific than tileset metadata so this returns
         // yellow not magenta
         var feature = new Cesium3DTileFeature(childContents["ll.b3dm"], 0);
-        expect(feature.getPropertyInherited("color")).toEqual(
-          new Cartesian4(255, 255, 0, 1.0)
-        );
+        expect(feature.getPropertyInherited("color")).toEqual([
+          255,
+          255,
+          0,
+          1.0,
+        ]);
 
         // group metadata is more specific than tileset metadata, so this returns
         // 2 not 5
