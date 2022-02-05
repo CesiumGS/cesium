8a24a4921d9a28017c80cecbfcdf1144ff51db25
Subhajit <subhajit.saha@thoughtworks.com>
2022-02-05 18:51:26 +0530

    Refactor code to incorporate review comments

--- Specs/Core/GregorianDateSpec.js
+++ Specs/Core/GregorianDateSpec.js
@@ -40,37 +40,37 @@ describe("Core/GregorianDate", function () {
 
     it("Constructs valid dates for edge cases of days", function () {
       expect(function () {
-        new GregorianDate(2022, 1, 31);
+        return new GregorianDate(2022, 1, 31);
       }).not.toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 3, 31);
+        return new GregorianDate(2022, 3, 31);
       }).not.toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 4, 30);
+        return new GregorianDate(2022, 4, 30);
       }).not.toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 5, 31);
+        return new GregorianDate(2022, 5, 31);
       }).not.toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 6, 30);
+        return new GregorianDate(2022, 6, 30);
       }).not.toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 7, 31);
+        return new GregorianDate(2022, 7, 31);
       }).not.toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 8, 31);
+        return new GregorianDate(2022, 8, 31);
       }).not.toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 9, 30);
+        return new GregorianDate(2022, 9, 30);
       }).not.toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 10, 31);
+        return new GregorianDate(2022, 10, 31);
       }).not.toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 11, 30);
+        return new GregorianDate(2022, 11, 30);
       }).not.toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 12, 31);
+        return new GregorianDate(2022, 12, 31);
       }).not.toThrowDeveloperError();
     });
 
@@ -161,139 +161,139 @@ describe("Core/GregorianDate", function () {
   describe("Negative scenarios", function () {
     it("Should throw error if invalid year is passed", function () {
       expect(function () {
-        new GregorianDate(-1, 2, 4, 23, 54, 0, 999.9, false);
+        return new GregorianDate(-1, 2, 4, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(0, 2, 4, 23, 54, 0, 999.9, false);
+        return new GregorianDate(0, 2, 4, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(10000, 2, 4, 23, 54, 0, 999.9, false);
+        return new GregorianDate(10000, 2, 4, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
     });
 
     it("Should throw error if invalid month is passed", function () {
       expect(function () {
-        new GregorianDate(2022, -1, 4, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2022, -1, 4, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 0, 4, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 0, 4, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 13, 4, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 13, 4, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
     });
 
     it("Should throw error if invalid day is passed", function () {
       expect(function () {
-        new GregorianDate(2022, 12, -10, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 12, -10, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 12, 0, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 12, 0, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 12, 32, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 12, 32, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2020, 2, 30, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2020, 2, 30, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 29, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 2, 29, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 11, 31, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 11, 31, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 4, 31, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 4, 31, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 6, 31, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 6, 31, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 9, 31, 23, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 9, 31, 23, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
     });
 
     it("Should throw error if invalid hours is passed", function () {
       expect(function () {
-        new GregorianDate(2022, 2, 4, -10, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 2, 4, -10, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, -1, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 2, 4, -1, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, 24, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 2, 4, 24, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 11, 4, 100, 54, 0, 999.9, false);
+        return new GregorianDate(2022, 11, 4, 100, 54, 0, 999.9, false);
       }).toThrowDeveloperError();
     });
 
     it("Should throw error if invalid minute is passed", function () {
       expect(function () {
-        new GregorianDate(2022, 2, 4, 15, -1, 0, 999.9, false);
+        return new GregorianDate(2022, 2, 4, 15, -1, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, 15, 60, 0, 999.9, false);
+        return new GregorianDate(2022, 2, 4, 15, 60, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, 7, 60, 0, 999.9, true);
+        return new GregorianDate(2022, 2, 4, 7, 60, 0, 999.9, true);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 11, 4, 0, -1, 0, 999.9, true);
+        return new GregorianDate(2022, 11, 4, 0, -1, 0, 999.9, true);
       }).toThrowDeveloperError();
     });
 
     it("Should throw error if invalid second is passed", function () {
       expect(function () {
-        new GregorianDate(2022, 2, 4, 15, 1, -1, 999.9, false);
+        return new GregorianDate(2022, 2, 4, 15, 1, -1, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, 15, 59, 60, 999.9, false);
+        return new GregorianDate(2022, 2, 4, 15, 59, 60, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, 7, 59, 61, 999.9, true);
+        return new GregorianDate(2022, 2, 4, 7, 59, 61, 999.9, true);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 11, 4, 0, 1, -1, 999.9, true);
+        return new GregorianDate(2022, 11, 4, 0, 1, -1, 999.9, true);
       }).toThrowDeveloperError();
     });
 
     it("Should throw error if invalid millisecond is passed", function () {
       expect(function () {
-        new GregorianDate(2022, 2, 4, 15, 1, 0, -1, false);
+        return new GregorianDate(2022, 2, 4, 15, 1, 0, -1, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, 15, 59, 59, 1000, false);
+        return new GregorianDate(2022, 2, 4, 15, 59, 59, 1000, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, 7, 59, 60, 1000, true);
+        return new GregorianDate(2022, 2, 4, 7, 59, 60, 1000, true);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 11, 4, 0, 1, 0, -12, true);
+        return new GregorianDate(2022, 11, 4, 0, 1, 0, -12, true);
       }).toThrowDeveloperError();
     });
 
     it("Should throw error if invalid type is passed", function () {
       expect(function () {
-        new GregorianDate("2022A", 2, 4, 15, 1, 0, 999.9, false);
+        return new GregorianDate("2022A", 2, 4, 15, 1, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, "Two", 4, 15, 1, 0, 999.9, false);
+        return new GregorianDate(2022, "Two", 4, 15, 1, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, "F0UR", 15, 1, 0, 999.9, false);
+        return new GregorianDate(2022, 2, "F0UR", 15, 1, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, "15th", 1, 0, 999.9, false);
+        return new GregorianDate(2022, 2, 4, "15th", 1, 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, 15, "1st", 0, 999.9, false);
+        return new GregorianDate(2022, 2, 4, 15, "1st", 0, 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, 15, 1, "Zero", 999.9, false);
+        return new GregorianDate(2022, 2, 4, 15, 1, "Zero", 999.9, false);
       }).toThrowDeveloperError();
       expect(function () {
-        new GregorianDate(2022, 2, 4, 15, 1, 0, "999,9O", false);
+        return new GregorianDate(2022, 2, 4, 15, 1, 0, "999,9O", false);
       }).toThrowDeveloperError();
     });
   });
