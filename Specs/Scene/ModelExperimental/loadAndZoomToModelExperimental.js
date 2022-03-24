a0f1bc17cdec07ebe924ef5b3fde5ba6726ad413
Janine Liu <janine.h.liu@gmail.com>
2022-03-24 16:21:49 -0400

    Add tests to ModelExperimental

--- Specs/Scene/ModelExperimental/loadAndZoomToModelExperimental.js
+++ Specs/Scene/ModelExperimental/loadAndZoomToModelExperimental.js
@@ -24,6 +24,7 @@ function loadAndZoomToModelExperimental(options, scene) {
         instanceFeatureIdLabel: options.instanceFeatureIdLabel,
         incrementallyLoadTextures: options.incrementallyLoadTextures,
         lightColor: options.lightColor,
+        imageBasedLighting: options.imageBasedLighting,
         backFaceCulling: options.backFaceCulling,
         showCreditsOnScreen: options.showCreditsOnScreen,
       });
