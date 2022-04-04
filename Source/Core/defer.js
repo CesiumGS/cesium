c8ad7bf97fc10f4c8b159ea07dd562e5951191df
Chris Cooper <chris@propelleraero.com.au>
2022-04-05 09:35:51 +1000

    typescript export for defer

--- Source/Core/defer.js
+++ Source/Core/defer.js
@@ -1,30 +1,31 @@
 /**
  * A function used to resolve a promise upon completion .
- * @callback defer.resolve
+ * @callback resolve
  *
  * @param {*} value The resulting value.
  */
 
 /**
  * A function used to reject a promise upon failure.
- * @callback defer.reject
+ * @callback reject
  *
  * @param {*} error The error.
  */
 
 /**
  * An object which contains a promise object, and functions to resolve or reject the promise.
- *
- * @typedef {Object} defer.deferred
- * @property {defer.resolve} resolve Resolves the promise when called.
- * @property {defer.reject} reject Rejects the promise when called.
- * @property {Promise} promise Promise object.
+ * @typedef {Object} deferred
+ * @property {resolve} resolve Resolves the promise when called.
+ * @property {reject} reject Rejects the promise when called.
+ * @property {Promise<any>} promise Promise object.
  */
 
 /**
  * Creates a deferred object, containing a promise object, and functions to resolve or reject the promise.
- * @returns {defer.deferred}
- * @private
+ *
+ * @function
+ *
+ * @returns {deferred}
  */
 function defer() {
   let resolve;
