77c4794a67173d231b1cc19d9bbe5053bf759e15
James Bromwell <943160+thw0rted@users.noreply.github.com>
2022-04-27 18:30:11 +0200

    Revert change to primitive types

--- Source/Core/buildModuleUrl.js
+++ Source/Core/buildModuleUrl.js
@@ -93,8 +93,8 @@ let implementation;
  * Given a relative URL under the Cesium base URL, returns an absolute URL.
  * @function
  *
- * @param {string} relativeUrl The relative path.
- * @returns {string} The absolutely URL representation of the provided path.
+ * @param {String} relativeUrl The relative path.
+ * @returns {String} The absolutely URL representation of the provided path.
  *
  * @example
  * const viewer = new Cesium.Viewer("cesiumContainer", {
@@ -132,7 +132,7 @@ buildModuleUrl._clearBaseResource = function () {
 
 /**
  * Sets the base URL for resolving modules.
- * @param {string} value The new base URL.
+ * @param {String} value The new base URL.
  */
 buildModuleUrl.setBaseUrl = function (value) {
   baseResource = Resource.DEFAULT.getDerivedResource({
@@ -144,7 +144,7 @@ buildModuleUrl.setBaseUrl = function (value) {
  * Gets the base URL for resolving modules.
  *
  * @function
- * @returns {string} the configured base URL
+ * @returns {String} The configured base URL
  */
 buildModuleUrl.getCesiumBaseUrl = getCesiumBaseUrl;
 
