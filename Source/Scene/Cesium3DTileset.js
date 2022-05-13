74329179b7bdbb93981dd5478df287c02ddca1b9
Sean Lilley <lilleyse@gmail.com>
2022-05-13 11:23:43 -0400

    Revert "Fix #10372 - tileset.debugShowUrl broken for implicit tiles"
    This reverts commit 5573ce5e00d6ff5a9024d9aa95d3199d1c39604f.

--- Source/Scene/Cesium3DTileset.js
+++ Source/Scene/Cesium3DTileset.js
@@ -2652,7 +2652,7 @@ function addTileDebugLabel(tile, tileset, position) {
       }
       attributes += urls.length;
     } else {
-      labelString += `\nUrl: ${tile.content.url}`;
+      labelString += `\nUrl: ${tile._header.content.uri}`;
       attributes++;
     }
   }
