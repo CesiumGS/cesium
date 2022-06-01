7cfb076b6d70721248dd477263ca2d80353fdc8d
Peter Gagliardi <ptrgags@gmail.com>
2022-06-01 15:31:42 -0400

    Move material texture specs

--- Specs/Scene/ModelExperimental/StatisticsPipelineStageSpec.js
+++ Specs/Scene/ModelExperimental/StatisticsPipelineStageSpec.js
@@ -10,6 +10,9 @@ import createScene from "../../createScene.js";
 import waitForLoaderProcess from "../../waitForLoaderProcess.js";
 
 describe("Scene/ModelExperimental/StatisticsPipelineStage", function () {
+  const boomBox = "./Data/Models/PBR/BoomBox/BoomBox.gltf";
+  const boomBoxSpecularGlossiness =
+    "./Data/Models/PBR/BoomBoxSpecularGlossiness/BoomBox.gltf";
   const boxTextured =
     "./Data/Models/GltfLoader/BoxTextured/glTF-Binary/BoxTextured.glb";
   const pointCloudRGB =
@@ -63,9 +66,7 @@ describe("Scene/ModelExperimental/StatisticsPipelineStage", function () {
     return waitForLoaderProcess(gltfLoader, scene);
   }
 
-  it("counts memory for a model", function () {
-    fail();
-  });
+  it("counts memory for a model", function () {});
 
   it("_countGeometry computes memory usage for a triangle mesh", function () {
     return loadGltf(boxTextured).then(function (gltfLoader) {
@@ -118,8 +119,7 @@ describe("Scene/ModelExperimental/StatisticsPipelineStage", function () {
   });
 
   it("_countGeometry computes memory usage correctly for attributes with CPU copy", function () {
-    // This should create a copy of both the positions and indices on the
-    // CPU
+    // This will create a copy of both the positions and indices on the CPU
     const options = {
       loadPositionsFor2D: true,
       loadIndicesForWireframe: true,
@@ -223,6 +223,56 @@ describe("Scene/ModelExperimental/StatisticsPipelineStage", function () {
       expectedLength += primitive.indices.buffer.sizeInBytes;
 
       expect(statistics.geometryByteLength).toBe(expectedLength);
+    });
+  });
+
+  it("_countMaterialTextures updates memory statistics for metallic roughness", function () {
+    return loadGltf(boomBox).then(function (gltfLoader) {
+      const components = gltfLoader.components;
+      const primitive = components.nodes[0].primitives[0];
+
+      const material = primitive.material;
+      const metallicRoughness = material.metallicRoughness;
+      const statistics = new ModelExperimentalStatistics();
+
+      StatisticsPipelineStage._countMaterialTextures(
+        statistics,
+        primitive.material
+      );
+
+      const totalTextureSize =
+        material.emissiveTexture.texture.sizeInBytes +
+        material.normalTexture.texture.sizeInBytes +
+        material.occlusionTexture.texture.sizeInBytes +
+        metallicRoughness.baseColorTexture.texture.sizeInBytes;
+      // metallic roughness texture is the same image file as the base
+      // color texture so it is not counted
+      expect(statistics.texturesByteLength).toBe(totalTextureSize);
+    });
+  });
+
+  it("_countMaterialTextures updates memory statistics for specular glossiness", function () {
+    return loadGltf(boomBoxSpecularGlossiness).then(function (gltfLoader) {
+      const components = gltfLoader.components;
+      const primitive = components.nodes[0].primitives[0];
+
+      const material = primitive.material;
+      const specularGlossiness = material.specularGlossiness;
+      const statistics = new ModelExperimentalStatistics();
+
+      StatisticsPipelineStage._countMaterialTextures(
+        statistics,
+        primitive.material
+      );
+
+      const totalTextureSize =
+        material.emissiveTexture.texture.sizeInBytes +
+        material.normalTexture.texture.sizeInBytes +
+        material.occlusionTexture.texture.sizeInBytes +
+        specularGlossiness.diffuseTexture.texture.sizeInBytes +
+        specularGlossiness.specularGlossinessTexture.texture.sizeInBytes;
+
+      expect(statistics.texturesByteLength).toBe(totalTextureSize);
     });
   });
 });
