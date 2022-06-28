a4f5bc2a3acf9ca14d31c8180a23fd0f54de9bd8
Jeshurun Hembd <jeshurun@cesium.com>
2022-06-28 09:45:06 -0400

    Remove remaining calls to arraySlice and arrayFill polyfills

--- Specs/Core/EllipseGeometrySpec.js
+++ Specs/Core/EllipseGeometrySpec.js
@@ -1,4 +1,3 @@
-import { arrayFill } from "../../Source/Cesium.js";
 import { Cartesian3 } from "../../Source/Cesium.js";
 import { EllipseGeometry } from "../../Source/Cesium.js";
 import { Ellipsoid } from "../../Source/Cesium.js";
@@ -160,8 +159,7 @@ describe("Core/EllipseGeometry", function () {
 
     const offset = m.attributes.applyOffset.values;
     expect(offset.length).toEqual(numVertices);
-    let expected = new Array(offset.length);
-    expected = arrayFill(expected, 1);
+    const expected = new Array(offset.length).fill(1);
     expect(offset).toEqual(expected);
   });
 
@@ -184,10 +182,10 @@ describe("Core/EllipseGeometry", function () {
 
     const offset = m.attributes.applyOffset.values;
     expect(offset.length).toEqual(numVertices);
-    let expected = new Array(offset.length);
-    expected = arrayFill(expected, 0);
-    expected = arrayFill(expected, 1, 0, 16);
-    expected = arrayFill(expected, 1, 32, 40);
+    const expected = new Array(offset.length)
+      .fill(0)
+      .fill(1, 0, 16)
+      .fill(1, 32, 40);
     expect(offset).toEqual(expected);
   });
 
@@ -210,8 +208,7 @@ describe("Core/EllipseGeometry", function () {
 
     const offset = m.attributes.applyOffset.values;
     expect(offset.length).toEqual(numVertices);
-    let expected = new Array(offset.length);
-    expected = arrayFill(expected, 1);
+    const expected = new Array(offset.length).fill(1);
     expect(offset).toEqual(expected);
   });
 
