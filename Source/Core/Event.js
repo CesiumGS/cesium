86867542a0bc37b1d11effa40450d672b3f5e315
Chris Cooper <chris@propelleraero.com.au>
2022-07-22 11:58:16 +1000

    Potential fix for raiseEvent typescript

--- Source/Core/Event.js
+++ Source/Core/Event.js
@@ -120,7 +120,7 @@ function compareNumber(a, b) {
 /**
  * Raises the event by calling each registered listener with all supplied arguments.
  *
- * @param {...Parameters<Listener>} arguments This method takes any number of parameters and passes them through to the listener functions.
+ * @param {Parameters<Listener>} arguments This method takes any number of parameters and passes them through to the listener functions.
  *
  * @see Event#addEventListener
  * @see Event#removeEventListener
