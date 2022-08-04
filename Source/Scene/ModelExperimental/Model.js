9296b9251a081e0753775ee4b9b77e7c15473f94
Janine Liu <janine.h.liu@gmail.com>
2022-08-04 17:19:02 -0400

    Add back experimental tags [skip ci]

--- Source/Scene/ModelExperimental/Model.js
+++ Source/Scene/ModelExperimental/Model.js
@@ -1191,6 +1191,7 @@ Object.defineProperties(Model.prototype, {
    * @memberof Model.prototype
    *
    * @type {String}
+   * @experimental This feature is using part of the 3D Tiles spec that is not final and is subject to change without Cesium's standard deprecation policy.
    */
   featureIdLabel: {
     get: function () {
@@ -1226,6 +1227,7 @@ Object.defineProperties(Model.prototype, {
    * @memberof Model.prototype
    *
    * @type {String}
+   * @experimental This feature is using part of the 3D Tiles spec that is not final and is subject to change without Cesium's standard deprecation policy.
    */
   instanceFeatureIdLabel: {
     get: function () {
