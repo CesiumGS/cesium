23468f3352454e391405a6cfe84bc1ec474dc872
Jeshurun Hembd <jeshurun@cesium.com>
2022-09-17 18:26:42 -0400

    Fix import syntax

--- Source/Scene/SpatialNode.js
+++ Source/Scene/SpatialNode.js
@@ -3,7 +3,7 @@ import Cartesian3 from "../Core/Cartesian3.js";
 import CesiumMath from "../Core/Math.js";
 import defined from "../Core/defined.js";
 import DeveloperError from "../Core/DeveloperError.js";
-import KeyframeNode from "./KeyframeNode";
+import KeyframeNode from "./KeyframeNode.js";
 import Matrix3 from "../Core/Matrix3.js";
 import OrientedBoundingBox from "../Core/OrientedBoundingBox.js";
 
