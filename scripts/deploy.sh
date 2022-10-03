084ae8e5092ac13bf4c2c3a04847e5eb0c8af86c
Sanjeet Suhag <suhagsanjeet@gmail.com>
2022-10-03 14:53:41 -0400

    Adds check for branch in deploy.sh, adds download package to dev deps

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-if [ $TRAVIS_REPO_SLUG == "CesiumGS/cesium" ]; then
-  npm --silent run deploy-s3 -- -b cesium-dev -d cesium/$TRAVIS_BRANCH --confirm -c 'no-cache'
+if [ $TRAVIS_REPO_SLUG == "CesiumGS/cesium" &&  $TRAVIS_BRANCH == "cesium.com" ]; then
+  npm --silent run deploy-s3 -- -c 'public, max-age=1800' --confirm
   npm --silent run deploy-status -- --status success --message Deployed
 fi
