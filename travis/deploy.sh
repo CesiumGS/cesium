a705a0cabe9e80f6c17b3731643a71d56c66aea4
Gabby Getz <gabby@cesium.com>
2022-10-05 12:37:48 -0400

    Update travis scripts

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,6 +4,7 @@ if [ $TRAVIS_REPO_SLUG == "CesiumGS/cesium" ]; then
   # Files deployed to cesium.com are "production", and should be cached at edge locations for 
   # better performance. Otherwise, this is a development deploy and nothing should be cached
   if [ $TRAVIS_BRANCH == "cesium.com" ]; then
+    echo "Uploading files to cesium.com..."
     npm --silent run deploy-s3 -- -b "cesium.com-next" -c 'public, max-age=1800' --confirm
   else
     npm --silent run deploy-s3 -- -b "cesium-dev" -d cesium/$TRAVIS_BRANCH -c 'no-cache' --confirm 
