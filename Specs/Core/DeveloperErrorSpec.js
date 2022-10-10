812103012b54873d9f35fe2777f5d23da3fc0dc2
Gabby Getz <gabby@cesium.com>
2022-10-10 13:22:25 -0400

    test release spec failure

--- Specs/Core/DeveloperErrorSpec.js
+++ Specs/Core/DeveloperErrorSpec.js
@@ -21,7 +21,7 @@ describe("Core/DeveloperError", function () {
     // Since we are using source maps, we won't be able to map to a specific file without help from the browser developer tools.
     // However, we should know the class if not minified
     if (!window.specsUsingRelease) {
-      expect(e.stack).toContain(name);
+      expect(e.stack).toContain("random other thing");
     }
   });
 
