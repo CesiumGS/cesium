8a79c543c99287498f5ea05cef7dc74dc7786f11
Sanjeet Suhag <sam.suhag@cesium.com>
2022-10-12 11:52:14 -0400

    WIP: Fixes test-workspaces.

--- travis/test-workspaces.sh
+++ travis/test-workspaces.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ev
 if [ $TRAVIS_BRANCH != "cesium.com" ]; then
-  npm --silent run build --workspace @cesium/engine
-  npm --silent run build --workspace @cesium/widgets
+  npm --silent run build -- --node
   npm --silent run test --workspace @cesium/engine -- --browsers ChromeCI --failTaskOnError --webgl-stub --suppressPassed
   npm --silent run test --workspace @cesium/widgets -- --browsers ChromeCI --failTaskOnError --webgl-stub --suppressPassed
 fi
