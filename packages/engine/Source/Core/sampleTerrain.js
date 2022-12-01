6056486a133ce05f5970a36a47cd416cde4d14ea
Cameron Armstrong <cameronscottarmstrong@gmail.com>
2022-12-01 17:05:22 +0800

    Fix style

--- packages/engine/Source/Core/sampleTerrain.js
+++ packages/engine/Source/Core/sampleTerrain.js
@@ -132,7 +132,9 @@ function doSampling(terrainProvider, level, positions) {
   const tileRequestSet = {}; // A unique set
   for (i = 0; i < positions.length; ++i) {
     const xy = tilingScheme.positionToTileXY(positions[i], level);
-    if (!xy) continue;
+    if (!xy) {
+      continue;
+    }
 
     const key = xy.toString();
 
