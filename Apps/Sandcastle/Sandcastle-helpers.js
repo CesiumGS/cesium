bf80efa061816c77918d19d89631c8ca608ce35a
Gabby Getz <gabby@cesium.com>
2023-01-31 16:59:38 -0500

    Use top level await in Sandcastle

--- Apps/Sandcastle/Sandcastle-helpers.js
+++ Apps/Sandcastle/Sandcastle-helpers.js
@@ -4,7 +4,7 @@
   window.embedInSandcastleTemplate = function (code, addExtraLine) {
     return (
       `${
-        "window.startup = function (Cesium) {\n" +
+        "window.startup = async function (Cesium) {\n" +
         "    'use strict';\n" +
         "//Sandcastle_Begin\n"
       }${addExtraLine ? "\n" : ""}${code}//Sandcastle_End\n` +
@@ -12,7 +12,11 @@
       `};\n` +
       `if (typeof Cesium !== 'undefined') {\n` +
       `    window.startupCalled = true;\n` +
-      `    window.startup(Cesium);\n` +
+      `    try {\n` +
+      `      window.startup(Cesium);\n` +
+      `    } catch (error) {\n` +
+      `      console.error(error);\n` +
+      `    }\n` +
       `}\n`
     );
   };
