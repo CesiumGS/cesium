fa1d94352489937930cf5739eae0795ae1d55874
Jeshurun Hembd <jeshurun@cesium.com>
2023-11-28 20:31:25 -0500

    Use azimuthal curvature to approximate ellipsoid normal for exaggeration

--- packages/engine/Source/Shaders/Model/VerticalExaggerationStageVS.glsl
+++ packages/engine/Source/Shaders/Model/VerticalExaggerationStageVS.glsl
@@ -1,20 +1,29 @@
 void verticalExaggerationStage(
   inout ProcessedAttributes attributes
 ) {
-  float eyeCenterDistance = length(czm_view[3]);
-  vec3 cameraGeoCentric = eyeCenterDistance * czm_eyeEllipsoidNormalEC;
-  vec3 vertexPositionEC = (czm_modelView * vec4(attributes.positionMC, 1.0)).xyz;
-  vec3 vertexGeoCentric = cameraGeoCentric + vertexPositionEC;
-  vec3 vertexNormal = normalize(vertexGeoCentric);
+  // Compute the distance from the camera to the local center of curvature.
+  vec4 vertexPositionENU = czm_modelToEnu * vec4(attributes.positionMC, 1.0);
+  vec2 vertexAzimuth = normalize(vertexPositionENU.xy);
+  // Curvature = 1 / radius of curvature.
+  float azimuthalCurvature = dot(vertexAzimuth * vertexAzimuth, czm_eyeEllipsoidCurvature);
+  float eyeToCenter = 1.0 / azimuthalCurvature + czm_eyeHeight;
 
-  // Compute the change in height above the ellipsoid, from camera to vertex position
+  // Compute the approximate ellipsoid normal at the vertex position.
+  // Uses a circular approximation for the Earth curvature along the geodesic.
+  vec3 vertexPositionEC = (czm_modelView * vec4(attributes.positionMC, 1.0)).xyz;
+  vec3 centerToVertex = eyeToCenter * czm_eyeEllipsoidNormalEC + vertexPositionEC;
+  vec3 vertexNormal = normalize(centerToVertex);
+
+  // Approximate the change in height above the ellipsoid, from camera to vertex position.
   float versine = 1.0 - dot(czm_eyeEllipsoidNormalEC, vertexNormal);
-  float dHeight = dot(vertexPositionEC, vertexNormal) - eyeCenterDistance * versine;
+  float dHeight = dot(vertexPositionEC, vertexNormal) - eyeToCenter * versine;
   float vertexHeight = czm_eyeHeight + dHeight;
 
+  // Transform the approximate vertex normal to model coordinates.
   vec3 vertexNormalMC = (czm_inverseModelView * vec4(vertexNormal, 0.0)).xyz;
   vertexNormalMC = normalize(vertexNormalMC);
 
+  // Compute the exaggeration and apply it along the approximate vertex normal.
   float stretch = u_verticalExaggerationAndRelativeHeight.x;
   float shift = u_verticalExaggerationAndRelativeHeight.y;
   float exaggeration = (vertexHeight - shift) * (stretch - 1.0);
