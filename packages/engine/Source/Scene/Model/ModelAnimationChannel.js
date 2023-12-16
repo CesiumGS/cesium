b57f9fb52e3a6444086922ef0fb4394c0cc2eb51
Marco Hutter <javagl@javagl.de>
2023-12-16 16:35:22 +0100

    Use proper type for scratch variables

--- packages/engine/Source/Scene/Model/ModelAnimationChannel.js
+++ packages/engine/Source/Scene/Model/ModelAnimationChannel.js
@@ -215,7 +215,8 @@ function createSplines(times, points, interpolation, path, count) {
   return splines;
 }
 
-let scratchVariable;
+let scratchCartesian3;
+let scratchQuaternion;
 
 function initialize(runtimeChannel) {
   const channel = runtimeChannel._channel;
@@ -240,10 +241,10 @@ function initialize(runtimeChannel) {
   switch (path) {
     case AnimatedPropertyType.TRANSLATION:
     case AnimatedPropertyType.SCALE:
-      scratchVariable = new Cartesian3();
+      scratchCartesian3 = new Cartesian3();
       break;
     case AnimatedPropertyType.ROTATION:
-      scratchVariable = new Quaternion();
+      scratchQuaternion = new Quaternion();
       break;
     case AnimatedPropertyType.WEIGHTS:
       // This is unused when setting a node's morph weights.
@@ -286,7 +287,20 @@ ModelAnimationChannel.prototype.animate = function (time) {
       : spline.wrapTime(time);
 
     // This sets the translate, rotate, and scale properties.
-    runtimeNode[path] = spline.evaluate(localAnimationTime, scratchVariable);
+    if (
+      path === AnimatedPropertyType.TRANSLATION ||
+      path === AnimatedPropertyType.SCALE
+    ) {
+      runtimeNode[path] = spline.evaluate(
+        localAnimationTime,
+        scratchCartesian3
+      );
+    } else if (path === AnimatedPropertyType.ROTATION) {
+      runtimeNode[path] = spline.evaluate(
+        localAnimationTime,
+        scratchQuaternion
+      );
+    }
   }
 };
 
