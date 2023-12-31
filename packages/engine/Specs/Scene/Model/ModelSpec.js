3fbe372e41d2ad68e76df2f7daf943783112f315
Jeshurun Hembd <jeshurun@cesium.com>
2023-12-31 08:15:33 -0500

    Reset scene.verticalExaggeration after each spec

--- packages/engine/Specs/Scene/Model/ModelSpec.js
+++ packages/engine/Specs/Scene/Model/ModelSpec.js
@@ -141,6 +141,7 @@ describe(
       scene.primitives.removeAll();
       scene2D.primitives.removeAll();
       sceneCV.primitives.removeAll();
+      scene.verticalExaggeration = 1.0;
       ResourceCache.clearForSpecs();
     });
 
