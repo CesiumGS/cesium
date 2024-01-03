71157a0693c188d5340f6da1311ff0788a36635d
Peter Gagliardi <ptrgags@gmail.com>
2024-01-03 11:18:52 -0500

    Add spec for ensuring the constructor options work

--- packages/engine/Specs/Scene/Cesium3DTilesetSpec.js
+++ packages/engine/Specs/Scene/Cesium3DTilesetSpec.js
@@ -1106,13 +1106,13 @@ describe(
         const center = Cartesian3.fromRadians(centerLongitude, centerLatitude);
         scene.camera.lookAt(center, new HeadingPitchRange(0.0, 0.0, distance));
 
-        // Set dynamic SSE to false (default)
+        // Turn off dynamic SSE
         tileset.dynamicScreenSpaceError = false;
         scene.renderForSpecs();
         expect(statistics.visited).toEqual(1);
         expect(statistics.numberOfCommands).toEqual(1);
 
-        // Set dynamic SSE to true, now the root is not rendered
+        // Turn on dynamic SSE, now the root is not rendered
         tileset.dynamicScreenSpaceError = true;
         tileset.dynamicScreenSpaceErrorDensity = 1.0;
         tileset.dynamicScreenSpaceErrorFactor = 10.0;
@@ -1151,6 +1151,39 @@ describe(
 
     it("uses dynamic screen space error for local tileset with sphere", function () {
       return testDynamicScreenSpaceError(withTransformSphereUrl, 144.0);
+    });
+
+    it("dynamic screen space error constructor options work", function () {
+      const options = {
+        dynamicScreenSpaceError: true,
+        dynamicScreenSpaceErrorDensity: 1.0,
+        dynamicScreenSpaceErrorFactor: 10.0,
+        dynamicScreenSpaceErrorHeightFalloff: 0.5,
+      };
+      const distance = 103.0;
+      return Cesium3DTilesTester.loadTileset(
+        scene,
+        withTransformBoxUrl,
+        options
+      ).then(function (tileset) {
+        // Make sure the values match the constructor, not hard-coded defaults
+        // like in https://github.com/CesiumGS/cesium/issues/11677
+        expect(tileset.dynamicScreenSpaceError).toBe(true);
+        expect(tileset.dynamicScreenSpaceErrorDensity).toBe(1.0);
+        expect(tileset.dynamicScreenSpaceErrorFactor).toBe(10.0);
+        expect(tileset.dynamicScreenSpaceErrorHeightFalloff).toBe(0.5);
+
+        const statistics = tileset._statistics;
+
+        // Horizon view, only root is in view, however due to dynamic SSE,
+        // it will not render.
+        const center = Cartesian3.fromRadians(centerLongitude, centerLatitude);
+        scene.camera.lookAt(center, new HeadingPitchRange(0.0, 0.0, distance));
+
+        scene.renderForSpecs();
+        expect(statistics.visited).toEqual(0);
+        expect(statistics.numberOfCommands).toEqual(0);
+      });
     });
 
     it("additive refinement - selects root when sse is met", function () {
