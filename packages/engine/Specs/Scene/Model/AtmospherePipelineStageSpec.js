7b930e2df3fb9895e5b0789c374fb20ab9c5d857
Peter Gagliardi <ptrgags@gmail.com>
2024-01-11 15:07:19 -0500

    Mark specs as requiring WebGL

--- packages/engine/Specs/Scene/Model/AtmospherePipelineStageSpec.js
+++ packages/engine/Specs/Scene/Model/AtmospherePipelineStageSpec.js
@@ -7,107 +7,111 @@ import {
 } from "../../../index.js";
 import ShaderBuilderTester from "../../../../../Specs/ShaderBuilderTester.js";
 
-describe("Scene/Model/AtmospherePipelineStage", function () {
-  const mockModel = {
-    boundingSphere: {
-      center: Cartesian3.fromDegrees(0, 0, 0),
-    },
-  };
-
-  function mockFrameState() {
-    return {
-      camera: {
-        // position the camera a little bit east of the model
-        // and slightly above
-        positionWC: Cartesian3.fromDegrees(0.01, 0, 1),
-      },
-      fog: {
-        density: 2e-4,
+describe(
+  "Scene/Model/AtmospherePipelineStage",
+  function () {
+    const mockModel = {
+      boundingSphere: {
+        center: Cartesian3.fromDegrees(0, 0, 0),
       },
     };
-  }
 
-  function mockRenderResources() {
-    return {
-      shaderBuilder: new ShaderBuilder(),
-      uniformMap: {},
-    };
-  }
+    function mockFrameState() {
+      return {
+        camera: {
+          // position the camera a little bit east of the model
+          // and slightly above
+          positionWC: Cartesian3.fromDegrees(0.01, 0, 1),
+        },
+        fog: {
+          density: 2e-4,
+        },
+      };
+    }
 
-  it("configures shader", function () {
-    const renderResources = mockRenderResources();
-    const frameState = mockFrameState();
+    function mockRenderResources() {
+      return {
+        shaderBuilder: new ShaderBuilder(),
+        uniformMap: {},
+      };
+    }
 
-    AtmospherePipelineStage.process(renderResources, mockModel, frameState);
+    it("configures shader", function () {
+      const renderResources = mockRenderResources();
+      const frameState = mockFrameState();
 
-    const shaderBuilder = renderResources.shaderBuilder;
+      AtmospherePipelineStage.process(renderResources, mockModel, frameState);
 
-    ShaderBuilderTester.expectHasVertexDefines(shaderBuilder, [
-      "HAS_ATMOSPHERE",
-      "COMPUTE_POSITION_WC_ATMOSPHERE",
-    ]);
-    ShaderBuilderTester.expectHasFragmentDefines(shaderBuilder, [
-      "HAS_ATMOSPHERE",
-      "COMPUTE_POSITION_WC_ATMOSPHERE",
-    ]);
+      const shaderBuilder = renderResources.shaderBuilder;
 
-    ShaderBuilderTester.expectHasVaryings(shaderBuilder, [
-      "vec3 v_atmosphereRayleighColor;",
-      "vec3 v_atmosphereMieColor;",
-      "float v_atmosphereOpacity;",
-    ]);
+      ShaderBuilderTester.expectHasVertexDefines(shaderBuilder, [
+        "HAS_ATMOSPHERE",
+        "COMPUTE_POSITION_WC_ATMOSPHERE",
+      ]);
+      ShaderBuilderTester.expectHasFragmentDefines(shaderBuilder, [
+        "HAS_ATMOSPHERE",
+        "COMPUTE_POSITION_WC_ATMOSPHERE",
+      ]);
 
-    ShaderBuilderTester.expectVertexLinesEqual(shaderBuilder, [
-      _shadersAtmosphereStageVS,
-    ]);
-    ShaderBuilderTester.expectFragmentLinesEqual(shaderBuilder, [
-      _shadersAtmosphereStageFS,
-    ]);
+      ShaderBuilderTester.expectHasVaryings(shaderBuilder, [
+        "vec3 v_atmosphereRayleighColor;",
+        "vec3 v_atmosphereMieColor;",
+        "float v_atmosphereOpacity;",
+      ]);
 
-    ShaderBuilderTester.expectHasVertexUniforms(shaderBuilder, []);
-    ShaderBuilderTester.expectHasFragmentUniforms(shaderBuilder, [
-      "uniform bool u_isInFog;",
-    ]);
-  });
+      ShaderBuilderTester.expectVertexLinesEqual(shaderBuilder, [
+        _shadersAtmosphereStageVS,
+      ]);
+      ShaderBuilderTester.expectFragmentLinesEqual(shaderBuilder, [
+        _shadersAtmosphereStageFS,
+      ]);
 
-  it("u_isInFog() is false if the camera is at the model center", function () {
-    const renderResources = mockRenderResources();
-    const frameState = mockFrameState();
+      ShaderBuilderTester.expectHasVertexUniforms(shaderBuilder, []);
+      ShaderBuilderTester.expectHasFragmentUniforms(shaderBuilder, [
+        "uniform bool u_isInFog;",
+      ]);
+    });
 
-    frameState.camera.positionWC = Cartesian3.clone(
-      mockModel.boundingSphere.center,
-      frameState.camera.positionWC
-    );
+    it("u_isInFog() is false if the camera is at the model center", function () {
+      const renderResources = mockRenderResources();
+      const frameState = mockFrameState();
 
-    AtmospherePipelineStage.process(renderResources, mockModel, frameState);
+      frameState.camera.positionWC = Cartesian3.clone(
+        mockModel.boundingSphere.center,
+        frameState.camera.positionWC
+      );
 
-    const uniformMap = renderResources.uniformMap;
-    expect(uniformMap.u_isInFog()).toBe(false);
-  });
+      AtmospherePipelineStage.process(renderResources, mockModel, frameState);
 
-  it("u_isInFog() is false if the camera is in space", function () {
-    const renderResources = mockRenderResources();
-    const frameState = mockFrameState();
+      const uniformMap = renderResources.uniformMap;
+      expect(uniformMap.u_isInFog()).toBe(false);
+    });
 
-    // For this case, the fact that Fog decreases the density to 0 when
-    // the camera is far above the model is what causes u_isInFog to
-    // be false.
-    frameState.camera.positionWC = Cartesian3.fromDegrees(0.001, 0, 100000);
-    frameState.fog.density = 0;
+    it("u_isInFog() is false if the camera is in space", function () {
+      const renderResources = mockRenderResources();
+      const frameState = mockFrameState();
 
-    AtmospherePipelineStage.process(renderResources, mockModel, frameState);
+      // For this case, the fact that Fog decreases the density to 0 when
+      // the camera is far above the model is what causes u_isInFog to
+      // be false.
+      frameState.camera.positionWC = Cartesian3.fromDegrees(0.001, 0, 100000);
+      frameState.fog.density = 0;
 
-    const uniformMap = renderResources.uniformMap;
-    expect(uniformMap.u_isInFog()).toBe(false);
-  });
+      AtmospherePipelineStage.process(renderResources, mockModel, frameState);
 
-  it("u_isInFog() is true when the tile is in fog", function () {
-    const renderResources = mockRenderResources();
-    const frameState = mockFrameState();
+      const uniformMap = renderResources.uniformMap;
+      expect(uniformMap.u_isInFog()).toBe(false);
+    });
 
-    AtmospherePipelineStage.process(renderResources, mockModel, frameState);
+    it("u_isInFog() is true when the tile is in fog", function () {
+      const renderResources = mockRenderResources();
+      const frameState = mockFrameState();
 
-    const uniformMap = renderResources.uniformMap;
-    expect(uniformMap.u_isInFog()).toBe(true);
-  });
-});
+      AtmospherePipelineStage.process(renderResources, mockModel, frameState);
+
+      const uniformMap = renderResources.uniformMap;
+      expect(uniformMap.u_isInFog()).toBe(true);
+    });
+  },
+  "WebGL"
+);
