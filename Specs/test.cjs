7391e4ab05b2f604312c3ad6b8571e9c3ccdd82f
Josh <8007967+jjspace@users.noreply.github.com>
2024-01-18 16:10:49 -0500

    swap node internal packages out for fetch

--- Specs/test.cjs
+++ Specs/test.cjs
@@ -1,8 +1,6 @@
 /*eslint-env node*/
 "use strict";
 
-// NodeJS smoke screen test
-
 const assert = require("node:assert");
 const {
   Cartographic,
@@ -10,16 +8,23 @@ const {
   sampleTerrain,
 } = require("cesium");
 
+// NodeJS smoke screen test
+
 async function test() {
+  console.log("start");
   const provider = await createWorldTerrainAsync();
+  console.log("loaded terrain");
   const results = await sampleTerrain(provider, 11, [
     Cartographic.fromDegrees(86.925145, 27.988257),
     Cartographic.fromDegrees(87.0, 28.0),
   ]);
+  console.log("sampled terrain");
   assert(results[0].height > 5000);
   assert(results[0].height < 10000);
   assert(results[1].height > 5000);
   assert(results[1].height < 10000);
+
+  console.log("all assertions passed");
 }
 
-test();
+test().finally(() => console.log("done"));
