581540fca04e2bbb79d87eb65f65952b2d7a6b37
unknown <ranade.siddhesh@gmail.com>
2024-01-23 16:37:44 -0500

    Add Google sheets API and check for usernames

---
+++ .github/actions/check-for-CLA/index.js
@@ -0,0 +1,114 @@
+import { google } from "googleapis";
+
+// TODO: id: process.env.PR_NUMBER
+const PULL_REQUST_INFO = {
+  id: 11781,
+  owner: process.env.GITHUB_REPOSITORY.split('/')[0],
+  repoName: process.env.GITHUB_REPOSITORY.split('/')[1],
+  username: process.env.GITHUB_ACTOR,
+  gitHubToken: process.env.GITHUB_TOKEN
+};
+
+/* TODO: Store in repo secrets */
+/* TODO: Replace with actual Cesium spreadsheet Ids - these are test sheets that I'll delete, so okay to git push */
+const GOOGLE_SHEETS_INFO = {
+  individualCLASheetId: '1oRRS8OG4MfXaQ8uA4uWQWukaOqxEE3N-JuqzrqGGeaE',
+  corporateCLASheetId: '1dnoqifzpXB81G1V4bsVJYM3D19gXuwyVZZ-IgNgCkC8'
+};
+
+const main = async () => {
+  console.log('main()');
+  console.log(PULL_REQUST_INFO.repoName, PULL_REQUST_INFO.owner, PULL_REQUST_INFO.username);
+
+  let hasSignedCLA;
+  let errorFoundOnCLACheck;
+
+  try {
+      hasSignedCLA = await checkIfUserHasSignedAnyCLA();
+  } catch (error) {
+    console.log('ERROR2 ', error);
+    errorFoundOnCLACheck = error.toString();
+  }
+
+  console.log('pre-comment...');
+  // const response = await postCommentOnPullRequest(hasSignedCLA, errorFoundOnCLACheck);
+  console.log('post-comment, response: ', response);
+};
+
+const checkIfUserHasSignedAnyCLA = async () => {    
+  const googleSheetsApi = await getGoogleSheetsApiClient();
+
+  let foundIndividualCLA = await checkIfIndividualCLAFound(googleSheetsApi);
+  console.log('CLA #1 ', foundIndividualCLA);
+  if (foundIndividualCLA) {
+      return true;
+  }
+
+  let foundCorporateCLA = await checkIfCorporateCLAFound(googleSheetsApi);
+  console.log('CLA #2 ', foundCorporateCLA);
+  return foundCorporateCLA;
+};
+
+const getGoogleSheetsApiClient = async () => {
+  const auth = new google.auth.GoogleAuth({
+      // TODO: get from GitHub secrets
+      // (JSON contains keys for my personal Service account, so okay to git push)
+      keyFile: 'TempGoogleConfig.json',
+      scopes: ['https://www.googleapis.com/auth/spreadsheets']
+  });
+  const googleAuthClient = await auth.getClient();
+
+  return google.sheets({version: 'v4', auth: googleAuthClient });
+};
+
+const checkIfIndividualCLAFound = async (googleSheetsApi) => {
+  const response = await googleSheetsApi.spreadsheets.values.get({
+      spreadsheetId: GOOGLE_SHEETS_INFO.individualCLASheetId,
+      range: 'D2:D'
+  });
+
+  const rows = response.data.values;
+  for (let i = 0; i < rows.length; i++) {
+      if(rows[i].length === 0) {
+          continue;
+      }
+
+      const rowUsername = rows[i][0].toLowerCase();
+      if (PULL_REQUST_INFO.username.toLowerCase() === rowUsername) {
+          return true;
+      }
+  }
+
+  return false;
+};
+
+const checkIfCorporateCLAFound = async (googleSheetsApi) => {
+  const response = await googleSheetsApi.spreadsheets.values.get({
+      spreadsheetId: GOOGLE_SHEETS_INFO.corporateCLASheetId,
+      range: 'H2:H'
+  });
+
+  const rows = response.data.values;
+  for (let i = 0; i < rows.length; i++) {
+      if(rows[i].length === 0) {
+          continue;
+      }
+
+      // We're more lenient with the ScheduleA username check since it's an unformatted text field.
+      let rowScheduleA = rows[i][0].toLowerCase();
+      rowScheduleA = rowScheduleA.replace(/\n/g, ' ');
+      const words = rowScheduleA.split(' ');
+
+      for (let j = 0; j < words.length; j++) {
+          // Checking for substrings because many input their 
+          // GitHub username as "github.com/username".
+          if (words[j].includes(PULL_REQUST_INFO.username.toLowerCase())) {
+              return true;
+          }
+      }
+  }
+
+  return false;
+};
+
+main();
