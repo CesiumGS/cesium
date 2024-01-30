2dd276f98a751e44f007b30e8a7c1eb70329b35e
Josh <8007967+jjspace@users.noreply.github.com>
2024-01-30 13:22:01 -0500

    remove water mask option

--- packages/engine/Source/Core/createWorldTerrainAsync.js
+++ packages/engine/Source/Core/createWorldTerrainAsync.js
@@ -8,7 +8,6 @@ import defaultValue from "./defaultValue.js";
  *
  * @param {Object} [options] Object with the following properties:
  * @param {Boolean} [options.requestVertexNormals=false] Flag that indicates if the client should request additional lighting information from the server if available.
- * @param {Boolean} [options.requestWaterMask=false] Flag that indicates if the client should request per tile water masks from the server if available.
  * @returns {Promise<CesiumTerrainProvider>} A promise that resolves to the created CesiumTerrainProvider
  *
  * @see Ion
@@ -24,11 +23,10 @@ import defaultValue from "./defaultValue.js";
  * }
  *
  * @example
- * // Create Cesium World Terrain with water and normals.
+ * // Create Cesium World Terrain with normals.
  * try {
  *   const viewer1 = new Cesium.Viewer("cesiumContainer", {
  *     terrainProvider: await Cesium.createWorldTerrainAsync({
- *       requestWaterMask: true,
  *       requestVertexNormals: true
  *     });
  *   });
@@ -42,7 +40,6 @@ function createWorldTerrainAsync(options) {
 
   return CesiumTerrainProvider.fromIonAssetId(1, {
     requestVertexNormals: defaultValue(options.requestVertexNormals, false),
-    requestWaterMask: defaultValue(options.requestWaterMask, false),
   });
 }
 export default createWorldTerrainAsync;
