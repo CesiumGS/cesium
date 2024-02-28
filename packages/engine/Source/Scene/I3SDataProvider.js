6486058e088f58a597853baae15dabda146751e1
Tamrat Belayneh <tbelayneh@esri.com>
2024-02-27 20:08:52 -0800

    Remove console out if geoid transform is not needed

--- packages/engine/Source/Scene/I3SDataProvider.js
+++ packages/engine/Source/Scene/I3SDataProvider.js
@@ -792,9 +792,6 @@ async function loadGeoidData(provider) {
   const geoidTerrainProvider = provider._geoidTiledTerrainProvider;
 
   if (!defined(geoidTerrainProvider)) {
-    console.log(
-      "No Geoid Terrain service provided - no geoid conversion will be performed."
-    );
     return;
   }
 
