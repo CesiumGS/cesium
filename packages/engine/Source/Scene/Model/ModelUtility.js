e2e92260a7644caa7f188ca24164c2b4612a178b
Jeshurun Hembd <jeshurun@cesium.com>
2024-05-10 10:22:18 -0400

    Add data and Sandcastle for KHR_materials_anisotropy

--- packages/engine/Source/Scene/Model/ModelUtility.js
+++ packages/engine/Source/Scene/Model/ModelUtility.js
@@ -358,6 +358,7 @@ ModelUtility.supportedExtensions = {
   KHR_materials_common: true,
   KHR_materials_pbrSpecularGlossiness: true,
   KHR_materials_specular: true,
+  KHR_materials_anisotropy: true,
   KHR_materials_unlit: true,
   KHR_mesh_quantization: true,
   KHR_texture_basisu: true,
