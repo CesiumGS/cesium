010367342d692879d14e7b742bed23a884500aa7
Jason Sobotka <jason.sobotka@cesium.com>
2024-06-07 16:20:51 -0500

    initial vertex calcs for splats

--- packages/engine/Source/Shaders/Model/ModelVS.glsl
+++ packages/engine/Source/Shaders/Model/ModelVS.glsl
@@ -140,8 +140,60 @@ void main()
         gl_PointSize = pointCloudPointSizeStylingStage(attributes, metadata);
         #else
         gl_PointSize = 1.0;
-        #endif
 
+        //convert gaussian scale and rot to covariance matrix
+        vec3 vscale = attributes.scale;
+
+        uvec4 centerWorldPos = uvec4(attributes.positionMC.x, attributes.positionMC.y, attributes.positionMC.z, 0);
+        vec4 cam = czm_view * vec4(uintBitsToFloat(centerWorldPos.xyz), 1);
+        vec4 pos2d = czm_projection * cam;
+
+        float clip = 1.2 * pos2d.w;
+        if (pos2d.z < -clip || pos2d.x < -clip || pos2d.x > clip || pos2d.y < -clip || pos2d.y > clip) {
+            gl_Position = vec4(0.0, 0.0, 2.0, 1.0);
+            return;
+        }
+
+        //calc matrix from scale and rot
+        mat3 ms = mat3(
+        vscale.x, 0., 0.,
+        0., vscale.y, 0.,
+        0., 0., vscale.z
+        );
+        float x = attributes.rotation.x;
+        float y = attributes.rotation.y;
+        float z = attributes.rotation.z;
+        float w = attributes.rotation.w;
+        mat3 mr = mat3(
+            1.-2.*(y*y + z*z),   2.*(x*y - w*z),   2.*(x*z + w*y),
+            2.*(x*y + w*z), 1.-2.*(x*x + z*z),   2.*(y*z - w*x),
+            2.*(x*z - w*y),   2.*(y*z + w*x), 1.-2.*(x*x + y*y)
+        );
+        mat3 Mrs =  mr * ms;
+
+        mat3 J = mat3(
+        czm_viewerPositionWC.x / cam.z, 0., -(czm_viewerPositionWC.x * cam.x) / (cam.z * cam.z),
+        0., -czm_viewerPositionWC.y / cam.z, (czm_viewerPositionWC.y * cam.y) / (cam.z * cam.z),
+        0., 0., 0.
+        );
+
+        mat3 T = transpose(mat3(czm_view)) * J;
+        mat3 cov2d = transpose(T) * Mrs * T;
+
+        // decompose covariance
+        float mid = (cov2d[0][0] + cov2d[1][1]) / 2.0;
+        float radius = length(vec2((cov2d[0][0] - cov2d[1][1]) / 2.0, cov2d[0][1]));
+        float lambda1 = mid + radius, lambda2 = mid - radius;
+
+        if(lambda2 < 0.0) return;
+        vec2 diagonalVector = normalize(vec2(cov2d[0][1], lambda1 - cov2d[0][0]));
+        vec2 majorAxis = min(sqrt(2.0 * lambda1), 1024.0) * diagonalVector;
+        vec2 minorAxis = min(sqrt(2.0 * lambda2), 1024.0) * vec2(diagonalVector.y, -diagonalVector.x);
+
+        gl_PointSize *= majorAxis.x;
+
+
+        #endif
         gl_PointSize *= show;
     #endif
 
