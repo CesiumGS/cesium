ea539620ea43643a3d65867c25c1c49418dda247
Jason Sobotka <jason.sobotka@cesium.com>
2024-06-08 17:12:48 -0500

    removed early returns. we seem to hit them all the time
    interesting scaling decomposed covariance

--- packages/engine/Source/Shaders/Model/ModelVS.glsl
+++ packages/engine/Source/Shaders/Model/ModelVS.glsl
@@ -139,7 +139,7 @@ void main()
         #elif defined(HAS_POINT_CLOUD_POINT_SIZE_STYLE) || defined(HAS_POINT_CLOUD_ATTENUATION)
         gl_PointSize = pointCloudPointSizeStylingStage(attributes, metadata);
         #else
-        gl_PointSize = 1.0;
+        //gl_PointSize = 1.0;
 
         //convert gaussian scale and rot to covariance matrix
         vec3 vscale = attributes.scale;
@@ -149,11 +149,12 @@ void main()
         vec4 pos2d = czm_projection * cam;
 
         float clip = 1.2 * pos2d.w;
+        /*
         if (pos2d.z < -clip || pos2d.x < -clip || pos2d.x > clip || pos2d.y < -clip || pos2d.y > clip) {
             gl_Position = vec4(0.0, 0.0, 2.0, 1.0);
             return;
         }
-
+*/
         //calc matrix from scale and rot
         mat3 ms = mat3(
         vscale.x, 0., 0.,
@@ -185,13 +186,12 @@ void main()
         float radius = length(vec2((cov2d[0][0] - cov2d[1][1]) / 2.0, cov2d[0][1]));
         float lambda1 = mid + radius, lambda2 = mid - radius;
 
-        if(lambda2 < 0.0) return;
+        //if(lambda2 < 0.0) return;
         vec2 diagonalVector = normalize(vec2(cov2d[0][1], lambda1 - cov2d[0][0]));
         vec2 majorAxis = min(sqrt(2.0 * lambda1), 1024.0) * diagonalVector;
         vec2 minorAxis = min(sqrt(2.0 * lambda2), 1024.0) * vec2(diagonalVector.y, -diagonalVector.x);
 
-        gl_PointSize *= majorAxis.x;
-
+        gl_PointSize = sqrt(2.0 * lambda1);
 
         #endif
         gl_PointSize *= show;
