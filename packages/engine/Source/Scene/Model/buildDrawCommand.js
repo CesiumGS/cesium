21ef2d765f6dd8a6399f31f8baa9a0592be29065
Marco Hutter <javagl@javagl.de>
2024-08-01 18:44:52 +0200

    Debug output for review and tests

--- packages/engine/Source/Scene/Model/buildDrawCommand.js
+++ packages/engine/Source/Scene/Model/buildDrawCommand.js
@@ -78,6 +78,27 @@ function buildDrawCommand(primitiveRenderResources, frameState) {
     );
   }
 
+  console.log("buildDrawCommand");
+  console.log("  positionMin", primitiveRenderResources.positionMin);
+  console.log("  positionMax", primitiveRenderResources.positionMax);
+  console.log(
+    "  instancingTranslationMin ",
+    primitiveRenderResources.runtimeNode.instancingTranslationMin
+  );
+  console.log(
+    "  instancingTranslationMax ",
+    primitiveRenderResources.runtimeNode.instancingTranslationMax
+  );
+  console.log(
+    "  node scale ",
+    primitiveRenderResources.runtimeNode.computedTransform[0]
+  );
+  console.log(
+    "  instances transformInWorldSpace ",
+    primitiveRenderResources.runtimeNode.node?.instances?.transformInWorldSpace
+  );
+  console.log("  radius", primitiveRenderResources.boundingSphere.radius);
+
   // Initialize render state with default values
   let renderState = clone(
     RenderState.fromCache(primitiveRenderResources.renderStateOptions),
@@ -117,7 +138,7 @@ function buildDrawCommand(primitiveRenderResources, frameState) {
     pickId: pickId,
     instanceCount: primitiveRenderResources.instanceCount,
     primitiveType: primitiveRenderResources.primitiveType,
-    debugShowBoundingVolume: model.debugShowBoundingVolume,
+    debugShowBoundingVolume: true, //model.debugShowBoundingVolume,
     castShadows: castShadows,
     receiveShadows: receiveShadows,
   });
