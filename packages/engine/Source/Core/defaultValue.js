4aca55ec41dd6d3c9150556641786a1180d029f2
anpaulan <paulan21095@gmail.com>
2024-10-06 19:09:14 -0400

    cleanup unnecessary code

--- packages/engine/Source/Core/defaultValue.js
+++ packages/engine/Source/Core/defaultValue.js
@@ -1,19 +1,16 @@
 /**
  * Returns the first parameter if not undefined, otherwise the second parameter.
  * Useful for setting a default value for a parameter.
- * @namespace defaultValue
- * @global
  * @function
- * 
+ *
  * @param {*} a
  * @param {*} b
  * @returns {*} Returns the first parameter if not undefined, otherwise the second parameter.
  *
  * @example
  * param = Cesium.defaultValue(param, 'default');
- * 
- * @property {object} EMPTY_OBJECT - A frozen empty object that can be used as the default value 
- * for options passed as an object literal.
+ *
+ * @property {object} EMPTY_OBJECT - A frozen empty object that can be used as the default value for options passed as an object literal.
  */
 function defaultValue(a, b) {
   if (a !== undefined && a !== null) {
