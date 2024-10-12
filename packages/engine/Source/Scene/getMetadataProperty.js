1a7dbb64171282d6a330d02b0156df262b88987e
Marco Hutter <javagl@javagl.de>
2024-10-12 14:23:03 +0200

    Do not try to pick property attributes

--- packages/engine/Source/Scene/getMetadataProperty.js
+++ packages/engine/Source/Scene/getMetadataProperty.js
@@ -1,19 +1,15 @@
 import defined from "../Core/defined.js";
 
 /**
- * Return the `PropertyTextureProperty` or `PropertyAttributeProperty` from
- * the given `StructuralMetadata` that matches the given description.
+ * Return the `PropertyTextureProperty` from the given `StructuralMetadata`
+ * that matches the given description.
  *
  * If the given structural metadata is `undefined`, then `undefined` is returned.
  *
- * Otherwise, this method will check all the property textures and property
- * attributes in the given structural metadata.
+ * Otherwise, this method will check all the property textures in the given
+ * structural metadata.
  *
  * If it finds a property texture that has a class with an `_id` that matches
- * the given name, and that contains a property for the given property name, then
- * this property is returned.
- *
- * If it finds a property attribute that has a class with an `_id` that matches
  * the given name, and that contains a property for the given property name, then
  * this property is returned.
  *
@@ -22,7 +18,7 @@ import defined from "../Core/defined.js";
  * @param {StructuralMetadata} structuralMetadata The structural metadata
  * @param {string} className The name of the metadata class
  * @param {string} propertyName The name of the metadata property
- * @returns {PropertyTextureProperty|PropertyAttributeProperty|undefined}
+ * @returns {PropertyTextureProperty|undefined}
  * @private
  */
 function getMetadataProperty(structuralMetadata, className, propertyName) {
@@ -40,17 +36,13 @@ function getMetadataProperty(structuralMetadata, className, propertyName) {
       }
     }
   }
-  const propertyAttributes = structuralMetadata.propertyAttributes;
-  for (const propertyAttribute of propertyAttributes) {
-    const metadataClass = propertyAttribute.class;
-    if (metadataClass.id === className) {
-      const properties = propertyAttribute.properties;
-      const property = properties[propertyName];
-      if (defined(property)) {
-        return property;
-      }
-    }
-  }
+  // Note: This could check for property attributes in a similar
+  // way. But since picking arbitrary property attributes via the
+  // frame buffer is not supported yet, returning "undefined" here
+  // causes the picking to bail out early and safely when no
+  // property texture was found.
+  // See https://github.com/CesiumGS/cesium/issues/12225
+  return undefined;
 }
 
 export default getMetadataProperty;
