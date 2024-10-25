0542d6c9d06710bfabf9dbc61ffe2e3c231c8411
entenity <1026795018@qq.com>
2024-10-25 22:23:24 +0800

    cesium

--- project/src/pages/index.vue
+++ project/src/pages/index.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="viewer" id="viewer"></div>
+  <div
+    class="viewer"
+    id="viewer"
+  ></div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import * as Cesium from 'cesium'
 
-onMounted(() => {
-  const viewer = new Cesium.Viewer("viewer", {
+onMounted(async () => {
+  Cesium.Ion.defaultAccessToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3MWMyZWVjOS1lNmMzLTQ5NjQtOWQ2NC0zYTA4NTc0OGJkYjMiLCJpZCI6NjYzNjgsImlhdCI6MTYzMDkxMTIyOH0.nJMg8VBzAAtEiCZ0hdXU_LL5lRSxsdFWRYaz2ZQ82ow'
+  const viewer = new Cesium.Viewer('viewer', {
     baseLayerPicker: false,
-    useBrowserRecommendedResolution: true,
+    useBrowserRecommendedResolution: false,
     timeline: false,
     animation: false,
-    sceneMode: Cesium.SceneMode.SCENE3D
-  });
+    sceneMode: Cesium.SceneMode.SCENE3D,
+    terrainProvider: await Cesium.CesiumTerrainProvider.fromIonAssetId(1, {
+      requestVertexNormals: true
+    })
+  })
   console.log(viewer)
 })
 </script>
--- scripts/build.js
+++ scripts/build.js
@@ -280,7 +280,7 @@ function generateDeclaration(workspace, file) {
     assignmentName = `_shaders${assignmentName}`;
   }
   assignmentName = assignmentName.replace(/(\.|-)/g, "_");
-  return `export { ${assignmentName} } from '@${scope}/${workspace}';`;
+  return `export { ${assignmentName} } from '../packages/${workspace}';`;
 }
 
 /**
