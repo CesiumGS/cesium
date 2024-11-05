421e5dda69820dd7e9a947f5644567325368d2d7
jjspace <8007967+jjspace@users.noreply.github.com>
2024-11-05 16:33:11 -0500

    restructure api, create tileset from model id

--- packages/engine/Source/Core/ITwin.js
+++ packages/engine/Source/Core/ITwin.js
@@ -1,4 +1,76 @@
+import defined from "./defined.js";
+import DeveloperError from "./DeveloperError.js";
 import Resource from "./Resource.js";
+import RuntimeError from "./RuntimeError.js";
+
+/**
+ * @enum {string}
+ */
+const ExportStatus = Object.freeze({
+  NotStarted: "NotStarted",
+  InProgress: "InProgress",
+  Complete: "Complete",
+  Invalid: "Invalid",
+});
+
+/**
+ * Type of an export currently, only GLTF and 3DFT are documented
+ * The CESIUM option is what we were told to use with Sandcastle
+ * I've also seen the IMODEL one but don't know where it's from
+ * @enum {string}
+ */
+const ExportType = Object.freeze({
+  "3DFT": "3DFT",
+  GLFT: "GLTF",
+  IMODEL: "IMODEL",
+  CESIUM: "CESIUM",
+});
+
+/**
+ * @typedef {Object} GeometryOptions
+ * @property {boolean} includeLines
+ * @property {number} chordTol
+ * @property {number} angleTol
+ * @property {number} decimationTol
+ * @property {number} maxEdgeLength
+ * @property {number} minBRepFeatureSize
+ * @property {number} minLineStyleComponentSize
+ */
+
+/**
+ * @typedef {Object} ViewDefinitionFilter
+ * @property {string[]} models Array of included model IDs.
+ * @property {string[]} categories Array of included category IDs.
+ * @property {string[]} neverDrawn Array of element IDs to filter out.
+ */
+
+/**
+ * @typedef {Object} StartExport
+ * @property {string} iModelId
+ * @property {string} changesetId
+ * @property {ExportType} exportType Type of mesh to create. Currently, only GLTF and 3DFT are supported and undocumented CESIUM option
+ * @property {GeometryOptions} geometryOptions
+ * @property {ViewDefinitionFilter} viewDefinitionFilter
+ */
+
+/**
+ * @typedef {Object} Link
+ * @property {string} href
+ */
+
+/**
+ * @typedef {Object} Export
+ * @property {string} id
+ * @property {string} displayName
+ * @property {ExportStatus} status
+ * @property {StartExport} request
+ * @property {{mesh: Link}} _links
+ */
+
+/**
+ * @typedef {Object} ExportResponse
+ * @property {Export} export
+ */
 
 /**
  * Default settings for accessing the iTwin platform.
@@ -34,7 +106,7 @@ const ITwin = {};
 ITwin.defaultAccessToken = undefined;
 
 /**
- * Gets or sets the default Google Map Tiles API endpoint.
+ * Gets or sets the default iTwin API endpoint.
  *
  * @type {string|Resource}
  * @default https://api.bentley.com
@@ -43,24 +115,192 @@ ITwin.apiEndpoint = new Resource({
   url: "https://api.bentley.com",
 });
 
-// TODO: this should only be needed if we have a way to generate really long term access tokens
-// to sample data that is accessible to everyone
-// ITwin.getDefaultTokenCredit = function (providedKey) {
-//   if (providedKey !== defaultAccessToken) {
-//     return undefined;
-//   }
+/**
+ * @param {string} exportId
+ */
+ITwin.getExport = async function (exportId) {
+  if (!defined(ITwin.defaultAccessToken)) {
+    throw new DeveloperError("Must set ITwin.defaultAccessToken first");
+  }
 
-//   if (!defined(defaultTokenCredit)) {
-//     const defaultTokenMessage =
-//       '<b> \
-//             This application is using Cesium\'s default ion access token. Please assign <i>Cesium.Ion.defaultAccessToken</i> \
-//             with an access token from your ion account before making any Cesium API calls. \
-//             You can sign up for a free ion account at <a href="https://cesium.com">https://cesium.com</a>.</b>';
+  const headers = {
+    Authorization: ITwin.defaultAccessToken,
+    Accept: "application/vnd.bentley.itwin-platform.v1+json",
+  };
 
-//     defaultTokenCredit = new Credit(defaultTokenMessage, true);
-//   }
+  // obtain export for specified export id
+  const url = `${ITwin.apiEndpoint}mesh-export/${exportId}`;
 
-//   return defaultTokenCredit;
-// };
+  // TODO: this request is _really_ slow, like 7 whole second alone for me
+  // Arun said this was kinda normal but to keep track of the `x-correlation-id` of any that take EXTRA long
+  const response = await fetch(url, { headers });
+  if (!response.ok) {
+    const result = await response.json();
+    if (response.status === 401) {
+      throw new RuntimeError(
+        `Unauthorized, bad token, wrong scopes or headers bad. ${result.error.details[0].code}`,
+      );
+    } else if (response.status === 404) {
+      throw new RuntimeError(`Requested export is not available ${exportId}`);
+    } else if (response.status === 429) {
+      throw new RuntimeError("Too many requests");
+    }
+    throw new RuntimeError(`Unknown request failure ${response.status}`);
+  }
+
+  /** @type {ExportResponse} */
+  const result = await response.json();
+  return result;
+};
+
+/**
+ * Get the list of exports for the given iModel + changeset
+ *
+ * @param {string} iModelId
+ * @param {string} changesetId
+ */
+ITwin.getExports = async function (iModelId, changesetId) {
+  if (!defined(ITwin.defaultAccessToken)) {
+    throw new DeveloperError("Must set ITwin.defaultAccessToken first");
+  }
+
+  const headers = {
+    Authorization: ITwin.defaultAccessToken,
+    Accept: "application/vnd.bentley.itwin-platform.v1+json",
+    Prefer: "return=representation", // or return=minimal (the default)
+  };
+
+  // obtain export for specified export id
+  let url = `${ITwin.apiEndpoint}mesh-export/?iModelId=${iModelId}`;
+  if (defined(changesetId) && changesetId !== "") {
+    url += `&changesetId=${changesetId}`;
+  }
+
+  const response = await fetch(url, { headers });
+  if (!response.ok) {
+    const result = await response.json();
+    if (response.status === 401) {
+      throw new RuntimeError(
+        `Unauthorized, bad token, wrong scopes or headers bad. ${result.error.details[0].code}`,
+      );
+    } else if (response.status === 422) {
+      throw new RuntimeError(
+        `Unprocessable Entity:${result.error.code} ${result.error.message}`,
+      );
+    } else if (response.status === 429) {
+      throw new RuntimeError("Too many requests");
+    }
+    throw new RuntimeError(`Unknown request failure ${response.status}`);
+  }
+
+  /** @type {{exports: Export[]}} */
+  const result = await response.json();
+  return result;
+};
+
+/**
+ * Start the export process for the given iModel + changeset.
+ *
+ * @param {string} iModelId
+ * @param {string} changesetId
+ */
+ITwin.createExportForModelId = async function (iModelId, changesetId) {
+  if (!defined(ITwin.defaultAccessToken)) {
+    throw new DeveloperError("Must set ITwin.defaultAccessToken first");
+  }
+
+  changesetId = changesetId ?? "";
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Authorization: ITwin.defaultAccessToken,
+      Accept: "application/vnd.bentley.itwin-platform.v1+json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      iModelId,
+      changesetId,
+      exportType: "CESIUM",
+    }),
+  };
+
+  // initiate mesh export
+  const response = await fetch(
+    `https://api.bentley.com/mesh-export/`,
+    requestOptions,
+  );
+
+  if (!response.ok) {
+    const result = await response.json();
+    if (response.status === 401) {
+      console.error(
+        result.error.code,
+        result.error.message,
+        result.error.details,
+      );
+      throw new RuntimeError(
+        "Unauthorized, bad token, wrong scopes or headers bad",
+      );
+    } else if (response.status === 403) {
+      console.error(result.error.code, result.error.message);
+      throw new RuntimeError("Not allowed, forbidden");
+    } else if (response.status === 422) {
+      console.error(result.error.code, result.error.message);
+      console.error(result.error.details);
+      throw new RuntimeError("Unprocessable: Cannot create export job");
+    } else if (response.status === 429) {
+      throw new RuntimeError("Too many requests");
+    }
+
+    throw new RuntimeError(`Unknown request failure ${response.status}`);
+  }
+
+  /** @type {ExportResponse} */
+  const result = await response.json();
+  return result.export.id;
+};
+
+/**
+ * Delete the specified export
+ *
+ * TODO: I'm not sure if we want this or not. Might belong better as an APP level function
+ * I just started creating helpers for all the routes under the `mesh-export` API
+ * for ease of access during testing
+ *
+ * @param {string} exportId
+ */
+ITwin.deleteExport = async function (exportId) {
+  if (!defined(ITwin.defaultAccessToken)) {
+    throw new DeveloperError("Must set ITwin.defaultAccessToken first");
+  }
+  const headers = {
+    Authorization: ITwin.defaultAccessToken,
+    Accept: "application/vnd.bentley.itwin-platform.v1+json",
+  };
+
+  // obtain export for specified export id
+  const url = `${ITwin.apiEndpoint}mesh-export/${exportId}`;
+
+  const response = await fetch(url, { method: "DELETE", headers });
+  if (!response.ok) {
+    const result = await response.json();
+    if (response.status === 401) {
+      throw new RuntimeError(
+        `Unauthorized, bad token, wrong scopes or headers bad. ${result.error.details[0].code}`,
+      );
+    } else if (response.status === 404) {
+      throw new RuntimeError("Export not found");
+    } else if (response.status === 422) {
+      throw new RuntimeError(
+        `Unprocessable Entity: ${result.error.code} ${result.error.message}`,
+      );
+    } else if (response.status === 429) {
+      throw new RuntimeError("Too many requests");
+    }
+    throw new RuntimeError(`Unknown request failure ${response.status}`);
+  }
+};
 
 export default ITwin;
+export { ExportStatus, ExportType };
--- packages/engine/Source/Scene/createIModel3DTileset.js
+++ packages/engine/Source/Scene/createIModel3DTileset.js
@@ -1,81 +1,55 @@
 import Cesium3DTileset from "./Cesium3DTileset.js";
 import defined from "../Core/defined.js";
 import Resource from "../Core/Resource.js";
-import ITwin from "../Core/ITwin.js";
+import ITwin, { ExportStatus, ExportType } from "../Core/ITwin.js";
 import DeveloperError from "../Core/DeveloperError.js";
+import RuntimeError from "../Core/RuntimeError.js";
 
 function delay(ms) {
   return new Promise((res) => setTimeout(res, ms));
 }
 
 /**
- * @enum {string}
+ * @param {Export} exportObj
+ * @param {Cesium3DTileset.ConstructorOptions} [options]
  */
-const ExportStatus = Object.freeze({
-  NotStarted: "NotStarted",
-  InProgress: "InProgress",
-  Complete: "Complete",
-  Invalid: "Invalid",
-});
+async function loadExport(exportObj, options) {
+  let status = exportObj.status;
 
-/**
- * Type of an export currently, only GLTF and 3DFT are documented
- * The CESIUM option is what we were told to use with Sandcastle
- * I've also seen the IMODEL one but don't know where it's from
- * @enum {string}
- */
-const ExportType = Object.freeze({
-  "3DFT": "3DFT",
-  GLFT: "GLTF",
-  IMODEL: "IMODEL",
-  CESIUM: "CESIUM",
-});
+  if (exportObj.request.exportType !== ExportType.CESIUM) {
+    // This is an undocumented value but I think it's the only one we want to load
+    // TODO: should we even be checking this?
+    throw new Error(`Wrong export type ${exportObj.request.exportType}`);
+  }
 
-/**
- * @typedef {Object} GeometryOptions
- * @property {boolean} includeLines
- * @property {number} chordTol
- * @property {number} angleTol
- * @property {number} decimationTol
- * @property {number} maxEdgeLength
- * @property {number} minBRepFeatureSize
- * @property {number} minLineStyleComponentSize
- */
+  const timeoutAfter = 300000;
+  const start = Date.now();
+  // wait until the export is complete
+  while (status !== ExportStatus.Complete) {
+    await delay(5000);
+    exportObj = (await ITwin.getExport(exportObj.id)).export;
+    status = exportObj.status;
+    console.log(`Export is ${status}`);
 
-/**
- * @typedef {Object} ViewDefinitionFilter
- * @property {string[]} models Array of included model IDs.
- * @property {string[]} categories Array of included category IDs.
- * @property {string[]} neverDrawn Array of element IDs to filter out.
- */
+    if (Date.now() - start > timeoutAfter) {
+      throw new RuntimeError("Export did not complete in time.");
+    }
+  }
 
-/**
- * @typedef {Object} StartExport
- * @property {string} iModelId
- * @property {string} changesetId
- * @property {ExportType} exportType Type of mesh to create. Currently, only GLTF and 3DFT are supported and undocumented CESIUM option
- * @property {GeometryOptions} geometryOptions
- * @property {ViewDefinitionFilter} viewDefinitionFilter
- */
+  // This link is only valid 1 hour
+  let tilesetUrl = exportObj._links.mesh.href;
+  const splitStr = tilesetUrl.split("?");
+  // is there a cleaner way to do this?
+  tilesetUrl = `${splitStr[0]}/tileset.json?${splitStr[1]}`;
 
-/**
- * @typedef {Object} Link
- * @property {string} href
- */
+  const resource = new Resource({
+    url: tilesetUrl,
+  });
 
-/**
- * @typedef {Object} Export
- * @property {string} id
- * @property {string} displayName
- * @property {ExportStatus} status
- * @property {StartExport} request
- * @property {{mesh: Link}} _links
- */
+  return Cesium3DTileset.fromUrl(resource, options);
+}
 
-/**
- * @typedef {Object} ExportResponse
- * @property {Export} export
- */
+const createIModel3DTileset = {};
 
 /**
  * Creates a {@link Cesium3DTileset} instance for the Google Photorealistic 3D Tiles tileset.
@@ -101,271 +75,53 @@ const ExportType = Object.freeze({
  *   console.log(`Error creating tileset: ${error}`);
  * }
  */
-async function createIModel3DTileset(exportId, options) {
+createIModel3DTileset.fromExportId = async function (exportId, options) {
   if (!defined(ITwin.defaultAccessToken)) {
     throw new DeveloperError("Must set ITwin.defaultAccessToken first");
   }
 
   options = options ?? {};
 
-  const timeoutAfter = 300000;
-  const start = Date.now();
-  let result = await createIModel3DTileset.getExport(exportId);
-  let status = result.export.status;
-
-  if (result.export.request.exportType !== ExportType.CESIUM) {
-    // This is an undocumented value but I think it's the only one we want to load
-    // TODO: should we even be checking this?
-    throw new Error(`Wrong export type ${result.export.request.exportType}`);
-  }
-
-  // wait until the export is complete
-  while (status !== ExportStatus.Complete) {
-    await delay(5000);
-    result = await createIModel3DTileset.getExport(exportId);
-    status = result.export.status;
-    console.log(`Export is ${status}`);
-
-    if (Date.now() - start > timeoutAfter) {
-      throw new Error("Export did not complete in time.");
-    }
-  }
-
-  // This link is only valid 1 hour
-  let tilesetUrl = result.export._links.mesh.href;
-  const splitStr = tilesetUrl.split("?");
-  // is there a cleaner way to do this?
-  tilesetUrl = `${splitStr[0]}/tileset.json?${splitStr[1]}`;
-
-  const resource = new Resource({
-    url: tilesetUrl,
-  });
-
-  return Cesium3DTileset.fromUrl(resource, options);
-}
-
-/**
- * @param {string} exportId
- */
-createIModel3DTileset.getExport = async function (exportId) {
-  const headers = {
-    Authorization: ITwin.defaultAccessToken,
-    Accept: "application/vnd.bentley.itwin-platform.v1+json",
-  };
-
-  // obtain export for specified export id
-  const url = `${ITwin.apiEndpoint}mesh-export/${exportId}`;
-
-  // TODO: this request is _really_ slow, like 7 whole second alone for me
-  // Arun said this was kinda normal but to keep track of the `x-correlation-id` of any that take EXTRA long
-  const response = await fetch(url, { headers });
-  if (!response.ok) {
-    const result = await response.json();
-    if (response.status === 401) {
-      throw new Error(
-        `Unauthorized, bad token, wrong scopes or headers bad. ${result.error.details[0].code}`,
-      );
-    } else if (response.status === 404) {
-      throw new Error(`Requested export is not available ${exportId}`);
-    } else if (response.status === 429) {
-      throw new Error("Too many requests");
-    }
-    throw new Error(`Unknown request failure ${response.status}`);
-  }
-
-  /** @type {ExportResponse} */
-  const result = await response.json();
-  return result;
-};
-
-/**
- * Get the list of exports for the given iModel + changeset
- *
- * @param {string} iModelId
- * @param {string} changesetId
- */
-createIModel3DTileset.getExports = async function (iModelId, changesetId) {
-  if (!defined(ITwin.defaultAccessToken)) {
-    throw new DeveloperError("Must set ITwin.defaultAccessToken first");
-  }
-  const headers = {
-    Authorization: ITwin.defaultAccessToken,
-    Accept: "application/vnd.bentley.itwin-platform.v1+json",
-    Prefer: "return=representation", // or return=minimal (the default)
-  };
-
-  // obtain export for specified export id
-  let url = `${ITwin.apiEndpoint}mesh-export/?iModelId=${iModelId}`;
-  if (defined(changesetId) && changesetId !== "") {
-    url += `&changesetId=${changesetId}`;
-  }
-
-  const response = await fetch(url, { headers });
-  if (!response.ok) {
-    const result = await response.json();
-    if (response.status === 401) {
-      throw new Error(
-        `Unauthorized, bad token, wrong scopes or headers bad. ${result.error.details[0].code}`,
-      );
-    } else if (response.status === 422) {
-      throw new Error(
-        `Unprocessable Entity:${result.error.code} ${result.error.message}`,
-      );
-    } else if (response.status === 429) {
-      throw new Error("Too many requests");
-    }
-    throw new Error(`Unknown request failure ${response.status}`);
-  }
-
-  /** @type {{exports: Export[]}} */
-  const result = await response.json();
-  return result;
+  const result = await ITwin.getExport(exportId);
+  const tileset = await loadExport(result.export, options);
+  return tileset;
 };
 
 /**
  * Check the exports for the given iModel + changeset combination for any that
- * have the desired CESIUM type and return that one
+ * have the desired CESIUM type and returns the first one that matches as a new tileset.
  *
- * @param {string} iModelId
- * @param {string} changesetId
- */
-createIModel3DTileset.checkForCesiumExport = async function (
-  iModelId,
-  changesetId,
-) {
-  const { exports } = await createIModel3DTileset.getExports(
-    iModelId,
-    changesetId,
-  );
-  const cesiumExport = exports.find(
-    (e) => e.request.exportType === ExportType.CESIUM,
-  );
-  return cesiumExport;
-};
-
-/**
- * Start the export process for the given iModel + changeset.
+ * If there is not a CESIUM export you can create it using {@link ITwin.createExportForModelId}
  *
- * Pair this with the {@link checkForCesiumExport} function to avoid creating extra exports
+ * This function assumes one export per type per "iModel id + changeset id". If you need to create
+ * multiple exports per "iModel id + changeset id" you should switch to using {@link createIModel3DTileset}
+ * with the export id directly
  *
  * @example
- * const cesiumExport = await Cesium.createIModel3DTileset.checkForCesiumExport(imodelId, changesetId);
- * let exportId = cesiumExport?.id;
- * if (!Cesium.defined(cesiumExport)) {
- *   exportId = await Cesium.createIModel3DTileset.createExportForModelId(
- *     imodelId,
- *     changesetId,
- *     accessToken,
- *   );
+ * // Try to load the corresponding tileset export or create it if it doesn't exist
+ * let tileset = await Cesium.createIModel3DTileset.fromModelIdimodelId, changesetId);
+ * if (!Cesium.defined(tileset)) {
+ *   const exportId = await Cesium.ITwin.createExportForModelId(imodelId, changesetId, accessToken);
+ *   tileset = await Cesium.createIModel3DTileset(exportId);
  * }
  *
  * @param {string} iModelId
  * @param {string} changesetId
+ * @param {Cesium3DTileset.ConstructorOptions} options
  */
-createIModel3DTileset.createExportForModelId = async function (
+createIModel3DTileset.fromModelId = async function (
   iModelId,
   changesetId,
+  options,
 ) {
-  if (!defined(ITwin.defaultAccessToken)) {
-    throw new DeveloperError("Must set ITwin.defaultAccessToken first");
-  }
-
-  console.log("Start Export");
-
-  changesetId = changesetId ?? "";
-
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      Authorization: ITwin.defaultAccessToken,
-      Accept: "application/vnd.bentley.itwin-platform.v1+json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      iModelId,
-      changesetId,
-      exportType: "CESIUM",
-    }),
-  };
-
-  // initiate mesh export
-  const response = await fetch(
-    `https://api.bentley.com/mesh-export/`,
-    requestOptions,
+  const { exports } = await ITwin.getExports(iModelId, changesetId);
+  const cesiumExport = exports.find(
+    (exportObj) => exportObj.request?.exportType === ExportType.CESIUM,
   );
-
-  if (!response.ok) {
-    const result = await response.json();
-    if (response.status === 401) {
-      console.error("Unauthorized, bad token, wrong scopes or headers bad");
-      console.error(
-        result.error.code,
-        result.error.message,
-        result.error.details,
-      );
-    } else if (response.status === 403) {
-      console.error("Not allowed, forbidden");
-      console.error(result.error.code, result.error.message);
-    } else if (response.status === 422) {
-      console.error("Unprocessable: Cannot create export job");
-      console.error(result.error.code, result.error.message);
-      console.error(result.error.details);
-    } else if (response.status === 429) {
-      console.log(
-        "Too many requests, retry after:",
-        response.headers.get("retry-after"),
-      );
-      console.error(result.error.code, result.error.message);
-    } else {
-      console.error("Bad request, unknown error", response);
-    }
-    return undefined;
+  if (!defined(cesiumExport)) {
+    return;
   }
-
-  /** @type {ExportResponse} */
-  const result = await response.json();
-  return result.export.id;
-};
-
-/**
- * Delete the specified export
- *
- * TODO: I'm not sure if we want this or not. Might belong better as an APP level function
- * I just started creating helpers for all the routes under the `mesh-export` API
- * for ease of access during testing
- *
- * @param {string} exportId
- */
-createIModel3DTileset.deleteExport = async function (exportId) {
-  if (!defined(ITwin.defaultAccessToken)) {
-    throw new DeveloperError("Must set ITwin.defaultAccessToken first");
-  }
-  const headers = {
-    Authorization: ITwin.defaultAccessToken,
-    Accept: "application/vnd.bentley.itwin-platform.v1+json",
-  };
-
-  // obtain export for specified export id
-  const url = `${ITwin.apiEndpoint}mesh-export/${exportId}`;
-
-  const response = await fetch(url, { method: "DELETE", headers });
-  if (!response.ok) {
-    const result = await response.json();
-    if (response.status === 401) {
-      throw new Error(
-        `Unauthorized, bad token, wrong scopes or headers bad. ${result.error.details[0].code}`,
-      );
-    } else if (response.status === 404) {
-      throw new Error("Export not found");
-    } else if (response.status === 422) {
-      throw new Error(
-        `Unprocessable Entity:${result.error.code} ${result.error.message}`,
-      );
-    } else if (response.status === 429) {
-      throw new Error("Too many requests");
-    }
-    throw new Error(`Unknown request failure ${response.status}`);
-  }
+  return loadExport(cesiumExport, options);
 };
 
 export default createIModel3DTileset;
