cefb360dfaa4e0b32bec5c95baeeb04a128fd068
Marco Hutter <javagl@javagl.de>
2025-01-21 21:54:50 +0100

    Fix spec to properly "mock" having content

--- packages/engine/Specs/Scene/Cesium3DTilesetSpec.js
+++ packages/engine/Specs/Scene/Cesium3DTilesetSpec.js
@@ -1407,7 +1407,7 @@ describe(
 
         const root = tileset.root;
         root.refine = Cesium3DTileRefine.REPLACE;
-        root.hasEmptyContent = false; // mock content
+        root.hasRenderableContent = true; // mock content
         tileset.maximumScreenSpaceError = 0.0; // Force root tile to always not meet SSE since this is just checking the request volume
 
         // Renders all 5 tiles
