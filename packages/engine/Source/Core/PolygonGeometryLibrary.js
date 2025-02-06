3f90e64ae90dcc0fcd475a77008c342aaa6ae583
jjspace <8007967+jjspace@users.noreply.github.com>
2025-02-06 17:20:53 -0500

    add tests

--- packages/engine/Source/Core/PolygonGeometryLibrary.js
+++ packages/engine/Source/Core/PolygonGeometryLibrary.js
@@ -312,6 +312,18 @@ PolygonGeometryLibrary.subdivideTexcoordRhumbLine = function (
   return texcoords;
 };
 
+/**
+ * Subdivide the line between 2 points every minDistance length
+ * If the points are already closer than minDistance the first will be returned
+ *
+ * @private
+ * @param {Ellipsoid} ellipsoid
+ * @param {Cartesian3} p0 start point
+ * @param {Cartesian3} p1 end point
+ * @param {number} minDistance minimum distance between points in radians
+ * @param {number[]} [result] if provided positions will be packed into this array starting at index 0
+ * @returns {number[]} Cartesian3 positions packed into an array
+ */
 PolygonGeometryLibrary.subdivideRhumbLine = function (
   ellipsoid,
   p0,
