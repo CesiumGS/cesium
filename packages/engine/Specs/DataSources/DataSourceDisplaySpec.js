5857c3fce49d29967fc8a804a4fc8487ce3a0b3f
jerome.fayot <jerome.fayot@airbus.com>
2025-03-01 04:18:09 +0100

    fix: created DataSourceDisplay.getBoundingSphereState to match convention that when a result is undefined, a new object is created

--- packages/engine/Specs/DataSources/DataSourceDisplaySpec.js
+++ packages/engine/Specs/DataSources/DataSourceDisplaySpec.js
@@ -132,6 +132,7 @@ describe(
 
         const result = new BoundingSphere();
         const state = display.getBoundingSphere(entity, true, result);
+        const stateOnly = display.getBoundingSphereState(entity, true, result);
 
         const expected = BoundingSphere.union(
           visualizer1.getBoundingSphereResult,
@@ -139,6 +140,7 @@ describe(
         );
 
         expect(state).toBe(BoundingSphereState.DONE);
+        expect(stateOnly).toEqual(state);
         expect(result).toEqual(expected);
       });
     });
@@ -174,8 +176,10 @@ describe(
 
         const result = new BoundingSphere();
         const state = display.getBoundingSphere(entity, true, result);
+        const stateOnly = display.getBoundingSphereState(entity, true, result);
 
         expect(state).toBe(BoundingSphereState.DONE);
+        expect(stateOnly).toEqual(state);
       });
     });
 
@@ -210,8 +214,10 @@ describe(
 
         const result = new BoundingSphere();
         const state = display.getBoundingSphere(entity, true, result);
+        const stateOnly = display.getBoundingSphereState(entity, true, result);
 
         expect(state).toBe(BoundingSphereState.DONE);
+        expect(stateOnly).toEqual(state);
         expect(result).toEqual(visualizer2.getBoundingSphereResult);
       });
     });
@@ -244,8 +250,10 @@ describe(
 
       const result = new BoundingSphere();
       const state = display.getBoundingSphere(entity, false, result);
+      const stateOnly = display.getBoundingSphereState(entity, false, result);
 
       expect(state).toBe(BoundingSphereState.PENDING);
+      expect(stateOnly).toEqual(state);
     });
 
     it("Fails bounding sphere for entity without visualization.", function () {
@@ -261,7 +269,9 @@ describe(
 
         const result = new BoundingSphere();
         const state = display.getBoundingSphere(entity, false, result);
+        const stateOnly = display.getBoundingSphereState(entity, false, result);
         expect(state).toBe(BoundingSphereState.FAILED);
+        expect(stateOnly).toEqual(state);
         display.destroy();
       });
     });
@@ -276,7 +286,9 @@ describe(
       const entity = new Entity();
       const result = new BoundingSphere();
       const state = display.getBoundingSphere(entity, false, result);
+      const stateOnly = display.getBoundingSphereState(entity, false, result);
       expect(state).toBe(BoundingSphereState.FAILED);
+      expect(stateOnly).toEqual(state);
       display.destroy();
     });
 
@@ -289,16 +301,20 @@ describe(
       expect(function () {
         display.getBoundingSphere(undefined, false, result);
       }).toThrowDeveloperError();
+      expect(function () {
+        display.getBoundingSphereState(undefined, false);
+      }).toThrowDeveloperError();
     });
 
-    it("Compute bounding sphere doesn't throw without result.", function () {
+    it("Compute bounding sphere throws without result.", function () {
       display = new DataSourceDisplay({
         dataSourceCollection: dataSourceCollection,
         scene: scene,
       });
       const entity = new Entity();
-      const state = display.getBoundingSphere(entity, false);
-      expect(state).toBe(BoundingSphereState.PENDING);
+      expect(function () {
+        display.getBoundingSphere(entity, false, undefined);
+      }).toThrowDeveloperError();
     });
 
     it("Compute bounding sphere throws without allowPartial.", function () {
@@ -310,6 +326,9 @@ describe(
       const result = new BoundingSphere();
       expect(function () {
         display.getBoundingSphere(entity, undefined, result);
+      }).toThrowDeveloperError();
+      expect(function () {
+        display.getBoundingSphereState(entity, undefined);
       }).toThrowDeveloperError();
     });
 
