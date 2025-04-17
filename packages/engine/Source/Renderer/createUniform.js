62a2b6da45e2cbbacd0f687a7e9ea19fefa8e6e5
2802241452 <2802241452@qq.com>
2025-04-17 09:00:43 +0800

    Solves the problem of not being able to bindTexture when cutting 3dtiles multiple times

--- packages/engine/Source/Renderer/createUniform.js
+++ packages/engine/Source/Renderer/createUniform.js
@@ -210,7 +210,9 @@ UniformSampler.prototype.set = function () {
   gl.activeTexture(gl.TEXTURE0 + this.textureUnitIndex);
 
   const v = this.value;
-  gl.bindTexture(v._target, v._texture);
+  if(v){
+    gl.bindTexture(v._target, v._texture);
+  }
 };
 
 UniformSampler.prototype._setSampler = function (textureUnitIndex) {
