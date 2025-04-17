793ba8897a0af2617374d853b44f2dec54e3b6cb
jjspace <8007967+jjspace@users.noreply.github.com>
2025-04-17 16:07:03 -0400

    elevate eslint config, run prettier

--- packages/sandcastle/src/App.tsx
+++ packages/sandcastle/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
