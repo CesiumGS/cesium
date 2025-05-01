0af802d83669f607b3901f98f1d8a6ea60eda57a
lukemckinstry <lukemckinstry@gmail.com>
2025-05-01 11:39:47 -0400

    wip doc notes

--- packages/engine/Source/Scene/Model/ModelSceneGraph.js
+++ packages/engine/Source/Scene/Model/ModelSceneGraph.js
@@ -526,6 +526,7 @@ const scratchPrimitivePositionMax = new Cartesian3();
  * Generates the {@link ModelDrawCommand} for each primitive in the model.
  * If the model is used for classification, a {@link ClassificationModelDrawCommand}
  * is generated for each primitive instead.
+ * Updates the bounding sphere for the scene graph and each primitive.
  * @param {Model} model TODO
  * @param {FrameState} frameState The current frame state. This is needed to
  * allocate GPU resources as needed.
@@ -734,6 +735,7 @@ ModelSceneGraph.prototype.computeBoundingVolumes = function (
   );
 
   this._rootBoundingSphere = boundingSphere;
+  // TODO: resolve model space bounding sphere
   this._boundingSphere = BoundingSphere.clone(boundingSphere);
 };
 
