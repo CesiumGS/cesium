cc9cff0bef540bfd691b819b985e4e06d4af450d
jerome.fayot <jerome.fayot@airbus.com>
2025-05-07 09:46:41 +0200

    fixed merge issue

--- packages/engine/Source/Scene/VoxelCylinderShape.js
+++ packages/engine/Source/Scene/VoxelCylinderShape.js
@@ -4,7 +4,6 @@ import Cartesian2 from "../Core/Cartesian2.js";
 import Cartesian3 from "../Core/Cartesian3.js";
 import Cartesian4 from "../Core/Cartesian4.js";
 import CesiumMath from "../Core/Math.js";
-import Check from "../Core/Check.js";
 import Matrix3 from "../Core/Matrix3.js";
 import Matrix4 from "../Core/Matrix4.js";
 import OrientedBoundingBox from "../Core/OrientedBoundingBox.js";
