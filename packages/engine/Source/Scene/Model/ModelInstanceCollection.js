ce3f303b5f4980175ee869db3d37e1591c4acefd
lukemckinstry <lukemckinstry@gmail.com>
2025-05-08 13:58:27 -0400

    model collection updates

--- packages/engine/Source/Scene/Model/ModelInstanceCollection.js
+++ packages/engine/Source/Scene/Model/ModelInstanceCollection.js
@@ -1,6 +1,4 @@
-import Frozen from "../../Core/Frozen.js";
 import defined from "../../Core/defined.js";
-import destroyObject from "../../Core/destroyObject.js";
 import DeveloperError from "../../Core/DeveloperError.js";
 import ModelInstance from "./ModelInstance.js";
 
@@ -13,33 +11,26 @@ import ModelInstance from "./ModelInstance.js";
  * @alias ModelInstanceCollection
  * @constructor
  *
- * @param {object} [options] Object with the following properties:
- * @param {ModelInstance[]} [options.instances] The 4x4 transformation matrix that transforms each label from model to world coordinates.
- *
  * @see ModelInstanceCollection#add
  * @see ModelInstanceCollection#remove
  * @see ModelInstance
  *
- * @demo {@link https://sandcastle.cesium.com/index.html?src=Labels.html|Cesium Sandcastle Labels Demo}
- *
  * @example
  * // Add instance transforms to a model
- * const modelInstance = model.instances.add(transfrom);
- * const boundingSphere = modelInstance.getBoundingSphere(model);
- * viewer.camera.flyToBoundingSphere(boundingSphere);
+ * const collection = new ModelInstanceCollection();
+ * const modelInstance = new ModelInstance(transform);
+ * collection.add(modelInstance);
  */
 
-function ModelInstanceCollection(options) {
-  options = options ?? Frozen.EMPTY_OBJECT;
-  this._instances = options.instances ?? [];
+function ModelInstanceCollection() {
+  this._instances = [];
   this._dirty = false;
 }
 
 Object.defineProperties(ModelInstanceCollection.prototype, {
   /**
    * Returns the number of model instances in this collection.  This is commonly used with
-   * {@link ModelInstanceCollection#get} to iterate over all the labels
-   * in the collection.
+   * {@link ModelInstanceCollection#get} to iterate over all the instances in the collection.
    * @memberof ModelInstanceCollection.prototype
    * @type {number}
    * @readonly
@@ -52,18 +43,17 @@ Object.defineProperties(ModelInstanceCollection.prototype, {
 });
 
 /**
- * Creates and adds an instance with the specified initial properties to the collection.
- * The added label is returned so it can be modified or removed from the collection later.
+ * Creates and adds an instance with the specified transform to the collection.
+ * The added instance is returned so it can be modified or removed from the collection later.
  *
  * @param {Matrix4} transform A transform that represents an instance of a Model
  * @returns {ModelInstance} The model instance that was added to the collection.
  *
- * @performance Calling <code>add</code> is expected constant time.  However, the collection's vertex buffer
- * is rewritten; this operations is <code>O(n)</code> and also incurs
- * CPU to GPU overhead.  For best performance, add as many instances as possible before
- * calling <code>update</code>.
+ * @performance Calling <code>add</code> is expected constant time. However, the collection's vertex buffer
+ * is rewritten at the next render cycle; this operations is <code>O(n)</code> and also incurs
+ * CPU to GPU overhead.
  *
- * @exception {DeveloperError} This object was destroyed, i.e., destroy() was called.
+ * @exception {DeveloperError} No transform was provided.
  *
  * @example
  * // Example:  Provide a transform to add a model instance to the collection
@@ -75,24 +65,29 @@ Object.defineProperties(ModelInstanceCollection.prototype, {
  * @see ModelInstanceCollection#removeAll
  */
 ModelInstanceCollection.prototype.add = function (transform) {
+  //>>includeStart('debug', pragmas.debug);
+  if (!defined(transform)) {
+    throw new DeveloperError("transform is required.");
+  }
+  //>>includeEnd('debug');
   const instance = new ModelInstance(transform, this);
   this._instances.push(instance);
   return instance;
 };
 
 /**
- * Removes an instance from the collection.  Once removed, an instance is no longer usable.
+ * Removes a ModelInstance from the collection and returns it. Indices are zero-based
+ * and increase as instances are added.  Removing an instance shifts all instances after
+ * it to the left, changing their indices.
  *
  * @param {ModelInstance} instance The instance to remove.
- * @returns {boolean} <code>true</code> if the instance was removed; <code>false</code> if the instance was not found in the collection.
+ * @returns {ModelInstance} The model instance that was removed from the collection.
  *
  * @performance Calling <code>remove</code> is expected constant time.  However, the collection's vertex buffer
- * is rewritten - an <code>O(n)</code> operation that also incurs CPU to GPU overhead.  For
- * best performance, remove as many instances as possible before calling <code>update</code>.
- * TODO - If you intend to temporarily hide an instance, it is usually more efficient to call
- * {@link Label#show} instead of removing and re-adding the label.
+ * is rewritten at the next render cycle; this operations is <code>O(n)</code> and also incurs
+ * CPU to GPU overhead.
  *
- * @exception {DeveloperError} This object was destroyed, i.e., destroy() was called.
+ * @exception {DeveloperError} No instance was provided.
  *
  *
  * @example
@@ -103,25 +98,22 @@ ModelInstanceCollection.prototype.add = function (transform) {
  * @see ModelInstanceCollection#removeAll
  * @see Label#show
  */
-ModelInstanceCollection.prototype.remove = function (instance) {
-  if (defined(instance) && instance._modelInstanceCollection === this) {
-    const index = this._instances.indexOf(instance);
-    if (index !== -1) {
-      this._instances.splice(index, 1);
-      return true;
-    }
+ModelInstanceCollection.prototype.remove = function (index) {
+  //>>includeStart('debug', pragmas.debug);
+  if (!defined(index)) {
+    throw new DeveloperError("instance is required.");
   }
-  return false;
+  //>>includeEnd('debug');
+  const instance = this._instances[index];
+  this._instances.splice(index, 1);
+  return instance;
 };
 
 /**
  * Removes all instances from the collection.
  *
- * @performance <code>O(n)</code>.  It is more efficient to remove all the labels
+ * @performance <code>O(n)</code>.  It is more efficient to remove all the instances
  * from a collection and then add new ones than to create a new collection entirely.
- *
- * @exception {DeveloperError} This object was destroyed, i.e., destroy() was called.
- *
  *
  * @example
  * instances.add(...);
@@ -137,22 +129,8 @@ ModelInstanceCollection.prototype.removeAll = function () {
 };
 
 /**
- * Check whether this collection contains a given instance.
- *
- * @param {ModelInstance} instance The instance to check for.
- * @returns {boolean} true if this collection contains the instance, false otherwise.
- *
- * @see ModelInstanceCollection#get
- *
- */
-ModelInstanceCollection.prototype.contains = function (instance) {
-  return defined(instance) && instance._modelInstanceCollection === this;
-};
-
-/**
- * Returns the instance in the collection at the specified index.  Indices are zero-based
- * and increase as instances are added.  Removing an instance shifts all instances after
- * it to the left, changing their indices.  This function is commonly used with
+ * Returns the instance in the collection at the specified index.  The instance remains
+ * in the collection. This function is commonly used with
  * {@link ModelInstanceCollection#length} to iterate over all the instances
  * in the collection.
  *
@@ -160,11 +138,9 @@ ModelInstanceCollection.prototype.contains = function (instance) {
  *
  * @returns {ModelInstance} The instance at the specified index.
  *
- * @performance Expected constant time.  If instances were removed from the collection and
- * {@link Scene#render} was not called, an implicit <code>O(n)</code>
- * operation is performed.
+ * @performance Expected constant time.
  *
- * @exception {DeveloperError} This object was destroyed, i.e., destroy() was called.
+ * @exception {DeveloperError} No index provided.
  *
  *
  * @example
@@ -181,41 +157,6 @@ ModelInstanceCollection.prototype.get = function (index) {
   //>>includeEnd('debug');
 
   return this._instances[index];
-};
-
-/**
- * Returns true if this object was destroyed; otherwise, false.
- * <br /><br />
- * If this object was destroyed, it should not be used; calling any function other than
- * <code>isDestroyed</code> will result in a {@link DeveloperError} exception.
- *
- * @returns {boolean} True if this object was destroyed; otherwise, false.
- *
- * @see ModelInstanceCollection#destroy
- */
-ModelInstanceCollection.prototype.isDestroyed = function () {
-  return false;
-};
-
-/**
- * Destroys the WebGL resources held by this object.  Destroying an object allows for deterministic
- * release of WebGL resources, instead of relying on the garbage collector to destroy this object.
- * <br /><br />
- * Once an object is destroyed, it should not be used; calling any function other than
- * <code>isDestroyed</code> will result in a {@link DeveloperError} exception.  Therefore,
- * assign the return value (<code>undefined</code>) to the object as done in the example.
- *
- * @exception {DeveloperError} This object was destroyed, i.e., destroy() was called.
- *
- *
- * @example
- * instances = instances && instances.destroy();
- *
- * @see ModelInstanceCollection#isDestroyed
- */
-ModelInstanceCollection.prototype.destroy = function () {
-  this.removeAll();
-  return destroyObject(this);
 };
 
 export default ModelInstanceCollection;
