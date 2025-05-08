55d6daebaef5a2317aa2ee686496b709a4a901a8
jjspace <8007967+jjspace@users.noreply.github.com>
2025-05-08 11:57:17 -0400

    we only care about GH CI

--- scripts/isCI.js
+++ scripts/isCI.js
@@ -6,16 +6,7 @@ const { env } = process;
 
 const isCI = !!(
   env.CI !== "false" && // Bypass all checks if CI env is explicitly set to 'false'
-  (env.BUILD_ID || // Jenkins, Cloudbees
-    env.BUILD_NUMBER || // Jenkins, TeamCity
-    env.CI || // Travis CI, CircleCI, Cirrus CI, Gitlab CI, Appveyor, CodeShip, dsari, Cloudflare Pages
-    env.CI_APP_ID || // Appflow
-    env.CI_BUILD_ID || // Appflow
-    env.CI_BUILD_NUMBER || // Appflow
-    env.CI_NAME || // Codeship and others
-    env.CONTINUOUS_INTEGRATION || // Travis CI, Cirrus CI
-    env.RUN_ID || // TaskCluster, dsari
-    false)
+  env.CI // GitHub CI
 );
 
 process.exit(isCI ? 0 : 1);
