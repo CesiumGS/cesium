cbb7cd3fde2b3f5a33a181bedaa465d90856715b
jjspace <8007967+jjspace@users.noreply.github.com>
2025-05-21 17:04:01 -0400

    pr comments

--- packages/sandcastle/src/Helpers.ts
+++ packages/sandcastle/src/Helpers.ts
@@ -1,7 +1,6 @@
 import Pako from "pako";
 
 export function embedInSandcastleTemplate(code: string, addExtraLine: boolean) {
-  console.log("embedSandcastle");
   return `window.startup = async function (Cesium, Sandcastle) {
   'use strict';
   //Sandcastle_Begin
@@ -29,19 +28,15 @@ export function makeCompressedBase64String(data: SandcastleSaveData) {
   // data stored in the hash as:
   // Base64 encoded, raw DEFLATE compressed JSON array where index 0 is code, index 1 is html
   const { code, html, baseHref } = data;
-  const encode = baseHref !== undefined ? [code, html, baseHref] : [code, html];
+  const encode = baseHref ? [code, html, baseHref] : [code, html];
   let jsonString = JSON.stringify(encode);
+
   // we save a few bytes by omitting the leading [" and trailing "] since they are always the same
   jsonString = jsonString.slice(2, 2 + jsonString.length - 4);
-  const pakoString = Pako.deflate(jsonString, {
-    raw: true,
-    level: 9,
-  });
-  let base64String = btoa(
-    // TODO: not 100% sure why I have to do this conversion manually anymore but it works
-    // https://stackoverflow.com/questions/12710001/how-to-convert-uint8-array-to-base64-encoded-string
-    String.fromCharCode(...new Uint8Array(pakoString)),
-  );
+  const pakoData = Pako.deflate(jsonString, { raw: true, level: 9 });
+
+  // https://stackoverflow.com/questions/12710001/how-to-convert-uint8-array-to-base64-encoded-string
+  let base64String = btoa(String.fromCharCode(...pakoData));
   base64String = base64String.replace(/=+$/, ""); // remove padding
 
   return base64String;
@@ -62,13 +57,13 @@ export function decodeBase64Data(base64String: string): SandcastleSaveData {
         return c.charCodeAt(0);
       }),
   );
-  let jsonString = Pako.inflate(dataArray, {
-    raw: true,
-    to: "string",
-  });
+
+  let jsonString = Pako.inflate(dataArray, { raw: true, to: "string" });
+
   // we save a few bytes by omitting the leading [" and trailing "] since they are always the same
   jsonString = `["${jsonString}"]`;
   const json = JSON.parse(jsonString);
+
   // index 0 is code, index 1 is html
   const code = json[0];
   const html = json[1];
