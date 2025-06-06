ea405ac7971b270cbb0fb9dd098df2ef3c6b5335
keyboardspecialist <jason.sobotka@cesium.com>
2025-06-06 15:18:29 -0500

    docs

--- packages/engine/Source/Scene/GaussianSplatSorter.js
+++ packages/engine/Source/Scene/GaussianSplatSorter.js
@@ -3,6 +3,11 @@ import FeatureDetection from "../Core/FeatureDetection.js";
 import RuntimeError from "../Core/RuntimeError.js";
 import TaskProcessor from "../Core/TaskProcessor.js";
 
+/** * A sorter for Gaussian splats that uses a task processor to handle sorting in parallel.
+ * This class is responsible for initializing the task processor and scheduling sorting tasks.
+ * * @constructor
+ * @private
+ */
 function GaussianSplatSorter() {}
 
 GaussianSplatSorter._maxSortingConcurrency = Math.max(
@@ -40,7 +45,16 @@ GaussianSplatSorter._getSorterTaskProcessor = function () {
 
   return GaussianSplatSorter._sorterTaskProcessor;
 };
-
+/**
+ * Sorts Gaussian splats using a radix sort algorithm. Sorted by distance from the camera.
+ * A new list of indexes is returned, which can be used to render the splats in the correct order.
+ *
+ * @param {Object} parameters - The parameters for sorting Gaussian splat indexes.
+ * @param {Object} parameters.primitive - The primitive containing positions and modelView matrices.
+ * @returns {Promise|undefined} A promise that resolves to the sorted indexes or undefined if the task cannot be scheduled.
+ * @exception {RuntimeError} Sorter could not be initialized.
+ * @private
+ */
 GaussianSplatSorter.radixSortIndexes = function (parameters) {
   const sorterTaskProcessor = GaussianSplatSorter._getSorterTaskProcessor();
   if (defined(GaussianSplatSorter._error)) {
