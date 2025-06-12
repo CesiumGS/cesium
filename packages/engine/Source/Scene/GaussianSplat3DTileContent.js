52b3d497649219d945e38a8c5baaba0163a834de
keyboardspecialist <jason.sobotka@cesium.com>
2025-06-12 14:44:26 -0500

    doc fixes and additions

--- packages/engine/Source/Scene/GaussianSplat3DTileContent.js
+++ packages/engine/Source/Scene/GaussianSplat3DTileContent.js
@@ -30,19 +30,29 @@ function GaussianSplat3DTileContent(loader, tileset, tile, resource) {
 
   /**
    * glTF primitive data that contains the Gaussian splat data needed for rendering.
+   * @type {undefined|Primitive}
    * @private
    */
   this.splatPrimitive = undefined;
 
   /**
    * Transform matrix to turn model coordinates into world coordinates.
-   * @type {Matrix4}
+   * @type {undefined|Matrix4}
    * @private
    */
   this.worldTransform = undefined;
 
   /**
-   * Part of the {@link Cesium3DTileContent} interface.
+   * Gets or sets if any feature's property changed.  Used to
+   * optimized applying a style when a feature's property changed.
+   * <p>
+   * This is used to implement the <code>Cesium3DTileContent</code> interface, but is
+   * not part of the public Cesium API.
+   * </p>
+   *
+   * @type {boolean}
+   *
+   * @private
    */
   this.featurePropertiesDirty = false;
 
@@ -58,18 +68,42 @@ function GaussianSplat3DTileContent(loader, tileset, tile, resource) {
 }
 
 Object.defineProperties(GaussianSplat3DTileContent.prototype, {
+  /**
+   * Gets the number of features in the tile.
+   *
+   * @memberof GaussianSplat3DTileContent.prototype
+   *
+   * @type {number}
+   * @readonly
+   */
   featuresLength: {
     get: function () {
       return 0;
     },
   },
 
+  /**
+   * Equal to the number of Gaussian splats in the tile. Each splat is represented by a median point and a set of attributes, so we can
+   * treat this as the number of points in the tile.
+   *
+   * @memberof GaussianSplat3DTileContent.prototype
+   *
+   * @type {number}
+   * @readonly
+   */
   pointsLength: {
     get: function () {
       return this.splatPrimitive.attributes[0].count;
     },
   },
-
+  /**
+   * Gets the number of triangles in the tile.
+   *
+   * @memberof Cesium3DTileContent.prototype
+   *
+   * @type {number}
+   * @readonly
+   */
   trianglesLength: {
     get: function () {
       return 0;
@@ -105,14 +139,32 @@ Object.defineProperties(GaussianSplat3DTileContent.prototype, {
       return texture.sizeInBytes / selectedTileLength;
     },
   },
-
+  /**
+   * Gets the amount of memory used by the batch table textures and any binary
+   * metadata properties not accounted for in geometryByteLength or
+   * texturesByteLength
+   *
+   * @memberof Cesium3DTileContent.prototype
+   *
+   * @type {number}
+   * @readonly
+   */
   batchTableByteLength: {
     // eslint-disable-next-line getter-return
     get: function () {
       return 0;
     },
   },
-
+  /**
+   * Gets the array of {@link Cesium3DTileContent} objects for contents that contain other contents, such as composite tiles. The inner contents may in turn have inner contents, such as a composite tile that contains a composite tile.
+   *
+   * @see {@link https://github.com/CesiumGS/3d-tiles/tree/main/specification/TileFormats/Composite|Composite specification}
+   *
+   * @memberof Cesium3DTileContent.prototype
+   *
+   * @type {Array}
+   * @readonly
+   */
   innerContents: {
     get: function () {
       return undefined;
@@ -179,13 +231,36 @@ Object.defineProperties(GaussianSplat3DTileContent.prototype, {
       return this._resource.getUrlComponent(true);
     },
   },
-
+  /**
+   * Gets the batch table for this content.
+   * <p>
+   * This is used to implement the <code>Cesium3DTileContent</code> interface, but is
+   * not part of the public Cesium API.
+   * </p>
+   *
+   * @type {Cesium3DTileBatchTable}
+   * @readonly
+   *
+   * @private
+   */
   batchTable: {
     get: function () {
       return undefined;
     },
   },
-
+  /**
+   * Gets the metadata for this content, whether it is available explicitly or via
+   * implicit tiling. If there is no metadata, this property should be undefined.
+   * <p>
+   * This is used to implement the <code>Cesium3DTileContent</code> interface, but is
+   * not part of the public Cesium API.
+   * </p>
+   *
+   * @type {ImplicitMetadataView|undefined}
+   *
+   * @private
+   * @experimental This feature is using part of the 3D Tiles spec that is not final and is subject to change without Cesium's standard deprecation policy.
+   */
   metadata: {
     get: function () {
       return this._metadata;
@@ -194,7 +269,20 @@ Object.defineProperties(GaussianSplat3DTileContent.prototype, {
       this._metadata = value;
     },
   },
-
+  /**
+   * Gets the group for this content if the content has metadata (3D Tiles 1.1) or
+   * if it uses the <code>3DTILES_metadata</code> extension. If neither are present,
+   * this property should be undefined.
+   * <p>
+   * This is used to implement the <code>Cesium3DTileContent</code> interface, but is
+   * not part of the public Cesium API.
+   * </p>
+   *
+   * @type {Cesium3DTileContentGroup|undefined}
+   *
+   * @private
+   * @experimental This feature is using part of the 3D Tiles spec that is not final and is subject to change without Cesium's standard deprecation policy.
+   */
   group: {
     get: function () {
       return this._group;
@@ -294,28 +382,94 @@ GaussianSplat3DTileContent.prototype.update = function (primitive, frameState) {
 
   this._resourcesLoaded = loader.process(frameState);
 };
-
+/**
+ * Returns whether the feature has this property.
+ *
+ * @param {number} batchId The batchId for the feature.
+ * @param {string} name The case-sensitive name of the property.
+ * @returns {boolean} <code>true</code> if the feature has this property; otherwise, <code>false</code>.
+ */
 GaussianSplat3DTileContent.prototype.hasProperty = function (batchId, name) {
   return false;
 };
 
+/**
+ * Returns the {@link Cesium3DTileFeature} object for the feature with the
+ * given <code>batchId</code>.  This object is used to get and modify the
+ * feature's properties.
+ * <p>
+ * Features in a tile are ordered by <code>batchId</code>, an index used to retrieve their metadata from the batch table.
+ * </p>
+ *
+ * @see {@link https://github.com/CesiumGS/3d-tiles/tree/main/specification/TileFormats/BatchTable}.
+ *
+ * @param {number} batchId The batchId for the feature.
+ * @returns {Cesium3DTileFeature} The corresponding {@link Cesium3DTileFeature} object.
+ *
+ * @exception {DeveloperError} batchId must be between zero and {@link Cesium3DTileContent#featuresLength} - 1.
+ */
 GaussianSplat3DTileContent.prototype.getFeature = function (batchId) {
   return undefined;
 };
+/**
+     * Called when {@link Cesium3DTileset#debugColorizeTiles} changes.
+     * <p>
+     * This is used to implement the <code>Cesium3DTileContent</code> interface, but is
+     * not part of the public Cesium API.
+     * </p>
+     *
+     * @param {boolean} enabled Whether to enable or disable debug settings.
+     * @returns {Cesium3DTileFeature} The corresponding {@link Cesium3DTileFeature} object.
 
+     * @private
+     */
 GaussianSplat3DTileContent.prototype.applyDebugSettings = function (
   enabled,
   color,
 ) {};
-
+/**
+ * Apply a style to the content
+ * <p>
+ * This is used to implement the <code>Cesium3DTileContent</code> interface, but is
+ * not part of the public Cesium API.
+ * </p>
+ *
+ * @param {Cesium3DTileStyle} style The style.
+ *
+ * @private
+ */
 GaussianSplat3DTileContent.prototype.applyStyle = function (style) {};
-
+/**
+ * Find an intersection between a ray and the tile content surface that was rendered. The ray must be given in world coordinates.
+ *
+ * @param {Ray} ray The ray to test for intersection.
+ * @param {FrameState} frameState The frame state.
+ * @param {Cartesian3|undefined} [result] The intersection or <code>undefined</code> if none was found.
+ * @returns {Cartesian3|undefined} The intersection or <code>undefined</code> if none was found.
+ *
+ * @private
+ */
 GaussianSplat3DTileContent.prototype.pick = function (ray, frameState, result) {
   return undefined;
 };
-
+/**
+ * Returns true if this object was destroyed; otherwise, false.
+ * <br /><br />
+ * If this object was destroyed, it should not be used; calling any function other than
+ * <code>isDestroyed</code> will result in a {@link DeveloperError} exception.
+ * <p>
+ * This is used to implement the <code>Cesium3DTileContent</code> interface, but is
+ * not part of the public Cesium API.
+ * </p>
+ *
+ * @returns {boolean} <code>true</code> if this object was destroyed; otherwise, <code>false</code>.
+ *
+ * @see Cesium3DTileContent#destroy
+ *
+ * @private
+ */
 GaussianSplat3DTileContent.prototype.isDestroyed = function () {
-  return false;
+  return this.isDestroyed;
 };
 
 /**
