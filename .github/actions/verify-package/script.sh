bf07b3c1399cfb9308717e3d4be1712d8bbe04e6
jjspace <8007967+jjspace@users.noreply.github.com>
2025-07-11 17:31:59 -0400

    allow deploy to finish

--- .github/actions/verify-package/script.sh
+++ .github/actions/verify-package/script.sh
@@ -15,7 +15,11 @@ cp cesium*.tgz ../test
 cp Specs/test.*js ../test
 cd ../test
 
-npm install cesium*.tgz
+npm install cesium-1.*.tgz
+npm install cesium-engine*.tgz
+npm install cesium-widgets*.tgz
+# TODO: figure out why installing sandcastle hangs infinitely
+# npm install cesium-sandcastle*.tgz
 NODE_ENV=development node test.cjs
 NODE_ENV=production node test.cjs
 node test.mjs
