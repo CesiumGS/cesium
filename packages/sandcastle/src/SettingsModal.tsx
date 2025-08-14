8992acfee99e705a83e0afe8d869631669092c0a
jjspace <8007967+jjspace@users.noreply.github.com>
2025-08-14 13:39:39 -0400

    define styles for dialog boxes and update settings modal

--- packages/sandcastle/src/SettingsModal.tsx
+++ packages/sandcastle/src/SettingsModal.tsx
@@ -1,12 +1,19 @@
-import { Dialog, DialogDismiss, DialogHeading } from "@ariakit/react";
-import "./SettingsModal.css";
+import { DialogDismiss } from "@ariakit/react";
 import { useContext } from "react";
 import {
   AvailableFontId,
   availableFonts,
   SettingsContext,
 } from "./SettingsContext";
-import { Button, Field, Select, Switch } from "@stratakit/bricks";
+import { Button, Field, IconButton, Select, Switch } from "@stratakit/bricks";
+import {
+  SandcastleDialog,
+  SandcastleDialogFooter,
+  SandcastleDialogHeading,
+} from "./SandcastleDialog";
+import "./SettingsModal.css";
+import InfoBadge from "./InfoBadge";
+import { moon, sun } from "./icons";
 
 export function SettingsModal({
   open,
@@ -20,64 +27,80 @@ export function SettingsModal({
   const selectedFont = availableFonts[settings.fontFamily];
 
   return (
-    <Dialog open={open} onClose={() => setOpen(false)} className="dialog">
-      <DialogHeading className="heading">Settings</DialogHeading>
-      <p>Theme: {settings.theme}</p>
-      <Button
-        onClick={() => {
-          if (settings.theme === "dark") {
-            updateSettings({ theme: "light" });
-          } else {
-            updateSettings({ theme: "dark" });
-          }
-        }}
-      >
-        Change Theme
-      </Button>
-      <p>Font size: {settings.fontSize}</p>
-      <Button
-        onClick={() => {
-          if (settings.fontSize === "large") {
-            updateSettings({ fontSize: "small" });
-          } else {
-            updateSettings({ fontSize: "large" });
-          }
-        }}
-      >
-        Change Font Size
-      </Button>
-      <Select.Root>
-        <Select.HtmlSelect
-          className="tag-select"
-          value={settings.fontFamily}
-          onChange={(e) => {
-            updateSettings({ fontFamily: e.target.value as AvailableFontId });
-          }}
-        >
-          {Object.entries(availableFonts).map(([id, family]) => (
-            <option value={id} key={id}>
-              {family.readableName}
-            </option>
-          ))}
-        </Select.HtmlSelect>
-      </Select.Root>
-      <Field.Root>
-        <Field.Control
-          render={
-            <Switch
-              defaultChecked={settings.fontLigatures}
-              onChange={(e) => {
-                updateSettings({ fontLigatures: e.target.checked });
-              }}
-              disabled={!selectedFont.supportsLigatures}
-            />
-          }
-        />
-        <Field.Label>Use Font Ligatures</Field.Label>
-      </Field.Root>
-      <div>
-        <DialogDismiss className="button">OK</DialogDismiss>
+    <SandcastleDialog open={open} onClose={() => setOpen(false)}>
+      <SandcastleDialogHeading>Settings</SandcastleDialogHeading>
+      <div className="settings-row">
+        <div>Theme</div>
+        <div className="multi-option horizontal">
+          <IconButton
+            icon={sun}
+            // @ts-expect-error tone works but is not passed through the types from Button
+            tone={settings.theme === "light" ? "accent" : "neutral"}
+            label="Light Mode"
+            onClick={() => {
+              if (settings.theme === "dark") {
+                updateSettings({ theme: "light" });
+              }
+            }}
+          />
+          <IconButton
+            icon={moon}
+            // @ts-expect-error tone works but is not passed through the types from Button
+            tone={settings.theme === "dark" ? "accent" : "neutral"}
+            label="Dark Mode"
+            onClick={() => {
+              if (settings.theme === "light") {
+                updateSettings({ theme: "dark" });
+              }
+            }}
+          />
+        </div>
       </div>
-    </Dialog>
+      <div className="settings-row">
+        <div>Editor Font</div>
+        <div className="multi-option">
+          <Select.Root>
+            <Select.HtmlSelect
+              className="tag-select"
+              value={settings.fontFamily}
+              onChange={(e) => {
+                updateSettings({
+                  fontFamily: e.target.value as AvailableFontId,
+                });
+              }}
+            >
+              {Object.entries(availableFonts).map(([id, family]) => (
+                <option value={id} key={id}>
+                  {family.readableName}
+                </option>
+              ))}
+            </Select.HtmlSelect>
+          </Select.Root>
+          <Field.Root>
+            <Field.Control
+              render={
+                <Switch
+                  defaultChecked={settings.fontLigatures}
+                  onChange={(e) => {
+                    updateSettings({ fontLigatures: e.target.checked });
+                  }}
+                  disabled={!selectedFont.supportsLigatures}
+                />
+              }
+            />
+            <Field.Label>
+              Use Font Ligatures
+              <InfoBadge
+                content="Not all fonts support ligatures"
+                placement="bottom"
+              />
+            </Field.Label>
+          </Field.Root>
+        </div>
+      </div>
+      <SandcastleDialogFooter>
+        <DialogDismiss render={<Button>Done</Button>}></DialogDismiss>
+      </SandcastleDialogFooter>
+    </SandcastleDialog>
   );
 }
