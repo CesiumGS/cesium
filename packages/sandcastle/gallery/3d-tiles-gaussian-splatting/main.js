8ca1824be4cb70f0729322f5226bf7812966d285
jjspace <8007967+jjspace@users.noreply.github.com>
2025-09-02 19:10:55 -0400

    add missing sandcastles and update gaussian splat sandcastle

--- packages/sandcastle/gallery/3d-tiles-gaussian-splatting/main.js
+++ packages/sandcastle/gallery/3d-tiles-gaussian-splatting/main.js
@@ -1,64 +1,18 @@
 import * as Cesium from "cesium";
 
-const viewer = new Cesium.Viewer("cesiumContainer", {
-  terrain: Cesium.Terrain.fromWorldTerrain(),
-});
+const viewer = new Cesium.Viewer("cesiumContainer");
 
 try {
-  const left = await Cesium.Cesium3DTileset.fromIonAssetId(3443920);
-  viewer.scene.primitives.add(left);
-  left.splitDirection = Cesium.SplitDirection.LEFT;
-
+  const tileset = await Cesium.Cesium3DTileset.fromIonAssetId(3667783);
+  viewer.scene.primitives.add(tileset);
   viewer.zoomTo(
-    left,
+    tileset,
     new Cesium.HeadingPitchRange(
-      Cesium.Math.toRadians(-50),
-      Cesium.Math.toRadians(-20),
-      100.0,
+      Cesium.Math.toRadians(0.0),
+      Cesium.Math.toRadians(-15.0),
+      200.0,
     ),
   );
-
-  const right = await Cesium.Cesium3DTileset.fromIonAssetId(3443919);
-  viewer.scene.primitives.add(right);
-  right.splitDirection = Cesium.SplitDirection.RIGHT;
 } catch (error) {
   console.log(`Error loading tileset: ${error}`);
 }
-
-// Sync the position of the slider with the split position
-const slider = document.getElementById("slider");
-viewer.scene.splitPosition =
-  slider.offsetLeft / slider.parentElement.offsetWidth;
-
-const handler = new Cesium.ScreenSpaceEventHandler(slider);
-
-let moveActive = false;
-
-function move(movement) {
-  if (!moveActive) {
-    return;
-  }
-
-  const relativeOffset = movement.endPosition.x;
-  const splitPosition =
-    (slider.offsetLeft + relativeOffset) / slider.parentElement.offsetWidth;
-  slider.style.left = `${100.0 * splitPosition}%`;
-  viewer.scene.splitPosition = splitPosition;
-}
-
-handler.setInputAction(function () {
-  moveActive = true;
-}, Cesium.ScreenSpaceEventType.LEFT_DOWN);
-handler.setInputAction(function () {
-  moveActive = true;
-}, Cesium.ScreenSpaceEventType.PINCH_START);
-
-handler.setInputAction(move, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
-handler.setInputAction(move, Cesium.ScreenSpaceEventType.PINCH_MOVE);
-
-handler.setInputAction(function () {
-  moveActive = false;
-}, Cesium.ScreenSpaceEventType.LEFT_UP);
-handler.setInputAction(function () {
-  moveActive = false;
-}, Cesium.ScreenSpaceEventType.PINCH_END);
