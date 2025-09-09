2853e6ddef8c13d3959dd2b2b4f60c3777cb8901
lukemckinstry <lukemckinstry@gmail.com>
2025-09-09 14:29:15 -0400

    google 2d updates

--- packages/engine/Source/Scene/Google2DImageryProvider.js
+++ packages/engine/Source/Scene/Google2DImageryProvider.js
@@ -11,31 +11,6 @@ const defaultCredit = new Credit(
 );
 
 /**
- * @typedef {object} Google2DImageryProvider.ConstructorOptions
- *
- * Initialization options for the Google2DImageryProvider constructor.
- * Google requires a session token to use the 2D Tiles api. You can fetch your own session token and provide it to the constructor, or you can
- * provide mapType, language and region and the imagery provider class will create your session token
- *
- * @property {Resource|string} [url='https://tile.googleapis.com/v1/2dtiles/'] The Google server url.
- * @property {string} apiKey the Google api key
- * @property {string} sessionToken The Google session token that tracks the current state of your map and viewport.
- * @property {string} mapType the type of basemap, accepted values are roadmap, satellite, terrain, & imagery
- * @property {string} [language='en_US'] an IETF language tag that specifies the language used to display information on the tiles
- * @property {string} [region='US'] A Common Locale Data Repository region identifier (two uppercase letters) that represents the physical location of the user.
- * @property {string} [imageFormat] The file format to return. Valid values are either jpeg or png. If you don't specify an imageFormat, then the best format for the tile is chosen automatically by the Google tile service.
- * @property {number} [tilesize=512] The size of the image tiles.
- * @property {boolean} [scaleFactor] Determines if tiles are rendered at a @2x scale factor.
- * @property {Ellipsoid} [ellipsoid=Ellipsoid.default] The ellipsoid.  If not specified, the default ellipsoid is used.
- * @property {number} [minimumLevel=0] The minimum level-of-detail supported by the imagery provider.  Take care when specifying
- *                 this that the number of tiles at the minimum level is small, such as four or less.  A larger number is likely
- *                 to result in rendering problems.
- * @property {number} [maximumLevel] The maximum level-of-detail supported by the imagery provider, or undefined if there is no limit.
- * @property {Rectangle} [rectangle=Rectangle.MAX_VALUE] The rectangle, in radians, covered by the image.
- * @property {Credit|string} [credit] A credit for the data source, which is displayed on the canvas.
- */
-
-/**
  * <div class="notice">
  * This object is normally not instantiated directly, use {@link Google2DImageryProvider.fromMapType} or {@link Google2DImageryProvider.fromSessionToken}.
  * </div>
@@ -74,7 +49,6 @@ function Google2DImageryProvider(options) {
   this._sessionToken = options.sessionToken;
   this._tileWidth = options.tileWidth;
   this._tileHeight = options.tileHeight;
-  this._imageFormat = options.imageFormat;
 }
 
 Object.defineProperties(Google2DImageryProvider.prototype, {
@@ -238,42 +212,29 @@ Object.defineProperties(Google2DImageryProvider.prototype, {
 
 /**
  * Creates an {@link ImageryProvider} which provides 2D global tiled imagery from Google.
- * @param {Google2DImageryMapType} mapType The map type of the Google map imagery. Valid options are {@link ArcGisBaseMapType.SATELLITE}, {@link ArcGisBaseMapType.OCEANS}, and {@link ArcGisBaseMapType.HILLSHADE}.
  * @param {object} options Object with the following properties:
- * @property {string} apiKey the Google api key
- * @property {string} sessionToken The Google session token that tracks the current state of your map and viewport.
- * @property {number} [tilesize=512] The size of the image tiles.
- * @property {boolean} [scaleFactor] Determines if tiles are rendered at a @2x scale factor.
- * @property {Ellipsoid} [ellipsoid=Ellipsoid.default] The ellipsoid.  If not specified, the default ellipsoid is used.
- * @property {number} [minimumLevel=0] The minimum level-of-detail supported by the imagery provider.  Take care when specifying
+ * @param {Google2DImageryMapType} options.mapType The map type of the Google map imagery. Valid options are {@link ArcGisBaseMapType.SATELLITE}, {@link ArcGisBaseMapType.OCEANS}, and {@link ArcGisBaseMapType.HILLSHADE}.
+ * @param {string} options.apiKey the Google api key
+ * @param {string} [options.language='en_US'] an IETF language tag that specifies the language used to display information on the tiles
+ * @param {string} [options.region='US'] A Common Locale Data Repository region identifier (two uppercase letters) that represents the physical location of the user.
+ * @param {string} [options.imageFormat] The file format to return. Valid values are either jpeg or png. If you don't specify an imageFormat, then the best format for the tile is chosen automatically by the Google tile service.
+ * @param {Ellipsoid} [options.ellipsoid=Ellipsoid.default] The ellipsoid.  If not specified, the default ellipsoid is used.
+ * @param {number} [options.minimumLevel=0] The minimum level-of-detail supported by the imagery provider.  Take care when specifying
  *                 this that the number of tiles at the minimum level is small, such as four or less.  A larger number is likely
  *                 to result in rendering problems.
- * @property {number} [maximumLevel] The maximum level-of-detail supported by the imagery provider, or undefined if there is no limit.
- * @property {Rectangle} [rectangle=Rectangle.MAX_VALUE] The rectangle, in radians, covered by the image.
- * @property {Credit|string} [credit] A credit for the data source, which is displayed on the canvas.
-
-
-* @param {Google2DImageryProvider.ConstructorOptions} [options] Object describing initialization options.
+ * @param {number} [options.maximumLevel] The maximum level-of-detail supported by the imagery provider, or undefined if there is no limit.
+ * @param {Rectangle} [options.rectangle=Rectangle.MAX_VALUE] The rectangle, in radians, covered by the image.
+ * @param {Credit|string} [credit] A credit for the data source, which is displayed on the canvas.
+ *
  * @returns {Promise<Google2DImageryProvider>} A promise that resolves to the created Google2DImageryProvider.
  *
  * @example
- * // Set the default access token for accessing ArcGIS Image Tile service
- * Cesium.ArcGisMapService.defaultAccessToken = "<ArcGIS Access Token>";
- *
- * // Add a base layer from a default ArcGIS basemap
- * const provider = await Cesium.ArcGisMapServerImageryProvider.fromBasemapType(
- *   Cesium.ArcGisBaseMapType.SATELLITE);
- *
- * @example
- * // Add a base layer from a default ArcGIS Basemap
- * const viewer = new Cesium.Viewer("cesiumContainer", {
- *   baseLayer: Cesium.ImageryLayer.fromProviderAsync(
- *     Cesium.Google2DImageryProvider.fromMapType(
- *       Cesium.Google2DMapType.HILLSHADE, {
- *         token: "<Google 2D tiles Access Token>"
- *       }
- *     )
- *   ),
+ * // Google 2D imagery provider
+ * const googleTilesProvider = Cesium.Google2DImageryProvider.fromMapType({
+ *     apiKey: 'thisIsMyApiKey',
+ *     mapType: "SATELLITE",
+ *     language: "en_US",
+ *     region: "US"
  * });
  */
 
@@ -308,28 +269,34 @@ Google2DImageryProvider.fromMapType = async function (options) {
     apiKey: options.apiKey,
     tileWidth: sessionJson.tileWidth,
     tileHeight: sessionJson.tileHeight,
-    imageFormat: sessionJson.imageFormat,
     ...options,
   });
 };
 
 /**
- * Creates an {@link ImageryProvider} which provides tiled imagery hosted by an ArcGIS MapServer.  By default, the server's pre-cached tiles are
- * used, if available.
+ * Creates an {@link ImageryProvider} which provides 2D global tiled imagery from Google.
  *
  * @param {object} options Object with the following properties:
- * @property {string} mapType the type of basemap, accepted values are roadmap, satellite, terrain, & imagery
- * @property {string} [language='en_US'] an IETF language tag that specifies the language used to display information on the tiles
- * @param {ArcGisMapServerImageryProvider.ConstructorOptions} [options] Object describing initialization options.
- * @returns {Promise<ArcGisMapServerImageryProvider>} A promise that resolves to the created ArcGisMapServerImageryProvider.
+ * @param {string} options.apiKey the Google api key
+ * @param {string} options.sessionToken The Google session token that tracks the current state of your map and viewport.
+ * @param {string} options.tileWidth The width of each tile in pixels.
+ * @param {string} options.tileHeight The height of each tile in pixels.
+ * @param {Ellipsoid} [options.ellipsoid=Ellipsoid.default] The ellipsoid.  If not specified, the default ellipsoid is used.
+ * @param {number} [options.minimumLevel=0] The minimum level-of-detail supported by the imagery provider.  Take care when specifying
+ *                 this that the number of tiles at the minimum level is small, such as four or less.  A larger number is likely
+ *                 to result in rendering problems.
+ * @param {number} [options.maximumLevel] The maximum level-of-detail supported by the imagery provider, or undefined if there is no limit.
+ * @param {Rectangle} [options.rectangle=Rectangle.MAX_VALUE] The rectangle, in radians, covered by the image.
+ *
+ * @returns {Promise<Google2DImageryProvider>} A promise that resolves to the created Google2DImageryProvider.
  *
  * @example
- * const esri = await Cesium.ArcGisMapServerImageryProvider.fromUrl(
- *     "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer"
- * );
+ * // Google 2D imagery provider
+ * const googleTilesProvider = Cesium.Google2DImageryProvider.fromSessionToken({
+ *     apiKey: 'thisIsMyApiKey',
+ *     sessionToken: 'thisIsSessionToken'
+ * });
  *
- * @exception {RuntimeError} metadata spatial reference specifies an unknown WKID
- * @exception {RuntimeError} metadata fullExtent.spatialReference specifies an unknown WKID
  */
 
 Google2DImageryProvider.fromSessionToken = function (options) {
@@ -348,12 +315,6 @@ Google2DImageryProvider.fromSessionToken = function (options) {
   //>>includeStart('debug', pragmas.debug);
   if (!defined(options.tileHeight)) {
     throw new DeveloperError("options.tileHeight is required.");
-  }
-  //>>includeEnd('debug');
-
-  //>>includeStart('debug', pragmas.debug);
-  if (!defined(options.imageFormat)) {
-    throw new DeveloperError("options.imageFormat is required.");
   }
   //>>includeEnd('debug');
 
@@ -394,8 +355,8 @@ Google2DImageryProvider.fromSessionToken = function (options) {
   const provider = new UrlTemplateImageryProvider({
     url: resource,
     credit: credit,
-    tileWidth: this._tileWidth,
-    tileHeight: this._tileHeight,
+    tileWidth: options.tileWidth,
+    tileHeight: options.tileHeight,
     maximumLevel: 22,
     ellipsoid: options.ellipsoid,
     rectangle: options.rectangle,
@@ -439,19 +400,15 @@ Google2DImageryProvider.prototype.requestImage = function (
 };
 
 /**
- * Asynchronously determines what features, if any, are located at a given longitude and latitude within
- * a tile. This function is optional, so it may not exist on all ImageryProviders.
- *
+ * Picking features is not currently supported by this imagery provider, so this function simply returns
+ * undefined.
  *
  * @param {number} x The tile X coordinate.
  * @param {number} y The tile Y coordinate.
  * @param {number} level The tile level.
  * @param {number} longitude The longitude at which to pick features.
  * @param {number} latitude  The latitude at which to pick features.
- * @return {Promise<ImageryLayerFeatureInfo[]>|undefined} A promise for the picked features that will resolve when the asynchronous
- *                   picking completes.  The resolved value is an array of {@link ImageryLayerFeatureInfo}
- *                   instances.  The array may be empty if no features are found at the given location.
- *                   It may also be undefined if picking is not supported.
+ * @return {undefined} Undefined since picking is not supported.
  */
 Google2DImageryProvider.prototype.pickFeatures = function (
   x,
@@ -460,7 +417,7 @@ Google2DImageryProvider.prototype.pickFeatures = function (
   longitude,
   latitude,
 ) {
-  return this._imageryProvider.pickFeatures(x, y, level, longitude, latitude);
+  return undefined;
 };
 
 async function createGoogleImagerySession(options) {
