dcc4ec107b14adc2d9c4e31dd02f2bb72a137548
jjspace <8007967+jjspace@users.noreply.github.com>
2025-09-23 11:16:14 -0400

    fix ci build double //

--- packages/sandcastle/vite.config.app.ts
+++ packages/sandcastle/vite.config.app.ts
@@ -6,25 +6,25 @@ import { dirname, join } from "path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Use this when the static files are hosted at a "nested" URL like in CI
-const pathPrefix = (path: string) => `${process.env.BASE_URL ?? ""}${path}`;
+const pathPrefix = (path: string) => `${process.env.BASE_URL ?? "/"}${path}`;
 
 const newConfig = createSandcastleConfig({
   outDir: join(__dirname, "../../Apps/Sandcastle2"),
-  viteBase: pathPrefix("/Apps/Sandcastle2"),
-  cesiumBaseUrl: pathPrefix("/Build/CesiumUnminified"),
+  viteBase: pathPrefix("Apps/Sandcastle2"),
+  cesiumBaseUrl: pathPrefix("Build/CesiumUnminified"),
   commitSha: JSON.stringify(process.env.GITHUB_SHA ?? undefined),
   imports: {
     cesium: {
-      path: pathPrefix("/Source/Cesium.js"),
-      typesPath: pathPrefix("/Source/Cesium.d.ts"),
+      path: pathPrefix("Source/Cesium.js"),
+      typesPath: pathPrefix("Source/Cesium.d.ts"),
     },
     "@cesium/engine": {
-      path: pathPrefix("/packages/engine/Build/Unminified/index.js"),
-      typesPath: pathPrefix("/packages/engine/index.d.ts"),
+      path: pathPrefix("packages/engine/Build/Unminified/index.js"),
+      typesPath: pathPrefix("packages/engine/index.d.ts"),
     },
     "@cesium/widgets": {
-      path: pathPrefix("/packages/widgets/Build/Unminified/index.js"),
-      typesPath: pathPrefix("/packages/widgets/index.d.ts"),
+      path: pathPrefix("packages/widgets/Build/Unminified/index.js"),
+      typesPath: pathPrefix("packages/widgets/index.d.ts"),
     },
   },
 });
