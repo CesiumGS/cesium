1b6cb6e621c13133825f6b6db84ca2596582c495
lukemckinstry <lukemckinstry@gmail.com>
2025-12-11 14:38:23 -0500

    initial panorama classes

---
+++ packages/engine/Source/Scene/CubeMapPanorama.js
@@ -0,0 +1,115 @@
+import DeveloperError from "../Core/DeveloperError.js";
+import SkyBox from "./SkyBox.js";
+
+/**
+ * A panorama defined by 6 images composing the faces of a cube map. The sky box is defined using the True Equator Mean Equinox (TEME) axes.
+ * <p>
+ * This is only supported in 3D.  The sky box is faded out when morphing to 2D or Columbus view.  The size of
+ * the sky box must not exceed {@link Scene#maximumCubeMapSize}.
+ * </p>
+ *
+ * @alias SkyBox
+ * @constructor
+ *
+ * @param {object} options Object with the following properties:
+ * @param {object} [options.sources] The source URL or <code>Image</code> object for each of the six cube map faces.  See the example below.
+ * @param {Credit|string} [credit] A credit for the data source, which is displayed on the canvas.
+ * @param {boolean} [options.show=true] Determines if this primitive will be shown.
+ *
+ *
+ * @example
+ * cubeMapPanorama = new Cesium.CubeMapPanorama({
+ *   sources : {
+ *     positiveX : 'skybox_px.png',
+ *     negativeX : 'skybox_nx.png',
+ *     positiveY : 'skybox_py.png',
+ *     negativeY : 'skybox_ny.png',
+ *     positiveZ : 'skybox_pz.png',
+ *     negativeZ : 'skybox_nz.png'
+ *   }
+ * });
+ * scene.skyBox = cubeMapPanorama;
+ *
+ * @see Scene#skyBox
+ * @see Transforms.computeTemeToPseudoFixedMatrix
+ */
+function CubeMapPanorama(options) {
+  /**
+   * The sources used to create the cube map panorama faces: an object
+   * with <code>positiveX</code>, <code>negativeX</code>, <code>positiveY</code>,
+   * <code>negativeY</code>, <code>positiveZ</code>, and <code>negativeZ</code> properties.
+   * These can be either URLs or <code>Image</code> objects.
+   *
+   * @type {object}
+   * @default undefined
+   */
+  this.sources = options.sources;
+  this._sources = undefined;
+
+  const skyBox = new SkyBox({
+    sources: this.sources,
+  });
+  return skyBox;
+}
+
+Object.defineProperties(CubeMapPanorama.prototype, {
+  /**
+   * Determines if the panorama will be shown.
+   * @memberof Panorama.prototype
+   * @type {boolean}
+   * @readonly
+   */
+  show: {
+    get: DeveloperError.throwInstantiationError,
+  },
+
+  /**
+   * The alpha blending value of this layer, with 0.0 representing fully transparent and
+   * 1.0 representing fully opaque.
+   * @memberof Panorama.prototype
+   * @type {number}
+   * @readonly
+   */
+  alpha: {
+    get: DeveloperError.throwInstantiationError,
+  },
+
+  /**
+   * Gets the height of each tile, in pixels.
+   * @memberof Panorama.prototype
+   * @type {boolean}
+   * @readonly
+   */
+  debugShowExtents: {
+    get: DeveloperError.throwInstantiationError,
+  },
+});
+
+/**
+ * Gets the sources for the panorama
+ *
+ * @returns {Image[]} The source images for the panorama.
+ */
+CubeMapPanorama.prototype.getSources = function () {
+  return this._sources;
+};
+
+/**
+ * Gets the transform for the panorama
+ *
+ * @returns {Matrix4} The transform for the panorama.
+ */
+CubeMapPanorama.prototype.getTransform = function () {
+  return undefined;
+};
+
+/**
+ * Gets the credits for the panorama
+ *
+ * @returns {Credit[]} The credits for the panorama.
+ */
+CubeMapPanorama.prototype.getCredits = function () {
+  return this.credit;
+};
+
+export default CubeMapPanorama;
