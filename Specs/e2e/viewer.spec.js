a3ff6fae0a0050b338707b3ea9a626af40430623
Erin Ingram <47707444+eringram@users.noreply.github.com>
2025-12-16 13:43:40 -0500

    dummy test

--- Specs/e2e/viewer.spec.js
+++ Specs/e2e/viewer.spec.js
@@ -1,12 +1,19 @@
 import { test, expect } from "./test.js";
 
-test("Viewer renders", async ({ cesiumPage }) => {
-  await cesiumPage.goto();
+// test("Viewer renders", async ({ cesiumPage }) => {
+//   await cesiumPage.goto();
 
-  await cesiumPage.page.evaluate(() => {
-    const viewer = new Cesium.Viewer("cesiumContainer");
-  });
+//   await cesiumPage.page.evaluate(() => {
+//     const viewer = new Cesium.Viewer("cesiumContainer");
+//   });
 
-  await cesiumPage.page.waitForLoadState("networkidle");
-  await expect(cesiumPage.page).toHaveScreenshot();
+//   await cesiumPage.page.waitForLoadState("networkidle");
+//   await expect(cesiumPage.page).toHaveScreenshot();
+// });
+
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
 });
