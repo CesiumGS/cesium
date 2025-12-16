18f90a6279c14c74f7edf25e7d5ede2e812ce3d4
jdehorty <justin.dehorty@gmail.com>
2025-12-16 11:14:39 -0500

    Exclude auto-generated SpecList.js from version control

--- packages/core-math/Specs/SpecList.js
+++
@@ -1,7 +0,0 @@
-import "./Cartesian2Spec.js";
-import "./Cartesian3Spec.js";
-import "./Cartesian4Spec.js";
-import "./MathSpec.js";
-import "./Matrix2Spec.js";
-import "./Matrix3Spec.js";
-import "./Matrix4Spec.js";
