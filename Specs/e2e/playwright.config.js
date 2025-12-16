80ec798d26afe03f58a6e9eb777dddcbcaf116de
Erin Ingram <47707444+eringram@users.noreply.github.com>
2025-12-16 10:33:17 -0500

    Chromium fix?

--- Specs/e2e/playwright.config.js
+++ Specs/e2e/playwright.config.js
@@ -53,6 +53,7 @@ export default defineConfig({
       name: "chromium",
       use: {
         ...devices["Desktop Chrome"],
+        channel: "chromium",
         viewport: defaultViewport,
         launchOptions: {
           // this forces chrome to use the gpu for webgl which greatly speeds up tests
