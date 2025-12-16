6344df80695848b90ce82df22d93d8dc0a9526ef
jdehorty <justin.dehorty@gmail.com>
2025-12-16 11:08:19 -0500

    Add @cesium/core-math package infrastructure
    Create the foundational package configuration for the new core-math
    workspace package that will house CesiumJS's mathematical primitives.
    
    New files:
    - package.json: NPM package configuration with ES module support
    - index.js: Barrel exports for Cartesian2/3/4, Matrix2/3/4, CesiumMath
    - tsd-conf.json: TypeScript definition generation config
    - README.md: Package documentation
    - Source utilities: Check, defined, DeveloperError, RuntimeError, Frozen
    - Specs/SpecList.js: Test file registry
    
    This is Track 1 of Project Buildology - establishing standalone package
    infrastructure to enable AI-driven ES6 class conversion in Track 2.

---
+++ packages/core-math/Source/DeveloperError.js
@@ -0,0 +1,75 @@
+import defined from "./defined.js";
+
+/**
+ * Constructs an exception object that is thrown due to a developer error, e.g., invalid argument,
+ * argument out of range, etc.  This exception should only be thrown during development;
+ * it usually indicates a bug in the calling code.  This exception should never be
+ * caught; instead the calling code should strive not to generate it.
+ * <br /><br />
+ * On the other hand, a {@link RuntimeError} indicates an exception that may
+ * be thrown at runtime, e.g., out of memory, that the calling code should be prepared
+ * to catch.
+ *
+ * @alias DeveloperError
+ * @constructor
+ * @extends Error
+ *
+ * @param {string} [message] The error message for this exception.
+ *
+ * @see RuntimeError
+ */
+function DeveloperError(message) {
+  /**
+   * 'DeveloperError' indicating that this exception was thrown due to a developer error.
+   * @type {string}
+   * @readonly
+   */
+  this.name = "DeveloperError";
+
+  /**
+   * The explanation for why this exception was thrown.
+   * @type {string}
+   * @readonly
+   */
+  this.message = message;
+
+  //Browsers such as IE don't have a stack property until you actually throw the error.
+  let stack;
+  try {
+    throw new Error();
+  } catch (e) {
+    stack = e.stack;
+  }
+
+  /**
+   * The stack trace of this exception, if available.
+   * @type {string}
+   * @readonly
+   */
+  this.stack = stack;
+}
+
+if (defined(Object.create)) {
+  DeveloperError.prototype = Object.create(Error.prototype);
+  DeveloperError.prototype.constructor = DeveloperError;
+}
+
+DeveloperError.prototype.toString = function () {
+  let str = `${this.name}: ${this.message}`;
+
+  if (defined(this.stack)) {
+    str += `\n${this.stack.toString()}`;
+  }
+
+  return str;
+};
+
+/**
+ * @private
+ */
+DeveloperError.throwInstantiationError = function () {
+  throw new DeveloperError(
+    "This function defines an interface and should not be called directly.",
+  );
+};
+export default DeveloperError;
---
+++ packages/core-math/Specs/SpecList.js
@@ -0,0 +1,7 @@
+import "./Cartesian2Spec.js";
+import "./Cartesian3Spec.js";
+import "./Cartesian4Spec.js";
+import "./MathSpec.js";
+import "./Matrix2Spec.js";
+import "./Matrix3Spec.js";
+import "./Matrix4Spec.js";
