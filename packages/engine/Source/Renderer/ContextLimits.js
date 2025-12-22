2d71ee8df7d89186f65c9d6aeeec664c3539e30a
Jeshurun Hembd <jeshurun@cesium.com>
2025-12-22 13:17:51 -0500

    Update comments in ContextLimits to include WebGL1 minimums

--- packages/engine/Source/Renderer/ContextLimits.js
+++ packages/engine/Source/Renderer/ContextLimits.js
@@ -32,11 +32,13 @@ const ContextLimits = {
 Object.defineProperties(ContextLimits, {
   /**
    * The maximum number of texture units that can be used from the vertex and fragment
-   * shader with this WebGL implementation.  The minimum is 32.  If both shaders access the
-   * same texture unit, this counts as two texture units.
+   * shader with this WebGL implementation.
+   * If both shaders access the same texture unit, this counts as two texture units.
+   * The minimum in WebGL2 contexts is 32, or 8 in WebGL1 contexts.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_COMBINED_TEXTURE_IMAGE_UNITS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_COMBINED_TEXTURE_IMAGE_UNITS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/glGet.xml|glGet in OpenGL ES 2.0} for WebGL1 contexts.
    */
   maximumCombinedTextureImageUnits: {
     get: function () {
@@ -46,10 +48,12 @@ Object.defineProperties(ContextLimits, {
 
   /**
    * The approximate maximum cube map width and height supported by this WebGL implementation.
-   * The minimum is 2048, but most desktop and laptop implementations will support much larger sizes like 8,192.
+   * The minimum in WebGL2 contexts is 2048, but most desktop and laptop implementations will support much larger sizes like 8192.
+   * The minimum in WebGL1 contexts is 16.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_CUBE_MAP_TEXTURE_SIZE</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_CUBE_MAP_TEXTURE_SIZE</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/glGet.xml|glGet in OpenGL ES 2.0} for WebGL1 contexts.
    */
   maximumCubeMapSize: {
     get: function () {
@@ -59,10 +63,12 @@ Object.defineProperties(ContextLimits, {
 
   /**
    * The maximum number of <code>vec4</code>, <code>ivec4</code>, and <code>bvec4</code>
-   * uniforms that can be used by a fragment shader with this WebGL implementation.  The minimum is 224.
+   * uniforms that can be used by a fragment shader with this WebGL implementation.
+   * The minimum in WebGL2 contexts is 224, or 16 in WebGL1 contexts.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_FRAGMENT_UNIFORM_VECTORS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_FRAGMENT_UNIFORM_VECTORS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/glGet.xml|glGet in OpenGL ES 2.0} for WebGL1 contexts.
    */
   maximumFragmentUniformVectors: {
     get: function () {
@@ -71,10 +77,12 @@ Object.defineProperties(ContextLimits, {
   },
 
   /**
-   * The maximum number of texture units that can be used from the fragment shader with this WebGL implementation.  The minimum is 16.
+   * The maximum number of texture units that can be used from the fragment shader with this WebGL implementation.
+   * The minimum in WebGL2 contexts is 16, or 8 in WebGL1 contexts.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_TEXTURE_IMAGE_UNITS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_TEXTURE_IMAGE_UNITS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/glGet.xml|glGet in OpenGL ES 2.0} for WebGL1 contexts.
    */
   maximumTextureImageUnits: {
     get: function () {
@@ -84,10 +92,12 @@ Object.defineProperties(ContextLimits, {
 
   /**
    * The maximum renderbuffer width and height supported by this WebGL implementation.
-   * The minimum is 2048, but most desktop and laptop implementations will support much larger sizes like 8,192.
+   * The minimum in WebGL2 contexts is 2048, but most desktop and laptop implementations will support much larger sizes like 8192.
+   * The minimum in WebGL1 contexts is 1.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_RENDERBUFFER_SIZE</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_RENDERBUFFER_SIZE</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/glGet.xml|glGet in OpenGL ES 2.0} for WebGL1 contexts.
    */
   maximumRenderbufferSize: {
     get: function () {
@@ -97,10 +107,12 @@ Object.defineProperties(ContextLimits, {
 
   /**
    * The approximate maximum texture width and height supported by this WebGL implementation.
-   * The minimum is 2048, but most desktop and laptop implementations will support much larger sizes like 8,192.
+   * The minimum in WebGL2 contexts is 2048, but most desktop and laptop implementations will support much larger sizes like 8192.
+   * The minimum in WebGL1 contexts is 64.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_TEXTURE_SIZE</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_TEXTURE_SIZE</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/glGet.xml|glGet in OpenGL ES 2.0} for WebGL1 contexts.
    */
   maximumTextureSize: {
     get: function () {
@@ -109,11 +121,12 @@ Object.defineProperties(ContextLimits, {
   },
 
   /**
-   * The approximate maximum texture width, height, and depth supported by this WebGL implementation.
+   * The approximate maximum texture width, height, and depth supported by this WebGL2 implementation.
    * The minimum is 256, but most desktop and laptop implementations will support much larger sizes like 2048.
+   * 3D textures are not supported in WebGL1 contexts.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_3D_TEXTURE_SIZE</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_3D_TEXTURE_SIZE</code>.
    */
   maximum3DTextureSize: {
     get: function () {
@@ -123,10 +136,11 @@ Object.defineProperties(ContextLimits, {
 
   /**
    * The maximum number of <code>vec4</code> varying variables supported by this WebGL implementation.
-   * The minimum is 15.  Matrices and arrays count as multiple <code>vec4</code>s.
+   * The minimum is 15 in WebGL2 contexts, or 8 in WebGL1 contexts. Matrices and arrays count as multiple <code>vec4</code>s.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_VARYING_VECTORS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_VARYING_VECTORS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/glGet.xml|glGet in OpenGL ES 2.0} for WebGL1 contexts.
    */
   maximumVaryingVectors: {
     get: function () {
@@ -135,10 +149,12 @@ Object.defineProperties(ContextLimits, {
   },
 
   /**
-   * The maximum number of <code>vec4</code> vertex attributes supported by this WebGL implementation.  The minimum is 16.
+   * The maximum number of <code>vec4</code> vertex attributes supported by this WebGL implementation.
+   * The minimum is 16 in WebGL2 contexts, or 8 in WebGL1 contexts.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_VERTEX_ATTRIBS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_VERTEX_ATTRIBS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/glGet.xml|glGet in OpenGL ES 2.0} for WebGL1 contexts.
    */
   maximumVertexAttributes: {
     get: function () {
@@ -148,10 +164,11 @@ Object.defineProperties(ContextLimits, {
 
   /**
    * The maximum number of texture units that can be used from the vertex shader with this WebGL implementation.
-   * The minimum is 16.
+   * The minimum is 16 in WebGL2 contexts, or 0 in WebGL1 contexts.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_VERTEX_TEXTURE_IMAGE_UNITS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_VERTEX_TEXTURE_IMAGE_UNITS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/glGet.xml|glGet in OpenGL ES 2.0} for WebGL1 contexts.
    */
   maximumVertexTextureImageUnits: {
     get: function () {
@@ -161,10 +178,12 @@ Object.defineProperties(ContextLimits, {
 
   /**
    * The maximum number of <code>vec4</code>, <code>ivec4</code>, and <code>bvec4</code>
-   * uniforms that can be used by a vertex shader with this WebGL implementation.  The minimum is 256.
+   * uniforms that can be used by a vertex shader with this WebGL implementation.
+   * The minimum is 256 in WebGL2 contexts, or 128 in WebGL1 contexts.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_VERTEX_UNIFORM_VECTORS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_VERTEX_UNIFORM_VECTORS</code>.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/glGet.xml|glGet in OpenGL ES 2.0} for WebGL1 contexts.
    */
   maximumVertexUniformVectors: {
     get: function () {
@@ -173,10 +192,10 @@ Object.defineProperties(ContextLimits, {
   },
 
   /**
-   * The minimum aliased line width, in pixels, supported by this WebGL implementation.  It will be at most one.
+   * The minimum aliased line width, in pixels, supported by this WebGL implementation. It will be at most one.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>ALIASED_LINE_WIDTH_RANGE</code>.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>ALIASED_LINE_WIDTH_RANGE</code>.
    */
   minimumAliasedLineWidth: {
     get: function () {
@@ -185,10 +204,10 @@ Object.defineProperties(ContextLimits, {
   },
 
   /**
-   * The maximum aliased line width, in pixels, supported by this WebGL implementation.  It will be at least one.
+   * The maximum aliased line width, in pixels, supported by this WebGL implementation. It will be at least one.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>ALIASED_LINE_WIDTH_RANGE</code>.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>ALIASED_LINE_WIDTH_RANGE</code>.
    */
   maximumAliasedLineWidth: {
     get: function () {
@@ -197,10 +216,10 @@ Object.defineProperties(ContextLimits, {
   },
 
   /**
-   * The minimum aliased point size, in pixels, supported by this WebGL implementation.  It will be at most one.
+   * The minimum aliased point size, in pixels, supported by this WebGL implementation. It will be at most one.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>ALIASED_POINT_SIZE_RANGE</code>.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>ALIASED_POINT_SIZE_RANGE</code>.
    */
   minimumAliasedPointSize: {
     get: function () {
@@ -209,10 +228,10 @@ Object.defineProperties(ContextLimits, {
   },
 
   /**
-   * The maximum aliased point size, in pixels, supported by this WebGL implementation.  It will be at least one.
+   * The maximum aliased point size, in pixels, supported by this WebGL implementation. It will be at least one.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>ALIASED_POINT_SIZE_RANGE</code>.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>ALIASED_POINT_SIZE_RANGE</code>.
    */
   maximumAliasedPointSize: {
     get: function () {
@@ -221,10 +240,10 @@ Object.defineProperties(ContextLimits, {
   },
 
   /**
-   * The maximum supported width of the viewport.  It will be at least as large as the visible width of the associated canvas.
+   * The maximum supported width of the viewport. It will be at least as large as the visible width of the associated canvas.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_VIEWPORT_DIMS</code>.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_VIEWPORT_DIMS</code>.
    */
   maximumViewportWidth: {
     get: function () {
@@ -233,10 +252,10 @@ Object.defineProperties(ContextLimits, {
   },
 
   /**
-   * The maximum supported height of the viewport.  It will be at least as large as the visible height of the associated canvas.
+   * The maximum supported height of the viewport. It will be at least as large as the visible height of the associated canvas.
+   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet in OpenGL ES 3.0} with <code>MAX_VIEWPORT_DIMS</code>.
    * @memberof ContextLimits
    * @type {number}
-   * @see {@link https://registry.khronos.org/OpenGL-Refpages/es3.0/html/glGet.xhtml|glGet} with <code>MAX_VIEWPORT_DIMS</code>.
    */
   maximumViewportHeight: {
     get: function () {
