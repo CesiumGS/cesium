5cc5445e9e94b27e1c0c9585c80c21f3532ca2c1
Don McCurdy <1848368+donmccurdy@users.noreply.github.com>
2026-02-12 10:12:23 -0500

    fix tests and other utils usage

--- packages/engine/Specs/Core/PrimitiveTypeSpec.js
+++ packages/engine/Specs/Core/PrimitiveTypeSpec.js
@@ -1,36 +1,46 @@
-import { PrimitiveType } from "../../index.js";
+import { PrimitiveType, PrimitiveTypeUtils } from "../../index.js";
 
 describe("Core/PrimitiveType", function () {
   it("validate works", function () {
-    expect(PrimitiveType.validate(PrimitiveType.POINTS)).toBe(true);
-    expect(PrimitiveType.validate(PrimitiveType.LINES)).toBe(true);
-    expect(PrimitiveType.validate(PrimitiveType.LINE_LOOP)).toBe(true);
-    expect(PrimitiveType.validate(PrimitiveType.LINE_STRIP)).toBe(true);
-    expect(PrimitiveType.validate(PrimitiveType.TRIANGLES)).toBe(true);
-    expect(PrimitiveType.validate(PrimitiveType.TRIANGLE_STRIP)).toBe(true);
-    expect(PrimitiveType.validate(PrimitiveType.TRIANGLE_FAN)).toBe(true);
-    expect(PrimitiveType.validate(undefined)).toBe(false);
+    expect(PrimitiveTypeUtils.validate(PrimitiveType.POINTS)).toBe(true);
+    expect(PrimitiveTypeUtils.validate(PrimitiveType.LINES)).toBe(true);
+    expect(PrimitiveTypeUtils.validate(PrimitiveType.LINE_LOOP)).toBe(true);
+    expect(PrimitiveTypeUtils.validate(PrimitiveType.LINE_STRIP)).toBe(true);
+    expect(PrimitiveTypeUtils.validate(PrimitiveType.TRIANGLES)).toBe(true);
+    expect(PrimitiveTypeUtils.validate(PrimitiveType.TRIANGLE_STRIP)).toBe(
+      true,
+    );
+    expect(PrimitiveTypeUtils.validate(PrimitiveType.TRIANGLE_FAN)).toBe(true);
+    expect(PrimitiveTypeUtils.validate(undefined)).toBe(false);
   });
 
   it("isLines works", function () {
-    expect(PrimitiveType.isLines(PrimitiveType.POINTS)).toBe(false);
-    expect(PrimitiveType.isLines(PrimitiveType.LINES)).toBe(true);
-    expect(PrimitiveType.isLines(PrimitiveType.LINE_LOOP)).toBe(true);
-    expect(PrimitiveType.isLines(PrimitiveType.LINE_STRIP)).toBe(true);
-    expect(PrimitiveType.isLines(PrimitiveType.TRIANGLES)).toBe(false);
-    expect(PrimitiveType.isLines(PrimitiveType.TRIANGLE_STRIP)).toBe(false);
-    expect(PrimitiveType.isLines(PrimitiveType.TRIANGLE_FAN)).toBe(false);
-    expect(PrimitiveType.isLines(undefined)).toBe(false);
+    expect(PrimitiveTypeUtils.isLines(PrimitiveType.POINTS)).toBe(false);
+    expect(PrimitiveTypeUtils.isLines(PrimitiveType.LINES)).toBe(true);
+    expect(PrimitiveTypeUtils.isLines(PrimitiveType.LINE_LOOP)).toBe(true);
+    expect(PrimitiveTypeUtils.isLines(PrimitiveType.LINE_STRIP)).toBe(true);
+    expect(PrimitiveTypeUtils.isLines(PrimitiveType.TRIANGLES)).toBe(false);
+    expect(PrimitiveTypeUtils.isLines(PrimitiveType.TRIANGLE_STRIP)).toBe(
+      false,
+    );
+    expect(PrimitiveTypeUtils.isLines(PrimitiveType.TRIANGLE_FAN)).toBe(false);
+    expect(PrimitiveTypeUtils.isLines(undefined)).toBe(false);
   });
 
   it("isTriangles works", function () {
-    expect(PrimitiveType.isTriangles(PrimitiveType.POINTS)).toBe(false);
-    expect(PrimitiveType.isTriangles(PrimitiveType.LINES)).toBe(false);
-    expect(PrimitiveType.isTriangles(PrimitiveType.LINE_LOOP)).toBe(false);
-    expect(PrimitiveType.isTriangles(PrimitiveType.LINE_STRIP)).toBe(false);
-    expect(PrimitiveType.isTriangles(PrimitiveType.TRIANGLES)).toBe(true);
-    expect(PrimitiveType.isTriangles(PrimitiveType.TRIANGLE_STRIP)).toBe(true);
-    expect(PrimitiveType.isTriangles(PrimitiveType.TRIANGLE_FAN)).toBe(true);
-    expect(PrimitiveType.isTriangles(undefined)).toBe(false);
+    expect(PrimitiveTypeUtils.isTriangles(PrimitiveType.POINTS)).toBe(false);
+    expect(PrimitiveTypeUtils.isTriangles(PrimitiveType.LINES)).toBe(false);
+    expect(PrimitiveTypeUtils.isTriangles(PrimitiveType.LINE_LOOP)).toBe(false);
+    expect(PrimitiveTypeUtils.isTriangles(PrimitiveType.LINE_STRIP)).toBe(
+      false,
+    );
+    expect(PrimitiveTypeUtils.isTriangles(PrimitiveType.TRIANGLES)).toBe(true);
+    expect(PrimitiveTypeUtils.isTriangles(PrimitiveType.TRIANGLE_STRIP)).toBe(
+      true,
+    );
+    expect(PrimitiveTypeUtils.isTriangles(PrimitiveType.TRIANGLE_FAN)).toBe(
+      true,
+    );
+    expect(PrimitiveTypeUtils.isTriangles(undefined)).toBe(false);
   });
 });
