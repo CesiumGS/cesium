0e86ff16859c2d8f26306d8b4f46188c498c2078
Guillar1 <Guillar1@users.noreply.github.com>
2026-02-12 14:08:46 -0800

    Update main.tsx

--- packages/sandcastle/src/main.tsx
+++ packages/sandcastle/src/main.tsx
@@ -4,10 +4,6 @@ import App from "./App.tsx";
 import { SettingsProvider } from "./SettingsProvider.tsx";
 import { ModelProvider } from "./contexts/ModelContext.tsx";
 
-// Note: Monaco DiffEditor disposal errors are a known harmless issue in @monaco-editor/react v4.7.0
-// These errors occur due to a cleanup race condition but don't affect functionality
-// The errors are logged by Monaco's internal error handler and cannot be easily suppressed
-
 createRoot(document.getElementById("app-container")!).render(
   <StrictMode>
     <SettingsProvider>
