0370dc5ee04f95f51758e79bc04259c0e6e8783d
Erin Ingram <47707444+eringram@users.noreply.github.com>
2026-02-20 08:34:33 -0500

    Add point style test to resolve merge conflict

--- packages/engine/Specs/Scene/Model/MaterialPipelineStageSpec.js
+++ packages/engine/Specs/Scene/Model/MaterialPipelineStageSpec.js
@@ -94,6 +94,8 @@ describe(
       "./Data/Models/glTF-2.0/BoxAnisotropy/glTF/BoxAnisotropy.gltf";
     const clearcoatTestData =
       "./Data/Models/glTF-2.0/BoxClearcoat/glTF/BoxClearcoat.gltf";
+    const pointStyleTestData =
+      "./Data/Models/glTF-2.0/StyledPoints/points-r5-g8-b14-y10.gltf";
     const constantLodTestData =
       "./Data/Models/glTF-2.0/ConstantLod/gltf/ConstantLod_Checker.gltf";
 
@@ -908,6 +910,50 @@ describe(
       expectUniformMap(uniformMap, {
         u_testTexture: mockTexture,
       });
+    });
+
+    it("adds point diameter uniforms for BENTLEY_materials_point_style extension", async function () {
+      const gltfLoader = await loadGltf(pointStyleTestData);
+      const primitive = gltfLoader.components.nodes[0].primitives[0];
+      const renderResources = mockRenderResources();
+      const { shaderBuilder, uniformMap } = renderResources;
+
+      MaterialPipelineStage.process(renderResources, primitive, mockFrameState);
+
+      ShaderBuilderTester.expectHasVertexDefines(shaderBuilder, [
+        "HAS_POINT_DIAMETER",
+      ]);
+      ShaderBuilderTester.expectHasFragmentDefines(shaderBuilder, [
+        "HAS_BASE_COLOR_FACTOR",
+        "HAS_METALLIC_FACTOR",
+        "HAS_POINT_DIAMETER",
+        "USE_METALLIC_ROUGHNESS",
+      ]);
+      ShaderBuilderTester.expectHasVertexUniforms(shaderBuilder, [
+        "uniform float u_pointDiameter;",
+      ]);
+
+      // The first primitive has pointDiameter of 5, and mockFrameState.pixelRatio is 1.0
+      const expectedPointDiameter = 5 * mockFrameState.pixelRatio;
+      expectUniformMap(uniformMap, {
+        u_pointDiameter: expectedPointDiameter,
+      });
+    });
+
+    it("does not add point diameter uniforms when pointDiameter is undefined", async function () {
+      // Use triangle model which has no BENTLEY_materials_point_style extension
+      const gltfLoader = await loadGltf(triangle);
+      const primitive = gltfLoader.components.nodes[0].primitives[0];
+      const renderResources = mockRenderResources();
+      const { shaderBuilder, uniformMap } = renderResources;
+
+      MaterialPipelineStage.process(renderResources, primitive, mockFrameState);
+
+      ShaderBuilderTester.expectHasVertexDefines(shaderBuilder, []);
+      ShaderBuilderTester.expectHasFragmentDefines(shaderBuilder, [
+        "USE_METALLIC_ROUGHNESS",
+      ]);
+      expect(uniformMap.u_pointDiameter).toBeUndefined();
     });
 
     it("processes EXT_textureInfo_constant_lod extension", async function () {
