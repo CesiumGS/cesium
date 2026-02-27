13341c42d259bf37046e7e78578cc3700ac5855b
Adam Morris <adam@cesium.com>
2026-02-27 15:34:12 -0600

    Deleting some dead code accidentally checked in

--- packages/engine/Source/Scene/GltfLoader.js
+++ packages/engine/Source/Scene/GltfLoader.js
@@ -1843,10 +1843,6 @@ function loadMaterial(loader, gltfMaterial, frameState) {
   return material;
 }
 
-function loadExternalReferences() {
-  // TODO
-}
-
 // for EXT_mesh_features
 function loadFeatureIdAttribute(featureIds, positionalLabel) {
   const featureIdAttribute = new FeatureIdAttribute();
@@ -2828,8 +2824,7 @@ function parse(loader, frameState) {
     loader._sortedPropertyTableIds = Object.keys(allPropertyTableIds).sort();
     loader._sortedFeatureTextureIds = Object.keys(allFeatureTextureIds).sort();
   }
-
-  const externalReferences = loadExternalReferences(gltf);
+  
   const nodes = loadNodes(loader, frameState);
   const skins = loadSkins(loader, nodes);
   const animations = loadAnimations(loader, nodes);
