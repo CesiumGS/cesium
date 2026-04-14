03ff889467f5526dc6204a6006ba4e5f16e3837b
Matt Schwartz <matt.schwartz@bentley.com>
2026-04-14 16:37:39 -0500

    Makes geometry accessor more robust with RAII style resource acquisition

--- packages/engine/Source/Scene/GeometryAccessor.js
+++ packages/engine/Source/Scene/GeometryAccessor.js
@@ -5,42 +5,93 @@ import DeveloperError from "../Core/DeveloperError.js";
 /** @import Cartesian3 from "../Core/Cartesian3.js"; */
 
 /**
- * Interface for reading and updating mesh geometry, agnostic of the underlying rendering representation.
+ * Abstract class for reading and writing mesh geometry, agnostic of the underlying rendering representation.
  * Must be implemented by render-side geometry classes to be compatible with EditableMesh.
  *
- * @interface
+ * Callers must access geometry through {@link GeometryAccessor#withReadAccess}
+ * or {@link GeometryAccessor#withWriteAccess}. These methods allow the
+ * implementation to create short-lived reader and writer objects that own any
+ * staged resources required for the duration of the callback.
+ *
+ * Generally speaking, assume acquiring read or write access is slow and should only be called sparingly (i.e. not for each element read / written).
+ * Callbacks are expected to complete synchronously.
+ *
+ * Enforcing whether nested contexts are allowed is up to the implementation.
+ *
+ * @abstract
  * @private
  */
 class GeometryAccessor {
   /**
-   * Begins a geometry read session.
-   * Implementations may use this to set up temporary state, lock resources, etc.
+   * Executes a function in a context that provides geometry read access.
+   *
+   * @template T
+   * @param {(reader: GeometryReader) => T} callback The callback to run.
+   * @returns {T} The callback result.
    */
-  beginRead() {
+  withReadAccess(callback) {
+    const reader = this._createReader();
+    try {
+      return callback(reader);
+    } finally {
+      reader.destroy();
+    }
+  }
+
+  /**
+   * Executes a function in a context that provides geometry write access.
+   *
+   * @template T
+   * @param {(writer: GeometryWriter) => T} callback The callback to run.
+   * @returns {T} The callback result.
+   */
+  withWriteAccess(callback) {
+    const writer = this._createWriter();
+    try {
+      return callback(writer);
+    } finally {
+      writer.destroy();
+    }
+  }
+
+  /**
+   * Creates a reader for a geometry read session.
+   * Implementations may use this to stage data, allocate temporary state, and
+   * acquire any resources needed by the returned reader.
+   *
+   * @protected
+   * @returns {GeometryReader}
+   */
+  _createReader() {
     DeveloperError.throwInstantiationError();
   }
 
   /**
-   * Ends a geometry read session.
-   * Implementations may use this to tear down temporary state, unlock resources, etc., set up by {@link GeometryAccessor#beginRead}.
+   * Creates a writer for a geometry write session.
+   * Implementations may use this to stage mutable data, allocate temporary
+   * state, and acquire any resources needed by the returned writer.
+   *
+   * @protected
+   * @returns {GeometryWriter}
    */
-  endRead() {
+  _createWriter() {
     DeveloperError.throwInstantiationError();
   }
+}
 
+/**
+ * Abstract base class for a scoped geometry read session.
+ * Implementations own any resources required to read geometry and must release
+ * them when {@link GeometryReader#destroy} is called.
+ *
+ * @abstract
+ * @private
+ */
+class GeometryReader {
   /**
-   * Begins a geometry write session.
-   * Implementations may use this to set up temporary state, lock resources, etc., in preparation for geometry updates.
+   * Releases resources acquired for this read session.
    */
-  beginWrite() {
-    DeveloperError.throwInstantiationError();
-  }
-
-  /**
-   * Ends a geometry write session.
-   * Implementations may use this to tear down temporary state, unlock resources, etc., set up by {@link GeometryAccessor#beginWrite}.
-   */
-  endWrite() {
+  destroy() {
     DeveloperError.throwInstantiationError();
   }
 
@@ -77,7 +128,6 @@ class GeometryAccessor {
 
   /**
    * Gets the model-space position of a render vertex.
-   * Accessors may assume this is only called between beginRead and endRead.
    *
    * @param {number} vertexIndex The render vertex index.
    * @returns {Cartesian3}
@@ -85,10 +135,19 @@ class GeometryAccessor {
   getVertexPosition(vertexIndex) {
     DeveloperError.throwInstantiationError();
   }
+}
 
+/**
+ * Abstract base class for a scoped geometry write session.
+ * A writer also supports all read operations exposed by
+ * {@link GeometryReader}.
+ *
+ * @abstract
+ * @private
+ */
+class GeometryWriter extends GeometryReader {
   /**
    * Updates the model-space position of a render vertex.
-   * Accessors may assume this is only called between beginWrite and endWrite.
    *
    * @param {number} vertexIndex The render vertex index.
    * @param {Cartesian3} position The new position for the vertex.
@@ -98,4 +157,5 @@ class GeometryAccessor {
   }
 }
 
+export { GeometryReader, GeometryWriter };
 export default GeometryAccessor;
