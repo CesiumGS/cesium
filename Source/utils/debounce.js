cb91cb43177f653372197df7e5abc17a3dfb0c22
Dheeraj Sharma <xsharmax60062@gmail.com>
2026-04-28 00:32:51 +0530

    Add debounce function to utils

---
+++ Source/utils/debounce.js
@@ -0,0 +1,10 @@
+export function debounce(func, delay = 300) {
+  let timeoutId;
+
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
