b3651d20c15aa513c2f41d99a18588317c465254
Matt Schwartz <matt.schwartz@bentley.com>
2026-04-28 22:21:19 -0400

    Implements Selection API

--- packages/edit/Source/Mesh/Selection.js
+++ packages/edit/Source/Mesh/Selection.js
@@ -1,9 +1,7 @@
 import { Event } from "@cesium/engine";
 
 /** @import Vertex from "./Vertex"; */
-/** @import Edge from "./Edge"; */
-/** @import Face from "./Face"; */
-/** @import { MeshComponent } from "./MeshComponent"; */
+/** @import MeshComponent from "./MeshComponent"; */
 
 /**
  * @typedef {object} SelectionChange
@@ -18,12 +16,16 @@ import { Event } from "@cesium/engine";
  */
 class Selection {
   constructor() {
-    /** @type {Set<Vertex>} */
-    this._vertices = new Set();
-    /** @type {Set<Edge>} */
-    this._edges = new Set();
-    /** @type {Set<Face>} */
-    this._faces = new Set();
+    /** @type {Set<MeshComponent>} */
+    this._components = new Set();
+
+    /**
+     * Reference counts per closure vertex - i.e. the number of currently-selected
+     * components incident to each vertex. The keys of this map are the current
+     * vertex closure of the selection.
+     * @type {Map<Vertex, number>}
+     */
+    this._vertexClosureCounts = new Map();
 
     /**
      * Raised after any public mutator changes the contents of this selection.
@@ -36,64 +38,156 @@ class Selection {
     this.changed = new Event();
   }
 
-  /** @type {ReadonlySet<Vertex>} */
-  get vertices() {
-    return this._vertices;
+  /** @type {ReadonlySet<MeshComponent>} */
+  get components() {
+    return this._components;
   }
 
-  /** @type {ReadonlySet<Edge>} */
-  get edges() {
-    return this._edges;
-  }
-
-  /** @type {ReadonlySet<Face>} */
-  get faces() {
-    return this._faces;
+  /** @type {number} */
+  get size() {
+    return this._components.size;
   }
 
   /**
    * @param {MeshComponent} component
    * @returns {boolean}
    */
-  has(component) {}
+  has(component) {
+    return this._components.has(component);
+  }
 
   /**
    * Returns the closure of this selection under vertex incidence: selected
    * vertices, plus the endpoints of selected edges, plus the rings of selected
    * faces, deduplicated.
    *
-   * @returns {Set<Vertex>}
+   * @returns {IterableIterator<Vertex>}
    */
-  vertexClosure() {}
+  vertexClosure() {
+    return this._vertexClosureCounts.keys();
+  }
 
   /**
    * Adds the given components to the selection.
    * @param {Iterable<MeshComponent>} components
    */
-  add(components) {}
+  add(components) {
+    /** @type {Set<MeshComponent>} */
+    const added = new Set();
+    for (const component of components) {
+      if (this._components.has(component)) {
+        continue;
+      }
+
+      this._components.add(component);
+      added.add(component);
+      this.#addToClosure(component);
+    }
+    if (added.size === 0) {
+      return;
+    }
+    this.changed.raiseEvent({ added, removed: new Set() });
+  }
 
   /**
    * Removes the given components from the selection.
    * @param {Iterable<MeshComponent>} components
    */
-  remove(components) {}
+  remove(components) {
+    /** @type {Set<MeshComponent>} */
+    const removed = new Set();
+    for (const component of components) {
+      if (!this._components.has(component)) {
+        continue;
+      }
+      this._components.delete(component);
+      removed.add(component);
+      this.#removeFromClosure(component);
+    }
+    if (removed.size === 0) {
+      return;
+    }
+    this.changed.raiseEvent({ added: new Set(), removed });
+  }
 
   /**
    * For each component, adds it if it is not already in the selection, otherwise removes it.
    * @param {Iterable<MeshComponent>} components
    */
-  toggle(components) {}
+  toggle(components) {
+    /** @type {Set<MeshComponent>} */
+    const added = new Set();
+    /** @type {Set<MeshComponent>} */
+    const removed = new Set();
+    for (const component of components) {
+      if (this._components.has(component)) {
+        this._components.delete(component);
+        removed.add(component);
+        this.#removeFromClosure(component);
+      } else {
+        this._components.add(component);
+        added.add(component);
+        this.#addToClosure(component);
+      }
+    }
+    if (added.size === 0 && removed.size === 0) {
+      return;
+    }
+    this.changed.raiseEvent({ added, removed });
+  }
 
   /**
    * Replaces the current selection with the given components.
+   *
+   * Note: implemented as {@link Selection#clear} followed by
+   * {@link Selection#add}, so it raises {@link Selection#changed} up to twice.
    * @param {Iterable<MeshComponent>} components
    */
-  set(components) {}
+  set(components) {
+    this.clear();
+    this.add(components);
+  }
 
   /**
    * Clears all selected components.
    */
-  clear() {}
+  clear() {
+    if (this._components.size === 0) {
+      return;
+    }
+
+    const removed = new Set(this._components);
+
+    this._components.clear();
+    this._vertexClosureCounts.clear();
+
+    this.changed.raiseEvent({ added: new Set(), removed });
+  }
+
+  /**
+   * @param {MeshComponent} component
+   */
+  #addToClosure(component) {
+    for (const vertex of component.vertices()) {
+      const count = this._vertexClosureCounts.get(vertex) ?? 0;
+      this._vertexClosureCounts.set(vertex, count + 1);
+    }
+  }
+
+  /**
+   * @param {MeshComponent} component
+   */
+  #removeFromClosure(component) {
+    for (const vertex of component.vertices()) {
+      const count = this._vertexClosureCounts.get(vertex) - 1;
+      if (count === 0) {
+        this._vertexClosureCounts.delete(vertex);
+        continue;
+      }
+
+      this._vertexClosureCounts.set(vertex, count);
+    }
+  }
 }
 
 export default Selection;
