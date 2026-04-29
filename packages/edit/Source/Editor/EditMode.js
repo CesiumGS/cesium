6586ec08bd433ea6e92c00e9633bb569d8c8e770
Matt Schwartz <matt.schwartz@bentley.com>
2026-04-28 21:26:47 -0400

    Outlines MeshEditor and EditMode

---
+++ packages/edit/Source/Editor/EditMode.js
@@ -0,0 +1,12 @@
+/**
+ * Selection / interaction granularity for a {@link MeshEditor}.
+ *
+ * @enum {string}
+ */
+const EditMode = Object.freeze({
+  VERTEX: "VERTEX",
+  EDGE: "EDGE",
+  FACE: "FACE",
+});
+
+export default EditMode;
