ca12a42d83ef8c8fbf6903381e87e977bdd417cd
Don McCurdy <1848368+donmccurdy@users.noreply.github.com>
2026-04-29 20:53:10 -0400

    Types: Fix type checks against remaining enums

--- packages/engine/Source/Scene/BufferPrimitiveCollection.js
+++ packages/engine/Source/Scene/BufferPrimitiveCollection.js
@@ -212,7 +212,6 @@ class BufferPrimitiveCollection {
 
     this._allocatePrimitiveBuffer();
     this._allocatePositionBuffer(
-      // @ts-expect-error Requires https://github.com/CesiumGS/cesium/pull/13203.
       options.positionDatatype ?? ComponentDatatype.DOUBLE,
     );
     this._allocateMaterialBuffer();
@@ -635,7 +634,6 @@ class BufferPrimitiveCollection {
 
   /** @param {object} frameState */
   update(frameState) {
-    // @ts-expect-error Requires https://github.com/CesiumGS/cesium/pull/13203.
     if (/** @type {FrameState} */ (frameState).mode !== SceneMode.SCENE3D) {
       oneTimeWarning(
         "bufferprim-scenemode",
