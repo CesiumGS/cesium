33a734622ea0f6b7626b894e5628c225dfc3de63
Matt Schwartz <matt.schwartz@bentley.com>
2026-04-30 18:46:01 -0400

    Uses edit mode to control pickable and renderable components

--- packages/edit/Source/Editor/EditMode.js
+++ packages/edit/Source/Editor/EditMode.js
@@ -1,13 +1,74 @@
+import TopologyComponents from "../Mesh/TopologyComponents.js";
+
 /**
- * Selection / interaction granularity for a {@link MeshEditor}.
+ * An EditMode is more than an enum. It's a policy - determining things like which mesh components are rendered and pickable.
  *
- * @enum {string}
+ * Built-in modes are exposed as singletons:
+ * {@link EditMode.NONE}, {@link EditMode.VERTEX}, {@link EditMode.EDGE},
+ * {@link EditMode.FACE}. Applications that need a different policy can
+ * subclass {@link EditMode}
+ *
+ * @experimental This feature is not final and is subject to change without
+ *   Cesium's standard deprecation policy.
  */
-const EditMode = Object.freeze({
-  NONE: "NONE",
-  VERTEX: "VERTEX",
-  EDGE: "EDGE",
-  FACE: "FACE",
-});
+class EditMode {
+  /**
+   * Bitmask of {@link TopologyComponents} that the topology overlay should
+   * draw in the regular render pass for this mode. Default: nothing.
+   *
+   * @returns {number}
+   */
+  get renderableComponents() {
+    return TopologyComponents.NONE;
+  }
+
+  /**
+   * Bitmask of {@link TopologyComponents} that the topology overlay should
+   * emit pick commands for in this mode. Default: nothing pickable.
+   *
+   * @returns {number}
+   */
+  get pickableComponents() {
+    return TopologyComponents.NONE;
+  }
+}
+
+class NoneEditMode extends EditMode {}
+
+class VertexEditMode extends EditMode {
+  get renderableComponents() {
+    return TopologyComponents.VERTICES | TopologyComponents.EDGES;
+  }
+  get pickableComponents() {
+    return TopologyComponents.VERTICES;
+  }
+}
+
+class EdgeEditMode extends EditMode {
+  get renderableComponents() {
+    return TopologyComponents.EDGES;
+  }
+  get pickableComponents() {
+    return TopologyComponents.EDGES;
+  }
+}
+
+class FaceEditMode extends EditMode {
+  get renderableComponents() {
+    return TopologyComponents.EDGES;
+  }
+  get pickableComponents() {
+    return TopologyComponents.FACES;
+  }
+}
+
+/** Editor is idle. Topology overlay does not render or pick anything. */
+EditMode.NONE = new NoneEditMode();
+/** Vertex selection. Renders points and edges; picks vertices. */
+EditMode.VERTEX = new VertexEditMode();
+/** Edge selection. Renders edges; picks edges. */
+EditMode.EDGE = new EdgeEditMode();
+/** Face selection. Renders edges; picks faces. */
+EditMode.FACE = new FaceEditMode();
 
 export default EditMode;
