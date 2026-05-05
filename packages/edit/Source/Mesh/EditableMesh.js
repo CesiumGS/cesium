06e2503a3e79283ecda16f199306a1d02a02c640
Matt Schwartz <matt.schwartz@bentley.com>
2026-05-04 20:19:52 -0400

    Makes half edge mesh robust to boundary edges

--- packages/edit/Source/Mesh/EditableMesh.js
+++ packages/edit/Source/Mesh/EditableMesh.js
@@ -19,8 +19,6 @@ import Vertex from "./Vertex";
  */
 const scratchComponents = [];
 
-const scratchPositionArray = [0, 0, 0];
-
 /**
  * Editable half-edge mesh backed by a render-side GeometryAccessor.
  *
