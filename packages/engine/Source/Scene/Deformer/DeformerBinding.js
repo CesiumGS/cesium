c360961485a49ba35da318f25dcb456b69970efd
Matt Schwartz <matt.schwartz@bentley.com>
2026-05-14 12:29:23 -0400

    Adds prerender updates for gpu resources in deformers

--- packages/engine/Source/Scene/Deformer/DeformerBinding.js
+++ packages/engine/Source/Scene/Deformer/DeformerBinding.js
@@ -1,6 +1,8 @@
 // @ts-check
 
+import destroyObject from "../../Core/destroyObject";
 import DeveloperError from "../../Core/DeveloperError";
+/** @import Context from "../../Renderer/Context"; */
 
 /**
  * For now, an empty class that deformers can subclass to store deformer-related binding data.
@@ -9,6 +11,19 @@ import DeveloperError from "../../Core/DeveloperError";
 class DeformerBinding {
   constructor() {
     DeveloperError.throwInstantiationError();
+  }
+
+  /**
+   * Called on prerender by the owning Deformer with the webgl context.
+   * This gives the binding a chance to create any GPU resources it needs.
+   * @param {Context} context
+   */
+  initialize(context) {
+    DeveloperError.throwInstantiationError();
+  }
+
+  destroy() {
+    return destroyObject(this);
   }
 }
 
