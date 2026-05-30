80fbe913246e6e652fc56dd33ea141cd89a3492b
Jeshurun Hembd <jeshurun@cesium.com>
2026-05-29 21:29:27 -0400

    PR feedback part 1

--- packages/engine/Source/Scene/buildVoxelCustomShader.js
+++ packages/engine/Source/Scene/buildVoxelCustomShader.js
@@ -1,10 +1,15 @@
+// @ts-check
+
 import Color from "../Core/Color.js";
+import createColorRamp from "../Core/createColorRamp.js";
 import CustomShader from "./Model/CustomShader.js";
 import defined from "../Core/defined.js";
 import MetadataComponentType from "./MetadataComponentType.js";
 import MetadataType from "./MetadataType.js";
 import TextureUniform from "./Model/TextureUniform.js";
 import UniformType from "./Model/UniformType.js";
+
+/** @import VoxelProvider from "./VoxelProvider.js"; */
 
 /**
  * Builds a custom shader for a voxel primitive based on information
@@ -26,17 +31,38 @@ function buildVoxelCustomShader(provider) {
   // Loop over metadata properties and build a shader for the first property
   // that has a supported type and component type.
   for (let i = 0; i < names.length; i++) {
+    if (!rangeIsValid(minimumValues?.[i], maximumValues?.[i])) {
+      continue;
+    }
     const shader = constructMetadataShader(
       names[i],
       types[i],
       componentTypes[i],
-      minimumValues?.[i],
-      maximumValues?.[i],
+      minimumValues[i],
+      maximumValues[i],
     );
     if (defined(shader)) {
       return shader;
     }
   }
+}
+
+/**
+ * Check if a range of values is valid for constructing a meaningful shader.
+ *
+ * @param {number[]} minimumValue The minimum value of the range.
+ * @param {number[]} maximumValue The maximum value of the range.
+ * @returns {boolean} True if the range is valid, false otherwise.
+ *
+ * @private
+ */
+function rangeIsValid(minimumValue, maximumValue) {
+  if (!Array.isArray(minimumValue) || !Array.isArray(maximumValue)) {
+    return false;
+  }
+  const minimumComponent = Math.min(...minimumValue);
+  const maximumComponent = Math.max(...maximumValue);
+  return minimumComponent < maximumComponent;
 }
 
 /**
@@ -58,10 +84,6 @@ function constructMetadataShader(
   minimumValue,
   maximumValue,
 ) {
-  if (!defined(minimumValue) || !defined(maximumValue)) {
-    // We need minimum and maximum values to construct a shader.
-    return;
-  }
   if (
     type === MetadataType.VEC4 &&
     componentType === MetadataComponentType.FLOAT32
@@ -93,10 +115,6 @@ function buildColorShader(name, minimumValue, maximumValue) {
     // The data is out of the range expected for colors, so a color shader will be meaningless.
     return;
   }
-  if (maxComponent - minComponent === 0) {
-    // The data is constant, so a color shader will be meaningless.
-    return;
-  }
 
   return new CustomShader({
     fragmentShaderText: `void fragmentMain(FragmentInput fsInput, inout czm_modelMaterial material)
@@ -113,16 +131,11 @@ function buildColorShader(name, minimumValue, maximumValue) {
  * @param {string} name The name of the metadata property.
  * @param {number[]} minimumValue The minimum value of the metadata property.
  * @param {number[]} maximumValue The maximum value of the metadata property.
- * @returns {CustomShader|undefined} A custom shader for the metadata property, or undefined if a shader could not be constructed.
+ * @returns {CustomShader} A custom shader for the metadata property.
  *
  * @private
  */
 function buildColorMapShader(name, minimumValue, maximumValue) {
-  if (maximumValue - minimumValue === 0) {
-    // The data is constant, so a color map shader will be meaningless.
-    return;
-  }
-
   // Perceptually uniform color map similar to the "viridis" color map used in scientific visualization.
   const colorMapWidth = 128;
   const colorMap = createColorRamp(
@@ -170,51 +183,17 @@ function buildColorMapShader(name, minimumValue, maximumValue) {
     fragmentShaderText: `void fragmentMain(FragmentInput fsInput, inout czm_modelMaterial material)
   {
       float value = fsInput.metadata.${name};
-      float valMin = fsInput.metadataStatistics.${name}.min;
-      float valMax = fsInput.metadataStatistics.${name}.max;
       vec3 voxelNormal = fsInput.attributes.normalEC;
       float diffuse = max(0.0, dot(voxelNormal, czm_lightDirectionEC));
       float lighting = 0.5 + 0.5 * diffuse;
 
       if (value >= u_minimumValue && value <= u_maximumValue) {
-        float lerp = (value - valMin) / (valMax - valMin);
+        float lerp = (value - u_minimumValue) / (u_maximumValue - u_minimumValue);
         material.diffuse = texture(u_colorMap, vec2(lerp, 0.5)).rgb * lighting;
         material.alpha = 1.0;
       }
   }`,
   });
-}
-
-/**
- * Create a color ramp that linearly interpolates between the given colors.
- *
- * @param {Color[]} colors The array of colors to use for the color ramp.
- * @param {number} rampWidth The width of the color ramp texture to create.
- * @returns {Uint8Array} A typed array representing the color ramp.
- *
- * @private
- */
-function createColorRamp(colors, rampWidth) {
-  const ramp = document.createElement("canvas");
-  ramp.width = rampWidth;
-  ramp.height = 1;
-  const ctx = ramp.getContext("2d");
-  const grd = ctx.createLinearGradient(0, 0, ramp.width, 0);
-
-  const stepSize = 1 / (colors.length - 1);
-  for (let i = 0; i < colors.length; i++) {
-    const color = colors[i];
-    const stop = i * stepSize;
-    const cssColor = color.toCssColorString();
-    grd.addColorStop(stop, cssColor);
-  }
-
-  ctx.fillStyle = grd;
-  ctx.fillRect(0, 0, ramp.width, ramp.height);
-
-  const imageData = ctx.getImageData(0, 0, ramp.width, ramp.height);
-  const array = new Uint8Array(imageData.data.buffer);
-  return array;
 }
 
 export default buildVoxelCustomShader;
