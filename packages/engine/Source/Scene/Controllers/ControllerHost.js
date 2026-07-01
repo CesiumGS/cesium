6c2384fcc2fb730bbccddc2126d88bddd3d49fa9
Gabby Getz <gabby@cesium.com>
2026-07-01 09:40:07 -0400

    Update packages/engine/Source/Scene/Controllers/ControllerHost.js
    Co-authored-by: Marco Hutter <javagl@javagl.de>

--- packages/engine/Source/Scene/Controllers/ControllerHost.js
+++ packages/engine/Source/Scene/Controllers/ControllerHost.js
@@ -33,7 +33,7 @@ class ControllerHost {
    * Registers a controller implementation with this host.
    * @param {Controller} controller An implementation of the Controller interface to register with this host.
    * @param {HTMLElement} element The DOM element containing the Cesium scene.
-   * @param {number} [priority=0] An index, less than or equal to the current count of registed controllers, that defines the precedence of the new controller relative to those previously registered. A priority of <code>0</code> would mean the new controller would apply it's updates before any other controller. As subsequent controllers are updated, their effects are applied on top of any previous update effects. If omitted, the new controller becomes the highest priority, i.e., it's updates are applied after all other controllers.
+   * @param {number} [priority=0] An index, less than or equal to the current count of registed controllers, that defines the precedence of the new controller relative to those previously registered. A priority of <code>0</code> would mean the new controller would apply its updates before any other controller. As subsequent controllers are updated, their effects are applied on top of any previous update effects. If omitted, the new controller becomes the highest priority, i.e., its updates are applied after all other controllers.
    */
   registerController(controller, element, priority) {
     const index = priority ?? this.controllerCount;
