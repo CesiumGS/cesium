96dd1466dd07e58a806d3d29741e5e09dc0f5259
Matt Schwartz <matt.schwartz@bentley.com>
2026-07-31 17:07:53 -0400

    Always populate polygonRings in requestRectangleData

--- packages/engine/Source/Scene/ClippingPolygonCollection.js
+++ packages/engine/Source/Scene/ClippingPolygonCollection.js
@@ -1181,7 +1181,7 @@ ClippingPolygonCollection.prototype.requestRectangleData = function (
   Check.typeOf.object("context", context);
   //>>includeEnd('debug');
 
-  const vectorTileData = {};
+  const vectorTileData = { polygonRings: [] };
 
   if (this.length === 0) {
     return vectorTileData;
