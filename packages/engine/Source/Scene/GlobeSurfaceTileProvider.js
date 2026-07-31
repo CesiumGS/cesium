8328f71d98073dbbdec8cb58f0672b9ea77821a3
Matt Schwartz <matt.schwartz@bentley.com>
2026-07-31 12:32:58 -0400

    Adds guard against empty clipping geometry in globe

--- packages/engine/Source/Scene/GlobeSurfaceTileProvider.js
+++ packages/engine/Source/Scene/GlobeSurfaceTileProvider.js
@@ -3212,11 +3212,14 @@ function addDrawCommandsForTile(tileProvider, tile, frameState) {
 
     // update clipping polygons
     const clippingPolygons = tileProvider._clippingPolygons;
+    const hasClippingPolygonGeometry =
+      (clippingPolygonData?.polygonRings.length ?? 0) > 0;
     const clippingPolygonsEnabled =
       defined(clippingPolygons) &&
       clippingPolygons.enabled &&
       clippingPolygons.length > 0 &&
-      tile.isClipped;
+      tile.isClipped &&
+      hasClippingPolygonGeometry;
 
     surfaceShaderSetOptions.numberOfDayTextures = numberOfDayTextures;
     surfaceShaderSetOptions.applyBrightness = applyBrightness;
