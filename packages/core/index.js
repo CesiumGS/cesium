c7e51f79613b395cad27b7646341b6ffd4d87ad1
Jeshurun Hembd <jeshurun@cesium.com>
2026-08-25 20:02:49 -0400

    Scaffold for separate core package

---
+++ packages/core/index.js
@@ -0,0 +1,190 @@
+// Primitive utilities
+export { default as addAllToArray } from "@cesium/engine/Source/Core/addAllToArray.js";
+export { default as appendForwardSlash } from "@cesium/engine/Source/Core/appendForwardSlash.js";
+export { default as arrayRemoveDuplicates } from "@cesium/engine/Source/Core/arrayRemoveDuplicates.js";
+export { default as assert } from "@cesium/engine/Source/Core/assert.js";
+export { default as binarySearch } from "@cesium/engine/Source/Core/binarySearch.js";
+export { default as Check } from "@cesium/engine/Source/Core/Check.js";
+export { default as clone } from "@cesium/engine/Source/Core/clone.js";
+export { default as combine } from "@cesium/engine/Source/Core/combine.js";
+export { default as createGuid } from "@cesium/engine/Source/Core/createGuid.js";
+export { default as defined } from "@cesium/engine/Source/Core/defined.js";
+export { default as deprecationWarning } from "@cesium/engine/Source/Core/deprecationWarning.js";
+export { default as defer } from "@cesium/engine/Source/Core/defer.js";
+export { default as destroyObject } from "@cesium/engine/Source/Core/destroyObject.js";
+export { default as DeveloperError } from "@cesium/engine/Source/Core/DeveloperError.js";
+export { default as Event } from "@cesium/engine/Source/Core/Event.js";
+export { default as EventHelper } from "@cesium/engine/Source/Core/EventHelper.js";
+export { default as formatError } from "@cesium/engine/Source/Core/formatError.js";
+export { default as Frozen } from "@cesium/engine/Source/Core/Frozen.js";
+export { default as getAbsoluteUri } from "@cesium/engine/Source/Core/getAbsoluteUri.js";
+export { default as getBaseUri } from "@cesium/engine/Source/Core/getBaseUri.js";
+export { default as getExtensionFromUri } from "@cesium/engine/Source/Core/getExtensionFromUri.js";
+export { default as getFilenameFromUri } from "@cesium/engine/Source/Core/getFilenameFromUri.js";
+export { default as getJsonFromTypedArray } from "@cesium/engine/Source/Core/getJsonFromTypedArray.js";
+export { default as getMagic } from "@cesium/engine/Source/Core/getMagic.js";
+export { default as getStringFromTypedArray } from "@cesium/engine/Source/Core/getStringFromTypedArray.js";
+export { default as getTimestamp } from "@cesium/engine/Source/Core/getTimestamp.js";
+export { default as isBitSet } from "@cesium/engine/Source/Core/isBitSet.js";
+export { default as isBlobUri } from "@cesium/engine/Source/Core/isBlobUri.js";
+export { default as isCrossOriginUrl } from "@cesium/engine/Source/Core/isCrossOriginUrl.js";
+export { default as isDataUri } from "@cesium/engine/Source/Core/isDataUri.js";
+export { default as isLeapYear } from "@cesium/engine/Source/Core/isLeapYear.js";
+export { default as mergeSort } from "@cesium/engine/Source/Core/mergeSort.js";
+export { default as objectToQuery } from "@cesium/engine/Source/Core/objectToQuery.js";
+export { default as oneTimeWarning } from "@cesium/engine/Source/Core/oneTimeWarning.js";
+export { default as queryToObject } from "@cesium/engine/Source/Core/queryToObject.js";
+export { default as RuntimeError } from "@cesium/engine/Source/Core/RuntimeError.js";
+export { default as srgbToLinear } from "@cesium/engine/Source/Core/srgbToLinear.js";
+export { default as subdivideArray } from "@cesium/engine/Source/Core/subdivideArray.js";
+export { default as wrapFunction } from "@cesium/engine/Source/Core/wrapFunction.js";
+
+// Data structures
+export { default as AssociativeArray } from "@cesium/engine/Source/Core/AssociativeArray.js";
+export { default as DoubleEndedPriorityQueue } from "@cesium/engine/Source/Core/DoubleEndedPriorityQueue.js";
+export { default as DoublyLinkedList } from "@cesium/engine/Source/Core/DoublyLinkedList.js";
+export { default as Heap } from "@cesium/engine/Source/Core/Heap.js";
+export { default as ManagedArray } from "@cesium/engine/Source/Core/ManagedArray.js";
+export { default as Queue } from "@cesium/engine/Source/Core/Queue.js";
+
+// WebGL/GPU constants and enums (pure constant values, no runtime context required)
+export { default as ArcType } from "@cesium/engine/Source/Core/ArcType.js";
+export { default as AttributeCompression } from "@cesium/engine/Source/Core/AttributeCompression.js";
+export { default as ComponentDatatype } from "@cesium/engine/Source/Core/ComponentDatatype.js";
+export { default as CornerType } from "@cesium/engine/Source/Core/CornerType.js";
+export { default as GeometryOffsetAttribute } from "@cesium/engine/Source/Core/GeometryOffsetAttribute.js";
+export { default as GeometryType } from "@cesium/engine/Source/Core/GeometryType.js";
+export { default as IndexDatatype } from "@cesium/engine/Source/Core/IndexDatatype.js";
+export { default as Intersect } from "@cesium/engine/Source/Core/Intersect.js";
+export { default as PixelFormat } from "@cesium/engine/Source/Core/PixelFormat.js";
+export { default as PrimitiveType } from "@cesium/engine/Source/Core/PrimitiveType.js";
+export { default as VertexFormat } from "@cesium/engine/Source/Core/VertexFormat.js";
+export { default as WebGLConstants } from "@cesium/engine/Source/Core/WebGLConstants.js";
+export { default as WindingOrder } from "@cesium/engine/Source/Core/WindingOrder.js";
+
+// Math types
+export { default as CesiumMath } from "@cesium/engine/Source/Core/Math.js";
+export { default as Cartesian2 } from "@cesium/engine/Source/Core/Cartesian2.js";
+export { default as Cartesian3 } from "@cesium/engine/Source/Core/Cartesian3.js";
+export { default as Cartesian4 } from "@cesium/engine/Source/Core/Cartesian4.js";
+export { default as Color } from "@cesium/engine/Source/Core/Color.js";
+export { default as CubicRealPolynomial } from "@cesium/engine/Source/Core/CubicRealPolynomial.js";
+export { default as EncodedCartesian3 } from "@cesium/engine/Source/Core/EncodedCartesian3.js";
+export { default as HeadingPitchRoll } from "@cesium/engine/Source/Core/HeadingPitchRoll.js";
+export { default as Matrix2 } from "@cesium/engine/Source/Core/Matrix2.js";
+export { default as Matrix3 } from "@cesium/engine/Source/Core/Matrix3.js";
+export { default as Matrix4 } from "@cesium/engine/Source/Core/Matrix4.js";
+export { default as QuadraticRealPolynomial } from "@cesium/engine/Source/Core/QuadraticRealPolynomial.js";
+export { default as Quaternion } from "@cesium/engine/Source/Core/Quaternion.js";
+export { default as QuarticRealPolynomial } from "@cesium/engine/Source/Core/QuarticRealPolynomial.js";
+export { default as Spherical } from "@cesium/engine/Source/Core/Spherical.js";
+export { default as TranslationRotationScale } from "@cesium/engine/Source/Core/TranslationRotationScale.js";
+export { default as TridiagonalSystemSolver } from "@cesium/engine/Source/Core/TridiagonalSystemSolver.js";
+
+// Geospatial types
+export { default as Cartographic } from "@cesium/engine/Source/Core/Cartographic.js";
+export { default as Ellipsoid } from "@cesium/engine/Source/Core/Ellipsoid.js";
+export { default as EllipsoidGeodesic } from "@cesium/engine/Source/Core/EllipsoidGeodesic.js";
+export { default as EllipsoidRhumbLine } from "@cesium/engine/Source/Core/EllipsoidRhumbLine.js";
+export { default as EllipsoidTangentPlane } from "@cesium/engine/Source/Core/EllipsoidTangentPlane.js";
+export { default as GeographicProjection } from "@cesium/engine/Source/Core/GeographicProjection.js";
+export { default as HilbertOrder } from "@cesium/engine/Source/Core/HilbertOrder.js";
+export { default as MortonOrder } from "@cesium/engine/Source/Core/MortonOrder.js";
+export { default as S2Cell } from "@cesium/engine/Source/Core/S2Cell.js";
+export { default as scaleToGeodeticSurface } from "@cesium/engine/Source/Core/scaleToGeodeticSurface.js";
+export { default as Stereographic } from "@cesium/engine/Source/Core/Stereographic.js";
+export { default as WebMercatorProjection } from "@cesium/engine/Source/Core/WebMercatorProjection.js";
+
+// Bounding volumes and spatial queries
+export { default as AxisAlignedBoundingBox } from "@cesium/engine/Source/Core/AxisAlignedBoundingBox.js";
+export { default as barycentricCoordinates } from "@cesium/engine/Source/Core/barycentricCoordinates.js";
+export { default as BoundingRectangle } from "@cesium/engine/Source/Core/BoundingRectangle.js";
+export { default as BoundingSphere } from "@cesium/engine/Source/Core/BoundingSphere.js";
+export { default as IntersectionTests } from "@cesium/engine/Source/Core/IntersectionTests.js";
+export { default as Intersections2D } from "@cesium/engine/Source/Core/Intersections2D.js";
+export { default as Interval } from "@cesium/engine/Source/Core/Interval.js";
+export { default as OrientedBoundingBox } from "@cesium/engine/Source/Core/OrientedBoundingBox.js";
+export { default as Plane } from "@cesium/engine/Source/Core/Plane.js";
+export { default as pointInsideTriangle } from "@cesium/engine/Source/Core/pointInsideTriangle.js";
+export { default as Ray } from "@cesium/engine/Source/Core/Ray.js";
+export { default as Rectangle } from "@cesium/engine/Source/Core/Rectangle.js";
+
+// Time
+export { default as ClockRange } from "@cesium/engine/Source/Core/ClockRange.js";
+export { default as ClockStep } from "@cesium/engine/Source/Core/ClockStep.js";
+export { default as ExtrapolationType } from "@cesium/engine/Source/Core/ExtrapolationType.js";
+export { default as GregorianDate } from "@cesium/engine/Source/Core/GregorianDate.js";
+export { default as Iso8601 } from "@cesium/engine/Source/Core/Iso8601.js";
+export { default as JulianDate } from "@cesium/engine/Source/Core/JulianDate.js";
+export { default as LeapSecond } from "@cesium/engine/Source/Core/LeapSecond.js";
+export { default as TimeConstants } from "@cesium/engine/Source/Core/TimeConstants.js";
+export { default as TimeInterval } from "@cesium/engine/Source/Core/TimeInterval.js";
+export { default as TimeIntervalCollection } from "@cesium/engine/Source/Core/TimeIntervalCollection.js";
+export { default as TimeStandard } from "@cesium/engine/Source/Core/TimeStandard.js";
+
+// Interpolation
+export { default as CatmullRomSpline } from "@cesium/engine/Source/Core/CatmullRomSpline.js";
+export { default as ConstantSpline } from "@cesium/engine/Source/Core/ConstantSpline.js";
+export { default as HermitePolynomialApproximation } from "@cesium/engine/Source/Core/HermitePolynomialApproximation.js";
+export { default as HermiteSpline } from "@cesium/engine/Source/Core/HermiteSpline.js";
+export { default as InterpolationType } from "@cesium/engine/Source/Core/InterpolationType.js";
+export { default as LagrangePolynomialApproximation } from "@cesium/engine/Source/Core/LagrangePolynomialApproximation.js";
+export { default as LinearApproximation } from "@cesium/engine/Source/Core/LinearApproximation.js";
+export { default as LinearSpline } from "@cesium/engine/Source/Core/LinearSpline.js";
+export { default as MorphWeightSpline } from "@cesium/engine/Source/Core/MorphWeightSpline.js";
+export { default as QuaternionSpline } from "@cesium/engine/Source/Core/QuaternionSpline.js";
+export { default as Spline } from "@cesium/engine/Source/Core/Spline.js";
+export { default as SteppedSpline } from "@cesium/engine/Source/Core/SteppedSpline.js";
+
+// Geometry data types and processing
+export { default as ColorGeometryInstanceAttribute } from "@cesium/engine/Source/Core/ColorGeometryInstanceAttribute.js";
+export { default as DistanceDisplayCondition } from "@cesium/engine/Source/Core/DistanceDisplayCondition.js";
+export { default as DistanceDisplayConditionGeometryInstanceAttribute } from "@cesium/engine/Source/Core/DistanceDisplayConditionGeometryInstanceAttribute.js";
+export { default as Geometry } from "@cesium/engine/Source/Core/Geometry.js";
+export { default as GeometryAttribute } from "@cesium/engine/Source/Core/GeometryAttribute.js";
+export { default as GeometryAttributes } from "@cesium/engine/Source/Core/GeometryAttributes.js";
+export { default as GeometryFactory } from "@cesium/engine/Source/Core/GeometryFactory.js";
+export { default as GeometryInstance } from "@cesium/engine/Source/Core/GeometryInstance.js";
+export { default as GeometryInstanceAttribute } from "@cesium/engine/Source/Core/GeometryInstanceAttribute.js";
+export { default as GeometryPipeline } from "@cesium/engine/Source/Core/GeometryPipeline.js";
+export { default as NearFarScalar } from "@cesium/engine/Source/Core/NearFarScalar.js";
+export { default as OffsetGeometryInstanceAttribute } from "@cesium/engine/Source/Core/OffsetGeometryInstanceAttribute.js";
+export { default as PolygonHierarchy } from "@cesium/engine/Source/Core/PolygonHierarchy.js";
+export { default as PolygonPipeline } from "@cesium/engine/Source/Core/PolygonPipeline.js";
+export { default as PolylinePipeline } from "@cesium/engine/Source/Core/PolylinePipeline.js";
+export { default as ShowGeometryInstanceAttribute } from "@cesium/engine/Source/Core/ShowGeometryInstanceAttribute.js";
+export { default as Tipsify } from "@cesium/engine/Source/Core/Tipsify.js";
+export { default as VerticalExaggeration } from "@cesium/engine/Source/Core/VerticalExaggeration.js";
+export { default as WireframeIndexGenerator } from "@cesium/engine/Source/Core/WireframeIndexGenerator.js";
+
+// Geometry shapes
+export { default as BoxGeometry } from "@cesium/engine/Source/Core/BoxGeometry.js";
+export { default as BoxOutlineGeometry } from "@cesium/engine/Source/Core/BoxOutlineGeometry.js";
+export { default as CircleGeometry } from "@cesium/engine/Source/Core/CircleGeometry.js";
+export { default as CircleOutlineGeometry } from "@cesium/engine/Source/Core/CircleOutlineGeometry.js";
+export { default as CoplanarPolygonGeometry } from "@cesium/engine/Source/Core/CoplanarPolygonGeometry.js";
+export { default as CoplanarPolygonOutlineGeometry } from "@cesium/engine/Source/Core/CoplanarPolygonOutlineGeometry.js";
+export { default as CorridorGeometry } from "@cesium/engine/Source/Core/CorridorGeometry.js";
+export { default as CorridorOutlineGeometry } from "@cesium/engine/Source/Core/CorridorOutlineGeometry.js";
+export { default as CylinderGeometry } from "@cesium/engine/Source/Core/CylinderGeometry.js";
+export { default as CylinderOutlineGeometry } from "@cesium/engine/Source/Core/CylinderOutlineGeometry.js";
+export { default as EllipseGeometry } from "@cesium/engine/Source/Core/EllipseGeometry.js";
+export { default as EllipseOutlineGeometry } from "@cesium/engine/Source/Core/EllipseOutlineGeometry.js";
+export { default as EllipsoidGeometry } from "@cesium/engine/Source/Core/EllipsoidGeometry.js";
+export { default as EllipsoidOutlineGeometry } from "@cesium/engine/Source/Core/EllipsoidOutlineGeometry.js";
+export { default as FrustumGeometry } from "@cesium/engine/Source/Core/FrustumGeometry.js";
+export { default as FrustumOutlineGeometry } from "@cesium/engine/Source/Core/FrustumOutlineGeometry.js";
+export { default as PlaneGeometry } from "@cesium/engine/Source/Core/PlaneGeometry.js";
+export { default as PlaneOutlineGeometry } from "@cesium/engine/Source/Core/PlaneOutlineGeometry.js";
+export { default as PolygonGeometry } from "@cesium/engine/Source/Core/PolygonGeometry.js";
+export { default as PolygonOutlineGeometry } from "@cesium/engine/Source/Core/PolygonOutlineGeometry.js";
+export { default as PolylineGeometry } from "@cesium/engine/Source/Core/PolylineGeometry.js";
+export { default as PolylineVolumeGeometry } from "@cesium/engine/Source/Core/PolylineVolumeGeometry.js";
+export { default as PolylineVolumeOutlineGeometry } from "@cesium/engine/Source/Core/PolylineVolumeOutlineGeometry.js";
+export { default as RectangleGeometry } from "@cesium/engine/Source/Core/RectangleGeometry.js";
+export { default as RectangleOutlineGeometry } from "@cesium/engine/Source/Core/RectangleOutlineGeometry.js";
+export { default as SimplePolylineGeometry } from "@cesium/engine/Source/Core/SimplePolylineGeometry.js";
+export { default as SphereGeometry } from "@cesium/engine/Source/Core/SphereGeometry.js";
+export { default as SphereOutlineGeometry } from "@cesium/engine/Source/Core/SphereOutlineGeometry.js";
+export { default as WallGeometry } from "@cesium/engine/Source/Core/WallGeometry.js";
+export { default as WallOutlineGeometry } from "@cesium/engine/Source/Core/WallOutlineGeometry.js";
