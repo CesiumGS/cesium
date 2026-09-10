42f5ecf6f32eb6e4868ab8c296443d463a90faa7
Ben Polinsky <78756012+ben-polinsky@users.noreply.github.com>
2026-09-10 12:21:27 -0400

    revise export exclusion list

--- scripts/build.js
+++ scripts/build.js
@@ -257,7 +257,6 @@ const workspaceSourceFiles = {
     "packages/engine/Source/**/*.js",
     "!packages/engine/Source/*.js",
     "!packages/engine/Source/Core/globalTypes.js",
-    "!packages/engine/Source/Core/WebAssemblyWorkerInitializer.js",
     "!packages/engine/Source/Workers/**",
     "packages/engine/Source/Workers/createTaskProcessorWorker.js",
     "!packages/engine/Source/ThirdParty/Workers/**.js",
