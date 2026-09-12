8489ce639823b39c64b59c0b09482c295e7c8ad4
Jeshurun Hembd <jeshurun@cesium.com>
2026-09-12 18:37:48 -0400

    Split typedArrayTypes out of globalTypes

--- packages/engine/Source/Core/globalTypes.js
+++ packages/engine/Source/Core/globalTypes.js
@@ -1,14 +1,4 @@
 /**
- * Union of all numeric typed array types.
- * @typedef {Float64Array|Float32Array|Uint32Array|Uint16Array|Uint8Array|Int32Array|Int16Array|Int8Array} TypedArray
- */
-
-/**
- * Union of all numeric typed array constructor types.
- * @typedef {Float64ArrayConstructor|Float32ArrayConstructor|Uint32ArrayConstructor|Uint16ArrayConstructor|Uint8ArrayConstructor|Int32ArrayConstructor|Int16ArrayConstructor|Int8ArrayConstructor} TypedArrayConstructor
- */
-
-/**
  * @typedef {{destroy: Function}} Destroyable
  */
 
