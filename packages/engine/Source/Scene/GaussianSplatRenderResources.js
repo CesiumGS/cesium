fb4a2a7e0847d17b91e9e9e9538e13398fb7ddbf
dbiguenet <110406974+dbiguenet@users.noreply.github.com>
2026-09-16 14:35:27 +0200

    Remove blending from GaussianSplatRenderResources

--- packages/engine/Source/Scene/GaussianSplatRenderResources.js
+++ packages/engine/Source/Scene/GaussianSplatRenderResources.js
@@ -3,7 +3,6 @@ import RenderState from "../Renderer/RenderState.js";
 import DepthFunction from "../Scene/DepthFunction.js";
 import ModelAlphaOptions from "./Model/ModelAlphaOptions.js";
 import ShaderDestination from "../Renderer/ShaderDestination.js";
-import WebGLConstants from "../Core/WebGLConstants.js";
 import combine from "../Core/combine.js";
 import defined from "../Core/defined.js";
 
@@ -101,19 +100,6 @@ function GaussianSplatRenderResources(primitive) {
       depthTest: {
         enabled: true,
         func: DepthFunction.LESS_OR_EQUAL,
-      },
-      cull: {
-        enabled: false,
-      },
-      depthMask: true,
-      blending: {
-        enabled: true,
-        equationRgb: WebGLConstants.FUNC_ADD,
-        equationAlpha: WebGLConstants.FUNC_ADD,
-        functionSourceRgb: WebGLConstants.SRC_ALPHA,
-        functionSourceAlpha: WebGLConstants.SRC_ALPHA,
-        functionDestinationRgb: WebGLConstants.ONE_MINUS_SRC_ALPHA,
-        functionDestinationAlpha: WebGLConstants.ONE_MINUS_SRC_ALPHA,
       },
     }),
   );
